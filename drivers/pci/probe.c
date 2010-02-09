da7b138ca69f654a8673c131dc6835ba74c68ff2
Don Dutile <ddutile@redhat.com>
2010-02-08 21:42:10 -0500

    PCI ACS support functions
    Patchwork-id: 23206
    O-Subject: Re: [RHEL6 PATCH 1/3] PCI ACS support functions
    Bugzilla: 523278
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    BZ 523278
    
    Base support functions for enabling ACS in PCIe switches.
    
    Backport of upstream
    5d990b627537e59a3a2f039ff588a4750e9c1a6a
    
    Please review & ACK.
    
    - Don
    >From b3aa8e239e450e48c6ae526cd0c8e45b9bcaa0ad Mon Sep 17 00:00:00 2001
    From: Donald Dutile <ddutile@redhat.com>
    Date: Thu, 28 Jan 2010 17:18:02 -0500
    Subject: [PATCH 1/3]  ACS support functions
    
     Backport of upstream ae21ee65e8bc228416bbcc8a1da01c56a847a60c
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -11,6 +11,7 @@
 #include <linux/cpumask.h>
 #include <acpi/acpi_hest.h>
 #include <linux/pci-aspm.h>
+#include <linux/iommu.h>
 #include "pci.h"
 
 #define CARDBUS_LATENCY_TIMER	176	/* secondary latency timer */
@@ -1022,6 +1023,10 @@ static void pci_init_capabilities(struct pci_dev *dev)
 
 	/* Single Root I/O Virtualization */
 	pci_iov_init(dev);
+
+	/* Enable ACS P2P upstream forwarding */
+	if (iommu_found())
+		pci_enable_acs(dev);
 }
 
 void pci_device_add(struct pci_dev *dev, struct pci_bus *bus)
