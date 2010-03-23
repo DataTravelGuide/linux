d0b69e81c953f23970421f3c2be1f8bb31b8ce83
Anthony Liguori <aliguori@redhat.com>
2010-03-22 22:43:38 -0400

    macvlan: add private dev_txq_stats_fold function
    Patchwork-id: 23711
    O-Subject: [PATCH 02/13] macvlan: add private dev_txq_stats_fold function
    Bugzilla: 553337 566731
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Herbert Xu <herbert.xu@redhat.com>
    RH-Acked-by: Michael S. Tsirkin <mst@redhat.com>
    
    From: Arnd Bergmann <arnd@arndb.de>
    
    BZ: #566731
    
    This function was added globally in 2.6.33-rc, but is not present in
    2.6.32, so we add it here to keep the driver up-to-date.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/net/macvlan.c
+++ drivers/net/macvlan.c
@@ -369,6 +369,31 @@ static int macvlan_init(struct net_device *dev)
 	return 0;
 }
 
+/**
+ *	dev_txq_stats_fold - fold tx_queues stats
+ *	@dev: device to get statistics from
+ *	@stats: struct net_device_stats to hold results
+ */
+static void macvlan_dev_txq_stats_fold(const struct net_device *dev,
+			struct net_device_stats *stats)
+{
+	unsigned long tx_bytes = 0, tx_packets = 0, tx_dropped = 0;
+	unsigned int i;
+	struct netdev_queue *txq;
+
+	for (i = 0; i < dev->num_tx_queues; i++) {
+		txq = netdev_get_tx_queue(dev, i);
+		tx_bytes   += txq->tx_bytes;
+		tx_packets += txq->tx_packets;
+		tx_dropped += txq->tx_dropped;
+	}
+	if (tx_bytes || tx_packets || tx_dropped) {
+		stats->tx_bytes   = tx_bytes;
+		stats->tx_packets = tx_packets;
+		stats->tx_dropped = tx_dropped;
+	}
+}
+
 static void macvlan_ethtool_get_drvinfo(struct net_device *dev,
 					struct ethtool_drvinfo *drvinfo)
 {
