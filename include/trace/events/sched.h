72b829c7120c39fb6033761da6ea24e898d03573
Mike Snitzer <snitzer@redhat.com>
2010-05-04 16:30:45 -0400

    Harmonize event field names and print output names
    Patchwork-id: 24769
    O-Subject: [PATCH 14/23] tracing: Harmonize event field names and print output
    	names
    Bugzilla: 588108
    RH-Acked-by: Jason Baron <jbaron@redhat.com>
    
    From: Ingo Molnar <mingo@redhat.com>
    
    BZ 588108
    
    Adjusted contents of this patch because upstream commit 75ec29ab848a7
    was staged for RHEL6 before this commit -- it would've been more natural
    to do keep upstream's order but in the end the final changes are the
    same.
    
    commit 434a83c3fbb951908a3a52040f7f0e0b8ba00dd0
    Author: Ingo Molnar <mingo@elte.hu>
    Date:   Thu Oct 15 11:50:39 2009 +0200
    
        events: Harmonize event field names and print output names
    
        Now that we can filter based on fields via perf record, people
        will start using filter expressions and will expect them to
        be obvious.
    
        The primary way to see which fields are available is by looking
        at the trace output, such as:
    
          gcc-18676 [000]   343.011728: irq_handler_entry: irq=0 handler=timer
          cc1-18677 [000]   343.012727: irq_handler_entry: irq=0 handler=timer
          cc1-18677 [000]   343.032692: irq_handler_entry: irq=0 handler=timer
          cc1-18677 [000]   343.033690: irq_handler_entry: irq=0 handler=timer
          cc1-18677 [000]   343.034687: irq_handler_entry: irq=0 handler=timer
          cc1-18677 [000]   343.035686: irq_handler_entry: irq=0 handler=timer
          cc1-18677 [000]   343.036684: irq_handler_entry: irq=0 handler=timer
    
        While 'irq==0' filters work, the 'handler==<x>' filter expression
        does not work:
    
          $ perf record -R -f -a -e irq:irq_handler_entry --filter handler=timer sleep 1
           Error: failed to set filter with 22 (Invalid argument)
    
        The problem is that while an 'irq' field exists and is recognized
        as a filter field - 'handler' does not exist - its name is 'name'
        in the output.
    
        To solve this, we need to synchronize the printout and the field
        names, wherever possible.
    
        In cases where the printout prints a non-field, we enclose
        that information in square brackets, such as:
    
          perf-1380  [013]   724.903505: softirq_exit: vec=9 [action=RCU]
          perf-1380  [013]   724.904482: softirq_exit: vec=1 [action=TIMER]
    
        This way users can use filter expressions more intuitively: all
        fields that show up as 'primary' (non-bracketed) information is
        filterable.
    
        This patch harmonizes the field names for all irq, bkl, power,
        sched and timer events.
    
        We might in fact think about dropping the print format bit of
        generic tracepoints altogether, and just print the fields that
        are being recorded.
    
        Cc: Li Zefan <lizf@cn.fujitsu.com>
        Cc: Tom Zanussi <tzanussi@gmail.com>
        Cc: Frederic Weisbecker <fweisbec@gmail.com>
        Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
        Cc: Mike Galbraith <efault@gmx.de>
        Cc: Paul Mackerras <paulus@samba.org>
        Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
        LKML-Reference: <new-submission>
        Signed-off-by: Ingo Molnar <mingo@elte.hu>
    
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- include/trace/events/sched.h
+++ include/trace/events/sched.h
@@ -26,7 +26,7 @@ TRACE_EVENT(sched_kthread_stop,
 		__entry->pid	= t->pid;
 	),
 
-	TP_printk("task %s:%d", __entry->comm, __entry->pid)
+	TP_printk("comm=%s pid=%d", __entry->comm, __entry->pid)
 );
 
 /*
@@ -46,7 +46,7 @@ TRACE_EVENT(sched_kthread_stop_ret,
 		__entry->ret	= ret;
 	),
 
-	TP_printk("ret %d", __entry->ret)
+	TP_printk("ret=%d", __entry->ret)
 );
 
 /*
@@ -73,7 +73,7 @@ TRACE_EVENT(sched_wait_task,
 		__entry->prio	= p->prio;
 	),
 
-	TP_printk("task %s:%d [%d]",
+	TP_printk("comm=%s pid=%d prio=%d",
 		  __entry->comm, __entry->pid, __entry->prio)
 );
 
@@ -94,7 +94,7 @@ DECLARE_EVENT_CLASS(sched_wakeup_template,
 		__field(	pid_t,	pid			)
 		__field(	int,	prio			)
 		__field(	int,	success			)
-		__field(	int,	cpu			)
+		__field(	int,	target_cpu		)
 	),
 
 	TP_fast_assign(
@@ -102,12 +102,12 @@ DECLARE_EVENT_CLASS(sched_wakeup_template,
 		__entry->pid		= p->pid;
 		__entry->prio		= p->prio;
 		__entry->success	= success;
-		__entry->cpu		= task_cpu(p);
+		__entry->target_cpu	= task_cpu(p);
 	),
 
-	TP_printk("task %s:%d [%d] success=%d [%03d]",
+	TP_printk("comm=%s pid=%d prio=%d success=%d target_cpu=%03d",
 		  __entry->comm, __entry->pid, __entry->prio,
-		  __entry->success, __entry->cpu)
+		  __entry->success, __entry->target_cpu)
 );
 
 DEFINE_EVENT(sched_wakeup_template, sched_wakeup,
@@ -157,7 +157,7 @@ TRACE_EVENT(sched_switch,
 		__entry->next_prio	= next->prio;
 	),
 
-	TP_printk("task %s:%d [%d] (%s) ==> %s:%d [%d]",
+	TP_printk("prev_comm=%s prev_pid=%d prev_prio=%d prev_state=%s ==> next_comm=%s next_pid=%d next_prio=%d",
 		__entry->prev_comm, __entry->prev_pid, __entry->prev_prio,
 		__entry->prev_state ?
 		  __print_flags(__entry->prev_state, "|",
@@ -192,7 +192,7 @@ TRACE_EVENT(sched_migrate_task,
 		__entry->dest_cpu	= dest_cpu;
 	),
 
-	TP_printk("task %s:%d [%d] from: %d  to: %d",
+	TP_printk("comm=%s pid=%d prio=%d orig_cpu=%d dest_cpu=%d",
 		  __entry->comm, __entry->pid, __entry->prio,
 		  __entry->orig_cpu, __entry->dest_cpu)
 );
@@ -215,7 +215,7 @@ DECLARE_EVENT_CLASS(sched_process_template,
 		__entry->prio		= p->prio;
 	),
 
-	TP_printk("task %s:%d [%d]",
+	TP_printk("comm=%s pid=%d prio=%d",
 		  __entry->comm, __entry->pid, __entry->prio)
 );
 
@@ -254,7 +254,7 @@ TRACE_EVENT(sched_process_wait,
 		__entry->prio		= current->prio;
 	),
 
-	TP_printk("task %s:%d [%d]",
+	TP_printk("comm=%s pid=%d prio=%d",
 		  __entry->comm, __entry->pid, __entry->prio)
 );
 
@@ -281,7 +281,7 @@ TRACE_EVENT(sched_process_fork,
 		__entry->child_pid	= child->pid;
 	),
 
-	TP_printk("parent %s:%d  child %s:%d",
+	TP_printk("comm=%s pid=%d child_comm=%s child_pid=%d",
 		__entry->parent_comm, __entry->parent_pid,
 		__entry->child_comm, __entry->child_pid)
 );
@@ -311,7 +311,7 @@ DECLARE_EVENT_CLASS(sched_stat_template,
 		__perf_count(delay);
 	),
 
-	TP_printk("task: %s:%d wait: %Lu [ns]",
+	TP_printk("comm=%s pid=%d delay=%Lu [ns]",
 			__entry->comm, __entry->pid,
 			(unsigned long long)__entry->delay)
 );
@@ -374,7 +374,7 @@ TRACE_EVENT(sched_stat_runtime,
 		__perf_count(runtime);
 	),
 
-	TP_printk("task: %s:%d runtime: %Lu [ns], vruntime: %Lu [ns]",
+	TP_printk("comm=%s pid=%d runtime=%Lu [ns] vruntime=%Lu [ns]",
 			__entry->comm, __entry->pid,
 			(unsigned long long)__entry->runtime,
 			(unsigned long long)__entry->vruntime)
