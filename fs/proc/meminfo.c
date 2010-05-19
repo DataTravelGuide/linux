95e0a84e04a1f85abe77b5aaff471436fc3473f1
Andrea Arcangeli <aarcange@redhat.com>
2010-05-19 02:27:16 -0400

    include anon hugepages into the anon stats
    Patchwork-id: 25252
    O-Subject: [PATCH 47 of 49 RHEL6] include anon hugepages into the anon stats
    Bugzilla: 556572
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    
    From: Andrea Arcangeli <aarcange@redhat.com>
    
    Backwards compatibility change.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- fs/proc/meminfo.c
+++ fs/proc/meminfo.c
@@ -131,7 +131,12 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 		K(i.freeswap),
 		K(global_page_state(NR_FILE_DIRTY)),
 		K(global_page_state(NR_WRITEBACK)),
-		K(global_page_state(NR_ANON_PAGES)),
+		K(global_page_state(NR_ANON_PAGES)
+#ifdef CONFIG_TRANSPARENT_HUGEPAGE
+		  + global_page_state(NR_ANON_TRANSPARENT_HUGEPAGES) *
+		  HPAGE_PMD_NR
+#endif
+		  ),
 		K(global_page_state(NR_FILE_MAPPED)),
 		K(global_page_state(NR_SHMEM)),
 		K(global_page_state(NR_SLAB_RECLAIMABLE) +
