3f3a1bf45a8b998227e25ec43a74fe3542f0677d
Andrea Arcangeli <aarcange@redhat.com>
2010-05-19 02:26:48 -0400

    set default to never
    Patchwork-id: 25234
    O-Subject: [PATCH 19 of 49 RHEL6] set default to never
    Bugzilla: 556572
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    
    From: Andrea Arcangeli <aarcange@redhat.com>
    
    Otherwise set_recommended_min_free_kbytes runs and it wastes memory if THP is
    never enabled.
    
    set_recommended_min_free_kbytes however is better set at boot, so it's
    recommended to use either echo always > at boot time from a rc script or
    transparent_hugepage=always.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -27,7 +27,6 @@
  * allocations.
  */
 unsigned long transparent_hugepage_flags __read_mostly =
-	(1<<TRANSPARENT_HUGEPAGE_REQ_MADV_FLAG)|
 	(1<<TRANSPARENT_HUGEPAGE_DEFRAG_KHUGEPAGED_FLAG);
 
 /* default scan 8*512 pte (or vmas) every 30 second */
