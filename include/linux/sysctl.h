bf6283f19e0d8abf39ec344565a8f54b4176d869
Amerigo Wang <amwang@redhat.com>
2010-05-27 10:07:34 -0400

    sysctl: add proc_do_large_bitmap
    Patchwork-id: 25827
    O-Subject: [Patch 2/3] sysctl: add proc_do_large_bitmap
    Bugzilla: 580970
    RH-Acked-by: David S. Miller <davem@redhat.com>
    
    From: Amerigo Wang <amwang@redhat.com>
    Date: Fri, 21 May 2010 10:00:52 +0800
    Subject: [PATCH 2/3] sysctl: add proc_do_large_bitmap
    
    	The new function can be used to read/write large bitmaps via /proc. A
    	comma separated range format is used for compact output and input
    	(e.g. 1,3-4,10-10).
    
    	Writing into the file will first reset the bitmap then update it
    	based on the given input.
    
    	Signed-off-by: Octavian Purdila <opurdila@ixiacom.com>
    	Signed-off-by: WANG Cong <amwang@redhat.com>
    	Cc: Eric W. Biederman <ebiederm@xmission.com>
    
    Signed-off-by: Amerigo Wang <amwang@redhat.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- include/linux/sysctl.h
+++ include/linux/sysctl.h
@@ -996,6 +996,8 @@ extern int proc_doulongvec_minmax(struct ctl_table *, int,
 				  void __user *, size_t *, loff_t *);
 extern int proc_doulongvec_ms_jiffies_minmax(struct ctl_table *table, int,
 				      void __user *, size_t *, loff_t *);
+extern int proc_do_large_bitmap(struct ctl_table *, int,
+				void __user *, size_t *, loff_t *);
 
 extern int do_sysctl (int __user *name, int nlen,
 		      void __user *oldval, size_t __user *oldlenp,
