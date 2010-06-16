c5caebeb0b06cfe1e3e05d945f5eda4f5cedd41a
Steve Best <sbest@redhat.com>
2010-06-16 15:12:19 -0400

    sched: Fix capacity calculations for SMT4
    Patchwork-id: 26253
    O-Subject: [PATCH RHEL6.0 BZ596304 1/3] sched: Fix capacity calculations for SMT4
    Bugzilla: 596304
    RH-Acked-by: David Howells <dhowells@redhat.com>
    
    RHBZ#:
    ======
    https://bugzilla.redhat.com/show_bug.cgi?id=596304
    
    Description:
    ============
    commit 9d5efe05eb0c904545a28b19c18b949f23334de0
    Author: Srivatsa Vaddagiri <vatsa@linux.vnet.ibm.com>
    Date:   Tue Jun 8 14:57:02 2010 +1000
    
        sched: Fix capacity calculations for SMT4
    
        Handle cpu capacity being reported as 0 on cores with more number of
        hardware threads. For example on a Power7 core with 4 hardware
        threads, core power is 1177 and thus power of each hardware thread is
        1177/4 = 294. This low power can lead to capacity for each hardware
        thread being calculated as 0, which leads to tasks bouncing within the
        core madly!
    
        Fix this by reporting capacity for hardware threads as 1, provided
        their power is not scaled down significantly because of frequency
        scaling or real-time tasks usage of cpu.
    
        Signed-off-by: Srivatsa Vaddagiri <vatsa@linux.vnet.ibm.com>
        Signed-off-by: Michael Neuling <mikey@neuling.org>
        Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
        Cc: Arjan van de Ven <arjan@linux.intel.com>
        LKML-Reference: <20100608045702.21D03CC895@localhost.localdomain>
        Signed-off-by: Ingo Molnar <mingo@elte.hu>
    
    RHEL6 backported info:
      - kernel/sched_fair.c => kernel/sched.c
      - added fix from https://patchwork.kernel.org/patch/105240/
    
    Brew:
    =====
    Built on all platforms. All patches brewed together.
    http://brewweb.devel.redhat.com/brew/taskinfo?taskID=2510506
    
    ===============================================================
    Steve Best
    IBM on-site partner
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- kernel/sched.c
+++ kernel/sched.c
@@ -3615,13 +3615,6 @@ static void update_cpu_power(struct sched_domain *sd, int cpu)
 	unsigned long power = SCHED_LOAD_SCALE;
 	struct sched_group *sdg = sd->groups;
 
-	if (sched_feat(ARCH_POWER))
-		power *= arch_scale_freq_power(sd, cpu);
-	else
-		power *= default_scale_freq_power(sd, cpu);
-
-	power >>= SCHED_LOAD_SHIFT;
-
 	if ((sd->flags & SD_SHARE_CPUPOWER) && weight > 1) {
 		if (sched_feat(ARCH_POWER))
 			power *= arch_scale_smt_power(sd, cpu);
@@ -3630,6 +3623,15 @@ static void update_cpu_power(struct sched_domain *sd, int cpu)
 
 		power >>= SCHED_LOAD_SHIFT;
 	}
+
+	sdg->cpu_power_orig = power;
+
+	if (sched_feat(ARCH_POWER))
+		power *= arch_scale_freq_power(sd, cpu);
+	else
+		power *= default_scale_freq_power(sd, cpu);
+
+	power >>= SCHED_LOAD_SHIFT;
 
 	power *= scale_rt_power(cpu);
 	power >>= SCHED_LOAD_SHIFT;
@@ -3660,6 +3662,31 @@ static void update_group_power(struct sched_domain *sd, int cpu)
 	} while (group != child->groups);
 
 	sdg->cpu_power = power;
+}
+
+/*
+ * Try and fix up capacity for tiny siblings, this is needed when
+ * things like SD_ASYM_PACKING need f_b_g to select another sibling
+ * which on its own isn't powerful enough.
+ *
+ * See update_sd_pick_busiest() and check_asym_packing().
+ */
+static inline int
+fix_small_capacity(struct sched_domain *sd, struct sched_group *group)
+{
+	/*
+	 * Only siblings can have significantly less than SCHED_LOAD_SCALE
+	 */
+	if (sd->level != SD_LV_SIBLING)
+		return 0;
+
+	/*
+	 * If ~90% of the cpu_power is still there, we're good.
+	 */
+	if (group->cpu_power * 32 > group->cpu_power_orig * 29)
+		return 1;
+
+	return 0;
 }
 
 /**
@@ -3756,6 +3783,8 @@ static inline void update_sg_lb_stats(struct sched_domain *sd,
 
 	sgs->group_capacity =
 		DIV_ROUND_CLOSEST(group->cpu_power, SCHED_LOAD_SCALE);
+	if (!sgs->group_capacity)
+		sgs->group_capacity = fix_small_capacity(sd, group);
 }
 
 /**
@@ -4054,8 +4083,9 @@ ret:
  * find_busiest_queue - find the busiest runqueue among the cpus in group.
  */
 static struct rq *
-find_busiest_queue(struct sched_group *group, enum cpu_idle_type idle,
-		   unsigned long imbalance, const struct cpumask *cpus)
+find_busiest_queue(struct sched_domain *sd, struct sched_group *group,
+		   enum cpu_idle_type idle, unsigned long imbalance,
+		   const struct cpumask *cpus)
 {
 	struct rq *busiest = NULL, *rq;
 	unsigned long max_load = 0;
@@ -4065,6 +4095,9 @@ find_busiest_queue(struct sched_group *group, enum cpu_idle_type idle,
 		unsigned long power = power_of(i);
 		unsigned long capacity = DIV_ROUND_CLOSEST(power, SCHED_LOAD_SCALE);
 		unsigned long wl;
+
+		if (!capacity)
+			capacity = fix_small_capacity(sd, group);
 
 		if (!cpumask_test_cpu(i, cpus))
 			continue;
@@ -4147,7 +4180,7 @@ redo:
 		goto out_balanced;
 	}
 
-	busiest = find_busiest_queue(group, idle, imbalance, cpus);
+	busiest = find_busiest_queue(sd, group, idle, imbalance, cpus);
 	if (!busiest) {
 		schedstat_inc(sd, lb_nobusyq[idle]);
 		goto out_balanced;
@@ -4305,7 +4338,7 @@ redo:
 		goto out_balanced;
 	}
 
-	busiest = find_busiest_queue(group, CPU_NEWLY_IDLE, imbalance, cpus);
+	busiest = find_busiest_queue(sd, group, CPU_NEWLY_IDLE, imbalance, cpus);
 	if (!busiest) {
 		schedstat_inc(sd, lb_nobusyq[CPU_NEWLY_IDLE]);
 		goto out_balanced;
