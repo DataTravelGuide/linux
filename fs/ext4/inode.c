4e9cbd327b13d4c5a333ea684f383a21ed3c8cfd
Christoph Hellwig <chellwig@redhat.com>
2010-07-02 09:07:46 -0400

    direct-io: move aio_complete into ->end_io
    Patchwork-id: 26671
    O-Subject: [RHEL6 PATCH 1/3] direct-io: move aio_complete into ->end_io
    Bugzilla: 589985
    RH-Acked-by: Eric Sandeen <sandeen@redhat.com>
    RH-Acked-by: Dave Chinner <dchinner@redhat.com>
    
    From: Christoph Hellwig <hch@lst.de>
    
    Filesystems with unwritten extent support must not complete an AIO request
    until the transaction to convert the extent has been commited.  That means
    the aio_complete calls needs to be moved into the ->end_io callback so
    that the filesystem can control when to call it exactly.
    
    This makes a bit of a mess out of dio_complete and the ->end_io callback
    prototype even more complicated.  In addition ->end_io is now called with
    i_alloc_sem held for DIO_LOCKING filesystems.  The only filesystem that
    has both and ->end_io callback and sets DIO_LOCKING is ext4, which doesn't
    appear to do anything that could deadlock with i_alloc_sem in ->end_io.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Upstream commit: submitted but not commited yet
    BZ: 589985 (partial fix)
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3800,14 +3800,15 @@ static ext4_io_end_t *ext4_init_io_end (struct inode *inode)
 }
 
 static void ext4_end_io_dio(struct kiocb *iocb, loff_t offset,
-			    ssize_t size, void *private)
+			    ssize_t size, void *private, int ret,
+			    bool is_async)
 {
         ext4_io_end_t *io_end = iocb->private;
 	struct workqueue_struct *wq;
 
 	/* if not async direct IO or dio with 0 bytes write, just return */
 	if (!io_end || !size)
-		return;
+		goto out;
 
 	ext_debug("ext4_end_io_dio(): io_end 0x%p"
 		  "for inode %lu, iocb 0x%p, offset %llu, size %llu\n",
@@ -3818,7 +3819,7 @@ static void ext4_end_io_dio(struct kiocb *iocb, loff_t offset,
 	if (io_end->flag != DIO_AIO_UNWRITTEN){
 		ext4_free_io_end(io_end);
 		iocb->private = NULL;
-		return;
+		goto out;
 	}
 
 	io_end->offset = offset;
@@ -3832,6 +3833,9 @@ static void ext4_end_io_dio(struct kiocb *iocb, loff_t offset,
 	list_add_tail(&io_end->list,
 		 &EXT4_I(io_end->inode)->i_aio_dio_complete_list);
 	iocb->private = NULL;
+out:
+	if (is_async)
+		aio_complete(iocb, ret, 0);
 }
 /*
  * For ext4 extent files, ext4 will do direct-io write to holes,
