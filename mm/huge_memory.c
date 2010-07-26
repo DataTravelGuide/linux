32b8c9d045d7ef67d0b008f3bfd8e255357f7097
Rik van Riel <riel@redhat.com>
2010-07-26 18:51:38 -0400

    disable transparent hugepages by default on small systems
    Patchwork-id: 27103
    O-Subject: [RHEL6 PATCH -v2] disable transparent hugepages by default on small
    	systems
    Bugzilla: 618444
    RH-Acked-by: Andrea Arcangeli <aarcange@redhat.com>
    
    On small systems, the extra memory used by the anti-fragmentation
    memory reserve and simply because huge pages are smaller than large
    pages can easily outweigh the benefits of less TLB misses.
    
    In case of the crashdump kernel, OOMs have been observed due to
    the anti-fragmentation memory reserve taking up a large fraction
    of the crashdump image.
    
    This patch disables transparent hugepages on systems with less
    than 1GB of RAM, but the hugepage subsystem is fully initialized
    so administrators can enable THP through /sys if desired.
    
    v2: reduce the limit to 512MB
    
    Signed-off-by: Rik van Riel <riel@redhat.com>
    Acked-by: Avi Kiviti <avi@redhat.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -525,6 +525,14 @@ static int __init hugepage_init(void)
 
 	start_khugepaged();
 
+	/*
+	 * By default disable transparent hugepages on smaller systems,
+	 * where the extra memory used could hurt more than TLB overhead
+	 * is likely to save.  The admin can still enable it through /sys.
+	 */
+	if (totalram_pages < (512 << (20 - PAGE_SHIFT)))
+		transparent_hugepage_flags = 0;
+
 	set_recommended_min_free_kbytes();
 
 out:
