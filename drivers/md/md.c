d07f3c8355727a5ef680a3f94edb008fe58b7146
Doug Ledford <dledford@redhat.com>
2010-07-28 18:02:03 -0400

    Fix md raid partition detection update
    Patchwork-id: 27159
    O-Subject: Re: [Patch RHEL6.0] Fix md raid partition detection
    Bugzilla: 607477
    RH-Acked-by: Jay Fenlason <fenlason@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    On 07/22/2010 02:34 PM, Doug Ledford wrote:
    > The attached patch addresses bug 607477.  The majority of the
    > explanation is actually in the git commit log, so please see the patch
    > for full details.  If we can get this in snap9 it would be fantastic.
    >
    > The patch has been smoke tested here, and verified by Intel prior to
    > submission.
    >
    
    This patch has been included in 2.6.32-54, however upstream resolved the
    issue in a different manner.  Attached is the actual upstream commit.
    Therefore, persuant to my promise to update this patch if upstream
    solves it another way, I'm requesting that the next kernel revert this
    patch and replace it with the attached patch.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -5898,6 +5898,7 @@ static int md_open(struct block_device *bdev, fmode_t mode)
 	atomic_inc(&mddev->openers);
 	mutex_unlock(&mddev->open_mutex);
 
+	check_disk_size_change(mddev->gendisk, bdev);
  out:
 	return err;
 }
