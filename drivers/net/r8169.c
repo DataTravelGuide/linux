0976a9a020bb97ddb1512575cb8d09968fe0a0ee
Michal Schmidt <mschmidt@redhat.com>
2010-08-06 00:07:36 -0400

    r8169: disable PCIe ASPM
    Patchwork-id: 27435
    O-Subject: [RHEL6 PATCH] r8169: disable PCIe ASPM
    Bugzilla: 619880
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Stanislaw Gruszka <sgruszka@redhat.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=619880
    
    There are reports of Realtek NICs stopping working randomly.
    "pcie_aspm=off" has been identified as a workaround and confirmed
    by several Fedora users.
    We do not know yet if only specific devices are affected. The safe
    option is to disable ASPM for all r8169 NICs. We can revisit later if
    we find out which devices it is safe to use ASPM with.
    
    Brew build:
    https://brewweb.devel.redhat.com/taskinfo?taskID=2657980
    
    Testing:
    I never reproduced the hang myself, but I verified using lspci -vv
    that the patch disables ASPM:
     - before:
    	LnkCtl:	ASPM L1 Enabled; RCB 64 bytes Disabled- Retrain- CommClk+
    		ExtSynch- ClockPM+ AutWidDis- BWInt- AutBWInt-
     - after:
    	LnkCtl:	ASPM Disabled; RCB 64 bytes Disabled- Retrain- CommClk+
    		ExtSynch- ClockPM- AutWidDis- BWInt- AutBWInt-
    And the cards I have still work:
      10ec:8168 rev.06, RTL8168b/8111b, XID 0c100000
      10ec:8136 rev.01, RTL8101e,       XID 94200000
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/net/r8169.c
+++ drivers/net/r8169.c
@@ -23,6 +23,7 @@
 #include <linux/tcp.h>
 #include <linux/init.h>
 #include <linux/dma-mapping.h>
+#include <linux/pci-aspm.h>
 
 #include <asm/system.h>
 #include <asm/io.h>
@@ -3018,6 +3019,9 @@ rtl8169_init_one(struct pci_dev *pdev, const struct pci_device_id *ent)
 	mii->phy_id_mask = 0x1f;
 	mii->reg_num_mask = 0x1f;
 	mii->supports_gmii = !!(cfg->features & RTL_FEATURE_GMII);
+
+	pci_disable_link_state(pdev, PCIE_LINK_STATE_L0S | PCIE_LINK_STATE_L1 |
+			       PCIE_LINK_STATE_CLKPM);
 
 	/* enable device (incl. PCI PM wakeup and hotplug setup) */
 	rc = pci_enable_device(pdev);
