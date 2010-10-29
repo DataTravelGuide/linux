e4863096727d1da9b982b41442d218a945a8528c
Hendrik Brueckner <brueckner@redhat.com>
2010-10-29 13:24:03 -0400

    qeth: tolerate OLM-limitation
    Patchwork-id: 28982
    O-Subject: [RHEL6.1 PATCH 2/6] [s390x] qeth: tolerate OLM-limitation
    Bugzilla: 633568
    
    Description
    -----------
    z/OS might activate Optimized Latency Mode (OLM) for a connection
    through an OSA Express3 adapter, which reduces the number of allowed
    concurrent connections, if adapter is used in shared mode.
    
    Create a meaningful message, if activation of an OSA-connection
    fails due to an active OLM-connection on the shared OSA-adapter.
    
    Bugzilla
    --------
    BZ 633568
    https://bugzilla.redhat.com/show_bug.cgi?id=633568
    
    Upstream status of the patch
    ----------------------------
    The patch is upstream as of kernel version 2.6.35
    http://git.kernel.org/?p=linux/kernel/git/torvalds/linux-2.6.git;a=commitdiff;h=65a1f898efac136aeea65509e61ac6cdecd4f6bf
    
    Test status
    -----------
    The patch has been tested and fixes the problem.
    The fix has been verified by the IBM test department.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/s390/net/qeth_core_main.c
+++ drivers/s390/net/qeth_core_main.c
@@ -2037,6 +2037,13 @@ static int qeth_ulp_setup_cb(struct qeth_card *card, struct qeth_reply *reply,
 	memcpy(&card->token.ulp_connection_r,
 	       QETH_ULP_SETUP_RESP_CONNECTION_TOKEN(iob->data),
 	       QETH_MPC_TOKEN_LENGTH);
+	if (!strncmp("00S", QETH_ULP_SETUP_RESP_CONNECTION_TOKEN(iob->data),
+		     3)) {
+		QETH_DBF_TEXT(SETUP, 2, "olmlimit");
+		dev_err(&card->gdev->dev, "A connection could not be "
+			"established because of an OLM limit\n");
+		iob->rc = -EMLINK;
+	}
 	QETH_DBF_TEXT_(SETUP, 2, "  rc%d", iob->rc);
 	return 0;
 }
