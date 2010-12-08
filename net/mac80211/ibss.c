42b1e8fb37fa8355a5dbc99ac0a778109b469be0
John Linville <linville@redhat.com>
2010-12-08 18:50:38 -0500

    mac80211: allow station add/remove to sleep
    Patchwork-id: 30016
    O-Subject: [RHEL6.1 PATCH 3/7] backport "mac80211: allow station add/remove to
    	sleep"
    Bugzilla: 653978
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Stanislaw Gruszka <sgruszka@redhat.com>
    
    commit 34e895075e21be3e21e71d6317440d1ee7969ad0
    Author: Johannes Berg <johannes@sipsolutions.net>
    Date:   Wed Feb 3 13:59:58 2010 +0100
    
        mac80211: allow station add/remove to sleep
    
        Many drivers would like to sleep during station
        addition and removal, and currently have a high
        complexity there from not being able to.
    
        This introduces two new callbacks sta_add() and
        sta_remove() that drivers can implement instead
        of using sta_notify() and that can sleep, and
        the new sta_add() callback is also allowed to
        fail.
    
        The reason we didn't do this previously is that
        the IBSS code wants to insert stations from the
        RX path, which is a tasklet, so cannot sleep.
        This patch will keep the station allocation in
        that path, but moves adding the station to the
        driver out of line. Since the addition can now
        fail, we can have IBSS peer structs the driver
        rejected -- in that case we still talk to the
        station but never tell the driver about it in
        the control.sta pointer. If there will ever be
        a driver that has a low limit on the number of
        stations and that cannot talk to any stations
        that are not known to it, we need to do come up
        with a new strategy of handling larger IBSSs,
        maybe quicker expiry or rejecting peers.
    
        Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
        Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/ibss.c
+++ net/mac80211/ibss.c
@@ -272,10 +272,12 @@ static void ieee80211_rx_bss_info(struct ieee80211_sub_if_data *sdata,
 				    (unsigned long long) supp_rates,
 				    (unsigned long long) sta->sta.supp_rates[band]);
 #endif
-		} else
-			ieee80211_ibss_add_sta(sdata, mgmt->bssid, mgmt->sa, supp_rates);
-
-		rcu_read_unlock();
+			rcu_read_unlock();
+		} else {
+			rcu_read_unlock();
+			ieee80211_ibss_add_sta(sdata, mgmt->bssid, mgmt->sa,
+					       supp_rates, GFP_KERNEL);
+		}
 	}
 
 	bss = ieee80211_bss_info_update(local, rx_status, mgmt, len, elems,
@@ -367,7 +369,8 @@ static void ieee80211_rx_bss_info(struct ieee80211_sub_if_data *sdata,
 		       sdata->dev->name, mgmt->bssid);
 #endif
 		ieee80211_sta_join_ibss(sdata, bss);
-		ieee80211_ibss_add_sta(sdata, mgmt->bssid, mgmt->sa, supp_rates);
+		ieee80211_ibss_add_sta(sdata, mgmt->bssid, mgmt->sa,
+				       supp_rates, GFP_KERNEL);
 	}
 
  put_bss:
@@ -380,7 +383,8 @@ static void ieee80211_rx_bss_info(struct ieee80211_sub_if_data *sdata,
  * must be callable in atomic context.
  */
 struct sta_info *ieee80211_ibss_add_sta(struct ieee80211_sub_if_data *sdata,
-					u8 *bssid,u8 *addr, u32 supp_rates)
+					u8 *bssid,u8 *addr, u32 supp_rates,
+					gfp_t gfp)
 {
 	struct ieee80211_local *local = sdata->local;
 	struct sta_info *sta;
@@ -405,7 +409,7 @@ struct sta_info *ieee80211_ibss_add_sta(struct ieee80211_sub_if_data *sdata,
 	       wiphy_name(local->hw.wiphy), addr, sdata->dev->name);
 #endif
 
-	sta = sta_info_alloc(sdata, addr, GFP_ATOMIC);
+	sta = sta_info_alloc(sdata, addr, gfp);
 	if (!sta)
 		return NULL;
 
@@ -417,9 +421,9 @@ struct sta_info *ieee80211_ibss_add_sta(struct ieee80211_sub_if_data *sdata,
 
 	rate_control_rate_init(sta);
 
+	/* If it fails, maybe we raced another insertion? */
 	if (sta_info_insert(sta))
-		return NULL;
-
+		return sta_info_get(local, addr);
 	return sta;
 }
 
