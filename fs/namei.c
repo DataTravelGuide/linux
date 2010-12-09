28a19576d8c9b5f770f0375812a9debaad49ce56
Jeff Layton <jlayton@redhat.com>
2010-12-09 17:43:02 -0500

    Fix f_flags/f_mode in case of lookup_instantiate_filp() from open(pathname, 3)
    Patchwork-id: 30036
    O-Subject: [RHEL6.1 PATCH 03/23] BZ#656461: Fix f_flags/f_mode in case of
    	lookup_instantiate_filp() from open(pathname, 3)
    Bugzilla: 656461
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    
    From: Al Viro <viro@zeniv.linux.org.uk>
    
    Just set f_flags when shoving struct file into nameidata; don't
    postpone that until __dentry_open().  do_filp_open() has correct
    value; lookup_instantiate_filp() doesn't - we lose the difference
    between O_RDWR and 3 by that point.
    
    We still set .intent.open.flags, so no fs code needs to be changed.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Upstream commit: 482928d59db668b8d82a48717f78986d8cea72e9
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- fs/namei.c
+++ fs/namei.c
@@ -1704,6 +1704,7 @@ struct file *do_filp_open(int dfd, const char *pathname,
 		if (filp == NULL)
 			return ERR_PTR(-ENFILE);
 		nd.intent.open.file = filp;
+		filp->f_flags = open_flag;
 		nd.intent.open.flags = flag;
 		nd.intent.open.create_mode = 0;
 		error = do_path_lookup(dfd, pathname,
@@ -1749,6 +1750,7 @@ struct file *do_filp_open(int dfd, const char *pathname,
 	if (filp == NULL)
 		goto exit_parent;
 	nd.intent.open.file = filp;
+	filp->f_flags = open_flag;
 	nd.intent.open.flags = flag;
 	nd.intent.open.create_mode = mode;
 	dir = nd.path.dentry;
@@ -1789,7 +1791,7 @@ do_last:
 			mnt_drop_write(nd.path.mnt);
 			goto exit;
 		}
-		filp = nameidata_to_filp(&nd, open_flag);
+		filp = nameidata_to_filp(&nd);
 		mnt_drop_write(nd.path.mnt);
 		if (nd.root.mnt)
 			path_put(&nd.root);
@@ -1852,7 +1854,7 @@ ok:
 			mnt_drop_write(nd.path.mnt);
 		goto exit;
 	}
-	filp = nameidata_to_filp(&nd, open_flag);
+	filp = nameidata_to_filp(&nd);
 	if (!IS_ERR(filp)) {
 		error = ima_file_check(filp, acc_mode);
 		if (error) {
