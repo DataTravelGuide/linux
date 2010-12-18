fc6e707dde1fc76413988bdcb40e5b5a1ac0af22
Larry Woodman <lwoodman@redhat.com>
2010-12-17 19:57:46 -0500

    sched: Kill the broken and deadlockable cpuset_lock/cpuset_cpus_allowed_locked code
    Patchwork-id: 30647
    O-Subject: [RHEL6.1 PATCH V2 24/64] sched: Kill the broken and deadlockable
    	cpuset_lock/cpuset_cpus_allowed_locked code
    Bugzilla: 557364 611867 613812 635558 635562
    RH-Acked-by: Bob Picco <bpicco@redhat.com>
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    
    mainline commit 897f0b3c3ff40b443c84e271bef19bd6ae885195
    
        This patch just states the fact the cpusets/cpuhotplug interaction is
        broken and removes the deadlockable code which only pretends to work.
    
        - cpuset_lock() doesn't really work. It is needed for
          cpuset_cpus_allowed_locked() but we can't take this lock in
          try_to_wake_up()->select_fallback_rq() path.
    
        - cpuset_lock() is deadlockable. Suppose that a task T bound to CPU takes
          callback_mutex. If cpu_down(CPU) happens before T drops callback_mutex
          stop_machine() preempts T, then migration_call(CPU_DEAD) tries to take
          cpuset_lock() and hangs forever because CPU is already dead and thus
          T can't be scheduled.
    
        - cpuset_cpus_allowed_locked() is deadlockable too. It takes task_lock()
          which is not irq-safe, but try_to_wake_up() can be called from irq.
    
        Kill them, and change select_fallback_rq() to use cpu_possible_mask, like
        we currently do without CONFIG_CPUSETS.
    
        Also, with or without this patch, with or without CONFIG_CPUSETS, the
        callers of select_fallback_rq() can race with each other or with
        set_cpus_allowed() pathes.
    
        The subsequent patches try to to fix these problems.
    
        Signed-off-by: Oleg Nesterov <oleg@redhat.com>
        Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
        LKML-Reference: <20100315091003.GA9123@redhat.com>
        Signed-off-by: Ingo Molnar <mingo@elte.hu>
        Signed-off-by: Mike Galbraith <efault@gmx.de>
    Signed-off-by: Larry Woodman <lwoodman@redhat.com>
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- include/linux/cpuset.h
+++ include/linux/cpuset.h
@@ -21,8 +21,6 @@ extern int number_of_cpusets;	/* How many cpusets are defined in system? */
 extern int cpuset_init(void);
 extern void cpuset_init_smp(void);
 extern void cpuset_cpus_allowed(struct task_struct *p, struct cpumask *mask);
-extern void cpuset_cpus_allowed_locked(struct task_struct *p,
-				       struct cpumask *mask);
 extern nodemask_t cpuset_mems_allowed(struct task_struct *p);
 #define cpuset_current_mems_allowed (current->mems_allowed)
 void cpuset_init_current_mems_allowed(void);
@@ -68,9 +66,6 @@ extern const struct file_operations proc_cpuset_operations;
 struct seq_file;
 extern void cpuset_task_status_allowed(struct seq_file *m,
 					struct task_struct *task);
-
-extern void cpuset_lock(void);
-extern void cpuset_unlock(void);
 
 extern int cpuset_mem_spread_node(void);
 extern int cpuset_slab_spread_node(void);
@@ -141,11 +136,6 @@ static inline void cpuset_cpus_allowed(struct task_struct *p,
 {
 	cpumask_copy(mask, cpu_possible_mask);
 }
-static inline void cpuset_cpus_allowed_locked(struct task_struct *p,
-					      struct cpumask *mask)
-{
-	cpumask_copy(mask, cpu_possible_mask);
-}
 
 static inline nodemask_t cpuset_mems_allowed(struct task_struct *p)
 {
@@ -192,9 +182,6 @@ static inline void cpuset_task_status_allowed(struct seq_file *m,
 						struct task_struct *task)
 {
 }
-
-static inline void cpuset_lock(void) {}
-static inline void cpuset_unlock(void) {}
 
 static inline int cpuset_mem_spread_node(void)
 {
