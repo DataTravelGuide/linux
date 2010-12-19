f2dfe2f3aad2b27cd5cf89f60db0292d24a101c4
Mike Christie <mchristi@redhat.com>
2010-12-18 20:05:14 -0500

    ibft, x86: Change reserve_ibft_region() to find_ibft_region()
    Patchwork-id: 30675
    O-Subject: [RHEL 6.1 PATCH 01/33] ibft,
    	x86: Change reserve_ibft_region() to find_ibft_region()
    Bugzilla: 585751 635746
    RH-Acked-by: Rob Evers <revers@redhat.com>
    
    From: Mike Christie <mchristi@redhat.com>
    
    This is for BZ 635746 and 585751.
    
    >From upstream commit:
    
    commit 042be38e6106ed70b42d096ab4a1ed4187e510e6
    Author: Yinghai Lu <yinghai@kernel.org>
    Date:   Thu Apr 1 14:32:43 2010 -0700
    
        ibft, x86: Change reserve_ibft_region() to find_ibft_region()
    
        This allows arch code could decide the way to reserve the ibft.
    
        And we should reserve ibft as early as possible, instead of BOOTMEM
        stage, in case the table is in RAM range and is not reserved by BIOS
        (this will often be the case.)
    
        Move to just after find_smp_config().
    
        Also when CONFIG_NO_BOOTMEM=y, We will not have reserve_bootmem() anymore.
    
        -v2: fix typo about ibft pointed by Konrad Rzeszutek Wilk <konrad@darnok.org>
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -630,6 +630,16 @@ static int __init setup_elfcorehdr(char *arg)
 early_param("elfcorehdr", setup_elfcorehdr);
 #endif
 
+static __init void reserve_ibft_region(void)
+{
+	unsigned long addr, size = 0;
+
+	addr = find_ibft_region(&size);
+
+	if (size)
+		reserve_early_overlap_ok(addr, addr + size, "ibft");
+}
+
 #ifdef CONFIG_X86_RESERVE_LOW_64K
 static int __init dmi_low_memory_corruption(const struct dmi_system_id *d)
 {
@@ -984,6 +994,8 @@ void __init setup_arch(char **cmdline_p)
 	 */
 	find_smp_config();
 
+	reserve_ibft_region();
+
 	reserve_crashkernel();
 
 #ifdef CONFIG_X86_64
@@ -994,8 +1006,6 @@ void __init setup_arch(char **cmdline_p)
 	 */
 	dma32_reserve_bootmem();
 #endif
-
-	reserve_ibft_region();
 
 #ifdef CONFIG_KVM_CLOCK
 	kvmclock_init();
