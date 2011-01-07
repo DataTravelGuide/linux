31767f810a7aba6f01667a29ddc5ca853fdc1e97
Jarod Wilson <jarod@redhat.com>
2011-01-06 19:22:19 -0500

    kmsg_dump: build fixups
    Patchwork-id: 31125
    O-Subject: [RHEL6 PATCH 3/4] kmsg_dump: build fixups
    Bugzilla: 632041
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Amerigo Wang <amwang@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bug 632041 - FEAT RHEL6.1: add kmsg_dumper feature
    
    Drop-in patches from upstream, merges two build fixes.
    
    	From 2bd7b83471825ebabd3fefb05f42f3d3659bbf63 Mon Sep 17 00:00:00 2001
    	From: Randy Dunlap <randy.dunlap@oracle.com>
    	Date: Thu, 17 Dec 2009 15:27:27 -0800
    	Subject: [PATCH 3/4] kmsg_dump: build fixups
    
    	printk: fix new kernel-doc warnings
    
    	Fix kernel-doc warnings in printk.c:
    
    	Warning(kernel/printk.c:1422): No description found for parameter 'dumper'
    	Warning(kernel/printk.c:1422): Excess function parameter 'dump' description in 'kmsg_dump_register'
    	Warning(kernel/printk.c:1451): No description found for parameter 'dumper'
    	Warning(kernel/printk.c:1451): Excess function parameter 'dump' description in 'kmsg_dump_unregister'
    
    	Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    	kmsg_dump: fix build for CONFIG_PRINTK=n
    
    	kmsg_dump() fails to build when CONFIG_PRINTK=n; provide stubs
    	for the kmsg_dump*() functions when CONFIG_PRINTK=n.
    
    	kernel/printk.c: In function 'kmsg_dump':
    	kernel/printk.c:1501: error: 'log_buf_len' undeclared (first use in this function)
    	kernel/printk.c:1502: error: 'logged_chars' undeclared (first use in this function)
    	kernel/printk.c:1506: error: 'log_buf' undeclared (first use in this function)
    
    	Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    	Acked-by: Simon Kagstrom <simon.kagstrom@netinsight.net>
    	Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- kernel/printk.c
+++ kernel/printk.c
@@ -1411,14 +1411,13 @@ bool printk_timed_ratelimit(unsigned long *caller_jiffies,
 	return false;
 }
 EXPORT_SYMBOL(printk_timed_ratelimit);
-#endif
 
 static DEFINE_SPINLOCK(dump_list_lock);
 static LIST_HEAD(dump_list);
 
 /**
  * kmsg_dump_register - register a kernel log dumper.
- * @dump: pointer to the kmsg_dumper structure
+ * @dumper: pointer to the kmsg_dumper structure
  *
  * Adds a kernel log dumper to the system. The dump callback in the
  * structure will be called when the kernel oopses or panics and must be
@@ -1448,7 +1447,7 @@ EXPORT_SYMBOL_GPL(kmsg_dump_register);
 
 /**
  * kmsg_dump_unregister - unregister a kmsg dumper.
- * @dump: pointer to the kmsg_dumper structure
+ * @dumper: pointer to the kmsg_dumper structure
  *
  * Removes a dump device from the system. Returns zero on success and
  * %-EINVAL otherwise.
@@ -1531,3 +1530,4 @@ void kmsg_dump(enum kmsg_dump_reason reason)
 		dumper->dump(dumper, reason, s1, l1, s2, l2);
 	spin_unlock_irqrestore(&dump_list_lock, flags);
 }
+#endif
