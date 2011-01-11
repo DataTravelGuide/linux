ae54f6dd5a12ac185d9c7d2a2ba1ca99122e3bd2
John Villalovos <jvillalo@redhat.com>
2011-01-11 16:58:21 -0500

    cpu: Add xsaveopt cpufeature
    Patchwork-id: 31505
    O-Subject: [PATCH RHEL6.1 BZ492912 11/13] x86, cpu: Add xsaveopt cpufeature
    Bugzilla: 492912
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Bob Picco <bpicco@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Backport of:
        Upstream commit 40e1d7a4ffee5cb17f5c36f4c3c4a011ab103ebe
        Upstream Author: Suresh Siddha <suresh.b.siddha@intel.com>
        Upstream Date:  Mon Jul 19 16:05:51 2010 -0700
    
        x86, cpu: Add xsaveopt cpufeature
    
        Add cpu feature bit support for the XSAVEOPT instruction.
    
    Signed-off-by: John L. Villalovos <jvillalo@redhat.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- arch/x86/include/asm/cpufeature.h
+++ arch/x86/include/asm/cpufeature.h
@@ -168,6 +168,7 @@
 #define X86_FEATURE_ARAT	(7*32+ 1) /* Always Running APIC Timer */
 #define X86_FEATURE_CPB		(7*32+ 2) /* AMD Core Performance Boost */
 #define X86_FEATURE_EPB		(7*32+ 3) /* IA32_ENERGY_PERF_BIAS support */
+#define X86_FEATURE_XSAVEOPT	(7*32+4) /* "xsaveopt" Optimized Xsave */
 
 /* Virtualization flags: Linux defined */
 #define X86_FEATURE_TPR_SHADOW  (8*32+ 0) /* Intel TPR Shadow */
