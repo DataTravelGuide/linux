93130eec1336bbd1e0d2e3cd789019eba383099f
Mike Christie <mchristi@redhat.com>
2011-01-11 04:20:09 -0500

    bnx2x: make local function static and remove dead code
    Patchwork-id: 31370
    O-Subject: [RHEL 6.1 PATCH 096/122] bnx2x: make local function static and remove
    	dead code
    Bugzilla: 635942
    RH-Acked-by: Michal Schmidt <mschmidt@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    From: stephen hemminger <shemminger@vyatta.com>
    
    Make many functions that are only used in one file static.
    Remove dead code for override_led_value.
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 8d96286ae13a0bf4ec04341c9b25694adff38623)
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/net/bnx2x/bnx2x_cmn.c
+++ drivers/net/bnx2x/bnx2x_cmn.c
@@ -28,6 +28,7 @@
 
 #include "bnx2x_init.h"
 
+static int bnx2x_setup_irqs(struct bnx2x *bp);
 
 /* free skb in the packet ring at pos idx
  * return idx of last bd freed
@@ -2207,7 +2208,7 @@ int bnx2x_change_mac_addr(struct net_device *dev, void *p)
 }
 
 
-int bnx2x_setup_irqs(struct bnx2x *bp)
+static int bnx2x_setup_irqs(struct bnx2x *bp)
 {
 	int rc = 0;
 	if (bp->flags & USING_MSIX_FLAG) {
