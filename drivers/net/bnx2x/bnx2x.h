0a1f272fe8cadaf8c094f01e46eb69285c975a6c
Mike Christie <mchristi@redhat.com>
2011-01-11 04:20:27 -0500

    bnx2x: Update version number and a date
    Patchwork-id: 31395
    O-Subject: [RHEL 6.1 PATCH 114/122] bnx2x: Update version number and a date.
    Bugzilla: 635942
    RH-Acked-by: Michal Schmidt <mschmidt@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    From: Vladislav Zolotarov <vladz@broadcom.com>
    
    Signed-off-by: Vladislav Zolotarov <vladz@broadcom.com>
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit f404c2fea37e02bec7c8b6edddf5edd22ca60505)
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/net/bnx2x/bnx2x.h
+++ drivers/net/bnx2x/bnx2x.h
@@ -20,8 +20,8 @@
  * (you will need to reboot afterwards) */
 /* #define BNX2X_STOP_ON_ERROR */
 
-#define DRV_MODULE_VERSION      "1.60.00-6"
-#define DRV_MODULE_RELDATE      "2010/11/29"
+#define DRV_MODULE_VERSION      "1.60.00-7"
+#define DRV_MODULE_RELDATE      "2010/12/08"
 #define BNX2X_BC_VER            0x040200
 
 #if defined(CONFIG_VLAN_8021Q) || defined(CONFIG_VLAN_8021Q_MODULE)
