0c45584f2502af14d7c8e0ec6c3c15f32583fafa
Dean Nelson <dnelson@redhat.com>
2011-01-13 01:26:07 -0500

    e100: Add missing read memory barrier
    Patchwork-id: 31715
    O-Subject: [RHEL6.1 PATCH 09/10] e100/e1000*/igb*/ixgb*: Add missing read memory
    	barrier
    Bugzilla: 636331
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    
    Resolves RHBZ 636331
    
    Backport of relevant portion of:
    
    commit 2d0bb1c1f4524befe9f0fcf0d0cd3081a451223f
    Author: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Date:   Sun Aug 8 16:02:31 2010 +0000
    
        e100/e1000*/igb*/ixgb*: Add missing read memory barrier
    
        Based on patches from Sonny Rao and Milton Miller...
    
        Combined the patches to fix up clean_tx_irq and clean_rx_irq.
    
        The PowerPC architecture does not require loads to independent bytes
        to be ordered without adding an explicit barrier.
    
        In ixgbe_clean_rx_irq we load the status bit then load the packet data.
        With packet split disabled if these loads go out of order we get a
        stale packet, but we will notice the bad sequence numbers and drop it.
    
        The problem occurs with packet split enabled where the TCP/IP header
        and data are in different descriptors. If the reads go out of order
        we may have data that doesn't match the TCP/IP header. Since we use
        hardware checksumming this bad data is never verified and it makes it
        all the way to the application.
    
        This bug was found during stress testing and adding this barrier has
        been shown to fix it.  The bug can manifest as a data integrity issue
        (bad payload data) or as a BUG in skb_pull().
    
        This was a nasty bug to hunt down, if people agree with the fix I think
        it's a candidate for stable.
    
        Previously Submitted to e1000-devel only for ixgbe
    
        http://marc.info/?l=e1000-devel&m=126593062701537&w=3
    
        We've now seen this problem hit with other device drivers (e1000e mostly)
        So I'm resubmitting with fixes for other Intel Device Drivers with
        similar issues.
    
        CC: Milton Miller <miltonm@bga.com>
        CC: Anton Blanchard <anton@samba.org>
        CC: Sonny Rao <sonnyrao@us.ibm.com>
        CC: stable <stable@kernel.org>
        Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/e100.c
+++ drivers/net/e100.c
@@ -1928,6 +1928,7 @@ static int e100_rx_indicate(struct nic *nic, struct rx *rx,
 
 	netif_printk(nic, rx_status, KERN_DEBUG, nic->netdev,
 		     "status=0x%04X\n", rfd_status);
+	rmb(); /* read size after status bit */
 
 	/* If data isn't ready, nothing to indicate */
 	if (unlikely(!(rfd_status & cb_complete))) {
