399e327e280a35df9159eef1b52c331d61d4b6ba
Tomas Henzl <thenzl@redhat.com>
2011-01-21 14:29:48 -0500

    mptsas: Proper error handling is added after mpt_config timeout
    Patchwork-id: 32778
    O-Subject: [RHEL6.1 PATCH 17/32] mptsas: Proper error handling is added after
    	mpt_config timeout
    Bugzilla: 642618
    RH-Acked-by: Rob Evers <revers@redhat.com>
    
    Added proper error handling after mpt_config.
    Now check of MPI_IOCSTATUS_CONFIG_INVALID_PAGE is added.
    If error is MPI_IOCSTATUS_CONFIG_INVALID_PAGE, driver will return -ENODEV.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/message/fusion/mptsas.c
+++ drivers/message/fusion/mptsas.c
@@ -2523,6 +2523,12 @@ mptsas_sas_phy_pg0(MPT_ADAPTER *ioc, struct mptsas_phyinfo *phy_info,
 	cfg.action = MPI_CONFIG_ACTION_PAGE_READ_CURRENT;
 
 	error = mpt_config(ioc, &cfg);
+
+	if (error == MPI_IOCSTATUS_CONFIG_INVALID_PAGE) {
+		error = -ENODEV;
+		goto out_free_consistent;
+	}
+
 	if (error)
 		goto out_free_consistent;
 
@@ -2663,13 +2669,13 @@ mptsas_sas_expander_pg0(MPT_ADAPTER *ioc, struct mptsas_portinfo *port_info,
 	cfg.action = MPI_CONFIG_ACTION_PAGE_READ_CURRENT;
 
 	error = mpt_config(ioc, &cfg);
-	if (error)
-		goto out_free_consistent;
-
-	if (!buffer->NumPhys) {
+	if (error == MPI_IOCSTATUS_CONFIG_INVALID_PAGE) {
 		error = -ENODEV;
 		goto out_free_consistent;
 	}
+
+	if (error)
+		goto out_free_consistent;
 
 	/* save config data */
 	port_info->num_phys = (buffer->NumPhys) ? buffer->NumPhys : 1;
@@ -2746,7 +2752,7 @@ mptsas_sas_expander_pg1(MPT_ADAPTER *ioc, struct mptsas_phyinfo *phy_info,
 
 	if (error == MPI_IOCSTATUS_CONFIG_INVALID_PAGE) {
 		error = -ENODEV;
-		goto out;
+		goto out_free_consistent;
 	}
 
 	if (error)
