b03aeb25681141c291e9114181ef62c3c9cad2ad
Steve Dickson <SteveD@redhat.com>
2011-01-24 19:01:45 -0500

    pnfs: layout roc code
    Patchwork-id: 32845
    O-Subject: [RHEL6.1 PATCH 28/29] pnfs: layout roc code
    Bugzilla: 479351
    RH-Acked-by: Jeff Layton <jlayton@redhat.com>
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    
    From: Fred Isaman <iisaman@netapp.com>
    
    A layout can request return-on-close.  How this interacts with the
    forgetful model of never sending LAYOUTRETURNS is a bit ambiguous.
    We forget any layouts marked roc, and wait for them to be completely
    forgotten before continuing with the close.  In addition, to compensate
    for races with any inflight LAYOUTGETs, and the fact that we do not get
    any layout stateid back from the server, we set the barrier to the worst
    case scenario of current_seqid + number of outstanding LAYOUTGETS.
    
    Signed-off-by: Fred Isaman <iisaman@netapp.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- fs/nfs/client.c
+++ fs/nfs/client.c
@@ -243,6 +243,11 @@ static void nfs_cb_idr_remove_locked(struct nfs_client *clp)
 		idr_remove(&cb_ident_idr, clp->cl_cb_ident);
 }
 
+static void pnfs_init_server(struct nfs_server *server)
+{
+	rpc_init_wait_queue(&server->roc_rpcwaitq, "pNFS ROC");
+}
+
 #else
 static void nfs4_shutdown_client(struct nfs_client *clp)
 {
@@ -255,6 +260,11 @@ void nfs_cleanup_cb_ident_idr(void)
 static void nfs_cb_idr_remove_locked(struct nfs_client *clp)
 {
 }
+
+static void pnfs_init_server(struct nfs_server *server)
+{
+}
+
 #endif /* CONFIG_NFS_V4 */
 
 /*
@@ -1022,6 +1032,8 @@ static struct nfs_server *nfs_alloc_server(void)
 		kfree(server);
 		return NULL;
 	}
+
+	pnfs_init_server(server);
 
 	return server;
 }
