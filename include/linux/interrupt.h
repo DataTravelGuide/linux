c6b69df47998c466951ca455a21e2578100370c3
Andrew Jones <drjones@redhat.com>
2011-01-26 07:58:39 -0500

    Add new IRQ flag IRQF_NO_SUSPEND
    Patchwork-id: 32878
    O-Subject: [RHEL 6.1 PATCH 1/5 v2] irq: Add new IRQ flag IRQF_NO_SUSPEND
    Bugzilla: 671147
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    
    BZ671147
    
    This is a partial backport of upstream commit
    685fd0b4ea3f0f1d5385610b0d5b57775a8d5842, but the backport is even
    more true to the summary then the original commit was, as we _only_
    add thew new flag IRQF_NO_SUSPEND.
    
    The purpose of the flag is to enable no-suspend behavior as the
    name implies. Ideally we would redefine the IRQF_TIMER flag to
    absorb this new flag, but then only test against the new flag when
    checking if we should avoid suspending. That would allow us to stop
    overloading the purpose of IRQF_TIMER. However, to preserve kABI we
    should keep the old flags the same and just introduce the new one to
    be used in the future.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -55,6 +55,8 @@
  * IRQF_ONESHOT - Interrupt is not reenabled after the hardirq handler finished.
  *                Used by threaded interrupts which need to keep the
  *                irq line disabled until the threaded handler has been run.
+ * IRQF_NO_SUSPEND - Do not disable this IRQ during suspend
+ *
  */
 #define IRQF_DISABLED		0x00000020
 #define IRQF_SAMPLE_RANDOM	0x00000040
@@ -65,6 +67,7 @@
 #define IRQF_NOBALANCING	0x00000800
 #define IRQF_IRQPOLL		0x00001000
 #define IRQF_ONESHOT		0x00002000
+#define IRQF_NO_SUSPEND		0x00004000
 
 /*
  * Bits used by threaded handlers:
