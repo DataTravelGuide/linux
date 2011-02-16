8f7b7755777aa7ee8f115687d80eb48f6762d0ef
Tomas Henzl <thenzl@redhat.com>
2011-02-16 15:01:03 -0500

    hpsa: Fix use of unitialized variable
    Patchwork-id: 33351
    O-Subject: [RHEL6.1 PATCH 027/112] hpsa: Fix use of unitialized variable
    Bugzilla: 630060
    RH-Acked-by: Rob Evers <revers@redhat.com>
    
    Fix use of unitialized variable in add_msa2xxx_enclosure_device.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -1593,6 +1593,8 @@ static int add_msa2xxx_enclosure_device(struct ctlr_info *h,
 	if (lun == 0) /* if lun is 0, then obviously we have a lun 0. */
 		return 0;
 
+	memset(scsi3addr, 0, 8);
+	scsi3addr[3] = target;
 	if (is_hba_lunid(scsi3addr))
 		return 0; /* Don't add the RAID controller here. */
 
@@ -1607,8 +1609,6 @@ static int add_msa2xxx_enclosure_device(struct ctlr_info *h,
 		return 0;
 	}
 
-	memset(scsi3addr, 0, 8);
-	scsi3addr[3] = target;
 	if (hpsa_update_device_info(h, scsi3addr, this_device))
 		return 0;
 	(*nmsa2xxx_enclosures)++;
