acca2f1382782f8ffbf61b572b2df047899010fa
Tomas Henzl <thenzl@redhat.com>
2011-02-16 15:01:24 -0500

    hpsa: add new transport_mode sys entry
    Patchwork-id: 33376
    O-Subject: [RHEL6.1 PATCH 048/112] hpsa: add new transport_mode sys entry
    Bugzilla: 630060
    RH-Acked-by: Rob Evers <revers@redhat.com>
    
    This is so that it is possible to tell if the driver is running
    the controller in simple mode, or in performant mode.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -175,6 +175,8 @@ static ssize_t host_show_firmware_revision(struct device *dev,
 	     struct device_attribute *attr, char *buf);
 static ssize_t host_show_commands_outstanding(struct device *dev,
 	     struct device_attribute *attr, char *buf);
+static ssize_t host_show_transport_mode(struct device *dev,
+	struct device_attribute *attr, char *buf);
 static void hpsa_update_scsi_devices(struct ctlr_info *h, int hostno);
 static ssize_t host_store_rescan(struct device *dev,
 	 struct device_attribute *attr, const char *buf, size_t count);
@@ -206,6 +208,8 @@ static DEVICE_ATTR(firmware_revision, S_IRUGO,
 	host_show_firmware_revision, NULL);
 static DEVICE_ATTR(commands_outstanding, S_IRUGO,
 	host_show_commands_outstanding, NULL);
+static DEVICE_ATTR(transport_mode, S_IRUGO,
+	host_show_transport_mode, NULL);
 
 static struct device_attribute *hpsa_sdev_attrs[] = {
 	&dev_attr_raid_level,
@@ -218,6 +222,7 @@ static struct device_attribute *hpsa_shost_attrs[] = {
 	&dev_attr_rescan,
 	&dev_attr_firmware_revision,
 	&dev_attr_commands_outstanding,
+	&dev_attr_transport_mode,
 	NULL,
 };
 
@@ -325,6 +330,18 @@ static ssize_t host_show_commands_outstanding(struct device *dev,
 	struct ctlr_info *h = shost_to_hba(shost);
 
 	return snprintf(buf, 20, "%d\n", h->commands_outstanding);
+}
+
+static ssize_t host_show_transport_mode(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	struct ctlr_info *h;
+	struct Scsi_Host *shost = class_to_shost(dev);
+
+	h = shost_to_hba(shost);
+	return snprintf(buf, 20, "%s\n",
+		h->transMethod == CFGTBL_Trans_Performant ?
+			"performant" : "simple");
 }
 
 /* Enqueuing and dequeuing functions for cmdlists. */
