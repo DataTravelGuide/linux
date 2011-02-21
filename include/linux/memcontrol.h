e734e55d4d68b124ec6c832cd38eb205c0bbffe8
Johannes Weiner <jweiner@redhat.com>
2011-02-21 13:33:44 -0500

    memcg: make memcg's file mapped consistent with global VM
    Patchwork-id: 33469
    O-Subject: [RHEL6 PATCH] memcg: make memcg's file mapped consistent with global
    	VM
    Bugzilla: 679021
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    
    bz: 679021
    brew: 3126539
    
    commit d8046582d5ee24448800e71c6933fdb6813aa062
    Author: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Date:   Tue Dec 15 16:47:09 2009 -0800
    
        memcg: make memcg's file mapped consistent with global VM
    
        In global VM, FILE_MAPPED is used but memcg uses MAPPED_FILE.  This makes
        grep difficult.  Replace memcg's MAPPED_FILE with FILE_MAPPED
    
        And in global VM, mapped shared memory is accounted into FILE_MAPPED.
        But memcg doesn't. fix it.
        Note:
          page_is_file_cache() just checks SwapBacked or not.
          So, we need to check PageAnon.
    
        Cc: Balbir Singh <balbir@in.ibm.com>
        Reviewed-by: Daisuke Nishimura <nishimura@mxp.nes.nec.co.jp>
        Signed-off-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Johannes Weiner <jweiner@redhat.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -120,7 +120,7 @@ static inline bool mem_cgroup_disabled(void)
 	return false;
 }
 
-void mem_cgroup_update_mapped_file_stat(struct page *page, int val);
+void mem_cgroup_update_file_mapped(struct page *page, int val);
 unsigned long mem_cgroup_soft_limit_reclaim(struct zone *zone, int order,
 						gfp_t gfp_mask, int nid,
 						int zid);
@@ -284,7 +284,7 @@ mem_cgroup_print_oom_info(struct mem_cgroup *memcg, struct task_struct *p)
 {
 }
 
-static inline void mem_cgroup_update_mapped_file_stat(struct page *page,
+static inline void mem_cgroup_update_file_mapped(struct page *page,
 							int val)
 {
 }
