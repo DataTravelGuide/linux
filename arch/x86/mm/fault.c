871d41cb2d686b75c2c94a9864ea52cb0947409b
Andrea Arcangeli <aarcange@redhat.com>
2011-02-28 22:34:13 -0500

    fix pgd_lock deadlock
    Patchwork-id: 33688
    O-Subject: [PATCH RHEL6.1] fix pgd_lock deadlock #2
    Bugzilla: 671477
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    
    This is a new approach. Waiting upstream to comment on it to be sure
    it's enough. If it's enough the removal of _irqsave isn't needed
    anymore for RHEL (I'll push it later upstream as a cleanup).
    
    https://bugzilla.redhat.com/show_bug.cgi?id=671477
    http://brewweb.devel.redhat.com/brew/taskinfo?taskID=3143355
    
    ===
    Subject: xen: stop taking the page_table_lock with irq disabled
    
    From: Andrea Arcangeli <aarcange@redhat.com>
    
    It's forbidden to take the page_table_lock with the irq disabled or if there's
    contention the IPIs (for tlb flushes) sent with the page_table_lock held will
    never run leading to a deadlock.
    
    Only Xen needs the page_table_lock and Xen won't need IPI TLB flushes hence the
    deadlock doesn't exist for Xen.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -18,6 +18,8 @@
 #include <asm/pgalloc.h>		/* pgd_*(), ...			*/
 #include <asm/kmemcheck.h>		/* kmemcheck_*(), ...		*/
 
+#include <xen/xen.h>			/* xen_pv_domain()		*/
+
 /*
  * Page fault error code bits:
  *
@@ -235,14 +237,16 @@ void vmalloc_sync_all(void)
 
 		spin_lock_irqsave(&pgd_lock, flags);
 		list_for_each_entry(page, &pgd_list, lru) {
-			spinlock_t *pgt_lock;
+			struct mm_struct *mm;
 			pmd_t *ret;
 
-			pgt_lock = &pgd_page_get_mm(page)->page_table_lock;
+			mm = pgd_page_get_mm(page);
 
-			spin_lock(pgt_lock);
+			if (mm)
+				spin_lock(&mm->page_table_lock);
 			ret = vmalloc_sync_one(page_address(page), address);
-			spin_unlock(pgt_lock);
+			if (mm)
+				spin_unlock(&mm->page_table_lock);
 
 			if (!ret)
 				break;
@@ -356,19 +360,21 @@ void vmalloc_sync_all(void)
 		spin_lock_irqsave(&pgd_lock, flags);
 		list_for_each_entry(page, &pgd_list, lru) {
 			pgd_t *pgd;
-			spinlock_t *pgt_lock;
+			struct mm_struct *mm;
 
 			pgd = (pgd_t *)page_address(page) + pgd_index(address);
 
-			pgt_lock = &pgd_page_get_mm(page)->page_table_lock;
-			spin_lock(pgt_lock);
+			mm = pgd_page_get_mm(page);
+			if (mm)
+				spin_lock(&mm->page_table_lock);
 
 			if (pgd_none(*pgd))
 				set_pgd(pgd, *pgd_ref);
 			else
 				BUG_ON(pgd_page_vaddr(*pgd) != pgd_page_vaddr(*pgd_ref));
 
-			spin_unlock(pgt_lock);
+			if (mm)
+				spin_unlock(&mm->page_table_lock);
 		}
 		spin_unlock_irqrestore(&pgd_lock, flags);
 	}
