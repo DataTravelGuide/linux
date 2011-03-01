8e7f616764fd4b6db8a79629568a785573344ca6
Andrea Arcangeli <aarcange@redhat.com>
2011-03-01 00:03:25 -0500

    optimistic migration limited to movable pageblocks
    Patchwork-id: 33694
    O-Subject: [PATCH RHEL6.1 07/22] optimistic migration limited to movable
    	pageblocks
    Bugzilla: 674147
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    RH-Acked-by: Johannes Weiner <jweiner@redhat.com>
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    
    From: Mel Gorman <mel@csn.ul.ie>
    
    try_to_compact_pages() is initially called to only migrate pages
    asychronously and kswapd always compacts asynchronously.  Both are being
    optimistic so it is important to complete the work as quickly as possible
    to minimise stalls.
    
    This patch alters the scanner when asynchronous to only consider
    MIGRATE_MOVABLE pageblocks as migration candidates.  This reduces stalls
    when allocating huge pages while not impairing allocation success rates as
    a full scan will be performed if necessary after direct reclaim.
    
    Signed-off-by: Mel Gorman <mel@csn.ul.ie>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -237,6 +237,7 @@ static unsigned long isolate_migratepages(struct zone *zone,
 					struct compact_control *cc)
 {
 	unsigned long low_pfn, end_pfn;
+	unsigned long last_pageblock_nr = 0, pageblock_nr;
 	struct list_head *migratelist = &cc->migratepages;
 
 	/* Do not scan outside zone boundaries */
@@ -274,6 +275,20 @@ static unsigned long isolate_migratepages(struct zone *zone,
 		page = pfn_to_page(low_pfn);
 		if (PageBuddy(page))
 			continue;
+
+		/*
+		 * For async migration, also only scan in MOVABLE blocks. Async
+		 * migration is optimistic to see if the minimum amount of work
+		 * satisfies the allocation
+		 */
+		pageblock_nr = low_pfn >> pageblock_order;
+		if (!cc->sync && last_pageblock_nr != pageblock_nr &&
+				get_pageblock_migratetype(page) != MIGRATE_MOVABLE) {
+			low_pfn += pageblock_nr_pages;
+			low_pfn = ALIGN(low_pfn, pageblock_nr_pages) - 1;
+			last_pageblock_nr = pageblock_nr;
+			continue;
+		}
 
 		if (!PageLRU(page))
 			continue;
