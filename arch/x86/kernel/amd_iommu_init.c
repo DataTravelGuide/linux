7a2b05df3f51ba2cebdcd92bdee295af44b39015
Prarit Bhargava <prarit@redhat.com>
2011-03-07 15:39:15 -0500

    AMD IOMMU: Display warning for AMD systems
    Patchwork-id: 33832
    O-Subject: [RHEL6 BZ 681104 PATCH] AMD IOMMU: Display warning for AMD systems
    Bugzilla: 681104
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    RHEL6 commit 4a8664de0135f1df8f64a4ea9f602bab5189ac57 introduced a warning
    message that displays information from an AMD only driver during boot.
    
    The code is RHEL6 only, and the driver and warning should be ignored for
    Intel systems.
    
    Successfully tested by me.
    
    Resolves BZ 681104.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- arch/x86/kernel/amd_iommu_init.c
+++ arch/x86/kernel/amd_iommu_init.c
@@ -1497,6 +1497,11 @@ static int __init early_amd_iommu_detect(struct acpi_table_header *table)
 
 void __init amd_iommu_detect(void)
 {
+	struct cpuinfo_x86 *c = &cpu_data(0);
+
+	if (c->x86_vendor != X86_VENDOR_AMD)
+		return;
+
 	if (swiotlb || no_iommu || (iommu_detected && !gart_iommu_aperture))
 		return;
 
