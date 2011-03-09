4e85b0051be59773540c82f1be47e1c3d1af6791
Don Zickus <dzickus@redhat.com>
2011-03-08 23:01:12 -0500

    watchdog, nmi: Allow hardlockup to panic by default
    Patchwork-id: 33860
    O-Subject: [RHEL6 PATCH] [v2] watchdog, nmi: Allow hardlockup to panic by default
    Bugzilla: 677532
    RH-Acked-by: Andy Gospodarek <gospo@redhat.com>
    RH-Acked-by: Bob Picco <bpicco@redhat.com>
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    https://bugzilla.redhat.com/show_bug.cgi?id=677532
    
    Add a Kconfig option to allow users to set the hardlockup to panic
    by default.  Also add in a 'nmi_watchdog=nopanic' to override this.
    
    Sent upstream, haven't heard any responses.  Tested locally by me.
    
    v2:
       fixed strcmp to be 7 not 5 for nopanic
    
    Signed-off-by: Don Zickus <dzickus@redhat.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- kernel/watchdog.c
+++ kernel/watchdog.c
@@ -51,12 +51,15 @@ static int no_watchdog;
  * Should we panic when a soft-lockup or hard-lockup occurs:
  */
 #ifdef CONFIG_HARDLOCKUP_DETECTOR
-static int hardlockup_panic;
+static int hardlockup_panic = 
+			CONFIG_BOOTPARAM_HARDLOCKUP_PANIC_VALUE;
 
 static int __init hardlockup_panic_setup(char *str)
 {
 	if (!strncmp(str, "panic", 5))
 		hardlockup_panic = 1;
+	else if (!strncmp(str, "nopanic", 7))
+		hardlockup_panic = 0;
 	else if (!strncmp(str, "0", 1))
 		no_watchdog = 1;
 	return 1;
