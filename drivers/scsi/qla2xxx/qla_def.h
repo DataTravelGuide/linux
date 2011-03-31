847e3c0c2873a63ddaed9d597ed0f9ad7c929fa0
Chad Dupuis <cdupuis@redhat.com>
2011-03-31 17:02:54 -0400

    qla2xxx: Log fcport state transitions when debug messages are enabled
    Patchwork-id: 35201
    O-Subject: [RHEL 6.1 PATCH 10/17] qla2xxx: Log fcport state transitions when
    	debug messages are enabled.
    Bugzilla: 686341
    RH-Acked-by: Rob Evers <revers@redhat.com>
    RH-Acked-by: Tomas Henzl <thenzl@redhat.com>
    
    Bugzilla
    --------
    
    Bug 686341 (https://bugzilla.redhat.com/show_bug.cgi?id=686341)
    
    Upstream Status
    ---------------
    
    Posted to linux-scsi: http://marc.info/?l=linux-scsi&m=130151109128012&w=2
    
    Description
    -----------
    
    >From 1a4e2d9eb71b9f93bf881d9492f8985fffbff516 Mon Sep 17 00:00:00 2001
    From: Chad Dupuis <chad.dupuis@qlogic.com>
    Date: Wed, 2 Mar 2011 21:10:33 -0800
    Subject: [PATCH] qla2xxx: Log fcport state transitions when debug messages are enabled.
    
    Add the inline function qla2x00_set_port_state() so that when a fcport state
    transition happens we can log the state transition if debug messages are
    enabled.
    
    Signed-off-by: Chad Dupuis <chad.dupuis@qlogic.com>
    Signed-off-by: Madhuranath Iyengar <Madhu.Iyengar@qlogic.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -1720,6 +1720,14 @@ typedef struct fc_port {
 #define FCS_DEVICE_LOST		3
 #define FCS_ONLINE		4
 
+static const char * const port_state_str[] = {
+	"Unknown",
+	"UNCONFIGURED",
+	"DEAD",
+	"LOST",
+	"ONLINE"
+};
+
 /*
  * FC port flags.
  */
