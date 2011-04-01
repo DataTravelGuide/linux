0811fb36466841578b39be6f0ca69064407a8dc3
Jiri Olsa <jolsa@redhat.com>
2011-04-01 10:13:24 -0400

    perf: Fix task context scheduling
    Patchwork-id: 35301
    O-Subject: [PATCH RHEL6.1] BZ 688065 - perf: Fix task context scheduling
    Bugzilla: 688065
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    RH-Acked-by: Oleg Nesterov <oleg@redhat.com>
    RH-Acked-by: Bob Picco <bpicco@redhat.com>
    
    Bugzilla: 688065
    https://bugzilla.redhat.com/show_bug.cgi?id=688065
    
    Description:
    ============
    When task released its last perf counter, its context stays
    inside cpu context and is not cleared out, because the perf
    "scheduling out" callback is not called (as a consequence
    of releasing last task's event).
    
    If the task is then appropriately scheduled this could lead
    to the panic, as described here:
    http://marc.info/?l=linux-kernel&m=130098510028542&w=4
    
    Or just mess up the event value counting, as in the perf
    bultin test #3.
    
    For now, there's simple fix ensuring the context is allways
    scheduled out.
    
    Upstream status:
    ================
    - perf: Fix task context scheduling
      commit ab711fe08297de1485fff0a366e6db8828cafd6a
      Author: Peter Zijlstra <a.p.zijlstra@chello.nl>
      Date:   Thu Mar 31 10:29:26 2011 +0200
    
    Brew:
    =====
    https://brewweb.devel.redhat.com/taskinfo?taskID=3219211
    
    Tested:
    =======
    perf test #3 does not fail
    
    wbr,
    jirka
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -1059,9 +1059,6 @@ void perf_event_task_sched_out(struct task_struct *task, struct task_struct *nex
 {
 	perf_sw_event(PERF_COUNT_SW_CONTEXT_SWITCHES, 1, 1, NULL, 0);
 
-	if (likely(!atomic_read(&perf_task_events)))
-		return;
-
 	__perf_event_task_sched_out(task, next);
 }
 
