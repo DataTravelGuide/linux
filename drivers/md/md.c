4b99de2f77204a3017c1a4e98573942912340bc9
Doug Ledford <dledford@redhat.com>
2011-03-31 20:29:38 -0400

    add new param to_sync_page_io()
    Patchwork-id: 35240
    O-Subject: [PATCH 13/37] md-new-param-to_sync_page_io
    Bugzilla: 688725
    RH-Acked-by: Andy Gospodarek <gospo@redhat.com>
    RH-Acked-by: Mike Snitzer <snitzer@redhat.com>
    
    From: Jonathan Brassow <jbrassow@redhat.com>
    
    Add new parameter to 'sync_page_io'.
    
    The new parameter allows us to distinguish between metadata and data
    operations.  This becomes important later when we add the ability to
    use separate devices for data and metadata.
    
    upstream: ccebd4c4159462c96397ae9af9c667bb394d7b70
    
    Signed-off-by: Jonathan Brassow <jbrassow@redhat.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -794,7 +794,7 @@ static void bi_complete(struct bio *bio, int error)
 }
 
 int sync_page_io(mdk_rdev_t *rdev, sector_t sector, int size,
-		 struct page *page, int rw)
+		 struct page *page, int rw, bool metadata_op)
 {
 	struct bio *bio = bio_alloc_mddev(GFP_NOIO, 1, rdev->mddev);
 	struct completion event;
@@ -803,7 +803,10 @@ int sync_page_io(mdk_rdev_t *rdev, sector_t sector, int size,
 	rw |= (1 << BIO_RW_SYNCIO) | (1 << BIO_RW_UNPLUG);
 
 	bio->bi_bdev = rdev->bdev;
-	bio->bi_sector = sector;
+	if (metadata_op)
+		bio->bi_sector = sector + rdev->sb_start;
+	else
+		bio->bi_sector = sector + rdev->data_offset;
 	bio_add_page(bio, page, size, 0);
 	init_completion(&event);
 	bio->bi_private = &event;
@@ -828,7 +831,7 @@ static int read_disk_sb(mdk_rdev_t * rdev, int size)
 		return 0;
 
 
-	if (!sync_page_io(rdev, rdev->sb_start, size, rdev->sb_page, READ))
+	if (!sync_page_io(rdev, 0, size, rdev->sb_page, READ, true))
 		goto fail;
 	rdev->sb_loaded = 1;
 	return 0;
