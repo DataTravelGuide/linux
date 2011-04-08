cf8287421537cf294544ab7c143f1a5f17fa3051
Tomas Henzl <thenzl@redhat.com>
2011-04-08 12:27:40 -0400

    hpsa: fix pci_device_id table
    Patchwork-id: 35417
    O-Subject: [RHEL6.1 PATCH] hpsa: fix pci_device_id table
    Bugzilla: 684997
    RH-Acked-by: Rob Evers <revers@redhat.com>
    
    This is for bz#684997
    
    This was found during alpha testing - 'P411/P212: hpsa couldn't be found when installing'
    Some of the pci-id's  were incorrectly defined.
    This patch comes comes from HP and fixes the situation.
    The defines in RHEL5 and upstream are correct.
    
    http://brewweb.devel.redhat.com/brew/taskinfo?taskID=3243392
    
    Tomas
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -80,13 +80,6 @@ module_param(hpsa_simple_mode, int, S_IRUGO|S_IWUSR);
 MODULE_PARM_DESC(hpsa_simple_mode,
 	"Use 'simple mode' rather than 'performant mode'");
 
-#ifndef PCI_DEVICE_ID_HP_CISSF
-#define PCI_DEVICE_ID_HP_CISSF 0x333F
-#endif
-#ifndef PCI_DEVICE_ID_HP_CISSG
-#define PCI_DEVICE_ID_HP_CISSG 0x333B
-#endif
-
 /* define the PCI info for the cards we can control */
 static const struct pci_device_id hpsa_pci_device_id[] = {
 	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSE,     0x103C, 0x3241},
@@ -97,12 +90,13 @@ static const struct pci_device_id hpsa_pci_device_id[] = {
 	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSE,     0x103C, 0x324a},
 	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSE,     0x103C, 0x324b},
 	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSE,     0x103C, 0x3233},
-	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSG,     0x103C, 0x3350},
-	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSG,     0x103C, 0x3351},
-	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSG,     0x103C, 0x3352},
-	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSG,     0x103C, 0x3353},
-	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSG,     0x103C, 0x3354},
-	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSF,     0x103C, 0x333F},
+	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSF,     0x103C, 0x3350},
+	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSF,     0x103C, 0x3351},
+	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSF,     0x103C, 0x3352},
+	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSF,     0x103C, 0x3353},
+	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSF,     0x103C, 0x3354},
+	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSF,     0x103C, 0x3355},
+	{PCI_VENDOR_ID_HP,     PCI_DEVICE_ID_HP_CISSF,     0x103C, 0x3356},
 	{PCI_VENDOR_ID_HP,     PCI_ANY_ID,             PCI_ANY_ID, PCI_ANY_ID,
 		PCI_CLASS_STORAGE_RAID << 8, 0xffff << 8, 0},
 	{PCI_VENDOR_ID_COMPAQ,     PCI_ANY_ID,         PCI_ANY_ID, PCI_ANY_ID,
@@ -131,6 +125,8 @@ static struct board_type products[] = {
 	{0x3352103C, "Smart Array", &SA5_access},
 	{0x3353103C, "Smart Array", &SA5_access},
 	{0x3354103C, "Smart Array", &SA5_access},
+	{0x3355103C, "Smart Array", &SA5_access},
+	{0x3356103C, "Smart Array", &SA5_access},
 	{0xFFFF103C, "Unknown Smart Array", &SA5_access},
 };
 
