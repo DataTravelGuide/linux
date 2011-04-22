d8afacb669f0b1fa17bf5c7ce02b297c2d326aab
Mike Snitzer <snitzer@redhat.com>
2011-04-21 21:04:51 -0400

    make the flush insertion use the tail of the dispatch list
    Patchwork-id: 35590
    O-Subject: [RHEL6.2 PATCH 25/27] block: make the flush insertion use the tail of
    	the dispatch list
    Bugzilla: 672988
    RH-Acked-by: Vivek Goyal <vgoyal@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    From: Jens Axboe <jaxboe@fusionio.com>
    
    BZ 672988
    
    Upstream commit 53d63e6b0dfb95882ec0219ba6bbd50cde423794
    Author: Jens Axboe <jaxboe@fusionio.com>
    Date:   Wed Mar 30 13:27:09 2011 +0200
    
        block: make the flush insertion use the tail of the dispatch list
    
        It's not a preempt type request, in fact we have to insert it
        behind requests that do specify INSERT_FRONT.
    
        Signed-off-by: Jens Axboe <jaxboe@fusionio.com>
    
    Additional context (Tejun replying to himself, in reply to Vivek):
    On Wed, Mar 30, 2011 at 09:41:23AM +0200, Tejun Heo wrote:
    > IIRC, at least in the requeue path, some drivers depend on front
    > queueing for forward progress guarantee.  Forward progress for
    > prepping is guaranteed by mempool (or something like that) and when
    > the request is retried, it should stay at the front of the queue;
    > otherwise, prepping can stall with prepped requests stuck behind
    > unprepped ones.
    
    After writing the above, I think the current flush implementation
    actually is violating the above.  I think the problem is over-use of
    front insertion.  Flush can just append to the dispatch queue.  Front
    insertion should only be used by requeueing, really.
    
    Additional context (exchange between Jens and Tejun):
    On Wed, Mar 30, 2011 at 09:59:09AM +0200, Jens Axboe wrote:
    > Pure front insert should be used for requeue and internal commands (like
    > spin up this drive, or get error information). Flush should append to
    > the dispatch list.
    
    Yeah, right.
    
    Additional context (exchange between Vivek and Tejun):
    On Wed, Mar 30, 2011 at 11:22:48AM -0400, Vivek Goyal wrote:
    > So we need this as stable fix because FLUSH request can get ahead of
    > REQUEUED requests and it can break some drivers?
    
    Yes, I think so.  All we need is just replacing elv_insert() calls in
    blk-flush.c with list_add_tail().  Something like the following.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- block/blk-flush.c
+++ block/blk-flush.c
@@ -262,7 +262,7 @@ static bool blk_kick_flush(struct request_queue *q)
 	q->flush_rq.end_io = flush_end_io;
 
 	q->flush_pending_idx ^= 1;
-	elv_insert(q, &q->flush_rq, ELEVATOR_INSERT_REQUEUE);
+	list_add_tail(&q->flush_rq.queuelist, &q->queue_head);
 	return true;
 }
 
@@ -314,7 +314,7 @@ void blk_insert_flush(struct request *rq)
 	 */
 	if ((policy & REQ_FSEQ_DATA) &&
 	    !(policy & (REQ_FSEQ_PREFLUSH | REQ_FSEQ_POSTFLUSH))) {
-		list_add(&rq->queuelist, &q->queue_head);
+		list_add_tail(&rq->queuelist, &q->queue_head);
 		return;
 	}
 
