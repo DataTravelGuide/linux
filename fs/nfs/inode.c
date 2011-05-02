ae445a757f54737cbef451ab9dadda7a017fc209
Steve Dickson <SteveD@redhat.com>
2011-05-02 17:58:16 -0400

    Fix nfs_compat_user_ino64 so it doesn't cause problems if bit 31 or 63 are set in fileid
    Patchwork-id: 35720
    O-Subject: [RHEL6.2 PATCH 07/23] (try3-resend) Fix nfs_compat_user_ino64 so it
    	doesn't cause problems if bit 31 or 63 are set in fileid
    Bugzilla: 701373
    RH-Acked-by: Jeff Layton <jlayton@redhat.com>
    
    From: Frank Filz <ffilzlnx@us.ibm.com>
    
    The problem was use of an int32, which when converted to a uint64
    is sign extended resulting in a fileid that doesn't fit in 32 bits
    even though the intent of the function is to fit the fileid into
    32 bits.
    
    Signed-off-by: Frank Filz <ffilzlnx@us.ibm.com>
    Reviewed-by: Jeff Layton <jlayton@redhat.com>
    [Trond: Added an include for compat.h]
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- fs/nfs/inode.c
+++ fs/nfs/inode.c
@@ -36,6 +36,8 @@
 #include <linux/vfs.h>
 #include <linux/inet.h>
 #include <linux/nfs_xdr.h>
+#include <linux/slab.h>
+#include <linux/compat.h>
 
 #include <asm/system.h>
 #include <asm/uaccess.h>
@@ -88,7 +90,11 @@ int nfs_wait_bit_killable(void *word)
  */
 u64 nfs_compat_user_ino64(u64 fileid)
 {
-	int ino;
+#ifdef CONFIG_COMPAT
+	compat_ulong_t ino;
+#else	
+	unsigned long ino;
+#endif
 
 	if (enable_ino64)
 		return fileid;
