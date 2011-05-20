5f5284d6cfc07d8013150fb24b0cffbf47cdba4a
Hendrik Brueckner <brueckner@redhat.com>
2011-05-20 09:15:39 -0400

    cio: notify drivers of channel path events
    Patchwork-id: 35999
    O-Subject: [RHEL6.2 PATCH 09/14] [s390x] cio: notify drivers of channel path events
    Bugzilla: 694460
    
    Description
    -----------
    This patch adds a notification mechanism to inform
    ccw drivers about changes to channel paths, which
    occured while the device is online.
    
    Bugzilla
    --------
    BZ 694460
    https://bugzilla.redhat.com/show_bug.cgi?id=694460
    
    Upstream status of the patch
    ----------------------------
    The patch is upstream as of kernel version 2.6.37
    http://git.kernel.org/?p=linux/kernel/git/torvalds/linux-2.6.git;a=commitdiff;h=585b954e1f2fa325d425b0786e4525ac7c9ae575
    http://git.kernel.org/?p=linux/kernel/git/torvalds/linux-2.6.git;a=commitdiff;h=b17295e646562a2122ce84c1e55c9ae66fb6ae50
    
    Test status
    -----------
    The patch has been tested and fixes the problem.
    The fix has been verified by the IBM test department.
    
    Signed-off-by: Kyle McMartin <kmcmarti@redhat.com>

--- drivers/s390/cio/device.c
+++ drivers/s390/cio/device.c
@@ -1156,6 +1156,7 @@ err:
 static int io_subchannel_chp_event(struct subchannel *sch,
 				   struct chp_link *link, int event)
 {
+	struct ccw_device *cdev = sch_get_cdev(sch);
 	int mask;
 
 	mask = chp_ssd_get_mask(&sch->ssd_info, link);
@@ -1165,22 +1166,30 @@ static int io_subchannel_chp_event(struct subchannel *sch,
 	case CHP_VARY_OFF:
 		sch->opm &= ~mask;
 		sch->lpm &= ~mask;
+		if (cdev)
+			cdev->private->path_gone_mask |= mask;
 		io_subchannel_terminate_path(sch, mask);
 		break;
 	case CHP_VARY_ON:
 		sch->opm |= mask;
 		sch->lpm |= mask;
+		if (cdev)
+			cdev->private->path_new_mask |= mask;
 		io_subchannel_verify(sch);
 		break;
 	case CHP_OFFLINE:
 		if (cio_update_schib(sch))
 			return -ENODEV;
+		if (cdev)
+			cdev->private->path_gone_mask |= mask;
 		io_subchannel_terminate_path(sch, mask);
 		break;
 	case CHP_ONLINE:
 		if (cio_update_schib(sch))
 			return -ENODEV;
 		sch->lpm |= mask & sch->opm;
+		if (cdev)
+			cdev->private->path_new_mask |= mask;
 		io_subchannel_verify(sch);
 		break;
 	}
@@ -1833,6 +1842,7 @@ static void __ccw_device_pm_restore(struct ccw_device *cdev)
 	 * available again. Kick re-detection.
 	 */
 	cdev->private->flags.resuming = 1;
+	cdev->private->path_new_mask = LPM_ANYPATH;
 	css_schedule_eval(sch->schid);
 	spin_unlock_irq(sch->lock);
 	css_complete_work();
