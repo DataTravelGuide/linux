7686563a1d8f5affecaf5f4a0a2ef2fcc07c6d00
Steve Dickson <SteveD@redhat.com>
2011-05-31 20:55:28 -0400

    NFSv4.1: Ensure that layoutget uses the correct gfp modes
    Patchwork-id: 36303
    O-Subject: [RHEL6.2 PATCH 5/5] NFSv4.1: Ensure that layoutget uses the correct
    	gfp modes
    Bugzilla: 701788
    RH-Acked-by: Jeff Layton <jlayton@redhat.com>
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    
    From: Trond Myklebust <Trond.Myklebust@netapp.com>
    
    Currently, writebacks may end up recursing back into the filesystem due to
    GFP_KERNEL direct reclaims in the pnfs subsystem.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    
    Upstream commit: a75b9df9d3bfc3cd1083974c045ae31ce5f3434f
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- fs/nfs/write.c
+++ fs/nfs/write.c
@@ -938,7 +938,7 @@ static int nfs_flush_multi(struct nfs_pageio_descriptor *desc)
 	atomic_set(&req->wb_complete, requests);
 
 	BUG_ON(desc->pg_lseg);
-	lseg = pnfs_update_layout(desc->pg_inode, req->wb_context, IOMODE_RW);
+	lseg = pnfs_update_layout(desc->pg_inode, req->wb_context, IOMODE_RW, GFP_NOFS);
 	ClearPageError(page);
 	offset = 0;
 	nbytes = desc->pg_count;
@@ -1012,7 +1012,7 @@ static int nfs_flush_one(struct nfs_pageio_descriptor *desc)
 	}
 	req = nfs_list_entry(data->pages.next);
 	if ((!lseg) && list_is_singular(&data->pages))
-		lseg = pnfs_update_layout(desc->pg_inode, req->wb_context, IOMODE_RW);
+		lseg = pnfs_update_layout(desc->pg_inode, req->wb_context, IOMODE_RW, GFP_NOFS);
 
 	/* Set up the argument struct */
 	ret = nfs_write_rpcsetup(req, data, &nfs_write_full_ops, desc->pg_count, 0, lseg, desc->pg_ioflags);
