ad30eabc853dca3faae2c8c4df132e5794b319d1
Frantisek Hrbata <fhrbata@redhat.com>
2011-06-23 11:49:37 -0400

    oom: move badness() declaration into oom.h
    Patchwork-id: 36961
    O-Subject: [RHEL6.2 PATCH BZ645770 V2 37/65] oom: move badness() declaration
    	into oom.h
    Bugzilla: 645770
    RH-Acked-by: Oleg Nesterov <oleg@redhat.com>
    
    Bugzilla: 645770
    https://bugzilla.redhat.com/show_bug.cgi?id=645770
    
    Description:
    fuzz because of upstream commit
    
    oom: move sysctl declarations to oom.h
    8e4228e1edb922afa83366803a1e5b3fa8e987c2
    
    Upstream status: 74bcbf40546bb7500f2a7ba4ff3cc056a6bd004a
    --------------------------------------8<---------------------------------------
    	From 74bcbf40546bb7500f2a7ba4ff3cc056a6bd004a Mon Sep 17 00:00:00 2001
    	From: Andrew Morton <akpm@linux-foundation.org>
    	Date: Mon, 9 Aug 2010 17:19:43 -0700
    	Subject: [PATCH] oom: move badness() declaration into oom.h
    
    	Cc: Minchan Kim <minchan.kim@gmail.com>
    	Cc: David Rientjes <rientjes@google.com>
    	Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    	Cc: Oleg Nesterov <oleg@redhat.com>
    	Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    	Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    --------------------------------------8<---------------------------------------
    
    Brew build: 3421026
    https://brewweb.devel.redhat.com/taskinfo?taskID=3421026
    
    Test status:
    Basic functionality tested by me.
    
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- include/linux/oom.h
+++ include/linux/oom.h
@@ -14,6 +14,8 @@
 
 struct zonelist;
 struct notifier_block;
+struct mem_cgroup;
+struct task_struct;
 
 /*
  * Types of limitations to the nodes from which allocations may occur
@@ -44,5 +46,10 @@ static inline void oom_killer_enable(void)
 {
 	oom_killer_disabled = false;
 }
+
+/* The badness from the OOM killer */
+extern unsigned long badness(struct task_struct *p, struct mem_cgroup *mem,
+		      const nodemask_t *nodemask, unsigned long uptime);
+
 #endif /* __KERNEL__*/
 #endif /* _INCLUDE_LINUX_OOM_H */
