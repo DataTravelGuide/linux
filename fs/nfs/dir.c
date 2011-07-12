4e283e241d130c070edac7054d1039fceec1c625
Steve Dickson <SteveD@redhat.com>
2011-07-12 18:40:17 -0400

    NFS: Ensure that we update the readdir filp->f_pos correctly
    Patchwork-id: 37427
    O-Subject: [RHEL6.2 PATCH 1/3] NFS: Ensure that we update the readdir filp->f_pos correctly
    Bugzilla: 720712
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Jeff Layton <jlayton@redhat.com>
    
    From: Trond Myklebust <Trond.Myklebust@netapp.com>
    
    If we're doing a search by readdir cookie, we need to ensure that the
    resulting f_pos is updated. To do so, we need to update the
    desc->current_index, in the same way that we do in the search by
    file offset case.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    Upstream Commit: e47c085afb3d16cbc6a4bfb10a3b074bb7c58998
    
    Signed-off-by: Kyle McMartin <kmcmarti@redhat.com>

--- fs/nfs/dir.c
+++ fs/nfs/dir.c
@@ -288,7 +288,6 @@ int nfs_readdir_search_for_pos(struct nfs_cache_array *array, nfs_readdir_descri
 	if (diff >= array->size) {
 		if (array->eof_index >= 0)
 			goto out_eof;
-		desc->current_index += array->size;
 		return -EAGAIN;
 	}
 
@@ -309,6 +308,7 @@ int nfs_readdir_search_for_cookie(struct nfs_cache_array *array, nfs_readdir_des
 
 	for (i = 0; i < array->size; i++) {
 		if (array->array[i].cookie == *desc->dir_cookie) {
+			desc->file->f_pos = desc->current_index + i;
 			desc->cache_entry_index = i;
 			return 0;
 		}
@@ -340,6 +340,7 @@ int nfs_readdir_search_array(nfs_readdir_descriptor_t *desc)
 
 	if (status == -EAGAIN) {
 		desc->last_cookie = array->last_cookie;
+		desc->current_index += array->size;
 		desc->page_index++;
 	}
 	nfs_readdir_release_array(desc->page);
