f92191e9a8af3b2f32fa02836fb6d92d0cec7cfa
Mike Christie <mchristi@redhat.com>
2011-07-19 04:06:28 -0400

    bnx2i: Updated version
    Patchwork-id: 37719
    O-Subject: [RHEL6.2 PATCH 34/61] bnx2i: Updated version
    Bugzilla: 696760
    RH-Acked-by: Tomas Henzl <thenzl@redhat.com>
    
    From: Eddie Wai <eddie.wai@broadcom.com>
    
    Upstream commit reference:  ea9582d721a0d711fb046a25f5e94dcbbc5be410
    
    Bumped version from 2.6.2.3 to 2.7.0.3
    
    Signed-off-by: Eddie Wai <eddie.wai@broadcom.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/scsi/bnx2i/bnx2i_init.c
+++ drivers/scsi/bnx2i/bnx2i_init.c
@@ -18,8 +18,8 @@ static struct list_head adapter_list = LIST_HEAD_INIT(adapter_list);
 static u32 adapter_count;
 
 #define DRV_MODULE_NAME		"bnx2i"
-#define DRV_MODULE_VERSION	"2.6.2.3"
-#define DRV_MODULE_RELDATE	"Jan 06, 2010"
+#define DRV_MODULE_VERSION	"2.7.0.3"
+#define DRV_MODULE_RELDATE	"Jun 15, 2010"
 
 static char version[] __devinitdata =
 		"Broadcom NetXtreme II iSCSI Driver " DRV_MODULE_NAME \
