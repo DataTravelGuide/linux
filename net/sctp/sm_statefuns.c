ad6b3fe1b657d0d97c9ea3aa69ae0aa980dd1868
Thomas Graf <tgraf@redhat.com>
2011-07-25 15:25:40 -0400

    sctp: fix to check the source address of COOKIE-ECHO chunk
    Patchwork-id: 38518
    O-Subject: [RHEL6.2 PATCH 58/63] sctp: fix to check the source address of COOKIE-ECHO chunk
    Bugzilla: 679456
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    Backport of:
    commit de6becdc0844ff92b38ffd9f0c4db1d3de02835f
    Author: Wei Yongjun <yjwei@cn.fujitsu.com>
    Date:   Tue Apr 19 21:30:51 2011 +0000
    
        sctp: fix to check the source address of COOKIE-ECHO chunk
    
        SCTP does not check whether the source address of COOKIE-ECHO
        chunk is the original address of INIT chunk or part of the any
        address parameters saved in COOKIE in CLOSED state. So even if
        the COOKIE-ECHO chunk is from any address but with correct COOKIE,
        the COOKIE-ECHO chunk still be accepted. If the COOKIE is not from
        a valid address, the assoc should not be established.
    
        Signed-off-by: Wei Yongjun <yjwei@cn.fujitsu.com>
        Signed-off-by: Vlad Yasevich <vladislav.yasevich@hp.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Kyle McMartin <kmcmarti@redhat.com>

--- net/sctp/sm_statefuns.c
+++ net/sctp/sm_statefuns.c
@@ -390,8 +390,7 @@ sctp_disposition_t sctp_sf_do_5_1B_init(const struct sctp_endpoint *ep,
 		goto nomem_init;
 
 	/* The call, sctp_process_init(), can fail on memory allocation.  */
-	if (!sctp_process_init(new_asoc, chunk->chunk_hdr->type,
-			       sctp_source(chunk),
+	if (!sctp_process_init(new_asoc, chunk, sctp_source(chunk),
 			       (sctp_init_chunk_t *)chunk->chunk_hdr,
 			       GFP_ATOMIC))
 		goto nomem_init;
@@ -722,7 +721,7 @@ sctp_disposition_t sctp_sf_do_5_1D_ce(const struct sctp_endpoint *ep,
 	 */
 	peer_init = &chunk->subh.cookie_hdr->c.peer_init[0];
 
-	if (!sctp_process_init(new_asoc, chunk->chunk_hdr->type,
+	if (!sctp_process_init(new_asoc, chunk,
 			       &chunk->subh.cookie_hdr->c.peer_addr,
 			       peer_init, GFP_ATOMIC))
 		goto nomem_init;
@@ -1463,8 +1462,7 @@ static sctp_disposition_t sctp_sf_do_unexpected_init(
 	 * Verification Tag and Peers Verification tag into a reserved
 	 * place (local tie-tag and per tie-tag) within the state cookie.
 	 */
-	if (!sctp_process_init(new_asoc, chunk->chunk_hdr->type,
-			       sctp_source(chunk),
+	if (!sctp_process_init(new_asoc, chunk, sctp_source(chunk),
 			       (sctp_init_chunk_t *)chunk->chunk_hdr,
 			       GFP_ATOMIC))
 		goto nomem;
@@ -1693,8 +1691,7 @@ static sctp_disposition_t sctp_sf_do_dupcook_a(const struct sctp_endpoint *ep,
 	 */
 	peer_init = &chunk->subh.cookie_hdr->c.peer_init[0];
 
-	if (!sctp_process_init(new_asoc, chunk->chunk_hdr->type,
-			       sctp_source(chunk), peer_init,
+	if (!sctp_process_init(new_asoc, chunk, sctp_source(chunk), peer_init,
 			       GFP_ATOMIC))
 		goto nomem;
 
@@ -1779,8 +1776,7 @@ static sctp_disposition_t sctp_sf_do_dupcook_b(const struct sctp_endpoint *ep,
 	 * side effects--it is safe to run them here.
 	 */
 	peer_init = &chunk->subh.cookie_hdr->c.peer_init[0];
-	if (!sctp_process_init(new_asoc, chunk->chunk_hdr->type,
-			       sctp_source(chunk), peer_init,
+	if (!sctp_process_init(new_asoc, chunk, sctp_source(chunk), peer_init,
 			       GFP_ATOMIC))
 		goto nomem;
 
