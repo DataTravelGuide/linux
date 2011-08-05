339303bc0b5fcd55c6b0a65d9c60f646dcfb9e5f
Chad Dupuis <cdupuis@redhat.com>
2011-08-05 14:11:53 -0400

    IB/qib: Fix potential deadlock with link down interrupt
    Patchwork-id: 39511
    O-Subject: [RHEL 6.2 PATCH] IB/qib: Fix potential deadlock with link down
    	interrupt
    Bugzilla: 724021
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla
    --------
    
    Bug 724021 (https://bugzilla.redhat.com/show_bug.cgi?id=724021)
    
    Upstream Status
    ---------------
    
    linux-2.6 commit id 4356d0b64b9be53c507f66c572c1e275b4529239
    
    Brew Build
    ----------
    
    3536050
    
    Testing
    -------
    
    This patch was tested internally at QLogic.  The patch was test built against
    the 2.6.32-178.el6 kernel.
    
    Description
    -----------
    
    IB/qib: Fix potential deadlock with link down interrupt
    
    From: Ram Vepa <ram.vepa@qlogic.com>
    
    There is a possibility of a deadlock due to the way locks are acquired and
    released in qib_set_uevent_bits(). The function qib_set_uevent_bits() is called
    in process context calling spin_lock() and spin_unlock() while this same lock
    is acquired/released in interrupt context which can lead to a deadlock when
    running on the same cpu.
    
    The fix is to replace spin_lock() and spin_unlock() with spin_lock_irqsave()
    and spin_unlock_irqrestore() respectively in qib_set_uevent_bits().
    
    Signed-off-by: Mike Marciniszyn <mike.marciniszyn@qlogic.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/infiniband/hw/qib/qib_file_ops.c
+++ drivers/infiniband/hw/qib/qib_file_ops.c
@@ -1904,8 +1904,9 @@ int qib_set_uevent_bits(struct qib_pportdata *ppd, const int evtbit)
 	struct qib_ctxtdata *rcd;
 	unsigned ctxt;
 	int ret = 0;
+	unsigned long flags;
 
-	spin_lock(&ppd->dd->uctxt_lock);
+	spin_lock_irqsave(&ppd->dd->uctxt_lock, flags);
 	for (ctxt = ppd->dd->first_user_ctxt; ctxt < ppd->dd->cfgctxts;
 	     ctxt++) {
 		rcd = ppd->dd->rcd[ctxt];
@@ -1924,7 +1925,7 @@ int qib_set_uevent_bits(struct qib_pportdata *ppd, const int evtbit)
 		ret = 1;
 		break;
 	}
-	spin_unlock(&ppd->dd->uctxt_lock);
+	spin_unlock_irqrestore(&ppd->dd->uctxt_lock, flags);
 
 	return ret;
 }
