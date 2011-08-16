1b6ada9a2957176e7ebcb7bfd0a58556bef83ca6
Tomas Henzl <thenzl@redhat.com>
2011-08-16 14:10:03 -0400

    megaraid: fix FastPath and update to v5.40
    Patchwork-id: 40192
    O-Subject: [RHEL6.2 PATCH] megaraid: fix FastPath and update to v5.40
    Bugzilla: 726225
    RH-Acked-by: Rob Evers <revers@redhat.com>
    
    Hi, this is for bz#726225
    
    This patch comes from LSI it fixes
    FastPath I/O to work with degraded RAID 1.
    and adds .change_queue_depth support.
    The version string is also updated.

--- drivers/scsi/megaraid/megaraid_sas.h
+++ drivers/scsi/megaraid/megaraid_sas.h
@@ -33,9 +33,9 @@
 /*
  * MegaRAID SAS Driver meta data
  */
-#define MEGASAS_VERSION				"00.00.05.38-rh1"
-#define MEGASAS_RELDATE				"May. 11, 2011"
-#define MEGASAS_EXT_VERSION			"Wed. May. 11 17:00:00 PDT 2011"
+#define MEGASAS_VERSION				"00.00.05.40-rh1"
+#define MEGASAS_RELDATE				"Aug. 4, 2011"
+#define MEGASAS_EXT_VERSION			"Thu. Aug. 4 17:00:00 PDT 2011"
 
 /*
  * Device IDs
