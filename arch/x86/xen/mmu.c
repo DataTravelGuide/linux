06dcca516f8b1437c108d6ca0b90fa9d66b9a20a
Andrew Jones <drjones@redhat.com>
2011-09-13 10:44:06 -0400

    mm: Fix pgd_lock deadlock
    Patchwork-id: 40554
    O-Subject: [RHEL6.2 PATCH 2/3 v2] x86/mm: Fix pgd_lock deadlock
    Bugzilla: 691310
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    RH-Acked-by: Andrea Arcangeli <aarcange@redhat.com>
    RH-Acked-by: Johannes Weiner <jweiner@redhat.com>
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    
    From: Andrea Arcangeli <aarcange@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=691310
    
    It's forbidden to take the page_table_lock with the irq disabled
    or if there's contention the IPIs (for tlb flushes) sent with
    the page_table_lock held will never run leading to a deadlock.
    
    Nobody takes the pgd_lock from irq context so the _irqsave can be
    removed.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Acked-by: Rik van Riel <riel@redhat.com>
    Tested-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: <stable@kernel.org>
    LKML-Reference: <201102162345.p1GNjMjm021738@imap1.linux-foundation.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- arch/x86/xen/mmu.c
+++ arch/x86/xen/mmu.c
@@ -1007,10 +1007,9 @@ static void xen_pgd_pin(struct mm_struct *mm)
  */
 void xen_mm_pin_all(void)
 {
-	unsigned long flags;
 	struct page *page;
 
-	spin_lock_irqsave(&pgd_lock, flags);
+	spin_lock(&pgd_lock);
 
 	list_for_each_entry(page, &pgd_list, lru) {
 		if (!PagePinned(page)) {
@@ -1019,7 +1018,7 @@ void xen_mm_pin_all(void)
 		}
 	}
 
-	spin_unlock_irqrestore(&pgd_lock, flags);
+	spin_unlock(&pgd_lock);
 }
 
 /*
@@ -1120,10 +1119,9 @@ static void xen_pgd_unpin(struct mm_struct *mm)
  */
 void xen_mm_unpin_all(void)
 {
-	unsigned long flags;
 	struct page *page;
 
-	spin_lock_irqsave(&pgd_lock, flags);
+	spin_lock(&pgd_lock);
 
 	list_for_each_entry(page, &pgd_list, lru) {
 		if (PageSavePinned(page)) {
@@ -1133,7 +1131,7 @@ void xen_mm_unpin_all(void)
 		}
 	}
 
-	spin_unlock_irqrestore(&pgd_lock, flags);
+	spin_unlock(&pgd_lock);
 }
 
 void xen_activate_mm(struct mm_struct *prev, struct mm_struct *next)
