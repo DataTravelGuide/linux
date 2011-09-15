93336eb56e775e93020e44fe8e397570f90a147f
Andy Grover <agrover@redhat.com>
2011-09-14 23:15:02 -0400

    tcm_fc: Fix to activate non-offload path for FCoE target
    Patchwork-id: 40600
    O-Subject: [RHEL6.2 PATCH] tcm_fc: Fix to activate non-offload path for FCoE
    	target
    Bugzilla: 638007
    RH-Acked-by: Mike Christie <mchristi@redhat.com>
    
    BZ: 638007
    
    Problem: FCoE target by default was using DDP offload which is initiator specific, hence breaking target.
    To avoid KABI breakage  (which is caused due to addition of new function 'ddp_target' in net_device_ops and
    libfc_function_template), FCoE  target shall not use DDP offload, and continue to use non-offload path
    
    Fix: Remove the code from ft_write_pending function which was utilizing DDP offload functionality, so that
    non-offload path is active and functional.
    
    NOTE: This patch is for >= RHEL6.2 until new major release which accepts KABI breakage.
    
    Signed-off-by: Kiran Patil <kiran.patil@intel.com>
    Signed-off-by: Andy Grover <agrover@redhat.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/target/tcm_fc/tfc_cmd.c
+++ drivers/target/tcm_fc/tfc_cmd.c
@@ -246,30 +246,6 @@ int ft_write_pending(struct se_cmd *se_cmd)
 	fh = fc_frame_header_get(fp);
 	f_ctl = ntoh24(fh->fh_f_ctl);
 
-	/* Only if it is 'Exchange Responder' */
-	if (f_ctl & FC_FC_EX_CTX) {
-		/* Target is 'exchange responder' and sending XFER_READY
-		 * to 'exchange initiator (initiator)'
-		 */
-		if ((ep->xid <= lport->lro_xid) &&
-		    (fh->fh_r_ctl == FC_RCTL_DD_DATA_DESC)) {
-			if (se_cmd->se_cmd_flags & SCF_SCSI_DATA_SG_IO_CDB) {
-				/*
-				 * Map se_mem list to scatterlist, so that
-				 * DDP can be setup. DDP setup function require
-				 * scatterlist. se_mem_list is internal to
-				 * TCM/LIO target
-				 */
-				transport_do_task_sg_chain(se_cmd);
-				cmd->sg = T_TASK(se_cmd)->t_tasks_sg_chained;
-				cmd->sg_cnt =
-					T_TASK(se_cmd)->t_tasks_sg_chained_no;
-			}
-			if (cmd->sg && lport->tt.ddp_setup(lport, ep->xid,
-						    cmd->sg, cmd->sg_cnt))
-				cmd->was_ddp_setup = 1;
-		}
-	}
 	lport->tt.seq_send(lport, cmd->seq, fp);
 	return 0;
 }
