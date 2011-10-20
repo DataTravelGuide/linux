28c2a05d57ad1730fb52aaa3372a7e42fb9e40a6
Doug Ledford <dledford@redhat.com>
2011-10-20 19:44:27 -0400

    Revert "Restore max mqueue message size to its previous RHEL 5 value"
    Patchwork-id: 41986
    O-Subject: [Patch RHEL6.2 1/6] Revert "[kernel] Restore max mqueue message size
    	to its previous RHEL 5 value"
    Bugzilla: 746606
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    RH-Acked-by: Motohiro Kosaki <mkosaki@redhat.com>
    
    A slightly different version of this patch was accepted into
    Andrew Morton's for-next queue.  Revert this version and apply
    the upstream version.
    
    This reverts commit a4858ef5d16efe790f243d6a0bfdd62a8ce0527f.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- include/linux/ipc_namespace.h
+++ include/linux/ipc_namespace.h
@@ -84,56 +84,11 @@ static inline int ipcns_notify(unsigned long l) { return 0; }
 
 #ifdef CONFIG_POSIX_MQUEUE
 extern int mq_init_ns(struct ipc_namespace *ns);
-/* 
- * Default values:
- * MIN_*: Used as the lowest allowed user selectable value if attributes are
- *   passed in to the create call for message queues
- * DFLT_*: Used as the system wide default maximum values applied to normal,
- *   unprivileged programs.  These are what is used to initially set the values
- *   for the namespace in mq_init_ns().  The maximum values for unprivileged
- *   applications can be raised by setting the respective items in
- *   /proc/sys/fs/mqueue/
- * HARD_*: These are the system wide hard maximums applied to privileged
- *   applications.  The system admin can increase the unprivileged maximums
- *   only up to these hard limits, and privileged applications can only create
- *   queues up to these hard limits.  They are inviolate.
- * NOTE: While it is possible to raise both MSGMAX and MSGSIZEMAX to their
- *   maximum hard limits, it is not possible to create a queue with
- *   those values set to their hard limits.  This is due to the fact that
- *   msgmax * msgsizemax must not wrap a 32bit counter, and with both
- *   items set to thier maximum, it will.  In that case, the queue will
- *   be rejected at creation time.  Further, the sum of msgmax * msgsizemax
- *   for all queues attached to a single process can not wrap that same
- *   32bit counter, so even a perfectly valid size of queue can be
- *   rejected if the application already has too many other queues open
- *   and the size of this queue in addition to its other queues would
- *   exceed the size of that 32bit counter.
- *
- * *_QUEUESMAX: Maximum number of message queues per message queue namespace.
- *   We account message queues on a per namespace basis, however POSIX
- *   mandates that we allow a minimum maximum of 8 message queues to be
- *   opened per process.  There is no direct mapping of how we account
- *   message queue counts onto this POSIX mandate, so we set the default
- *   minimum such that it's unlikely any application will have a problem
- *   opening 8 queues if they so desire.
- * *_MSGMAX: Maximum of number of messages in a queue.  POSIX mandates that
- *   we support a minimum hard maximum of at least 32767.
- * *_MSGSIZEMAX: Maximum size of a message in a queue.  POSIX is silent on
- *   this option.  However, we know that there are existing users of Linux
- *   that rely upon MSGSIZEMAX being at least 5MBytes, with a preference
- *   for 10MBytes (discovered after the last change when MSGSIZEMAX was
- *   reduced from INT_MAX to 8192*128, breaking these current user's
- *   running configurations).
- */
-#define MIN_QUEUESMAX 		    1
-#define DFLT_QUEUESMAX		   32
-#define HARD_QUEUESMAX		 1024
-#define MIN_MSGMAX		    1
-#define DFLT_MSGMAX		   16
-#define HARD_MSGMAX		65536
-#define MIN_MSGSIZEMAX		  128
-#define DFLT_MSGSIZEMAX	     256*1024
-#define HARD_MSGSIZEMAX	 16*1024*1024
+/* default values */
+#define DFLT_QUEUESMAX 256     /* max number of message queues */
+#define DFLT_MSGMAX    10      /* max number of messages in each queue */
+#define HARD_MSGMAX    (131072/sizeof(void *))
+#define DFLT_MSGSIZEMAX 8192   /* max message size */
 #else
 static inline int mq_init_ns(struct ipc_namespace *ns) { return 0; }
 #endif
