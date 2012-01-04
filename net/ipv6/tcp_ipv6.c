c5c963c6d3dbf72878aeaa97bb07487ab0671f7b
Jiri Benc <jbenc@redhat.com>
2012-01-03 19:54:14 -0500

    ipv6: tcp: fix tcp_v6_conn_request()
    Patchwork-id: 42799
    O-Subject: [RHEL6.3 PATCH] ipv6: tcp: fix tcp_v6_conn_request()
    Bugzilla: 742099
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    RH-Acked-by: Thomas Graf <tgraf@redhat.com>
    
    https://bugzilla.redhat.com/show_bug.cgi?id=742099
    
    When performing netperf IPv6 TCP_CRR test, which heavily reuses TIME_WAIT
    sockets, connection is occasionally reset by the server during the 3-way
    handshake (the server sends SYN-ACK correctly but responds with RST to the
    following ACK).
    
    This is caused by an unsuccessful lookup of the connection request and is
    fixed by the following upstream commit (with somewhat inaccurate
    description):
    
    commit 4d0fe50c75a547088e4304e5eb5f521514dfae46
    Author: Eric Dumazet <eric.dumazet@gmail.com>
    Date:   Wed, 23 Nov 2011 17:29:23 -0500
    
    ipv6: tcp: fix tcp_v6_conn_request()
    
    Since linux 2.6.26 (commit c6aefafb7ec6 : Add IPv6 support to TCP SYN
    cookies), we can drop a SYN packet reusing a TIME_WAIT socket.
    
    (As a matter of fact we fail to send the SYNACK answer)
    
    As the client resends its SYN packet after a one second timeout, we
    accept it, because first packet removed the TIME_WAIT socket before
    being dropped.
    
    This probably explains why nobody ever noticed or complained.
    
    Brew:
    https://brewweb.devel.redhat.com/taskinfo?taskID=3914474
    
    Testing:
    By iterating "netperf -6 -H <remote_ipv6_addr> -t TCP_CRR -l 60"
    30 times without error. Without the patch, this ends with
    "send_tcp_conn_rr: data recv error: Connection reset by peer"
    in less than 5 iterations.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -1218,6 +1218,13 @@ static int tcp_v6_conn_request(struct sock *sk, struct sk_buff *skb)
 	if (!want_cookie)
 		TCP_ECN_create_request(req, tcp_hdr(skb));
 
+	treq->iif = sk->sk_bound_dev_if;
+
+	/* So that link locals have meaning */
+	if (!sk->sk_bound_dev_if &&
+	    ipv6_addr_type(&treq->rmt_addr) & IPV6_ADDR_LINKLOCAL)
+		treq->iif = inet6_iif(skb);
+
 	if (want_cookie) {
 		isn = cookie_v6_init_sequence(sk, skb, &req->mss);
 		req->cookie_ts = tmp_opt.tstamp_ok;
@@ -1228,12 +1235,6 @@ static int tcp_v6_conn_request(struct sock *sk, struct sk_buff *skb)
 			atomic_inc(&skb->users);
 			treq->pktopts = skb;
 		}
-		treq->iif = sk->sk_bound_dev_if;
-
-		/* So that link locals have meaning */
-		if (!sk->sk_bound_dev_if &&
-		    ipv6_addr_type(&treq->rmt_addr) & IPV6_ADDR_LINKLOCAL)
-			treq->iif = inet6_iif(skb);
 
 		isn = tcp_v6_init_sequence(skb);
 	}
