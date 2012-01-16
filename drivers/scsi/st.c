0b0b7a31b44209c504ffbbaf6176d66be6626527
Tomas Henzl <thenzl@redhat.com>
2012-01-16 13:11:36 -0500

    st: Store page order before driver buffer allocation
    Patchwork-id: 43065
    O-Subject: [RHEL6.3 PATCH 1/3] st: Store page order before driver buffer
    	allocation
    Bugzilla: 716593
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Rob Evers <revers@redhat.com>
    
    upstream commit
    373daacfce9ea0091cb3027572354ddebefb8ebb [SCSI] st: Store page order before driver buffer allocation
    
    The order of the pages allocated for the driver buffer must be stored before
    allocation because it is used in freeing already allocated pages if
    allocation fails.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/scsi/st.c
+++ drivers/scsi/st.c
@@ -17,7 +17,7 @@
    Last modified: 18-JAN-1998 Richard Gooch <rgooch@atnf.csiro.au> Devfs support
  */
 
-static const char *verstr = "20081215";
+static const char *verstr = "20101219rh";
 
 #include <linux/module.h>
 
@@ -3727,6 +3727,7 @@ static int enlarge_buffer(struct st_buffer * STbuffer, int new_size, int need_dm
 		     order < ST_MAX_ORDER && b_size < new_size;
 		     order++, b_size *= 2)
 			;  /* empty */
+		STbuffer->reserved_page_order = order;
 	}
 	if (max_segs * (PAGE_SIZE << order) < new_size) {
 		if (order == ST_MAX_ORDER)
@@ -3753,7 +3754,6 @@ static int enlarge_buffer(struct st_buffer * STbuffer, int new_size, int need_dm
 		segs++;
 	}
 	STbuffer->b_data = page_address(STbuffer->reserved_pages[0]);
-	STbuffer->reserved_page_order = order;
 
 	return 1;
 }
