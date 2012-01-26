a65861607da3bfbd8d38bb9313388669a7339d2f
Jes Sorensen <Jes.Sorensen@redhat.com>
2012-01-26 14:29:10 -0500

    don't give up looking for spares on first failure-to-add
    Patchwork-id: 43453
    O-Subject: [PATCH 121/122] md: don't give up looking for spares on first
    	failure-to-add
    Bugzilla: 747574
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Doug Ledford <dledford@redhat.com>
    
    From: NeilBrown <neilb@suse.de>
    
    Before performing a recovery we try to remove any spares that
    might not be working, then add any that might have become relevant.
    
    Currently we abort on the first spare that cannot be added.
    This is a false optimisation.
    It is conceivable that - depending on rules in the personality - a
    subsequent spare might be accepted.
    Also the loop does other things like count the available spares and
    reset the 'recovery_offset' value.
    
    If we abort early these might not happen properly.
    
    So remove the early abort.
    
    In particular if you have an array what is undergoing recovery and
    which has extra spares, then the recovery may not restart after as
    reboot as the could of 'spares' might end up as zero.
    
    Reported-by: Anssi Hannula <anssi.hannula@iki.fi>
    Signed-off-by: NeilBrown <neilb@suse.de>
    (cherry picked from commit 60fc13702a1b35118c1548e9c257fa038cecb658)
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -7386,8 +7386,7 @@ static int remove_and_add_spares(struct mddev *mddev)
 					spares++;
 					md_new_event(mddev);
 					set_bit(MD_CHANGE_DEVS, &mddev->flags);
-				} else
-					break;
+				}
 			}
 		}
 	}
