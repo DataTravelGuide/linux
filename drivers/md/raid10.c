8cafb9c667712384e727d14c079644d9a03daefe
Jes Sorensen <Jes.Sorensen@redhat.com>
2012-01-26 14:28:56 -0500

    Fix some bugs in recovery_disabled handling
    Patchwork-id: 43433
    O-Subject: [PATCH 107/122] md: Fix some bugs in recovery_disabled handling.
    Bugzilla: 747574
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Doug Ledford <dledford@redhat.com>
    
    From: NeilBrown <neilb@suse.de>
    
    In 3.0 we changed the way recovery_disabled was handle so that instead
    of testing against zero, we test an mddev-> value against a conf->
    value.
    Two problems:
      1/ one place in raid1 was missed and still sets to '1'.
      2/ We didn't explicitly set the conf-> value at array creation
         time.
         It defaulted to '0' just like the mddev value does so they
         could appear equal and thus disable recovery.
         This did not affect normal 'md' as it calls bind_rdev_to_array
         which changes the mddev value.  However the dmraid interface
         doesn't call this and so doesn't change ->recovery_disabled; so at
         array start all recovery is incorrectly disabled.
    
    So initialise the 'conf' value to one less that the mddev value, so
    the will only be the same when explicitly set that way.
    
    Reported-by: Jonathan Brassow <jbrassow@redhat.com>
    Signed-off-by: NeilBrown  <neilb@suse.de>
    (cherry picked from commit d890fa2b0586b6177b119643ff66932127d58afa)
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/md/raid10.c
+++ drivers/md/raid10.c
@@ -1421,6 +1421,7 @@ static int raid10_add_disk(struct mddev *mddev, struct md_rdev *rdev)
 		}
 
 		p->head_position = 0;
+		p->recovery_disabled = mddev->recovery_disabled - 1;
 		rdev->raid_disk = mirror;
 		err = 0;
 		if (rdev->saved_raid_disk != mirror)
@@ -2983,6 +2984,7 @@ static int run(struct mddev *mddev)
 			if (disk->rdev)
 				conf->fullsync = 1;
 		}
+		disk->recovery_disabled = mddev->recovery_disabled - 1;
 	}
 
 	if (mddev->recovery_cp != MaxSector)
