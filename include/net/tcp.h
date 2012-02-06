7621c044d160883a810e68f7472182d4663c213a
Thomas Graf <tgraf@redhat.com>
2012-02-06 10:12:52 -0500

    tcp: ECN blackhole should not force quickack mode
    Patchwork-id: 43828
    O-Subject: [RHEL6.3 PATCH 10/21] tcp: ECN blackhole should not force quickack
    	mode
    Bugzilla: 786956
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    
    Backport of:
    commit 7a269ffad72f3604b8982fa09c387670e0d2ee14
    Author: Eric Dumazet <eric.dumazet@gmail.com>
    Date:   Thu Sep 22 20:02:19 2011 +0000
    
        tcp: ECN blackhole should not force quickack mode
    
        While playing with a new ADSL box at home, I discovered that ECN
        blackhole can trigger suboptimal quickack mode on linux : We send one
        ACK for each incoming data frame, without any delay and eventual
        piggyback.
    
        This is because TCP_ECN_check_ce() considers that if no ECT is seen on a
        segment, this is because this segment was a retransmit.
    
        Refine this heuristic and apply it only if we seen ECT in a previous
        segment, to detect ECN blackhole at IP level.
    
        Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
        CC: Jamal Hadi Salim <jhs@mojatatu.com>
        CC: Jerry Chu <hkchu@google.com>
        CC: Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
        CC: Jim Gettys <jg@freedesktop.org>
        CC: Dave Taht <dave.taht@gmail.com>
        Acked-by: Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Thomas Graf <tgraf@redhat.com>
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -368,6 +368,7 @@ static inline void tcp_clear_options(struct tcp_options_received *rx_opt)
 #define	TCP_ECN_OK		1
 #define	TCP_ECN_QUEUE_CWR	2
 #define	TCP_ECN_DEMAND_CWR	4
+#define	TCP_ECN_SEEN		8
 
 static __inline__ void
 TCP_ECN_create_request(struct request_sock *req, struct tcphdr *th)
