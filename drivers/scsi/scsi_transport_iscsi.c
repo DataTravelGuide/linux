6f8d4048c1f4b3cc04cc2953574521b1be038ff1
Mike Christie <mchristi@redhat.com>
2012-02-09 02:06:54 -0500

    iscsi: fix setting of pid from netlink skb
    Patchwork-id: 44148
    O-Subject: [RHEL6.3 PATCH 1/4] iscsi: fix setting of pid from netlink skb
    Bugzilla: 747846
    RH-Acked-by: Rob Evers <revers@redhat.com>
    RH-Acked-by: Tomas Henzl <thenzl@redhat.com>
    
    From: Mike Christie <michaelc@cs.wisc.edu>
    
    >From the upstream patch:
    
    	NETLINK_CREDS's pid now returns 0, so I guess we are supposed to
    	be using NETLINK_CB. This changed while the patch to export the
    	pid was getting merged upstream, so it was not noticed until both
    	the network and iscsi changes were in the same tree.
    
    This patch has been posted here
    http://marc.info/?l=linux-scsi&m=132763366508672&w=2
    
    This patch is not needed in rhel for the reason above. I am just sending
    it be in sync with upstream. I have tested it by creating sessions and
    verifying that when multiple tools the proper pid is accessed.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/scsi/scsi_transport_iscsi.c
+++ drivers/scsi/scsi_transport_iscsi.c
@@ -1958,7 +1958,7 @@ iscsi_if_recv_msg(struct sk_buff *skb, struct nlmsghdr *nlh, uint32_t *group)
 	switch (nlh->nlmsg_type) {
 	case ISCSI_UEVENT_CREATE_SESSION:
 		err = iscsi_if_create_session(priv, ep, ev,
-					      NETLINK_CREDS(skb)->pid,
+					      NETLINK_CB(skb).pid,
 					      ev->u.c_session.initial_cmdsn,
 					      ev->u.c_session.cmds_max,
 					      ev->u.c_session.queue_depth);
@@ -1971,7 +1971,7 @@ iscsi_if_recv_msg(struct sk_buff *skb, struct nlmsghdr *nlh, uint32_t *group)
 		}
 
 		err = iscsi_if_create_session(priv, ep, ev,
-					NETLINK_CREDS(skb)->pid,
+					NETLINK_CB(skb).pid,
 					ev->u.c_bound_session.initial_cmdsn,
 					ev->u.c_bound_session.cmds_max,
 					ev->u.c_bound_session.queue_depth);
