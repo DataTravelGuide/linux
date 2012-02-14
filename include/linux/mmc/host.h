5396c91cc9b13dbf29be25851d9aa915fdac1667
Don Zickus <dzickus@redhat.com>
2012-02-13 19:37:41 -0500

    sd: add support for signal voltage switch procedure
    Patchwork-id: 44528
    O-Subject: [RHEL6 PATCH 08/30] mmc: sd: add support for signal voltage switch
    	procedure
    Bugzilla: 671335
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    https://bugzilla.redhat.com/show_bug.cgi?id=671335
    
    commit f2119df6b764609af4baceb68caf1e848c1c8aa7
    Author: Arindam Nath <arindam.nath@amd.com>
    Date:   Thu May 5 12:18:57 2011 +0530
    
        mmc: sd: add support for signal voltage switch procedure
    
        Host Controller v3.00 adds another Capabilities register. Apart
        from other things, this new register indicates whether the Host
        Controller supports SDR50, SDR104, and DDR50 UHS-I modes. The spec
        doesn't mention about explicit support for SDR12 and SDR25 UHS-I
        modes, so the Host Controller v3.00 should support them by default.
        Also if the controller supports SDR104 mode, it will also support
        SDR50 mode as well. So depending on the host support, we set the
        corresponding MMC_CAP_* flags. One more new register. Host Control2
        is added in v3.00, which is used during Signal Voltage Switch
        procedure described below.
    
        Since as per v3.00 spec, UHS-I supported hosts should set S18R
        to 1, we set S18R (bit 24) of OCR before sending ACMD41. We also
        need to set XPC (bit 28) of OCR in case the host can supply >150mA.
        This support is indicated by the Maximum Current Capabilities
        register of the Host Controller.
    
        If the response of ACMD41 has both CCS and S18A set, we start the
        signal voltage switch procedure, which if successfull, will switch
        the card from 3.3V signalling to 1.8V signalling. Signal voltage
        switch procedure adds support for a new command CMD11 in the
        Physical Layer Spec v3.01. As part of this procedure, we need to
        set 1.8V Signalling Enable (bit 3) of Host Control2 register, which
        if remains set after 5ms, means the switch to 1.8V signalling is
        successfull. Otherwise, we clear bit 24 of OCR and retry the
        initialization sequence. When we remove the card, and insert the
        same or another card, we need to make sure that we start with 3.3V
        signalling voltage. So we call mmc_set_signal_voltage() with
        MMC_SIGNAL_VOLTAGE_330 set so that we are back to 3.3V signalling
        voltage before we actually start initializing the card.
    
        Tested by Zhangfei Gao with a Toshiba uhs card and general hs card,
        on mmp2 in SDMA mode.
    
        Signed-off-by: Arindam Nath <arindam.nath@amd.com>
        Reviewed-by: Philip Rakity <prakity@marvell.com>
        Tested-by: Philip Rakity <prakity@marvell.com>
        Acked-by: Zhangfei Gao <zhangfei.gao@marvell.com>
        Signed-off-by: Chris Ball <cjb@laptop.org>
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -49,6 +49,11 @@ struct mmc_ios {
 #define MMC_TIMING_LEGACY	0
 #define MMC_TIMING_MMC_HS	1
 #define MMC_TIMING_SD_HS	2
+
+	unsigned char	signal_voltage;		/* signalling voltage (1.8V or 3.3V) */
+
+#define MMC_SIGNAL_VOLTAGE_330	0
+#define MMC_SIGNAL_VOLTAGE_180	1
 };
 
 struct mmc_host_ops {
@@ -107,6 +112,8 @@ struct mmc_host_ops {
 	int	(*get_cd)(struct mmc_host *host);
 
 	void	(*enable_sdio_irq)(struct mmc_host *host, int enable);
+
+	int	(*start_signal_voltage_switch)(struct mmc_host *host, struct mmc_ios *ios);
 };
 
 struct mmc_card;
@@ -152,6 +159,14 @@ struct mmc_host {
 #define MMC_CAP_DISABLE		(1 << 7)	/* Can the host be disabled */
 #define MMC_CAP_NONREMOVABLE	(1 << 8)	/* Nonremovable e.g. eMMC */
 #define MMC_CAP_WAIT_WHILE_BUSY	(1 << 9)	/* Waits while card is busy */
+#define MMC_CAP_UHS_SDR12	(1 << 15)	/* Host supports UHS SDR12 mode */
+#define MMC_CAP_UHS_SDR25	(1 << 16)	/* Host supports UHS SDR25 mode */
+#define MMC_CAP_UHS_SDR50	(1 << 17)	/* Host supports UHS SDR50 mode */
+#define MMC_CAP_UHS_SDR104	(1 << 18)	/* Host supports UHS SDR104 mode */
+#define MMC_CAP_UHS_DDR50	(1 << 19)	/* Host supports UHS DDR50 mode */
+#define MMC_CAP_SET_XPC_330	(1 << 20)	/* Host supports >150mA current at 3.3V */
+#define MMC_CAP_SET_XPC_300	(1 << 21)	/* Host supports >150mA current at 3.0V */
+#define MMC_CAP_SET_XPC_180	(1 << 22)	/* Host supports >150mA current at 1.8V */
 
 	/* host specific block data */
 	unsigned int		max_seg_size;	/* see blk_queue_max_segment_size */
