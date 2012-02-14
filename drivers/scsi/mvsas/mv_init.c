49864096641e9c9e4687c14fcdc63fb5ca6793a8
David Milburn <dmilburn@redhat.com>
2012-02-13 21:42:56 -0500

    libsas: kill sas_slave_destroy
    Patchwork-id: 44569
    O-Subject: [RHEL6.3 PATCH 04/59] BZ 782929 libsas: kill sas_slave_destroy
    Bugzilla: 782929
    RH-Acked-by: Jeff Garzik <jgarzik@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    
    git://git.kernel.org/pub/scm/linux/kernel/git/djbw/isci.git libsas-eh-reworks-v7
    
    commit d7ca96eb754b46e17c7515c5035bfa84c93210fd
    Author: Dan Williams <dan.j.williams@intel.com>
    Date:   Thu Nov 17 17:59:46 2011 -0800
    
        libsas: kill sas_slave_destroy
    
        Per commit 3e4ec344 "libata: kill ATA_FLAG_DISABLED" needing to set
        ATA_DEV_NONE is a holdover from before libsas converted to the
        "new-style" ata-eh.
    
        Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    
    BZ 782929
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/scsi/mvsas/mv_init.c
+++ drivers/scsi/mvsas/mv_init.c
@@ -45,7 +45,6 @@ static struct scsi_host_template mvs_sht = {
 	.queuecommand		= sas_queuecommand,
 	.target_alloc		= sas_target_alloc,
 	.slave_configure	= mvs_slave_configure,
-	.slave_destroy		= sas_slave_destroy,
 	.scan_finished		= mvs_scan_finished,
 	.scan_start		= mvs_scan_start,
 	.change_queue_depth	= sas_change_queue_depth,
