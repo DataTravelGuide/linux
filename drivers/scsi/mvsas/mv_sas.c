5487e77fec9c0896a4c8abb8dc4c2e4a9902812e
David Milburn <dmilburn@redhat.com>
2012-02-13 21:43:45 -0500

    mvsas: remove mvs_slave_{alloc|configure}
    Patchwork-id: 44619
    O-Subject: [RHEL6.3 PATCH 53/59] BZ 782929 mvsas: remove
    	mvs_slave_{alloc|configure}
    Bugzilla: 782929
    RH-Acked-by: Jeff Garzik <jgarzik@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    
    commit e211e2c7b6e7f470c818ef67c952954e1b46a46d
    Author: Dan Williams <dan.j.williams@intel.com>
    Date:   Tue Sep 20 15:10:55 2011 -0700
    
        [SCSI] mvsas: remove mvs_slave_{alloc|configure}
    
        libsas now handles:
        1/ limiting ata scanning to lun0
        2/ maximizing the queue_depth of sas devices (up to 256, mvsas only
           supports 64)
        3/ changes to /sys/block/<sdX>/device/queue_depth for ata devices
    
        Acked-by: Xiangliang Yu <yuxiangl@marvell.com>
        Signed-off-by: Dan Williams <dan.j.williams@intel.com>
        Signed-off-by: James Bottomley <JBottomley@Parallels.com>
    
    BZ 782929
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/scsi/mvsas/mv_sas.c
+++ drivers/scsi/mvsas/mv_sas.c
@@ -385,40 +385,6 @@ static void mvs_bytes_dmaed(struct mvs_info *mvi, int i)
 				   PORTE_BYTES_DMAED);
 }
 
-int mvs_slave_alloc(struct scsi_device *scsi_dev)
-{
-	struct domain_device *dev = sdev_to_domain_dev(scsi_dev);
-	if (dev_is_sata(dev)) {
-		/* We don't need to rescan targets
-		 * if REPORT_LUNS request is failed
-		 */
-		if (scsi_dev->lun > 0)
-			return -ENXIO;
-		scsi_dev->tagged_supported = 1;
-	}
-
-	return sas_slave_alloc(scsi_dev);
-}
-
-int mvs_slave_configure(struct scsi_device *sdev)
-{
-	struct domain_device *dev = sdev_to_domain_dev(sdev);
-	int ret = sas_slave_configure(sdev);
-
-	if (ret)
-		return ret;
-	if (dev_is_sata(dev)) {
-		/* may set PIO mode */
-	#if MV_DISABLE_NCQ
-		struct ata_port *ap = dev->sata_dev.ap;
-		struct ata_device *adev = ap->link.device;
-		adev->flags |= ATA_DFLAG_NCQ_OFF;
-		scsi_adjust_queue_depth(sdev, MSG_SIMPLE_TAG, 1);
-	#endif
-	}
-	return 0;
-}
-
 void mvs_scan_start(struct Scsi_Host *shost)
 {
 	int i, j;
