ead7910b805956aea64a618aadeb33a69f54a1d7
Michal Schmidt <mschmidt@redhat.com>
2012-02-14 18:52:07 -0500

    skbuff: add skb frag size accessors
    Patchwork-id: 44859
    O-Subject: [RHEL6.3 PATCH 2/7] skbuff: add skb frag size accessors
    Bugzilla: 741676
    RH-Acked-by: John Linville <linville@redhat.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=741676
    
    A dependency for the update of bnx2x.
    
    part of upstream commit 9e903e085262ffbf1fc44a17ac06058aca03524a:
        net: add skb frag size accessors
    
        To ease skb->truesize sanitization, its better to be able to localize
        all references to skb frags size.
    
        Define accessors : skb_frag_size() to fetch frag size, and
        skb_frag_size_{set|add|sub}() to manipulate it.
    
        Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=4040176
    
    Tested by me with the updated bnx2x.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -140,6 +140,26 @@ struct skb_frag_struct {
 	__u32 size;
 };
 
+static inline unsigned int skb_frag_size(const skb_frag_t *frag)
+{
+	return frag->size;
+}
+
+static inline void skb_frag_size_set(skb_frag_t *frag, unsigned int size)
+{
+	frag->size = size;
+}
+
+static inline void skb_frag_size_add(skb_frag_t *frag, int delta)
+{
+	frag->size += delta;
+}
+
+static inline void skb_frag_size_sub(skb_frag_t *frag, int delta)
+{
+	frag->size -= delta;
+}
+
 #define HAVE_HW_TIME_STAMP
 
 /**
@@ -1076,7 +1096,7 @@ static inline int skb_pagelen(const struct sk_buff *skb)
 	int i, len = 0;
 
 	for (i = (int)skb_shinfo(skb)->nr_frags - 1; i >= 0; i--)
-		len += skb_shinfo(skb)->frags[i].size;
+		len += skb_frag_size(&skb_shinfo(skb)->frags[i]);
 	return len + skb_headlen(skb);
 }
 
@@ -1100,7 +1120,7 @@ static inline void __skb_fill_page_desc(struct sk_buff *skb, int i,
 
 	frag->page		  = page;
 	frag->page_offset	  = off;
-	frag->size		  = size;
+	skb_frag_size_set(frag, size);
 }
 
 /**
@@ -1814,10 +1834,10 @@ static inline int skb_can_coalesce(struct sk_buff *skb, int i,
 				   struct page *page, int off)
 {
 	if (i) {
-		struct skb_frag_struct *frag = &skb_shinfo(skb)->frags[i - 1];
+		const struct skb_frag_struct *frag = &skb_shinfo(skb)->frags[i - 1];
 
 		return page == frag->page &&
-		       off == frag->page_offset + frag->size;
+		       off == frag->page_offset + skb_frag_size(frag);
 	}
 	return 0;
 }
