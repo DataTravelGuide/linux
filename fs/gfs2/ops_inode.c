d1c006ea1e38ee43f9b63afcdeddce64310b9f0e
Steven Whitehouse <swhiteho@redhat.com>
2012-02-15 12:00:37 -0500

    GFS2: Implement FITRIM ioctl
    Patchwork-id: 45019
    O-Subject: [RHEL 6.3] GFS2: Implement FITRIM ioctl (bz #656959)
    Bugzilla: 656959
    RH-Acked-by: Robert S Peterson <rpeterso@redhat.com>
    
    This patch adds support to GFS2 for the FITRIM ioctl. This
    is taken directly from an upstream patch. The upstream version
    was tested using the loopback dev, but since RHEL6's loop dev
    doesn't support trim, on RHEL6 the scsi_debug target was used
    to test it.
    
    This resolves bz #656959
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- fs/gfs2/ops_inode.c
+++ fs/gfs2/ops_inode.c
@@ -327,7 +327,7 @@ static int gfs2_unlink(struct inode *dir, struct dentry *dentry)
 	gfs2_holder_init(dip->i_gl, LM_ST_EXCLUSIVE, 0, ghs);
 	gfs2_holder_init(ip->i_gl,  LM_ST_EXCLUSIVE, 0, ghs + 1);
 
-	rgd = gfs2_blk2rgrpd(sdp, ip->i_no_addr);
+	rgd = gfs2_blk2rgrpd(sdp, ip->i_no_addr, 1);
 	gfs2_holder_init(rgd->rd_gl, LM_ST_EXCLUSIVE, 0, ghs + 2);
 
 
@@ -586,7 +586,7 @@ static int gfs2_rmdir(struct inode *dir, struct dentry *dentry)
 	gfs2_holder_init(dip->i_gl, LM_ST_EXCLUSIVE, 0, ghs);
 	gfs2_holder_init(ip->i_gl, LM_ST_EXCLUSIVE, 0, ghs + 1);
 
-	rgd = gfs2_blk2rgrpd(sdp, ip->i_no_addr);
+	rgd = gfs2_blk2rgrpd(sdp, ip->i_no_addr, 1);
 	gfs2_holder_init(rgd->rd_gl, LM_ST_EXCLUSIVE, 0, ghs + 2);
 
 	error = gfs2_glock_nq(ghs); /* parent */
@@ -790,7 +790,7 @@ static int gfs2_rename(struct inode *odir, struct dentry *odentry,
 		 * this is the case of the target file already existing
 		 * so we unlink before doing the rename
 		 */
-		nrgd = gfs2_blk2rgrpd(sdp, nip->i_no_addr);
+		nrgd = gfs2_blk2rgrpd(sdp, nip->i_no_addr, 1);
 		if (nrgd)
 			gfs2_holder_init(nrgd->rd_gl, LM_ST_EXCLUSIVE, 0, ghs + num_gh++);
 	}
