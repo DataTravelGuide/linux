3bdb418be395030d064db371457347f31e9bef6e
Tomas Henzl <thenzl@redhat.com>
2012-02-15 16:54:14 -0500

    cciss: add some older controllers to the kdump blacklist
    Patchwork-id: 45023
    O-Subject: [RHEL6.3 PATCH] cciss: add some older controllers to the kdump
    	blacklist
    Bugzilla: 738930
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    
    bz#738930
    
    Some other older controllers also do have problems to perform a kdump.
    Adding controllers to this list means that the driver will signal
    this non-ability via a resettable flag correctly.
    The unsupported list was created after a consultation with HP.
    
    Upstream post - https://lkml.org/lkml/2012/2/14/261
    Brew build - taskID=4045320
    
    Tomas
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/block/cciss.c
+++ drivers/block/cciss.c
@@ -576,6 +576,12 @@ static u32 unresettable_controller[] = {
 	0x40800E11, /* Smart Array 5i */
 	0x409C0E11, /* Smart Array 6400 */
 	0x409D0E11, /* Smart Array 6400 EM */
+	0x40700E11, /* Smart Array 5300 */
+	0x40820E11, /* Smart Array 532 */
+	0x40830E11, /* Smart Array 5312 */
+	0x409A0E11, /* Smart Array 641 */
+	0x409B0E11, /* Smart Array 642 */
+	0x40910E11, /* Smart Array 6i */
 };
 
 /* List of controllers which cannot even be soft reset */
@@ -583,6 +589,12 @@ static u32 soft_unresettable_controller[] = {
 	0x40800E11, /* Smart Array 5i */
 	0x409C0E11, /* Smart Array 6400 */
 	0x409D0E11, /* Smart Array 6400 EM */
+	0x40700E11, /* Smart Array 5300 */
+	0x40820E11, /* Smart Array 532 */
+	0x40830E11, /* Smart Array 5312 */
+	0x409A0E11, /* Smart Array 641 */
+	0x409B0E11, /* Smart Array 642 */
+	0x40910E11, /* Smart Array 6i */
 };
 
 static int ctlr_is_hard_resettable(u32 board_id)
