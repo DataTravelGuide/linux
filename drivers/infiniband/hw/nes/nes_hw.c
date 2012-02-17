74b600a8492d4a47b172b142ba3c9130d63ba7d3
Doug Ledford <dledford@redhat.com>
2012-02-16 22:18:56 -0500

    Fix common misspellings
    Patchwork-id: 45097
    O-Subject: [Patch RHEL6 006/216] Fix common misspellings
    Bugzilla: 738491 739139 749059 755741
    RH-Acked-by: John Feeney <jfeeney@redhat.com>
    
    From: Lucas De Marchi <lucas.demarchi@profusion.mobi>
    
    Fixes generated by 'codespell' and manually reviewed.
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@profusion.mobi>
    
    Partial backport of only the InfiniBand related changes
    
    Signed-off-by: Doug Ledford <dledford@redhat.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/infiniband/hw/nes/nes_hw.c
+++ drivers/infiniband/hw/nes/nes_hw.c
@@ -79,7 +79,7 @@ static void nes_terminate_start_timer(struct nes_qp *nesqp);
 
 #ifdef CONFIG_INFINIBAND_NES_DEBUG
 static unsigned char *nes_iwarp_state_str[] = {
-	"Non-Existant",
+	"Non-Existent",
 	"Idle",
 	"RTS",
 	"Closing",
@@ -90,7 +90,7 @@ static unsigned char *nes_iwarp_state_str[] = {
 };
 
 static unsigned char *nes_tcp_state_str[] = {
-	"Non-Existant",
+	"Non-Existent",
 	"Closed",
 	"Listen",
 	"SYN Sent",
