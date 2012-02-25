aec60e66755c71e8ce90ed567647fb86cf9b6130
John Linville <linville@redhat.com>
2012-02-24 21:50:07 -0500

    mac80211: Revert "mac80211: Speedup ieee80211_remove_interfaces()"
    Patchwork-id: 45516
    O-Subject: [RHEL 6 PATCH 060/102] mac80211: Revert "mac80211: Speedup
    	ieee80211_remove_interfaces()"
    Bugzilla: 766952
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    This reverts commit efe117ab8114f47f317b4803e5bc0104420bcba2.
    
    This commit uses APIs not available in RHEL6.
    
    bz766952
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- net/mac80211/iface.c
+++ net/mac80211/iface.c
@@ -1226,22 +1226,25 @@ void ieee80211_if_remove(struct ieee80211_sub_if_data *sdata)
 void ieee80211_remove_interfaces(struct ieee80211_local *local)
 {
 	struct ieee80211_sub_if_data *sdata, *tmp;
-	LIST_HEAD(unreg_list);
 
 	ASSERT_RTNL();
 
-	mutex_lock(&local->iflist_mtx);
 	list_for_each_entry_safe(sdata, tmp, &local->interfaces, list) {
+		/*
+		 * we cannot hold the iflist_mtx across unregister_netdevice,
+		 * but we only need to hold it for list modifications to lock
+		 * out readers since we're under the RTNL here as all other
+		 * writers.
+		 */
+		mutex_lock(&local->iflist_mtx);
 		list_del(&sdata->list);
+		mutex_unlock(&local->iflist_mtx);
 
 		if (ieee80211_vif_is_mesh(&sdata->vif))
 			mesh_path_flush_by_iface(sdata);
 
-		unregister_netdevice_queue(sdata->dev, &unreg_list);
+		unregister_netdevice(sdata->dev);
 	}
-	mutex_unlock(&local->iflist_mtx);
-	unregister_netdevice_many(&unreg_list);
-	list_del(&unreg_list);
 }
 
 static u32 ieee80211_idle_off(struct ieee80211_local *local,
