c2b0a8d213a2bdff13adb4c7d8038cc15afc7275
Thomas Graf <tgraf@redhat.com>
2012-03-07 14:34:54 -0500

    bonding: move slave MTU handling from sysfs
    Patchwork-id: 45732
    O-Subject: [RHEL6.3 PATCH 1/2] bonding: move slave MTU handling from sysfs V2
    Bugzilla: 799794
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Andy Gospodarek <gospo@redhat.com>
    
    Backport of:
    commit b15ba0fbdc2e54c3885fed91c54aeef7fe474033
    Author: Jiri Pirko <jpirko@redhat.com>
    Date:   Tue May 18 05:42:40 2010 +0000
    
        bonding: move slave MTU handling from sysfs V2
    
        V1->V2: corrected res/ret use
    
        For some reason, MTU handling (storing, and restoring) is taking  place in
        bond_sysfs. The correct place for this code is in bond_enslave, bond_release.
        So move it there.
    
        Signed-off-by: Jiri Pirko <jpirko@redhat.com>
        Signed-off-by: Jay Vosburgh <fubar@us.ibm.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Thomas Graf <tgraf@redhat.com>
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -1705,6 +1705,14 @@ int bond_enslave(struct net_device *bond_dev, struct net_device *slave_dev)
 	 */
 	new_slave->original_flags = slave_dev->flags;
 
+	/* Save slave's original mtu and then set it to match the bond */
+	new_slave->original_mtu = slave_dev->mtu;
+	res = dev_set_mtu(slave_dev, bond->dev->mtu);
+	if (res) {
+		pr_debug("Error %d calling dev_set_mtu\n", res);
+		goto err_free;
+	}
+
 	/*
 	 * Set the new_slave's queue_id to be zero.  Queue ID mapping
 	 * is set via sysfs or module option if desired.
@@ -1729,7 +1737,7 @@ int bond_enslave(struct net_device *bond_dev, struct net_device *slave_dev)
 		res = dev_set_mac_address(slave_dev, &addr);
 		if (res) {
 			pr_debug("Error %d calling set_mac_address\n", res);
-			goto err_free;
+			goto err_restore_mtu;
 		}
 	}
 
@@ -1969,6 +1977,9 @@ err_restore_mac:
 		dev_set_mac_address(slave_dev, &addr);
 	}
 
+err_restore_mtu:
+	dev_set_mtu(slave_dev, new_slave->original_mtu);
+
 err_free:
 	kfree(new_slave);
 
@@ -2169,6 +2180,8 @@ int bond_release(struct net_device *bond_dev, struct net_device *slave_dev)
 		addr.sa_family = slave_dev->type;
 		dev_set_mac_address(slave_dev, &addr);
 	}
+
+	dev_set_mtu(slave_dev, slave->original_mtu);
 
 	slave_dev->priv_flags &= ~(IFF_MASTER_8023AD | IFF_MASTER_ALB |
 				   IFF_SLAVE_INACTIVE | IFF_BONDING |
