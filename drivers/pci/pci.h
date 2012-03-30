bb1fe97b21a37be4b63bf1a89f0ed96644d81503
Myron Stowe <myron.stowe@redhat.com>
2012-03-30 16:42:35 -0400

    Move "pci reassigndev resource alignment" out of quirks.c
    Patchwork-id: 46094
    O-Subject: [RHEL6.4 kernel PATCH 13/14] PCI: Move "pci reassigndev resource alignment" out of quirks.c
    Bugzilla: 808213
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=808213
    Backport of pci/linux-next commit: 2069ecfbe14ebd71a6f98e8a00724e9adf4fe4ee
    Brew build: https://brewweb.devel.redhat.com/taskinfo?taskID=4230348
    Testing: Tested by me
    
    commit 2069ecfbe14ebd71a6f98e8a00724e9adf4fe4ee
    Author: Yinghai Lu <yinghai@kernel.org>
    Date:   Wed Feb 15 21:40:31 2012 -0800
    
    PCI: Move "pci reassigndev resource alignment" out of quirks.c
    
    This isn't really a quirk; calling it directly from pci_add_device makes
    more sense.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -218,11 +218,8 @@ static inline int pci_ari_enabled(struct pci_bus *bus)
 	return bus->self && bus->self->ari_enabled;
 }
 
-#ifdef CONFIG_PCI_QUIRKS
-extern int pci_is_reassigndev(struct pci_dev *dev);
-resource_size_t pci_specified_resource_alignment(struct pci_dev *dev);
+void pci_reassigndev_resource_alignment(struct pci_dev *dev);
 extern void pci_disable_bridge_window(struct pci_dev *dev);
-#endif
 
 /* Use "pci=[nosriov|sriov]" to disable or enable SRIOV */
 extern unsigned int pci_sriov_enabled;
