3a1a6a1ecedf9331290f086fa83d3359e04d8750
David Milburn <dmilburn@redhat.com>
2012-04-18 23:54:33 -0400

    isci: Redesign device suspension, abort, cleanup.
    Patchwork-id: 46405
    O-Subject: [RHEL6.3 PATCH 09/37] isci: Redesign device suspension, abort, cleanup.
    Bugzilla: 809954
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    
    commit d387f11cc473a5e3b562c6c255cf5c87394f10af
    Author: Jeff Skirvin <jeffrey.d.skirvin@intel.com>
    Date:   Thu Mar 8 22:41:54 2012 -0800
    
        isci: Redesign device suspension, abort, cleanup.
    
        This commit changes the means by which outstanding I/Os are handled
        for cleanup.
        The likelihood is that this commit will be broken into smaller pieces,
        however that will be a later revision.  Among the changes:
    
        - All completion structures have been removed from the tmf and
        abort paths.
        - Now using one completed I/O list, with the I/O completed in host bit being
        used to select error or normal callback paths.
    
        Signed-off-by: Jeff Skirvin <jeffrey.d.skirvin@intel.com>
        Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    
    BZ 809954
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/scsi/isci/remote_device.h
+++ drivers/scsi/isci/remote_device.h
@@ -85,7 +85,6 @@ struct isci_remote_device {
 	#define IDEV_GONE 3
 	#define IDEV_IO_READY 4
 	#define IDEV_IO_NCQERROR 5
-	#define IDEV_TXRX_SUSPENDED 6
 	unsigned long flags;
 	struct kref kref;
 	struct isci_port *isci_port;
@@ -106,10 +105,8 @@ struct isci_remote_device {
 #define ISCI_REMOTE_DEVICE_START_TIMEOUT 5000
 
 static inline struct isci_remote_device *isci_get_device(
-	struct domain_device *dev)
+	struct isci_remote_device *idev)
 {
-	struct isci_remote_device *idev = dev->lldd_dev;
-
 	if (idev)
 		kref_get(&idev->kref);
 	return idev;
@@ -378,4 +375,14 @@ enum sci_status isci_remote_device_reset_complete(
 enum sci_status
 sci_remote_device_abort_requests_pending_abort(
 	struct isci_remote_device *idev);
+
+enum sci_status isci_remote_device_suspend_terminate(
+	struct isci_host *ihost,
+	struct isci_remote_device *idev,
+	struct isci_request *ireq);
+
+enum sci_status isci_remote_device_terminate_requests(
+	struct isci_host *ihost,
+	struct isci_remote_device *idev,
+	struct isci_request *ireq);
 #endif /* !defined(_ISCI_REMOTE_DEVICE_H_) */
