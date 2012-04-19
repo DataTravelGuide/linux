0356b5b597edc861dea814484c9492ce4f604644
Jonathan E Brassow <jbrassow@redhat.com>
2012-04-18 23:08:51 -0400

    dm-raid: Set recovery flags on resume.
    Patchwork-id: 46395
    O-Subject: [RHEL6.3 PATCH] DM RAID: Set recovery flags on resume.
    Bugzilla: 811669
    RH-Acked-by: Milan Broz <mbroz@redhat.com>
    RH-Acked-by: Alasdair Kergon <agk@redhat.com>
    RH-Acked-by: Heinz Mauelshagen <heinzm@redhat.com>
    
    Bug #811669:
     https://bugzilla.redhat.com/show_bug.cgi?id=811669
    
    Brew Build:
     http://brewweb.devel.redhat.com/brew/taskinfo?taskID=4312632
    
    Testing:
     Testing shows issue is resolved.  (See bugzilla for more.)
    
    Upstream status:
     Pending.  Neil Brown has queued them for linux-next (i.e. 3.5)
     https://www.redhat.com/archives/dm-devel/2012-April/msg00043.html
    
     brassow
    
    Properly initialize MD recovery flags when resuming device-mapper devices.
    
    When a device-mapper device is suspended, all I/O must stop.  This is done by
    calling 'md_stop_writes' and 'mddev_suspend'.  These calls in-turn manipulate
    the recovery flags - including setting 'MD_RECOVERY_FROZEN'.  The DM device
    may have been suspended while recovery was not yet complete, so the process
    needs to pick-up where it left off.  Since 'mddev_resume' does not unset
    'MD_RECOVERY_FROZEN' and set 'MD_RECOVERY_NEEDED', we must do it ourselves.
    'MD_RECOVERY_NEEDED' can safely be set in 'mddev_resume', but 'MD_RECOVERY_FROZEN'
    must be set outside of 'mddev_resume' due to how MD handles RAID reshaping.
    (e.g.  It is possible for a user to delay reshaping a RAID5->RAID6 by purposefully
    setting 'MD_RECOVERY_FROZEN'.  Clearing it in 'mddev_resume' would override the
    desired behavior.)
    
    Because 'mddev_resume' already unconditionally calls 'md_wakeup_thread(mddev->thread)'
    there is no need to make this call from 'raid_resume' since it calls 'mddev_resume'.
    
    Signed-off-by: Jonathan Brassow <jbrassow@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -407,6 +407,7 @@ void mddev_resume(struct mddev *mddev)
 	wake_up(&mddev->sb_wait);
 	mddev->pers->quiesce(mddev, 0);
 
+	set_bit(MD_RECOVERY_NEEDED, &mddev->recovery);
 	md_wakeup_thread(mddev->thread);
 	md_wakeup_thread(mddev->sync_thread); /* possibly kick off a reshape */
 }
