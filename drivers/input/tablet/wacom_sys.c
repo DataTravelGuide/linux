ff801035c515c92231b02a33b72b1514beec80ed
Aristeu Rozanski <aris@redhat.com>
2012-05-08 14:22:00 -0400

    wacom: add support for Cintiq 24HD
    Patchwork-id: 47018
    O-Subject: [RHEL6.3 PATCH] wacom: add support for Cintiq 24HD
    Bugzilla: 773052
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    https://bugzilla.redhat.com/show_bug.cgi?id=773052
    
    This patch adds support for the new Cintiq 24HD.
    
    This patch is a dependency to another one coming up adding support to control
    the tablets' leds.
    
    upstream: 803296b678a43005e3bc0aaa1951d211bd76a054
    
    Tested using input-events verifying range, pressure, tilt, pen buttons,
    both tools and expresskeys buttons.
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/input/tablet/wacom_sys.c
+++ drivers/input/tablet/wacom_sys.c
@@ -235,6 +235,33 @@ void input_dev_g(struct input_dev *input_dev, struct wacom_wac *wacom_wac)
 	input_set_abs_params(input_dev, ABS_DISTANCE, 0, wacom_wac->features->distance_max, 0, 0);
 }
 
+void input_dev_24hd(struct input_dev *input_dev, struct wacom_wac *wacom_wac)
+{
+	input_dev->keybit[BIT_WORD(BTN_GAMEPAD)] |= BIT_MASK(BTN_A) | BIT_MASK(BTN_B) | BIT_MASK(BTN_C);
+	input_dev->keybit[BIT_WORD(BTN_GAMEPAD)] |= BIT_MASK(BTN_X) | BIT_MASK(BTN_Y) | BIT_MASK(BTN_Z);
+	input_dev->keybit[BIT_WORD(BTN_MISC)] |= BIT_MASK(BTN_0) | BIT_MASK(BTN_1) | BIT_MASK(BTN_2) |
+						 BIT_MASK(BTN_3) | BIT_MASK(BTN_4) | BIT_MASK(BTN_5) |
+						 BIT_MASK(BTN_6) | BIT_MASK(BTN_7) | BIT_MASK(BTN_8) |
+						 BIT_MASK(BTN_9);
+	input_dev->keybit[BIT_WORD(KEY_PROG1)] |= BIT_MASK(KEY_PROG1) | BIT_MASK(KEY_PROG2);
+	input_dev->keybit[BIT_WORD(KEY_PLAYCD)] |= BIT_MASK(KEY_PROG3);
+
+	input_set_abs_params(input_dev, ABS_Z, -900, 899, 0, 0);
+	input_set_abs_params(input_dev, ABS_THROTTLE, 0, 71, 0, 0);
+	input_set_capability(input_dev, EV_MSC, MSC_SERIAL);
+
+	input_dev->keybit[BIT_WORD(BTN_TOOL_RUBBER)] |= BIT_MASK(BTN_TOOL_RUBBER) | BIT_MASK(BTN_TOOL_PEN) |
+						BIT_MASK(BTN_TOOL_BRUSH) | BIT_MASK(BTN_TOOL_PENCIL) |
+						BIT_MASK(BTN_TOOL_AIRBRUSH) | BIT_MASK(BTN_STYLUS) |
+						BIT_MASK(BTN_STYLUS2);
+
+	input_set_abs_params(input_dev, ABS_DISTANCE,
+			     0, wacom_wac->features->distance_max, 0, 0);
+	input_set_abs_params(input_dev, ABS_WHEEL, 0, 1023, 0, 0);
+	input_set_abs_params(input_dev, ABS_TILT_X, 0, 127, 0, 0);
+	input_set_abs_params(input_dev, ABS_TILT_Y, 0, 127, 0, 0);
+}
+
 void input_dev_c21ux2(struct input_dev *input_dev, struct wacom_wac *wacom_wac)
 {
 	input_dev->keybit[BIT_WORD(BTN_GAMEPAD)] |= BIT_MASK(BTN_A) | BIT_MASK(BTN_B) | BIT_MASK(BTN_C);
