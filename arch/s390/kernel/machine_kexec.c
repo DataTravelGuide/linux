95032d1a7ee2d29bb937f247e558f9e3e774a223
Hendrik Brueckner <brueckner@redhat.com>
2012-06-18 12:48:59 -0400

    kdump: Use real mode for PSW restart and kexec
    Patchwork-id: 47562
    O-Subject: [RHEL6.4 PATCH] [s390x]kdump: Use real mode for PSW restart and kexec
    Bugzilla: 831278
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    Description
    -----------
    kdump can fail with DAT exeception on systems with many CPUs.
    
    Currently the PSW restart handler and kexec are executed in real
    mode with DAT=off. For kexec/kdump the function setup_regs() is
    called that uses the per-cpu variable "crash_notes". Because
    there are situations when the per-cpu implementation uses
    vmalloc memory, calling setup_regs() in real mode can cause a
    program check interrupt.
    
    Ensure that diag308_reset() does not change PSW bits to real
    mode. Call setup_regs() before we switch to real mode and call
    purgatory.
    
    Bugzilla
    --------
    BZ 831278
    https://bugzilla.redhat.com/show_bug.cgi?id=831278
    
    Upstream status of the patch
    ----------------------------
    The patch is upstream as of kernel version 3.5
    http://git.kernel.org/?p=linux/kernel/git/torvalds/linux-2.6.git;a=commitdiff;h=fa7c0043425624ed65b94a6fabe750c7b0af8719
    
    Test status
    -----------
    The patch has been tested and fixes the problem.
    The fix has been verified by the IBM test department.
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- arch/s390/kernel/machine_kexec.c
+++ arch/s390/kernel/machine_kexec.c
@@ -108,8 +108,8 @@ static void __do_machine_kdump(void *image)
 #ifdef CONFIG_CRASH_DUMP
 	int (*start_kdump)(int) = (void *)((struct kimage *) image)->start;
 
-	__load_psw_mask(PSW_BASE_BITS | PSW_DEFAULT_KEY);
 	setup_regs();
+	__load_psw_mask(PSW_BASE_BITS | PSW_DEFAULT_KEY);
 	start_kdump(1);
 #endif
 }
