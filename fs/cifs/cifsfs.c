737fa224c07696d0b61d4b4005440129cc823f66
Sachin Prabhu <sprabhu@redhat.com>
2012-07-27 14:35:56 -0400

    cifs: Remove unused inode number while fetching root inode
    Patchwork-id: 49046
    O-Subject: [PATCH 05/23] [bz823934] cifs: Remove unused inode number while fetching root inode
    Bugzilla: 823934
    RH-Acked-by: Jeff Layton <jlayton@redhat.com>
    
    Fixes bz 823934
    
    Backport of the following upstream patch
    --
    commit 9b6763e0aacf245b58687a372816a0a4aabf2b1e
    Author: Shirish Pargaonkar <shirishpargaonkar@gmail.com>
    Date:   Mon Feb 21 23:56:59 2011 -0600
    
    cifs: Remove unused inode number while fetching root inode
    
    ino is unused in function cifs_root_iget().
    
    Signed-off-by: Shirish Pargaonkar <shirishpargaonkar@gmail.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>
    --
    
    Signed-off-by: Sachin Prabhu <sprabhu@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- fs/cifs/cifsfs.c
+++ fs/cifs/cifsfs.c
@@ -132,7 +132,7 @@ cifs_read_super(struct super_block *sb, struct smb_vol *volume_info,
 	sb->s_op = &cifs_super_ops;
 	sb->s_blocksize = CIFS_MAX_MSGSIZE;
 	sb->s_blocksize_bits = 14;	/* default 2**14 = CIFS_MAX_MSGSIZE */
-	inode = cifs_root_iget(sb, ROOT_I);
+	inode = cifs_root_iget(sb);
 
 	if (IS_ERR(inode)) {
 		rc = PTR_ERR(inode);
