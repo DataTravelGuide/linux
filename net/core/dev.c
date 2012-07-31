d6ce39582b63abbaa3183bb1dc8013f355b34544
Amerigo Wang <amwang@redhat.com>
2012-07-31 08:46:28 -0400

    skbuff: add check for non-linear to warn_if_lro and needs_linearize
    Patchwork-id: 49133
    O-Subject: [RHEL6 Patch 2/3] skbuff: add check for non-linear to warn_if_lro and needs_linearize
    Bugzilla: 822737
    RH-Acked-by: Jesper Brouer <jbrouer@redhat.com>
    RH-Acked-by: Veaceslav Falico <vfalico@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    
    Cherry pick commit b78462ebc6a4ef9074aa80abebcdd470dc5f0ce0
    
    	skbuff: add check for non-linear to warn_if_lro and needs_linearize
    
    	We can avoid an unecessary cache miss by checking if the skb is non-linear
    	before accessing gso_size/gso_type in skb_warn_if_lro, the same can also be
    	done to avoid a cache miss on nr_frags if data_len is 0.
    
    	Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
    	Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Cong Wang <amwang@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2093,9 +2093,10 @@ static void skb_update_prio(struct sk_buff *skb)
 static inline int skb_needs_linearize(struct sk_buff *skb,
 				      struct net_device *dev)
 {
-	return (skb_has_frags(skb) && !(dev->features & NETIF_F_FRAGLIST)) ||
-	       (skb_shinfo(skb)->nr_frags && (!(dev->features & NETIF_F_SG) ||
-					      illegal_highdma(dev, skb)));
+	return skb_is_nonlinear(skb) &&
+	       ((skb_has_frags(skb) && !(dev->features & NETIF_F_FRAGLIST)) ||
+	        (skb_shinfo(skb)->nr_frags && (!(dev->features & NETIF_F_SG) ||
+					      illegal_highdma(dev, skb))));
 }
 
 /**
