0e8c686a289721ee12d08fdd9dddd96abb2c61fd
Rafael Aquini <aquini@redhat.com>
2012-08-07 21:08:41 -0400

    remove worrying dead code from find_get_pages()
    Patchwork-id: 49423
    O-Subject: [RHEL6 PATCH v2 02/29] mm: remove worrying dead code from find_get_pages()
    Bugzilla: 806049
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    
    Bugzilla: 806049
    https://bugzilla.redhat.com/show_bug.cgi?id=806049
    
    This patch is a backport for the following upstream commit:
    commit 9d8aa4ea855e0d64bb6926acb5618e6d1e2ed344
    Author: Hugh Dickins <hughd@google.com>
    Date:   Tue Mar 22 16:33:06 2011 -0700
    
        mm: remove worrying dead code from find_get_pages()
    
        The radix_tree_deref_retry() case in find_get_pages() has a strange little
        excrescence, not seen in the other gang lookups: it looks like the start
        of an abandoned attempt to guarantee forward progress in a case that
        cannot arise.
    
        ret should always be 0 here: if it isn't, then going back to restart will
        leak references to pages already gotten.  There used to be a comment
        saying nr_found is necessarily 1 here: that's not quite true, but the
        radix_tree_deref_retry() case is peculiar to the entry at index 0, when we
        race with it being moved out of the radix_tree root or back.
    
        Remove the worrisome two lines, add a brief comment here and in
        find_get_pages_contig() and find_get_pages_tag(), and a WARN_ON in
        find_get_pages() should it ever be seen elsewhere than at 0.
    
        Signed-off-by: Hugh Dickins <hughd@google.com>
        Cc: Nick Piggin <npiggin@kernel.dk>
        Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
        Cc: Wu Fengguang <fengguang.wu@intel.com>
        Cc: Salman Qazi <sqazi@google.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -833,9 +833,13 @@ repeat:
 		page = radix_tree_deref_slot((void **)pages[i]);
 		if (unlikely(!page))
 			continue;
+
+		/*
+		 * This can only trigger when the entry at index 0 moves out
+		 * of or back to the root: none yet gotten, safe to restart.
+		 */
 		if (radix_tree_deref_retry(page)) {
-			if (ret)
-				start = pages[ret-1]->index;
+			WARN_ON(start | i);
 			goto restart;
 		}
 
@@ -885,6 +889,11 @@ repeat:
 		page = radix_tree_deref_slot((void **)pages[i]);
 		if (unlikely(!page))
 			continue;
+
+		/*
+		 * This can only trigger when the entry at index 0 moves out
+		 * of or back to the root: none yet gotten, safe to restart.
+		 */
 		if (radix_tree_deref_retry(page))
 			goto restart;
 
@@ -945,6 +954,11 @@ repeat:
 		page = radix_tree_deref_slot((void **)pages[i]);
 		if (unlikely(!page))
 			continue;
+
+		/*
+		 * This can only trigger when the entry at index 0 moves out
+		 * of or back to the root: none yet gotten, safe to restart.
+		 */
 		if (radix_tree_deref_retry(page))
 			goto restart;
 
