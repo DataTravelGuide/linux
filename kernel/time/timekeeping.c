20d20ceccd2d78e17cab55ae927f80e2d0bab3a4
Prarit Bhargava <prarit@redhat.com>
2012-08-10 13:54:37 -0400

    timekeeping: Add missing update call in timekeeping_resume()
    Patchwork-id: 49533
    O-Subject: [RHEL6.4 PATCH BZ 836803 7/7] timekeeping: Add missing update call in timekeeping_resume()
    Bugzilla: 836803
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=836803
    
    commit 3e997130bd2e8c6f5aaa49d6e3161d4d29b43ab0
    Author: Thomas Gleixner <tglx@linutronix.de>
    Date:   Mon Jul 16 12:50:42 2012 -0400
    
        timekeeping: Add missing update call in timekeeping_resume()
    
        The leap second rework unearthed another issue of inconsistent data.
    
        On timekeeping_resume() the timekeeper data is updated, but nothing
        calls timekeeping_update(), so now the update code in the timer
        interrupt sees stale values.
    
        This has been the case before those changes, but then the timer
        interrupt was using stale data as well so this went unnoticed for quite
        some time.
    
        Add the missing update call, so all the data is consistent everywhere.
    
        Reported-by: Andreas Schwab <schwab@linux-m68k.org>
        Reported-and-tested-by: "Rafael J. Wysocki" <rjw@sisk.pl>
        Reported-and-tested-by: Martin Steigerwald <Martin@lichtvoll.de>
        Cc: LKML <linux-kernel@vger.kernel.org>
        Cc: Linux PM list <linux-pm@vger.kernel.org>
        Cc: John Stultz <johnstul@us.ibm.com>
        Cc: Ingo Molnar <mingo@kernel.org>
        Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>,
        Cc: Prarit Bhargava <prarit@redhat.com>
        Cc: stable@vger.kernel.org
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Signed-off-by: John Stultz <johnstul@us.ibm.com>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -675,6 +675,7 @@ static int timekeeping_resume(struct sys_device *dev)
 	timekeeper.clock->cycle_last = timekeeper.clock->read(timekeeper.clock);
 	timekeeper.ntp_error = 0;
 	timekeeping_suspended = 0;
+	timekeeping_update(false);
 	write_sequnlock_irqrestore(&timekeeper.lock, flags);
 
 	touch_softlockup_watchdog();
