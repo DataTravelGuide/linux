af61ccd5f8f68e1bee38e639298f260003bca638
Luming Yu <luyu@redhat.com>
2012-08-15 03:14:24 -0400

    HWPOISON: Clean up memory_failure() vs. __memory_failure()
    Patchwork-id: 49781
    O-Subject: [RHEL6.4 BZ696033 PATCH v2 01/14] HWPOISON: Clean up memory_failure() vs. __memory_failure()
    Bugzilla: 696033
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    commit cd42f4a3b2b1c4cbd997363dc57821953d73fd87
    Author: Tony Luck <tony.luck@intel.com>
    Date:   Thu Dec 15 10:48:12 2011 -0800
    
        There is only one caller of memory_failure(), all other users call
        __memory_failure() and pass in the flags argument explicitly. The
        lone user of memory_failure() will soon need to pass flags too.
    
        Add flags argument to the callsite in mce.c. Delete the old memory_failure()
        function, and then rename __memory_failure() without the leading "__".
    
        Provide clearer message when action optional memory errors are ignored.
    
        Acked-by: Borislav Petkov <bp@amd64.org>
        Signed-off-by: Tony Luck <tony.luck@intel.com>
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -986,7 +986,25 @@ static void clear_page_hwpoison_huge_page(struct page *hpage)
 		ClearPageHWPoison(hpage + i);
 }
 
-int __memory_failure(unsigned long pfn, int trapno, int flags)
+/**
+ * memory_failure - Handle memory failure of a page.
+ * @pfn: Page Number of the corrupted page
+ * @trapno: Trap number reported in the signal to user space.
+ * @flags: fine tune action taken
+ *
+ * This function is called by the low level machine check code
+ * of an architecture when it detects hardware memory corruption
+ * of a page. It tries its best to recover, which includes
+ * dropping pages, killing processes etc.
+ *
+ * The function is primarily of use for corruptions that
+ * happen outside the current execution context (e.g. when
+ * detected by a background scrubber)
+ *
+ * Must run in process context (e.g. a work queue) with interrupts
+ * enabled and no spinlocks hold.
+ */
+int memory_failure(unsigned long pfn, int trapno, int flags)
 {
 	struct page_state *ps;
 	struct page *p;
@@ -1158,29 +1176,7 @@ out:
 	unlock_page(hpage);
 	return res;
 }
-EXPORT_SYMBOL_GPL(__memory_failure);
-
-/**
- * memory_failure - Handle memory failure of a page.
- * @pfn: Page Number of the corrupted page
- * @trapno: Trap number reported in the signal to user space.
- *
- * This function is called by the low level machine check code
- * of an architecture when it detects hardware memory corruption
- * of a page. It tries its best to recover, which includes
- * dropping pages, killing processes etc.
- *
- * The function is primarily of use for corruptions that
- * happen outside the current execution context (e.g. when
- * detected by a background scrubber)
- *
- * Must run in process context (e.g. a work queue) with interrupts
- * enabled and no spinlocks hold.
- */
-void memory_failure(unsigned long pfn, int trapno)
-{
-	__memory_failure(pfn, trapno, 0);
-}
+EXPORT_SYMBOL_GPL(memory_failure);
 
 /**
  * unpoison_memory - Unpoison a previously poisoned page
