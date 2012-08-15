aaa590dea7b16bfe93b484b26eb6defd872b09c3
Steve Best <sbest@redhat.com>
2012-08-15 14:44:12 -0400

    cxgb3: Set vlan_feature on net_device
    Patchwork-id: 50001
    O-Subject: [PATCH RHEL6.4 BZ841993] cxgb3: Set vlan_feature on net_device
    Bugzilla: 841993
    RH-Acked-by: Thomas Graf <tgraf@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    RH-Acked-by: Jay Fenlason <fenlason@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    RHBZ#:
    ------
    https://bugzilla.redhat.com/show_bug.cgi?id=841993
    
    Description:
    ------------
    cxgb3 interface has a bad performance when VLAN is set. On my current
    setup, a PowerLinux 7R2, I am able to get around 7 Gbps on a TCP_STREAM
    (8 instances, 4k message).
    With this patch, I am able to reach 9.5 Gbps.
    
    Signed-off-by: Breno Leitao <brenohl@br.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    RHEL Version Found:
    -------------------
    RHEL 6.3
    
    Brew:
    -----
    http://brewweb.devel.redhat.com/brew/taskinfo?taskID=4711561
    
    Upstream:
    ---------
    http://git.kernel.org/?p=linux/kernel/git/torvalds/linux-2.6.git;a=commitdiff;h=1d962ecf1e8d42c09abac2f715239a592c9415de
    
    Test Status:
    ------------
    Breno Leitao helped me test this.
    
    ---------------------------------------------------------------
    Steve Best
    IBM on-site partner
    
    Proposed Patch:
    ---------------
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/net/cxgb3/cxgb3_main.c
+++ drivers/net/cxgb3/cxgb3_main.c
@@ -3190,6 +3190,9 @@ static void __devinit cxgb3_init_iscsi_mac(struct net_device *dev)
 	pi->iscsic.mac_addr[3] |= 0x80;
 }
 
+#define TSO_FLAGS (NETIF_F_TSO | NETIF_F_TSO6 | NETIF_F_TSO_ECN)
+#define VLAN_FEAT (NETIF_F_SG | NETIF_F_IP_CSUM | TSO_FLAGS | \
+			NETIF_F_IPV6_CSUM | NETIF_F_HIGHDMA)
 static int __devinit init_one(struct pci_dev *pdev,
 			      const struct pci_device_id *ent)
 {
@@ -3310,6 +3313,7 @@ static int __devinit init_one(struct pci_dev *pdev,
 		netdev->mem_end = mmio_start + mmio_len - 1;
 		netdev->features |= NETIF_F_SG | NETIF_F_IP_CSUM | NETIF_F_TSO;
 		netdev->features |= NETIF_F_GRO;
+		netdev->vlan_features |= netdev->features & VLAN_FEAT;
 		if (pci_using_dac)
 			netdev->features |= NETIF_F_HIGHDMA;
 
