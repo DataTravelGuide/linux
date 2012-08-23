2258b3a738bde9001576ab5ed24ae29b122c1977
Michael S. Tsirkin <mst@redhat.com>
2012-08-23 15:42:29 -0400

    kvm_host: add kvm_memslots wrapper
    Patchwork-id: 50301
    O-Subject: [PATCH RHEL6.4 15/20] kvm_host: add kvm_memslots wrapper
    Bugzilla: 835095
    RH-Acked-by: Marcelo Tosatti <mtosatti@redhat.com>
    
    Upstream uses kvm_memslots API for memslot
    access (introduced by commit 90d83dc3d49f5101addae962ccc1b4aff66b68d8).
    This is required for new RCU APIs upstream so we
    can just use rcu_dereference instead.
    Add an upstream-compatible API to make backport easier.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=835095
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -261,6 +261,11 @@ void kvm_exit(void);
 void kvm_get_kvm(struct kvm *kvm);
 void kvm_put_kvm(struct kvm *kvm);
 
+static inline struct kvm_memslots *kvm_memslots(struct kvm *kvm)
+{
+	return rcu_dereference(kvm->memslots);
+}
+
 #define HPA_MSB ((sizeof(hpa_t) * 8) - 1)
 #define HPA_ERR_MASK ((hpa_t)1 << HPA_MSB)
 static inline int is_error_hpa(hpa_t hpa) { return hpa >> HPA_MSB; }
