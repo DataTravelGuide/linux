6bef657e7db014820221f8e2bba3c8e47d123486
Myron Stowe <myron.stowe@redhat.com>
2012-08-24 04:35:48 -0400

    Introduce command line option to disable ARI
    Patchwork-id: 50371
    O-Subject: [RHEL6.4 PATCH 2/3] PCI / PCIe: Introduce command line option to disable ARI
    Bugzilla: 832591
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    
    Bugzilla: 832591
    https://bugzilla.redhat.com/show_bug.cgi?id=832591
    Upstream Status: 6748dcc269e52925993e0d68447858b41b88b4be
    
    commit 6748dcc269e52925993e0d68447858b41b88b4be
    Author: Rafael J. Wysocki <rjw@sisk.pl>
    Date:   Thu Mar 1 00:06:33 2012 +0100
    
    PCI / PCIe: Introduce command line option to disable ARI
    
    There are PCIe devices on the market that report ARI support but
    then fail to initialize correctly when ARI is actually used.  This
    leads to situations in which kernels 2.6.34 and newer fail to handle
    systems where the previous kernels worked without any apparent
    problems.  Unfortunately, it is currently unknown how many such
    devices are there.
    
    For this reason, introduce a new kernel command line option,
    pci=noari, allowing users to disable PCIe ARI altogether if they
    see problems with PCIe device initialization.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -57,6 +57,9 @@ unsigned long pci_cardbus_mem_size = DEFAULT_CARDBUS_MEM_SIZE;
 unsigned long pci_hotplug_io_size  = DEFAULT_HOTPLUG_IO_SIZE;
 unsigned long pci_hotplug_mem_size = DEFAULT_HOTPLUG_MEM_SIZE;
 
+/* If set, the PCIe ARI capability will not be used. */
+static bool pcie_ari_disabled;
+
 /**
  * pci_bus_max_busnr - returns maximum PCI bus number of given bus' children
  * @bus: pointer to PCI bus structure to search
@@ -1549,7 +1552,7 @@ void pci_enable_ari(struct pci_dev *dev)
 	u16 ctrl;
 	struct pci_dev *bridge;
 
-	if (!pci_is_pcie(dev) || dev->devfn)
+	if (pcie_ari_disabled || !pci_is_pcie(dev) || dev->devfn)
 		return;
 
 	pos = pci_find_ext_capability(dev, PCI_EXT_CAP_ID_ARI);
@@ -3409,6 +3412,8 @@ static int __init pci_setup(char *str)
 				pci_realloc();
 			} else if (!strcmp(str, "nodomains")) {
 				pci_no_domains();
+			} else if (!strncmp(str, "noari", 5)) {
+				pcie_ari_disabled = true;
 			} else if (!strncmp(str, "cbiosize=", 9)) {
 				pci_cardbus_io_size = memparse(str + 9, &str);
 			} else if (!strncmp(str, "cbmemsize=", 10)) {
