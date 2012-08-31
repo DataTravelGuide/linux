dd8e016e55a4357bdb5d6e27d27287deecae5d3f
Don Zickus <dzickus@redhat.com>
2012-08-30 21:42:37 -0400

    fix a NULL pointer dereference in hidraw_ioctl
    Patchwork-id: 50515
    O-Subject: [RHEL6 PATCH 2/3] HID: hidraw, fix a NULL pointer dereference in hidraw_ioctl
    Bugzilla: 839973
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    
    https://bugzilla.redhat.com/show_bug.cgi?id=839973
    
    commit d20d5ffab92f00188f360c44c791a5ffb988247c
    Author: Antonio Ospite <ospite@studenti.unina.it>
    Date:   Tue Oct 5 17:20:16 2010 +0200
    
        HID: hidraw, fix a NULL pointer dereference in hidraw_ioctl
    
        BUG: unable to handle kernel NULL pointer dereference at 0000000000000028
        IP: [<ffffffffa02c66b4>] hidraw_ioctl+0xfc/0x32c [hid]
        [...]
    
        This is reproducible by disconnecting the device while userspace does
        ioctl in a loop and doesn't check return values in order to exit the
        loop.
    
        Signed-off-by: Antonio Ospite <ospite@studenti.unina.it>
        Cc: stable@kernel.org
        Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/hid/hidraw.c
+++ drivers/hid/hidraw.c
@@ -250,6 +250,10 @@ static long hidraw_ioctl(struct file *file, unsigned int cmd,
 
 	mutex_lock(&minors_lock);
 	dev = hidraw_table[minor];
+	if (!dev) {
+		ret = -ENODEV;
+		goto out;
+	}
 
 	switch (cmd) {
 		case HIDIOCGRDESCSIZE:
@@ -323,6 +327,7 @@ static long hidraw_ioctl(struct file *file, unsigned int cmd,
 
 		ret = -ENOTTY;
 	}
+out:
 	mutex_unlock(&minors_lock);
 	return ret;
 }
