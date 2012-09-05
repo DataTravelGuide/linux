17a8dc8a9f3a8506aeab4507b723eb3b1dc54b44
Steve Dickson <SteveD@redhat.com>
2012-09-05 18:02:12 -0400

    nfs: Remove nfs4_setup_sequence from generic read code
    Patchwork-id: 50641
    O-Subject: [RHEL6.4 PATCH 10/14] NFS: Remove nfs4_setup_sequence from generic read code
    Bugzilla: 846714
    RH-Acked-by: Jeff Layton <jlayton@redhat.com>
    
    From: Bryan Schumaker <bjschuma@netapp.com>
    
    This is an NFS v4 specific operation, so it belongs in the NFS v4 code
    and not the generic client.
    
    Signed-off-by: Bryan Schumaker <bjschuma@netapp.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    Upstream Id: 038b32d23765f16fef48d44928c9353310b75adf
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- include/linux/nfs_xdr.h
+++ include/linux/nfs_xdr.h
@@ -1202,6 +1202,7 @@ struct nfs_rpc_ops {
 	int	(*set_capabilities)(struct nfs_server *, struct nfs_fh *);
 	__be32 *(*decode_dirent)(struct xdr_stream *, struct nfs_entry *, struct nfs_server *, int plus);
 	void	(*read_setup)   (struct nfs_read_data *, struct rpc_message *);
+	void	(*read_rpc_prepare)(struct rpc_task *, struct nfs_read_data *);
 	int	(*read_done)  (struct rpc_task *, struct nfs_read_data *);
 	void	(*write_setup)  (struct nfs_write_data *, struct rpc_message *);
 	void	(*write_rpc_prepare)(struct rpc_task *, struct nfs_write_data *);
