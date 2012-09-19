3ff8570ffbad0d33bf736845f8cf72bc39cd1d5a
Chad Dupuis <cdupuis@redhat.com>
2012-09-19 01:06:39 -0400

    qlge: Bumped driver version to 1.00.00.31
    Patchwork-id: 51342
    O-Subject: [RHEL 6.4 PATCH 9/9] qlge: Bumped driver version to 1.00.00.31
    Bugzilla: 826571
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    
    Bugzilla
    --------
    
    Bug 826571 (https://bugzilla.redhat.com/show_bug.cgi?id=826571)
    
    Upstream Status
    ---------------
    
    net commit id c278fa53c123282f753b2264fc62c0e9502a32fa
    
    Description
    -----------
    
    From: Jitendra Kalsaria <jitendra.kalsaria@qlogic.com>
    
    Subject: [PATCH 9/9] qlge: Bumped driver version to 1.00.00.31
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/net/qlge/qlge.h
+++ drivers/net/qlge/qlge.h
@@ -16,7 +16,7 @@
  */
 #define DRV_NAME  	"qlge"
 #define DRV_STRING 	"QLogic 10 Gigabit PCI-E Ethernet Driver "
-#define DRV_VERSION	"v1.00.00.30"
+#define DRV_VERSION	"v1.00.00.31"
 
 #define PFX "qlge: "
 
