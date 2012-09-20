c3f5255de18b2766a83a73785374aa07f3833cac
Chad Dupuis <cdupuis@redhat.com>
2012-09-19 21:24:54 -0400

    qla4xxx: Update func name from ql4_ to qla4_
    Patchwork-id: 51381
    O-Subject: [RHEL 6.4 PATCH 23/32] qla4xxx: Update func name from ql4_ to qla4_
    Bugzilla: 826570
    RH-Acked-by: Rob Evers <revers@redhat.com>
    
    Bugzilla
    --------
    
    Bug 826570 (https://bugzilla.redhat.com/show_bug.cgi?id=826570)
    
    Upstream Status
    ---------------
    
    scsi commit id b3a3c4b8233a3ce029f542a56071bb96011e6064
    
    Description
    -----------
    
    From: Vikas Chaudhary <vikas.chaudhary@qlogic.com>
    
    Subject: [PATCH 23/32] qla4xxx: Update func name from ql4_ to qla4_
    
    ql4_8xxx_mark_entry_skipped to qla4_8xxx_mark_entry_skipped
    ql4_attr_is_visible to qla4_attr_is_visible
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/scsi/qla4xxx/ql4_nx.c
+++ drivers/scsi/qla4xxx/ql4_nx.c
@@ -2075,7 +2075,7 @@ static int qla4_8xxx_minidump_process_rdmem(struct scsi_qla_host *ha,
 	return QLA_SUCCESS;
 }
 
-static void ql4_8xxx_mark_entry_skipped(struct scsi_qla_host *ha,
+static void qla4_8xxx_mark_entry_skipped(struct scsi_qla_host *ha,
 				struct qla8xxx_minidump_entry_hdr *entry_hdr,
 				int index)
 {
@@ -2159,13 +2159,13 @@ static int qla4_8xxx_collect_md_data(struct scsi_qla_host *ha)
 		 */
 		switch (entry_hdr->entry_type) {
 		case QLA82XX_RDEND:
-			ql4_8xxx_mark_entry_skipped(ha, entry_hdr, i);
+			qla4_8xxx_mark_entry_skipped(ha, entry_hdr, i);
 			break;
 		case QLA82XX_CNTRL:
 			rval = qla4_8xxx_minidump_process_control(ha,
 								  entry_hdr);
 			if (rval != QLA_SUCCESS) {
-				ql4_8xxx_mark_entry_skipped(ha, entry_hdr, i);
+				qla4_8xxx_mark_entry_skipped(ha, entry_hdr, i);
 				goto md_failed;
 			}
 			break;
@@ -2177,7 +2177,7 @@ static int qla4_8xxx_collect_md_data(struct scsi_qla_host *ha)
 			rval = qla4_8xxx_minidump_process_rdmem(ha, entry_hdr,
 								&data_ptr);
 			if (rval != QLA_SUCCESS) {
-				ql4_8xxx_mark_entry_skipped(ha, entry_hdr, i);
+				qla4_8xxx_mark_entry_skipped(ha, entry_hdr, i);
 				goto md_failed;
 			}
 			break;
@@ -2193,7 +2193,7 @@ static int qla4_8xxx_collect_md_data(struct scsi_qla_host *ha)
 			rval = qla4_8xxx_minidump_process_l2tag(ha, entry_hdr,
 								&data_ptr);
 			if (rval != QLA_SUCCESS) {
-				ql4_8xxx_mark_entry_skipped(ha, entry_hdr, i);
+				qla4_8xxx_mark_entry_skipped(ha, entry_hdr, i);
 				goto md_failed;
 			}
 			break;
@@ -2216,7 +2216,7 @@ static int qla4_8xxx_collect_md_data(struct scsi_qla_host *ha)
 			break;
 		case QLA82XX_RDNOP:
 		default:
-			ql4_8xxx_mark_entry_skipped(ha, entry_hdr, i);
+			qla4_8xxx_mark_entry_skipped(ha, entry_hdr, i);
 			break;
 		}
 
