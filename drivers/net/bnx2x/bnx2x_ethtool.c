58c158093952504d5f859e38d37e3f425fe3f4a7
Michal Schmidt <mschmidt@redhat.com>
2012-09-21 13:42:29 -0400

    bnx2x: Change bnx2x_tests_str_arr to static char
    Patchwork-id: 52246
    O-Subject: [RHEL6.4 PATCH 109/141] bnx2x: Change bnx2x_tests_str_arr to static char
    Bugzilla: 819842 826277 826295 826302
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    This patch changes the definition of bnx2x_tests_str_arr from char to static
    char. This correction will also eliminate the sparse warning created in commit
    cf2c1df62e065bfc15e38daf2d3479a56b320f29.
    
    Signed-off-by: Merav Sicron <meravs@broadcom.com>
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit ebe6e21d233bdc4f6ae5f5f649d13beb4ed7a763)
    
    Conflicts:
    
    	drivers/net/ethernet/broadcom/bnx2x/bnx2x_ethtool.c
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/net/bnx2x/bnx2x_ethtool.c
+++ drivers/net/bnx2x/bnx2x_ethtool.c
@@ -1688,7 +1688,7 @@ static int bnx2x_set_tso(struct net_device *dev, u32 data)
 	return 0;
 }
 
-char *bnx2x_tests_str_arr[BNX2X_NUM_TESTS_SF] = {
+static char *bnx2x_tests_str_arr[BNX2X_NUM_TESTS_SF] = {
 	"register_test (offline)    ",
 	"memory_test (offline)      ",
 	"int_loopback_test (offline)",
