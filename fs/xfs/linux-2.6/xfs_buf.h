3b35bd9c31ffe08abf4fe30029ef728a4fffe0a6
Carlos Maiolino <cmaiolino@redhat.com>
2012-09-21 03:31:59 -0400

    xfs: fix race while discarding buffers
    Patchwork-id: 51471
    O-Subject: [RHEL6 PATCH] xfs: fix race while discarding buffers
    Bugzilla: 829066
    RH-Acked-by: Dave Chinner <dchinner@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=829066
    
    Brew:
    http://brewweb.devel.redhat.com/brew/taskinfo?taskID=4894327
    
    Upstream:
    Patch already accepted upstream and on SGI's xfs tree under commit:
    e599b3253c5e49f7a2a579eef2fc2aa066989ef5
    Not yet merged on Linus's tree
    
    While xfs_buftarg_shrink() is freeing buffers from the dispose list (filled with
    buffers from lru list), there is a possibility to have xfs_buf_stale() racing
    with it, and removing buffers from dispose list before xfs_buftarg_shrink() does
    it.
    
    This happens because xfs_buftarg_shrink() handle the dispose list without
    locking and the test condition in xfs_buf_stale() checks for the buffer being in
    *any* list:
    
        if (!list_empty(&bp->b_lru))
    
    If the buffer happens to be on dispose list, this causes the buffer counter
    of lru list (btp->bt_lru_nr) to be decremented twice (once in
    xfs_buftarg_shrink() and another in xfs_buf_stale()) causing a wrong account
    usage of the lru list.
    
    This may cause xfs_buftarg_shrink() to return a wrong value to the memory
    shrinker shrink_slab(), and such account error may also cause an underflowed
    value to be returned; since the counter is lower than the current number of
    items in the lru list, a decrement may happen when the counter is 0, causing an
    underflow on the counter.
    
    The fix uses a new flag field (and a new buffer flag) to serialize buffer
    handling during the shrink process. The new flag field has been designed to use
    btp->bt_lru_lock/unlock instead of xfs_buf_lock/unlock mechanism.
    
    dchinner, sandeen, aquini and aris also deserve credits for this.
    
        Signed-off-by: Carlos Maiolino <cmaiolino@redhat.com>
        Reviewed-by: Ben Myers <bpm@sgi.com>
        Reviewed-by: Dave Chinner <dchinner@redhat.com>
        Signed-off-by: Ben Myers <bpm@sgi.com>
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- fs/xfs/linux-2.6/xfs_buf.h
+++ fs/xfs/linux-2.6/xfs_buf.h
@@ -44,27 +44,28 @@ typedef enum {
 	XBRW_ZERO = 3,			/* Zero target memory */
 } xfs_buf_rw_t;
 
-#define XBF_READ	(1 << 0) /* buffer intended for reading from device */
-#define XBF_WRITE	(1 << 1) /* buffer intended for writing to device */
-#define XBF_MAPPED	(1 << 2) /* buffer mapped (b_addr valid) */
-#define XBF_ASYNC	(1 << 4) /* initiator will not wait for completion */
-#define XBF_DONE	(1 << 5) /* all pages in the buffer uptodate */
-#define XBF_DELWRI	(1 << 6) /* buffer has dirty pages */
-#define XBF_STALE	(1 << 7) /* buffer has been staled, do not find it */
-#define XBF_ORDERED	(1 << 11)/* use ordered writes */
-#define XBF_READ_AHEAD	(1 << 12)/* asynchronous read-ahead */
-#define XBF_LOG_BUFFER	(1 << 13)/* this is a buffer used for the log */
+#define XBF_READ	 (1 << 0) /* buffer intended for reading from device */
+#define XBF_WRITE	 (1 << 1) /* buffer intended for writing to device */
+#define XBF_MAPPED	 (1 << 2) /* buffer mapped (b_addr valid) */
+#define XBF_ASYNC	 (1 << 4) /* initiator will not wait for completion */
+#define XBF_DONE	 (1 << 5) /* all pages in the buffer uptodate */
+#define XBF_DELWRI	 (1 << 6) /* buffer has dirty pages */
+#define XBF_STALE	 (1 << 7) /* buffer has been staled, do not find it */
+#define XBF_ORDERED	 (1 << 11)/* use ordered writes */
+#define XBF_READ_AHEAD	 (1 << 12)/* asynchronous read-ahead */
+#define XBF_LOG_BUFFER	 (1 << 13)/* this is a buffer used for the log */
 
 /* flags used only as arguments to access routines */
-#define XBF_LOCK	(1 << 14)/* lock requested */
-#define XBF_TRYLOCK	(1 << 15)/* lock requested, but do not wait */
-#define XBF_DONT_BLOCK	(1 << 16)/* do not block in current thread */
+#define XBF_LOCK	 (1 << 14)/* lock requested */
+#define XBF_TRYLOCK	 (1 << 15)/* lock requested, but do not wait */
+#define XBF_DONT_BLOCK	 (1 << 16)/* do not block in current thread */
 
 /* flags used only internally */
-#define _XBF_PAGES	(1 << 18)/* backed by refcounted pages */
-#define	_XBF_RUN_QUEUES	(1 << 19)/* run block device task queue	*/
-#define	_XBF_KMEM	(1 << 20)/* backed by heap memory */
-#define _XBF_DELWRI_Q	(1 << 21)/* buffer on delwri queue */
+#define _XBF_PAGES	 (1 << 18)/* backed by refcounted pages */
+#define	_XBF_RUN_QUEUES	 (1 << 19)/* run block device task queue */
+#define	_XBF_KMEM	 (1 << 20)/* backed by heap memory */
+#define _XBF_DELWRI_Q	 (1 << 21)/* buffer on delwri queue */
+#define _XBF_LRU_DISPOSE (1 << 24)/* buffer being discarded */
 
 typedef unsigned int xfs_buf_flags_t;
 
@@ -78,13 +79,14 @@ typedef unsigned int xfs_buf_flags_t;
 	{ XBF_STALE,		"STALE" }, \
 	{ XBF_ORDERED,		"ORDERED" }, \
 	{ XBF_READ_AHEAD,	"READ_AHEAD" }, \
-	{ XBF_LOCK,		"LOCK" },  	/* should never be set */\
-	{ XBF_TRYLOCK,		"TRYLOCK" }, 	/* ditto */\
+	{ XBF_LOCK,		"LOCK" },	/* should never be set */\
+	{ XBF_TRYLOCK,		"TRYLOCK" },	/* ditto */\
 	{ XBF_DONT_BLOCK,	"DONT_BLOCK" },	/* ditto */\
 	{ _XBF_PAGES,		"PAGES" }, \
 	{ _XBF_RUN_QUEUES,	"RUN_QUEUES" }, \
 	{ _XBF_KMEM,		"KMEM" }, \
-	{ _XBF_DELWRI_Q,	"DELWRI_Q" }
+	{ _XBF_DELWRI_Q,	"DELWRI_Q" }, \
+	{ _XBF_LRU_DISPOSE,	"LRU_DISPOSE" }
 
 typedef enum {
 	XBT_FORCE_SLEEP = 0,
@@ -139,7 +141,12 @@ typedef struct xfs_buf {
 	xfs_buf_flags_t		b_flags;	/* status flags */
 	struct semaphore	b_sema;		/* semaphore for lockables */
 
+	/*
+	 * concurrent access to b_lru and b_lru_flags are pretected by
+	 * bt_lru_lock and not by b_sema
+	 */
 	struct list_head	b_lru;		/* lru list */
+	xfs_buf_flags_t		b_lru_flags;	/* internal lru status flags */
 	wait_queue_head_t	b_waiters;	/* unpin waiters */
 	struct list_head	b_list;
 	struct xfs_perag	*b_pag;		/* contains rbtree root */
