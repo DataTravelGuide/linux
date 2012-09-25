0198ef99879cd24f2527e27d320ce704075b63dc
Nikolay Aleksandrov <nikolay@redhat.com>
2012-09-24 21:15:17 -0400

    sfc: Consistently test DEBUG macro, not EFX_ENABLE_DEBUG
    Patchwork-id: 53028
    O-Subject: [RHEL6.4 PATCH BZ 824042 063/142] sfc: Consistently test DEBUG macro, not EFX_ENABLE_DEBUG
    Bugzilla: 824042
    RH-Acked-by: Andy Gospodarek <gospo@redhat.com>
    
    From: Nikolay Aleksandrov <naleksan@redhat.com>
    
    Author: Ben Hutchings <bhutchings@solarflare.com>  2011-11-04 23:29:14
    
        The netif_dbg() macro is defined in <linux/netdevice.h>.  If the DEBUG
        macro is defined, it logs a message at 'debug' level, otherwise it
        does nothing.
    
        In net_driver.h we define DEBUG if EFX_ENABLE_DEBUG is defined, but
        this is too late for those source files that already got a
        definition of netif_dbg() by including <linux/netdevice.h>
    
        Get rid of EFX_ENABLE_DEBUG, and only define and test DEBUG.
    
        In mtd.c, we do not use DEBUG as a condition flag but are forced to
        use the DEBUG macro-function from <linux/mtd/mtd.h>.  Undefine DEBUG
        before including it.
    
        Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    
    	(cherry-picked from: 5f3f9d6c441faa323444b2f6b092d630fcd7d04c)
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/net/sfc/net_driver.h
+++ drivers/net/sfc/net_driver.h
@@ -13,10 +13,6 @@
 #ifndef EFX_NET_DRIVER_H
 #define EFX_NET_DRIVER_H
 
-#if defined(EFX_ENABLE_DEBUG) && !defined(DEBUG)
-#define DEBUG
-#endif
-
 #include <linux/version.h>
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
@@ -43,7 +39,7 @@
 
 #define EFX_DRIVER_VERSION	"3.1"
 
-#ifdef EFX_ENABLE_DEBUG
+#ifdef DEBUG
 #define EFX_BUG_ON_PARANOID(x) BUG_ON(x)
 #define EFX_WARN_ON_PARANOID(x) WARN_ON(x)
 #else
