bdd8b8ea47d3e46ab7deedd9b2a304e890f8c2a6
Ivan Vecera <ivecera@redhat.com>
2012-09-25 14:10:44 -0400

    be2net: Activate new FW after FW download for Lancer
    Patchwork-id: 53477
    O-Subject: [RHEL6.4 PATCH v2 39/57] be2net: Activate new FW after FW download for Lancer
    Bugzilla: 827593
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Nikolay Aleksandrov <nikolay@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    BZ: #827593
    
    Upstream commit:
    commit f67ef7bae8dd025086fdf8186958865bceb44595
    Author: Padmanabh Ratnakar <padmanabh.ratnakar@emulex.com>
    Date:   Thu Jul 12 03:57:09 2012 +0000
    
        be2net: Activate new FW after FW download for Lancer
    
        After FW download, activate new FW by invoking FW reset.
        Recreate rings once new FW is operational.
    
        Signed-off-by: Padmanabh Ratnakar <padmanabh.ratnakar@emulex.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/net/benet/be.h
+++ drivers/net/benet/be.h
@@ -382,6 +382,7 @@ struct be_adapter {
 	struct delayed_work work;
 	u16 work_counter;
 
+	struct delayed_work func_recovery_work;
 	u32 flags;
 	/* Ethtool knobs and info */
 	bool rx_csum; 		/* BE card must perform rx-checksumming */
@@ -389,9 +390,10 @@ struct be_adapter {
 	int if_handle;		/* Used to configure filtering */
 	u32 *pmac_id;		/* MAC addr handle used by BE card */
 
-	bool eeh_err;
-	bool ue_detected;
+	bool eeh_error;
 	bool fw_timeout;
+	bool hw_error;
+
 	u32 port_num;
 	bool promiscuous;
 	u32 function_mode;
@@ -597,7 +599,19 @@ static inline bool be_multi_rxq(const struct be_adapter *adapter)
 
 static inline bool be_error(struct be_adapter *adapter)
 {
-	return adapter->eeh_err || adapter->ue_detected || adapter->fw_timeout;
+	return adapter->eeh_error || adapter->hw_error || adapter->fw_timeout;
+}
+
+static inline bool be_crit_error(struct be_adapter *adapter)
+{
+	return adapter->eeh_error || adapter->hw_error;
+}
+
+static inline void  be_clear_all_error(struct be_adapter *adapter)
+{
+	adapter->eeh_error = false;
+	adapter->hw_error = false;
+	adapter->fw_timeout = false;
 }
 
 static inline bool be_is_wol_excluded(struct be_adapter *adapter)
