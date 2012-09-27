f86651a4badbb692bb1cef611973e87674b996db
Rik van Riel <riel@redhat.com>
2012-09-27 15:35:04 -0400

    page_alloc: do not call direct reclaim for THP allocations while compaction is deferred
    Patchwork-id: 53873
    O-Subject: [RHEL6.4 PATCH 13/21] mm: page allocator: do not call direct reclaim for THP allocations while compaction is deferred
    Bugzilla: 713546 783248
    RH-Acked-by: Jerome Marchand <jmarchan@redhat.com>
    RH-Acked-by: Johannes Weiner <jweiner@redhat.com>
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    Fixes bugs 783248 & 713546
    
      commit e9a127ba4b7a07e007af6d4c07006121438affaa
      Author: Mel Gorman <mgorman@suse.de>
      Date:   Thu Jan 12 17:19:41 2012 -0800
    
        mm: page allocator: do not call direct reclaim for THP allocations while compaction is deferred
    
        commit 66199712e9eef5aede09dbcd9dfff87798a66917 upstream.
    
        Stable note: Not tracked in Buzilla. This was part of a series that
        	reduced interactivity stalls experienced when THP was enabled.
    
        If compaction is deferred, direct reclaim is used to try to free enough
        pages for the allocation to succeed.  For small high-orders, this has a
        reasonable chance of success.  However, if the caller has specified
        __GFP_NO_KSWAPD to limit the disruption to the system, it makes more sense
        to fail the allocation rather than stall the caller in direct reclaim.
        This patch skips direct reclaim if compaction is deferred and the caller
        specifies __GFP_NO_KSWAPD.
    
        Async compaction only considers a subset of pages so it is possible for
        compaction to be deferred prematurely and not enter direct reclaim even in
        cases where it should.  To compensate for this, this patch also defers
        compaction only if sync compaction failed.
    
        Signed-off-by: Mel Gorman <mgorman@suse.de>
        Acked-by: Minchan Kim <minchan.kim@gmail.com>
        Reviewed-by: Rik van Riel<riel@redhat.com>
        Cc: Andrea Arcangeli <aarcange@redhat.com>
        Cc: Dave Jones <davej@redhat.com>
        Cc: Jan Kara <jack@suse.cz>
        Cc: Andy Isaacson <adi@hexapodia.org>
        Cc: Nai Xia <nai.xia@gmail.com>
        Cc: Johannes Weiner <jweiner@redhat.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
        Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1856,14 +1856,20 @@ static struct page *
 __alloc_pages_direct_compact(gfp_t gfp_mask, unsigned int order,
 	struct zonelist *zonelist, enum zone_type high_zoneidx,
 	nodemask_t *nodemask, int alloc_flags, struct zone *preferred_zone,
-	int migratetype, unsigned long *did_some_progress,
-	bool sync_migration)
+	int migratetype, bool sync_migration,
+	bool *deferred_compaction,
+	unsigned long *did_some_progress)
 {
 	struct page *page;
 	struct task_struct *p = current;
 
-	if (!order || compaction_deferred(preferred_zone))
+	if (!order)
 		return NULL;
+
+	if (compaction_deferred(preferred_zone)) {
+		*deferred_compaction = true;
+		return NULL;
+	}
 
 	p->flags |= PF_MEMALLOC;
 	*did_some_progress = try_to_compact_pages(zonelist, order, gfp_mask,
@@ -1892,7 +1898,13 @@ __alloc_pages_direct_compact(gfp_t gfp_mask, unsigned int order,
 		 * but not enough to satisfy watermarks.
 		 */
 		count_vm_event(COMPACTFAIL);
-		defer_compaction(preferred_zone);
+
+		/*
+		 * As async compaction considers a subset of pageblocks, only
+		 * defer if the failure was a sync compaction failure.
+		 */
+		if (sync_migration)
+			defer_compaction(preferred_zone);
 
 		cond_resched();
 	}
@@ -1904,8 +1916,9 @@ static inline struct page *
 __alloc_pages_direct_compact(gfp_t gfp_mask, unsigned int order,
 	struct zonelist *zonelist, enum zone_type high_zoneidx,
 	nodemask_t *nodemask, int alloc_flags, struct zone *preferred_zone,
-	int migratetype, unsigned long *did_some_progress,
-	bool sync_migration)
+	int migratetype, bool sync_migration,
+	bool *deferred_compaction,
+	unsigned long *did_some_progress)
 {
 	return NULL;
 }
@@ -2057,6 +2070,7 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	unsigned long did_some_progress;
 	struct task_struct *p = current;
 	bool sync_migration = false;
+	bool deferred_compaction = false;
 
 	/*
 	 * In the slowpath, we sanity check order to avoid ever trying to
@@ -2128,11 +2142,21 @@ rebalance:
 					zonelist, high_zoneidx,
 					nodemask,
 					alloc_flags, preferred_zone,
-					migratetype, &did_some_progress,
-					sync_migration);
+					migratetype, sync_migration,
+					&deferred_compaction,
+					&did_some_progress);
 	if (page)
 		goto got_pg;
 	sync_migration = true;
+
+	/*
+	 * If compaction is deferred for high-order allocations, it is because
+	 * sync compaction recently failed. In this is the case and the caller
+	 * has requested the system not be heavily disrupted, fail the
+	 * allocation now instead of entering direct reclaim
+	 */
+	if (deferred_compaction && (gfp_mask & __GFP_NO_KSWAPD))
+		goto nopage;
 
 	/* Try direct reclaim and then allocating */
 	page = __alloc_pages_direct_reclaim(gfp_mask, order,
@@ -2188,8 +2212,9 @@ rebalance:
 					zonelist, high_zoneidx,
 					nodemask,
 					alloc_flags, preferred_zone,
-					migratetype, &did_some_progress,
-					sync_migration);
+					migratetype, sync_migration,
+					&deferred_compaction,
+					&did_some_progress);
 		if (page)
 			goto got_pg;
 	}
