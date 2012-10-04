40c45ff74901b7254612202bed5c344568bf2d0d
Jesper Brouer <jbrouer@redhat.com>
2012-10-04 12:56:14 -0400

    ipvs: IPv6 MTU checking cleanup and bugfix
    Patchwork-id: 54040
    O-Subject: [RHEL6.4 PATCH 2/2] ipvs: IPv6 MTU checking cleanup and bugfix
    Bugzilla: 854066
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    RH-Acked-by: Thomas Graf <tgraf@redhat.com>
    
    From: Jesper Dangaard Brouer <jbrouer@redhat.com>
    
    Bugzilla: 854066
     https://bugzilla.redhat.com/show_bug.cgi?id=854066
    Upstream: 590e3f79a21edd2e9857ac3ced25ba6b2a491ef8
    Brew: 4936485 *PENDING*
    
    This patch is mostly a cleanup for IPv6 MTU checking.
    
    Backport of the following upstream commit to RHEL6.4:
    
    commit 590e3f79a21edd2e9857ac3ced25ba6b2a491ef8
    Author: Jesper Dangaard Brouer <brouer@redhat.com>
    Date:   Tue Aug 28 22:05:51 2012 +0200
    
        ipvs: IPv6 MTU checking cleanup and bugfix
    
        Cleaning up the IPv6 MTU checking in the IPVS xmit code, by using
        a common helper function __mtu_check_toobig_v6().
    
        The MTU check for tunnel mode can also use this helper as
        ntohs(old_iph->payload_len) + sizeof(struct ipv6hdr) is equal to
        skb->len.  And the 'mtu' variable have been adjusted before
        calling helper.
    
        Notice, this also fixes a bug, as the the MTU check in
        ip_vs_dr_xmit_v6() were missing a check for skb_is_gso().
    
        This bug e.g. caused issues for KVM IPVS setups, where different
        Segmentation Offloading techniques are utilized, between guests,
        via the virtio driver.  This resulted in very bad performance,
        due to the ICMPv6 "too big" messages didn't affect the sender.
    
        Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
        Signed-off-by: Patrick McHardy <kaber@trash.net>
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    Signed-off-by: Jesper Dangaard Brouer <jbrouer@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- net/netfilter/ipvs/ip_vs_xmit.c
+++ net/netfilter/ipvs/ip_vs_xmit.c
@@ -66,6 +66,15 @@ __ip_vs_dst_check(struct ip_vs_dest *dest, u32 rtos)
 	return dst;
 }
 
+static inline bool
+__mtu_check_toobig_v6(const struct sk_buff *skb, u32 mtu)
+{
+	if (skb->len > mtu && !skb_is_gso(skb)) {
+		return true; /* Packet size violate MTU size */
+	}
+	return false;
+}
+
 static struct rtable *
 __ip_vs_get_out_rt(struct sk_buff *skb, struct ip_vs_conn *cp, u32 rtos)
 {
@@ -324,7 +333,7 @@ ip_vs_bypass_xmit_v6(struct sk_buff *skb, struct ip_vs_conn *cp,
 
 	/* MTU checking */
 	mtu = dst_mtu(&rt->u.dst);
-	if (skb->len > mtu && !skb_is_gso(skb)) {
+	if (__mtu_check_toobig_v6(skb, mtu)) {
 		dst_release(&rt->u.dst);
 		icmpv6_send(skb, ICMPV6_PKT_TOOBIG, 0, mtu, skb->dev);
 		IP_VS_DBG_RL("%s(): frag needed\n", __func__);
@@ -468,7 +477,7 @@ ip_vs_nat_xmit_v6(struct sk_buff *skb, struct ip_vs_conn *cp,
 
 	/* MTU checking */
 	mtu = dst_mtu(&rt->u.dst);
-	if (skb->len > mtu && !skb_is_gso(skb)) {
+	if (__mtu_check_toobig_v6(skb, mtu)) {
 		dst_release(&rt->u.dst);
 		icmpv6_send(skb, ICMPV6_PKT_TOOBIG, 0, mtu, skb->dev);
 		IP_VS_DBG_RL_PKT(0, pp, skb, 0,
@@ -686,8 +695,8 @@ ip_vs_tunnel_xmit_v6(struct sk_buff *skb, struct ip_vs_conn *cp,
 	if (skb_dst(skb))
 		skb_dst(skb)->ops->update_pmtu(skb_dst(skb), mtu);
 
-	if (mtu < ntohs(old_iph->payload_len) + sizeof(struct ipv6hdr) &&
-	    !skb_is_gso(skb)) {
+	/* MTU checking: Notice that 'mtu' have been adjusted before hand */
+	if (__mtu_check_toobig_v6(skb, mtu)) {
 		icmpv6_send(skb, ICMPV6_PKT_TOOBIG, 0, mtu, skb->dev);
 		dst_release(&rt->u.dst);
 		IP_VS_DBG_RL("%s(): frag needed\n", __func__);
@@ -830,7 +839,7 @@ ip_vs_dr_xmit_v6(struct sk_buff *skb, struct ip_vs_conn *cp,
 
 	/* MTU checking */
 	mtu = dst_mtu(&rt->u.dst);
-	if (skb->len > mtu && !skb_is_gso(skb)) {
+	if (__mtu_check_toobig_v6(skb, mtu)) {
 		icmpv6_send(skb, ICMPV6_PKT_TOOBIG, 0, mtu, skb->dev);
 		dst_release(&rt->u.dst);
 		IP_VS_DBG_RL("%s(): frag needed\n", __func__);
@@ -981,7 +990,7 @@ ip_vs_icmp_xmit_v6(struct sk_buff *skb, struct ip_vs_conn *cp,
 
 	/* MTU checking */
 	mtu = dst_mtu(&rt->u.dst);
-	if (skb->len > mtu && !skb_is_gso(skb)) {
+	if (__mtu_check_toobig_v6(skb, mtu)) {
 		dst_release(&rt->u.dst);
 		icmpv6_send(skb, ICMPV6_PKT_TOOBIG, 0, mtu, skb->dev);
 		IP_VS_DBG_RL("%s(): frag needed\n", __func__);
