4bfbbdeb2bcd1a74c32b5152fad9d9e25b125c68
Steve Dickson <SteveD@redhat.com>
2012-10-04 21:09:31 -0400

    nfs: fix pnfs regression with directio writes
    Patchwork-id: 54055
    O-Subject: [RHEL6.4 PATCH v2 10/13] NFS: fix pnfs regression with directio writes
    Bugzilla: 858361
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Jeff Layton <jlayton@redhat.com>
    
    From: Fred Isaman <iisaman@netapp.com>
    
    Commit 57208fa7e51 "NFS: Create an write_pageio_init() function"
    did not modify the calls in direct.c, preventing direct io from
    using pnfs.  This reintroduces that capability.
    
    Signed-off-by: Fred Isaman <iisaman@netapp.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    Upstream Id: c95908e4c50d218f016e3866f5abf786055df635
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- fs/nfs/direct.c
+++ fs/nfs/direct.c
@@ -469,7 +469,7 @@ static void nfs_direct_write_reschedule(struct nfs_direct_req *dreq)
 	dreq->count = 0;
 	get_dreq(dreq);
 
-	nfs_pageio_init_write(&desc, dreq->inode, FLUSH_STABLE,
+	NFS_PROTO(dreq->inode)->write_pageio_init(&desc, dreq->inode, FLUSH_STABLE,
 			      &nfs_direct_write_completion_ops);
 	desc.pg_dreq = dreq;
 
@@ -768,11 +768,12 @@ static ssize_t nfs_direct_write_schedule_iovec(struct nfs_direct_req *dreq,
 					       loff_t pos)
 {
 	struct nfs_pageio_descriptor desc;
+	struct inode *inode = dreq->inode;
 	ssize_t result = 0;
 	size_t requested_bytes = 0;
 	unsigned long seg;
 
-	nfs_pageio_init_write(&desc, dreq->inode, FLUSH_COND_STABLE,
+	NFS_PROTO(inode)->write_pageio_init(&desc, inode, FLUSH_COND_STABLE,
 			      &nfs_direct_write_completion_ops);
 	desc.pg_dreq = dreq;
 	get_dreq(dreq);
