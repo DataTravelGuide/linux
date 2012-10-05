e025bddb9c4ce92f622434fa566cc6868892d260
Steve Dickson <SteveD@redhat.com>
2012-10-04 21:09:32 -0400

    nfs: fix several problems with _pnfs_return_layout
    Patchwork-id: 54054
    O-Subject: [RHEL6.4 PATCH v2 11/13] nfs4.1: fix several problems with _pnfs_return_layout
    Bugzilla: 858361
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Jeff Layton <jlayton@redhat.com>
    
    From: Fred Isaman <iisaman@netapp.com>
    
    _pnfs_return_layout had the following problems:
    
    - it did not call pnfs_free_lseg_list on all paths
    - it unintentionally did a forgetful return when there was no outstanding io
    - it raced with concurrent LAYOUTGETS
    
    Signed-off-by: Fred Isaman <iisaman@netapp.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    Upstream Id: a2e1d4f2e5ed83850de92a491ef225824cb457bd
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -5867,6 +5867,7 @@ static void nfs4_layoutreturn_done(struct rpc_task *task, void *calldata)
 {
 	struct nfs4_layoutreturn *lrp = calldata;
 	struct nfs_server *server;
+	struct pnfs_layout_hdr *lo = NFS_I(lrp->args.inode)->layout;
 
 	dprintk("--> %s\n", __func__);
 
@@ -5878,16 +5879,15 @@ static void nfs4_layoutreturn_done(struct rpc_task *task, void *calldata)
 		rpc_restart_call_prepare(task);
 		return;
 	}
+	spin_lock(&lo->plh_inode->i_lock);
 	if (task->tk_status == 0) {
-		struct pnfs_layout_hdr *lo = NFS_I(lrp->args.inode)->layout;
-
 		if (lrp->res.lrs_present) {
-			spin_lock(&lo->plh_inode->i_lock);
 			pnfs_set_layout_stateid(lo, &lrp->res.stateid, true);
-			spin_unlock(&lo->plh_inode->i_lock);
 		} else
 			BUG_ON(!list_empty(&lo->plh_segs));
 	}
+	lo->plh_block_lgets--;
+	spin_unlock(&lo->plh_inode->i_lock);
 	dprintk("<-- %s\n", __func__);
 }
 
