55630a9bc82dfb9d5961c27fd262c0917bf39e36
Nikolay Aleksandrov <nikolay@redhat.com>
2012-10-10 10:01:07 -0400

    sfc: Add TX queues for high-priority traffic
    Patchwork-id: 54363
    O-Subject: [RHEL6.4 PATCH BZ 824042 v2 003/142] sfc: Add TX queues for high-priority traffic
    Bugzilla: 824042
    RH-Acked-by: Andy Gospodarek <gospo@redhat.com>
    
    From: Ben Hutchings <bhutchings@solarflare.com>
    
    Implement the ndo_setup_tc() operation with 2 traffic classes.
    
    Current Solarstorm controllers do not implement TX queue priority, but
    they do allow queues to be 'paced' with an enforced delay between
    packets.  Paced and unpaced queues are scheduled in round-robin within
    two separate hardware bins (paced queues with a large delay may be
    placed into a third bin temporarily, but we won't use that).  If there
    are queues in both bins, the TX scheduler will alternate between them.
    
    If we make high-priority queues unpaced and best-effort queues paced,
    and high-priority queues are mostly empty, a single high-priority queue
    can then instantly take 50% of the packet rate regardless of how many
    of the best-effort queues have descriptors outstanding.
    
    We do not actually want an enforced delay between packets on best-
    effort queues, so we set the pace value to a reserved value that
    actually results in a delay of 0.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    (cherry picked from commit 94b274bf5fba6c75b922c8a23ad4b5639a168780)
    
    V2: Add matching lines from RHEL current to apply.
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/net/sfc/efx.h
+++ drivers/net/sfc/efx.h
@@ -37,6 +37,7 @@ extern netdev_tx_t
 efx_enqueue_skb(struct efx_tx_queue *tx_queue, struct sk_buff *skb);
 extern void efx_xmit_done(struct efx_tx_queue *tx_queue, unsigned int index);
 extern unsigned int efx_tx_max_skb_descs(struct efx_nic *efx);
+extern int efx_setup_tc(struct net_device *net_dev, u8 num_tc);
 
 /* RX */
 extern int efx_probe_rx_queue(struct efx_rx_queue *rx_queue);
