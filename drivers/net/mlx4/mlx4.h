aecf4b3ab745fb02b4848a9d3180ecacdaa0b515
Doug Ledford <dledford@redhat.com>
2012-10-12 16:08:16 -0400

    mlx4: Fix integer overflow issues around MTT table
    Patchwork-id: 54881
    O-Subject: [Patch RHEL6 159/193] mlx4_core: Fix integer overflow issues around MTT table
    Bugzilla: 826346 826348 826349
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    From: Yishai Hadas <yishaih@mellanox.com>
    
    Fix some issues around int variables used in data structures related
    to memory registration.
    
    Handle int overflow in mlx4_init_icm_table by using a u64 intermediate
    variable and changing struct mlx4_icm_table num_obj field to be u32.
    
    Change some more fields/variables to use u32 instead of int to prevent
    a case where the variable becomes negative when bit 31 is set.
    
    Also subtract log_mtts_per_seg from the exponent when computing
    num_mtt, since its added later on in that very same code area.
    
    This and the previous commit fixes some issues which actually prevent
    commit db5a7a65c058 ("mlx4_core: Scale size of MTT table with system
    RAM") from working.  Now, when the number of MTTs is scaled with the
    size of the RAM we can map up to 8TB.
    
    Signed-off-by: Yishai Hadas <yishaih@mellanox.com>
    Signed-off-by: Jack Morgenstein <jackm@mellanox.com>
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: Roland Dreier <roland@purestorage.com>
    (cherry picked from commit 3de819e6b642fdd51904f5f4d2716d7466a2f7f5)
    
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/net/mlx4/mlx4.h
+++ drivers/net/mlx4/mlx4.h
@@ -248,7 +248,7 @@ struct mlx4_bitmap {
 struct mlx4_buddy {
 	unsigned long	      **bits;
 	unsigned int	       *num_free;
-	int			max_order;
+	u32			max_order;
 	spinlock_t		lock;
 };
 
@@ -257,7 +257,7 @@ struct mlx4_icm;
 struct mlx4_icm_table {
 	u64			virt;
 	int			num_icm;
-	int			num_obj;
+	u32			num_obj;
 	int			obj_size;
 	int			lowmem;
 	int			coherent;
