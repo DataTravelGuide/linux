43bf3f6bac36c44583918e51241aa8fda4e93962
Luming Yu <luyu@redhat.com>
2012-10-15 12:54:53 -0400

    perf: Add Intel Nehalem-EX uncore support
    Patchwork-id: 54952
    O-Subject: [RHEL6.4 BZ696579 BZ696580 PATCH v4 11/19] perf/x86: Add Intel Nehalem-EX uncore support
    Bugzilla: 696579 696580
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    commit 254298c726b93bb8ed92774b4a209b479851fa6d
    Author: Yan, Zheng <zheng.z.yan@intel.com>
    Date:   Thu Jul 5 14:32:17 2012 +0800
    
    The uncore subsystem in Nehalem-EX consists of 7 components
    (U-Box, C-Box, B-Box, S-Box, R-Box, M-Box and W-Box). This
    patch is large because the way to program these boxes is
    diverse.
    
    Signed-off-by: Yan, Zheng <zheng.z.yan@intel.com>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/4FF534F1.3030307@intel.com
    [ Improved the code. ]
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- tools/perf/compat.h
+++ tools/perf/compat.h
@@ -1,23 +1,6 @@
 #ifndef COMPAT_H
 #define COMPAT_H
 
-enum perf_event_sample_format_compat {
-        PERF_SAMPLE_BRANCH_STACK                = 1U << 11,
-};
-
-enum perf_branch_sample_type_compat {
-	PERF_SAMPLE_BRANCH_USER         = 1U << 0, /* user branches */
-	PERF_SAMPLE_BRANCH_KERNEL       = 1U << 1, /* kernel branches */
-	PERF_SAMPLE_BRANCH_HV           = 1U << 2, /* hypervisor branches */
-
-	PERF_SAMPLE_BRANCH_ANY          = 1U << 3, /* any branch types */
-	PERF_SAMPLE_BRANCH_ANY_CALL     = 1U << 4, /* any call branch */
-	PERF_SAMPLE_BRANCH_ANY_RETURN   = 1U << 5, /* any return branch */
-	PERF_SAMPLE_BRANCH_IND_CALL     = 1U << 6, /* indirect calls */
-
-	PERF_SAMPLE_BRANCH_MAX          = 1U << 7, /* non-ABI */
-};
-
 #define PERF_SAMPLE_BRANCH_PLM_ALL \
 	(PERF_SAMPLE_BRANCH_USER|\
 	 PERF_SAMPLE_BRANCH_KERNEL|\
