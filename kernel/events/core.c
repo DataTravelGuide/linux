ce7dc4afab84c427a1101435dbeccdd1b9f2ad6f
Jiri Olsa <jolsa@redhat.com>
2012-11-12 12:07:43 -0500

    events: De-schedule a task context when removing the last event
    Patchwork-id: 55355
    O-Subject: [PATCH RHEL6.4 BZ874723 09/10] perf: De-schedule a task context when removing the last event
    Bugzilla: 874723
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    From: Jiri Olsa <Jiri Olsa jolsa@redhat.com>
    
    Bugzilla: 874723
    https://bugzilla.redhat.com/show_bug.cgi?id=874723
    
    Description:
    ============
    Since perf_install_in_context() will now install a context when we
    add the first event, we can de-schedule the context when the last
    event is removed.
    
    Upstream status:
    ================
    commit 64ce312618ef0e11d88def80effcefd1b59fdb1e
    Author: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Date:   Sat Apr 9 21:17:48 2011 +0200
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1141,6 +1141,10 @@ static int __perf_remove_from_context(void *info)
 	spin_lock(&ctx->lock);
 	event_sched_out(event, cpuctx, ctx);
 	list_del_event(event, ctx);
+	if (!ctx->nr_events && cpuctx->task_ctx == ctx) {
+		ctx->is_active = 0;
+		cpuctx->task_ctx = NULL;
+	}
 	spin_unlock(&ctx->lock);
 
 	return 0;
