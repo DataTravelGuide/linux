a5b03b295b950451fcf37ff57395987bbe7564a7
Ivan Vecera <ivecera@redhat.com>
2012-11-29 16:30:56 -0500

    be2net: remove type argument of be_cmd_mac_addr_query()
    Patchwork-id: 55662
    O-Subject: [RHEL6.4 PATCH 03/16] be2net: remove type argument of be_cmd_mac_addr_query()
    Bugzilla: 868896
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    BZ: #868896
    
    Upstream commit:
    commit 5ee4979ba5e71307e67ba24a49aa59a3481ae766
    Author: Sathya Perla <sathya.perla@emulex.com>
    Date:   Fri Sep 28 04:39:41 2012 +0000
    
        be2net: remove type argument of be_cmd_mac_addr_query()
    
        All invocations of this routine use the same type value.
    
        Signed-off-by: Sathya Perla <sathya.perla@emulex.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/net/benet/be_cmds.h
+++ drivers/net/benet/be_cmds.h
@@ -1769,7 +1769,7 @@ struct be_cmd_resp_set_profile_config {
 extern int be_pci_fnum_get(struct be_adapter *adapter);
 extern int be_fw_wait_ready(struct be_adapter *adapter);
 extern int be_cmd_mac_addr_query(struct be_adapter *adapter, u8 *mac_addr,
-			u8 type, bool permanent, u32 if_handle, u32 pmac_id);
+				 bool permanent, u32 if_handle, u32 pmac_id);
 extern int be_cmd_pmac_add(struct be_adapter *adapter, u8 *mac_addr,
 			u32 if_id, u32 *pmac_id, u32 domain);
 extern int be_cmd_pmac_del(struct be_adapter *adapter, u32 if_id,
