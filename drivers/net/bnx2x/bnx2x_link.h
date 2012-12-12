04530180e1a95b1c8e0b5a4e1654844ffedbff33
Michal Schmidt <mschmidt@redhat.com>
2012-12-12 16:26:44 -0500

    bnx2x: Prevent link flaps when booting from SAN
    Patchwork-id: 55842
    O-Subject: [RHEL6.4 PATCH 2/2] bnx2x: Prevent link flaps when booting from SAN.
    Bugzilla: 881068
    RH-Acked-by: Veaceslav Falico <vfalico@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    
    From: Barak Witkowski <barak@broadcom.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=881068
    
    It is possible that the driver is configured to operate with a certain
    link configuration which differs from the link's configuration during
    boot from SAN - this would cause the driver to flap the link.
    
    Said flap may be missed by specific switches, causing dcbx convergence
    to be too long and boot sequence to fail. Convergence is longer because
    switch ignores new dcbx packets due to counters mismatch, as only host
    side reset the counters due to the link flap.
    
    This patch causes the driver to ignore user's initial configuration during
    boot from SAN, and continues with the existing link configuration.
    
    Signed-off-by: Barak Witkowski <barak@broadcom.com>
    Signed-off-by: Yuval Mintz <yuvalmin@broadcom.com>
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit c63da990cd63153ce5203f96822638a7ef0f9bf7
     in the net-next git tree.)
    
    Conflicts:
    	drivers/net/bnx2x/bnx2x_link.h
       (because of #define FEATURE_CONFIG_MT_SUPPORT not yet in RHEL6)
    	drivers/net/bnx2x/bnx2x_main.c
       (__devinit annotations were removed upstream)
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/net/bnx2x/bnx2x_link.h
+++ drivers/net/bnx2x/bnx2x_link.h
@@ -267,6 +267,8 @@ struct link_params {
 #define FEATURE_CONFIG_AUTOGREEEN_ENABLED			(1<<9)
 #define FEATURE_CONFIG_BC_SUPPORTS_SFP_TX_DISABLED		(1<<10)
 #define FEATURE_CONFIG_DISABLE_REMOTE_FAULT_DET		(1<<11)
+#define FEATURE_CONFIG_BOOT_FROM_SAN			(1<<14)
+
 	/* Will be populated during common init */
 	struct bnx2x_phy phy[MAX_PHYS];
 
