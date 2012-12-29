dcf5f025ac9106d77d0fe448c3155a9d899748d9
Amerigo Wang <amwang@redhat.com>
2012-12-29 06:44:16 -0500

    dev_forward_skb() should clear skb_iif
    Patchwork-id: 56030
    O-Subject: [RHEL6.5 PATCH 22/45] net/core: dev_forward_skb() should clear skb_iif
    Bugzilla: 880142
    RH-Acked-by: Daniel Borkmann <dborkman@redhat.com>
    RH-Acked-by: Thomas Graf <tgraf@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    (cherry picked from commit 3b9785c6b0ff37ac4ef5085b38756283da84dceb)
    	While investigating another bug, I found that the code on the incoming path
    	in __netif_receive_skb will only set skb->skb_iif if it is already 0.  When
    	dev_forward_skb() is used in the case of interfaces like veth, skb_iif may
    	already have been set.  Making dev_forward_skb() cause the packet to look
    	like a newly received packet would seem to the the correct behaviour here,
    	as otherwise the wrong incoming interface can be reported for such a packet.
    
    	Signed-off-by: Benjamin LaHaise <bcrl@kvack.org>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Cong Wang <amwang@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -1553,6 +1553,7 @@ int dev_forward_skb(struct net_device *dev, struct sk_buff *skb)
 		kfree_skb(skb);
 		return NET_RX_DROP;
 	}
+	skb->iif = 0;
 	skb_dst_drop(skb);
 	skb->tstamp.tv64 = 0;
 	skb->pkt_type = PACKET_HOST;
