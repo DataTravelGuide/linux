b6131750767597102f90d0f373b8ce61ae82addf
Weiping Pan <wpan@redhat.com>
2013-01-09 07:02:37 -0500

    tcp: implement RFC 5961 3.2
    Patchwork-id: 56261
    O-Subject: [RHEL6.5 PATCH 1/5] tcp: implement RFC 5961 3.2
    Bugzilla: 843126
    RH-Acked-by: Daniel Borkmann <dborkman@redhat.com>
    RH-Acked-by: Thomas Graf <tgraf@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Jesper Brouer <jbrouer@redhat.com>
    
    commit 282f23c6ee343126156dd41218b22ece96d747e3
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Tue Jul 17 10:13:05 2012 +0200
    
        tcp: implement RFC 5961 3.2
    
        Implement the RFC 5691 mitigation against Blind
        Reset attack using RST bit.
    
        Idea is to validate incoming RST sequence,
        to match RCV.NXT value, instead of previouly accepted
        window : (RCV.NXT <= SEG.SEQ < RCV.NXT+RCV.WND)
    
        If sequence is in window but not an exact match, send
        a "challenge ACK", so that the other part can resend an
        RST with the appropriate sequence.
    
        Add a new sysctl, tcp_challenge_ack_limit, to limit
        number of challenge ACK sent per second.
    
        Add a new SNMP counter to count number of challenge acks sent.
        (netstat -s | grep TCPChallengeACK)
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Cc: Kiran Kumar Kella <kkiran@broadcom.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Weiping Pan <wpan@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -254,6 +254,7 @@ extern int sysctl_tcp_slow_start_after_idle;
 extern int sysctl_tcp_max_ssthresh;
 extern int sysctl_tcp_thin_linear_timeouts;
 extern int sysctl_tcp_thin_dupack;
+extern int sysctl_tcp_challenge_ack_limit;
 
 extern atomic_t tcp_memory_allocated;
 extern struct percpu_counter tcp_sockets_allocated;
