fe7875e5e2a2224436d353b258cbe592833566b2
Aristeu Rozanski <aris@redhat.com>
2013-01-09 19:01:20 -0500

    rcu: Convert call_rcu(whitelist_item_free) to kfree_rcu()
    Patchwork-id: 56291
    O-Subject: [RHEL6 PATCH 01/12] security: rcu: Convert call_rcu(whitelist_item_free) to kfree_rcu()
    Bugzilla: 819366
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    BZ#819366
    Upstream: 6034f7e603cd2dae8ed9a1d8d2ccfeb6b5c48d73
    Tested using my set of scripts for cgroups and device cgroups stress
    Brew: 5252927
    
    	commit 6034f7e603cd2dae8ed9a1d8d2ccfeb6b5c48d73
    	Author: Lai Jiangshan <laijs@cn.fujitsu.com>
    	Date:   Tue Mar 15 18:07:57 2011 +0800
    
    	    security,rcu: Convert call_rcu(whitelist_item_free) to kfree_rcu()
    
    	    The rcu callback whitelist_item_free() just calls a kfree(),
    	    so we use kfree_rcu() instead of the call_rcu(whitelist_item_free).
    
    	    Signed-off-by: Lai Jiangshan <laijs@cn.fujitsu.com>
    	    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    	    Acked-by: James Morris <jmorris@namei.org>
    	    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- security/device_cgroup.c
+++ security/device_cgroup.c
@@ -124,14 +124,6 @@ static int dev_whitelist_add(struct dev_cgroup *dev_cgroup,
 	return 0;
 }
 
-static void whitelist_item_free(struct rcu_head *rcu)
-{
-	struct dev_whitelist_item *item;
-
-	item = container_of(rcu, struct dev_whitelist_item, rcu);
-	kfree(item);
-}
-
 /*
  * called under devcgroup_mutex
  */
@@ -154,7 +146,7 @@ remove:
 		walk->access &= ~wh->access;
 		if (!walk->access) {
 			list_del_rcu(&walk->list);
-			call_rcu(&walk->rcu, whitelist_item_free);
+			kfree_rcu(walk, rcu);
 		}
 	}
 }
