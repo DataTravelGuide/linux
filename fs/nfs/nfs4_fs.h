24b4996fc8f075384066ed7e2903189be1a66215
Steve Dickson <SteveD@redhat.com>
2013-02-04 13:25:14 -0500

    nfs: nfs4_proc_renew should be declared static
    Patchwork-id: 56694
    O-Subject: [RHEL6.5 PATCH 10/17] NFSv4: nfs4_proc_renew should be declared static
    Bugzilla: 902965
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    
    From: Trond Myklebust <Trond.Myklebust@netapp.com>
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    Upstream Id: 8534d4ec055d854be6c94e8e5654fa87678ea5f7
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- fs/nfs/nfs4_fs.h
+++ fs/nfs/nfs4_fs.h
@@ -243,8 +243,6 @@ struct rpc_clnt *nfs4_create_sec_client(struct rpc_clnt *, struct inode *, struc
 extern int nfs4_proc_setclientid(struct nfs_client *, u32, unsigned short, struct rpc_cred *, struct nfs4_setclientid_res *);
 extern int nfs4_proc_setclientid_confirm(struct nfs_client *, struct nfs4_setclientid_res *arg, struct rpc_cred *);
 extern int nfs4_proc_exchange_id(struct nfs_client *clp, struct rpc_cred *cred);
-extern int nfs4_proc_async_renew(struct nfs_client *, struct rpc_cred *);
-extern int nfs4_proc_renew(struct nfs_client *, struct rpc_cred *);
 extern int nfs4_init_clientid(struct nfs_client *, struct rpc_cred *);
 extern int nfs41_init_clientid(struct nfs_client *, struct rpc_cred *);
 extern int nfs4_do_close(struct nfs4_state *state, gfp_t gfp_mask, int wait, bool roc);
