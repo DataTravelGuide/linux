e34484ee95ec80e2649cd888231a5c67f09d88f8
John Villalovos <jvillalo@redhat.com>
2013-02-21 23:24:28 -0500

    i2c-i801: Add Device IDs for Intel Lynx Point-LP PCH
    Patchwork-id: 57056
    O-Subject: [PATCH RHEL 6.5 BZ 847118 4/4] i2c-i801: Add Device IDs for Intel Lynx Point-LP PCH
    Bugzilla: 847118
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: "John L. Villalovos" <jvillalo@redhat.com>
    
    Backport of:
    Upstream commit 4a8f1ddde942e232387e6129ce4f4c412e43802f
    Upstream Author: James Ralston <james.d.ralston@intel.com>
    Upstream Date:   Mon Sep 10 10:14:02 2012 +0200
    
        i2c-i801: Add Device IDs for Intel Lynx Point-LP PCH
    
        Add the SMBus Device IDs for the Intel Lynx Point-LP PCH.
    
    Signed-off-by: John L. Villalovos <jvillalo@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/i2c/busses/i2c-i801.c
+++ drivers/i2c/busses/i2c-i801.c
@@ -51,6 +51,7 @@
   Patsburg (PCH) IDF    0x1d72     32     hard     yes     yes     yes
   Panther Point (PCH)   0x1e22     32     hard     yes     yes     yes
   Lynx Point (PCH)      0x8c22     32     hard     yes     yes     yes
+  Lynx Point-LP (PCH)   0x9c22     32     hard     yes     yes     yes
 
   Features supported by this driver:
   Software PEC                     no
@@ -140,6 +141,7 @@
 #define PCI_DEVICE_ID_INTEL_PATSBURG_SMBUS_IDF2	0x1d72
 #define PCI_DEVICE_ID_INTEL_PANTHERPOINT_SMBUS	0x1e22
 #define PCI_DEVICE_ID_INTEL_LYNXPOINT_SMBUS	0x8c22
+#define PCI_DEVICE_ID_INTEL_LYNXPOINT_LP_SMBUS	0x9c22
 
 struct i801_priv {
 	struct i2c_adapter adapter;
@@ -626,6 +628,7 @@ static const struct pci_device_id i801_ids[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_PATSBURG_SMBUS_IDF2) },
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_PANTHERPOINT_SMBUS) },
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_LYNXPOINT_SMBUS) },
+	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_LYNXPOINT_LP_SMBUS) },
 	{ 0, }
 };
 
