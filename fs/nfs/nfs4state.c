4ccfac2bacada7b36484eba1309a7abcc79efc57
Dave Wysochanski <dwysocha@redhat.com>
2013-03-06 16:52:22 -0500

    nfs: add more context to state manager error mesgs
    Patchwork-id: 57199
    O-Subject: [RHEL6.5 PATCH BZ 917711 5/5] NFS: add more context to state manager error mesgs
    Bugzilla: 917711
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    
    From: Weston Andros Adamson <dros@netapp.com>
    
    Backport 8ed27d4 to RHEL6.  Drop hunks which not in rhel6.
    Straightforward backport.
    
    Signed-off-by: Dave Wysochanski <dwysocha@redhat.com>
    
    commit 8ed27d4fb1ce95e65f5a3b26b02d3b77135cc7a1
    Author: Weston Andros Adamson <dros@netapp.com>
    Date:   Tue May 29 15:57:59 2012 -0400
    
        NFS: add more context to state manager error mesgs
    
        Signed-off-by: Weston Andros Adamson <dros@netapp.com>
        Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- fs/nfs/nfs4state.c
+++ fs/nfs/nfs4state.c
@@ -1694,10 +1694,12 @@ static void nfs4_set_lease_expired(struct nfs_client *clp, int status)
 static void nfs4_state_manager(struct nfs_client *clp)
 {
 	int status = 0;
+	const char *section = "", *section_sep = "";
 
 	/* Ensure exclusive access to NFSv4 state */
 	do {
 		if (test_and_clear_bit(NFS4CLNT_LEASE_EXPIRED, &clp->cl_state)) {
+			section = "lease expired";
 			/* We're going to have to re-establish a clientid */
 			status = nfs4_reclaim_lease(clp);
 			if (status) {
@@ -1716,6 +1718,7 @@ static void nfs4_state_manager(struct nfs_client *clp)
 		}
 
 		if (test_and_clear_bit(NFS4CLNT_CHECK_LEASE, &clp->cl_state)) {
+			section = "check lease";
 			status = nfs4_check_lease(clp);
 			if (test_bit(NFS4CLNT_LEASE_EXPIRED, &clp->cl_state))
 				continue;
@@ -1726,6 +1729,7 @@ static void nfs4_state_manager(struct nfs_client *clp)
 		/* Initialize or reset the session */
 		if (test_and_clear_bit(NFS4CLNT_SESSION_RESET, &clp->cl_state)
 		   && nfs4_has_session(clp)) {
+			section = "reset session";
 			status = nfs4_reset_session(clp);
 			if (test_bit(NFS4CLNT_LEASE_EXPIRED, &clp->cl_state))
 				continue;
@@ -1735,6 +1739,7 @@ static void nfs4_state_manager(struct nfs_client *clp)
 
 		/* First recover reboot state... */
 		if (test_bit(NFS4CLNT_RECLAIM_REBOOT, &clp->cl_state)) {
+			section = "reclaim reboot";
 			status = nfs4_do_reclaim(clp,
 				clp->cl_mvops->reboot_recovery_ops);
 			if (test_bit(NFS4CLNT_LEASE_EXPIRED, &clp->cl_state) ||
@@ -1749,6 +1754,7 @@ static void nfs4_state_manager(struct nfs_client *clp)
 
 		/* Now recover expired state... */
 		if (test_and_clear_bit(NFS4CLNT_RECLAIM_NOGRACE, &clp->cl_state)) {
+			section = "reclaim nograce";
 			status = nfs4_do_reclaim(clp,
 				clp->cl_mvops->nograce_recovery_ops);
 			if (test_bit(NFS4CLNT_LEASE_EXPIRED, &clp->cl_state) ||
@@ -1767,6 +1773,7 @@ static void nfs4_state_manager(struct nfs_client *clp)
 		/* Recall session slots */
 		if (test_and_clear_bit(NFS4CLNT_RECALL_SLOT, &clp->cl_state)
 		   && nfs4_has_session(clp)) {
+			section = "recall slot";
 			status = nfs4_recall_slot(clp);
 			if (status < 0)
 				goto out_error;
@@ -1783,8 +1790,11 @@ static void nfs4_state_manager(struct nfs_client *clp)
 	} while (atomic_read(&clp->cl_count) > 1);
 	return;
 out_error:
-	pr_warn_ratelimited("NFS: state manager failed on NFSv4 server %s"
-			" with error %d\n", clp->cl_hostname, -status);
+	if (strlen(section))
+		section_sep = ": ";
+	pr_warn_ratelimited("NFS: state manager%s%s failed on NFSv4 server %s"
+			" with error %d\n", section_sep, section,
+			clp->cl_hostname, -status);
 	nfs4_end_drain_session(clp);
 	nfs4_clear_state_manager_bit(clp);
 }
