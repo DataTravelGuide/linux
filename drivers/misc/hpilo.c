7482cc612c3a02e744c96e6aeb99f585e3e62de4
Tony Camuso <tcamuso@redhat.com>
2013-03-07 13:33:24 -0500

    hpilo: update module version
    Patchwork-id: 57255
    O-Subject: [RHEL6.5 PATCH 1/3] misc: hpilo: update module version
    Bugzilla: 826725
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Myron Stowe <mstowe@redhat.com>
    
    Upstream commit 98dcd59d was backported into RHE6 at commit
    0b2e701a. However, the backport failed to include the update to
    the MODULE_VERSION from the upstream commit. This patch corrects
    that, which allows cherry-picking of the upstream patches without
    conflict. The last patch in this set updates MODULE_VERSION again
    along with its other changes.
    
    Signed-off-by: Tony Camuso <tcamuso@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/misc/hpilo.c
+++ drivers/misc/hpilo.c
@@ -883,7 +883,7 @@ static void __exit ilo_exit(void)
 	class_destroy(ilo_class);
 }
 
-MODULE_VERSION("1.2");
+MODULE_VERSION("1.3");
 MODULE_ALIAS(ILO_NAME);
 MODULE_DESCRIPTION(ILO_NAME);
 MODULE_AUTHOR("David Altobelli <david.altobelli@hp.com>");
