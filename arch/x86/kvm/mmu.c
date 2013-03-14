dc66c7a56693197647dce3f931346c7d663cde0a
Marcelo Tosatti <mtosatti@redhat.com>
2013-03-13 22:40:57 -0400

    kvm/mmu: Fix free memory accounting race in mmu_alloc_roots()
    Patchwork-id: 57463
    O-Subject: [PATCH RHEL6.5] KVM: MMU: Fix free memory accounting race in mmu_alloc_roots()
    Bugzilla: 908751
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    RH-Acked-by: Gleb Natapov <gleb@redhat.com>
    
    BZ: 908751
    
    upstream commit f0f5933a1626c8df7b0bfd227819c66320fb4f0f
    Author: Avi Kivity <avi@redhat.com>
    
    KVM: MMU: Fix free memory accounting race in mmu_alloc_roots()
    
    We drop the mmu lock between freeing memory and allocating the roots; this
    allows some other vcpu to sneak in and allocate memory.
    
    While the race is benign (resulting only in temporary overallocation, not oom)
    it is simple and easy to fix by moving the freeing close to the allocation.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- arch/x86/kvm/mmu.c
+++ arch/x86/kvm/mmu.c
@@ -2162,6 +2162,7 @@ static int mmu_alloc_roots(struct kvm_vcpu *vcpu)
 		if (mmu_check_root(vcpu, root_gfn))
 			return 1;
 		spin_lock(&vcpu->kvm->mmu_lock);
+		kvm_mmu_free_some_pages(vcpu);
 		sp = kvm_mmu_get_page(vcpu, root_gfn, 0,
 				      PT64_ROOT_LEVEL, direct,
 				      ACC_ALL, NULL);
@@ -2190,6 +2191,7 @@ static int mmu_alloc_roots(struct kvm_vcpu *vcpu)
 		if (mmu_check_root(vcpu, root_gfn))
 			return 1;
 		spin_lock(&vcpu->kvm->mmu_lock);
+		kvm_mmu_free_some_pages(vcpu);
 		sp = kvm_mmu_get_page(vcpu, root_gfn, i << 30,
 				      PT32_ROOT_LEVEL, direct,
 				      ACC_ALL, NULL);
@@ -2559,9 +2561,6 @@ int kvm_mmu_load(struct kvm_vcpu *vcpu)
 	r = mmu_topup_memory_caches(vcpu);
 	if (r)
 		goto out;
-	spin_lock(&vcpu->kvm->mmu_lock);
-	kvm_mmu_free_some_pages(vcpu);
-	spin_unlock(&vcpu->kvm->mmu_lock);
 	r = mmu_alloc_roots(vcpu);
 	spin_lock(&vcpu->kvm->mmu_lock);
 	mmu_sync_roots(vcpu);
