c77b3375332ecf2a1bcd1e4c4e31b30da9e9c00e
Steve Dickson <SteveD@redhat.com>
2013-05-09 19:38:33 -0400

    nfs: Fix another reboot recovery race
    Patchwork-id: 58852
    O-Subject: [RHEL6.5 PATCH 22/22] NFSv4: Fix another reboot recovery race
    Bugzilla: 923939
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Jeff Layton <jlayton@redhat.com>
    
    From: Trond Myklebust <Trond.Myklebust@netapp.com>
    
    If the open_context for the file is not yet fully initialised,
    then open recovery cannot succeed, and since nfs4_state_find_open_context
    returns an ENOENT, we end up treating the file as being irrecoverable.
    
    What we really want to do, is just defer the recovery until later.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    Upstream Id: 91876b13b8b3cbff5cca8476d4b4eebe5f6038cc

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -1336,7 +1336,7 @@ static int nfs4_open_reclaim(struct nfs4_state_owner *sp, struct nfs4_state *sta
 
 	ctx = nfs4_state_find_open_context(state);
 	if (IS_ERR(ctx))
-		return PTR_ERR(ctx);
+		return -EAGAIN;
 	ret = nfs4_do_open_reclaim(ctx, state);
 	put_nfs_open_context(ctx);
 	return ret;
@@ -1766,7 +1766,7 @@ static int nfs4_open_expired(struct nfs4_state_owner *sp, struct nfs4_state *sta
 
 	ctx = nfs4_state_find_open_context(state);
 	if (IS_ERR(ctx))
-		return PTR_ERR(ctx);
+		return -EAGAIN;
 	ret = nfs4_do_open_expired(ctx, state);
 	put_nfs_open_context(ctx);
 	return ret;
@@ -1812,10 +1812,9 @@ static int _nfs4_open_and_get_state(struct nfs4_opendata *opendata,
 	if (server->caps & NFS_CAP_POSIX_LOCK)
 		set_bit(NFS_STATE_POSIX_LOCKS, &state->flags);
 
-	if (read_seqcount_retry(&sp->so_reclaim_seqcount, seq)) {
+	if (read_seqcount_retry(&sp->so_reclaim_seqcount, seq))
 		nfs4_schedule_stateid_recovery(server, state);
-		nfs4_wait_clnt_recover(server->nfs_client);
-	}
+
 	ret = 0;
 	*res = state;
 out:
