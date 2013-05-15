a91a0e81416f2bf776ce31db194bd3fa330087cb
Lenny Szubowicz <lszubowi@redhat.com>
2013-05-15 15:48:35 -0400

    intel_pstate: Fix intel_pstate_init() error path
    Patchwork-id: 59111
    O-Subject: [RHEL 6.5 PATCH 08/14] cpufreq / intel_pstate: Fix intel_pstate_init() error path
    Bugzilla: 956824
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    
    Description:
    
        Partial application of upstream 907cc908108b16ae87b7165be992511c968159f0
        We need the error path fix in the init function. However, in RHEL 6 we still
        need to keep the exit function, since the intel_pstate driver is a separate
        loadable kernel module.
    
    commit 907cc908108b16ae87b7165be992511c968159f0
    Author: Dirk Brandewie <dirk.brandewie@gmail.com>
    Date:   Tue Mar 5 14:15:27 2013 -0800
    
        cpufreq / intel_pstate: Fix intel_pstate_init() error path
    
        If cpufreq_register_driver() fails just free memory that has been
        allocated and return. intel_pstate_exit() function is removed since we
        are built-in only now there is no reason for a module exit procedure.
    
        Reported-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
        Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
        Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kernel/cpu/cpufreq/intel_pstate.c
+++ arch/x86/kernel/cpu/cpufreq/intel_pstate.c
@@ -779,7 +779,7 @@ static int __initdata no_load;
 
 static int __init intel_pstate_init(void)
 {
-	int rc = 0;
+	int cpu, rc = 0;
 	const struct x86_cpu_id *id;
 
 	if (no_load)
@@ -804,7 +804,16 @@ static int __init intel_pstate_init(void)
 	intel_pstate_sysfs_expose_params();
 	return rc;
 out:
-	intel_pstate_exit();
+	get_online_cpus();
+	for_each_online_cpu(cpu) {
+		if (all_cpu_data[cpu]) {
+			del_timer_sync(&all_cpu_data[cpu]->timer);
+			kfree(all_cpu_data[cpu]);
+		}
+	}
+
+	put_online_cpus();
+	vfree(all_cpu_data);
 	return -ENODEV;
 }
 device_initcall(intel_pstate_init);
