70b33aa9e5255d29da9a4eedad64231fbe8032be
Jiri Olsa <jolsa@redhat.com>
2013-06-07 12:22:32 -0400

    perf/evsel: Introduce is_group_member method
    Patchwork-id: 60048
    O-Subject: [PATCH RHEL6.5 BZ970529 144/146] perf evsel: Introduce is_group_member method
    Bugzilla: 970529
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    From: Arnaldo Carvalho de Melo <acme@redhat.com>
    
    Bugzilla: 970529
    https://bugzilla.redhat.com/show_bug.cgi?id=970529
    
    upstream status
    ===============
    commit 07ac002f2fcc74c5be47b656d9201d5de84dc53d
    Author: Arnaldo Carvalho de Melo <acme@redhat.com>
    Date:   Tue Nov 13 17:27:28 2012 -0300
    
    description
    ===========
    To clarify what is being tested, instead of assuming that evsel->leader
    == NULL means either an 'isolated' evsel or a 'group leader'.

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -525,14 +525,14 @@ XXX No dwarf unwind support in RHEL6
 	 * Disabling only independent events or group leaders,
 	 * keeping group members enabled.
 	 */
-	if (!evsel->leader)
+	if (!perf_evsel__is_group_member(evsel))
 		attr->disabled = 1;
 
 	/*
 	 * Setting enable_on_exec for independent events and
 	 * group leaders for traced executed by perf.
 	 */
-	if (perf_target__none(&opts->target) && (!evsel->leader))
+	if (perf_target__none(&opts->target) && !perf_evsel__is_group_member(evsel))
 		attr->enable_on_exec = 1;
 }
 
@@ -713,7 +713,7 @@ static int get_group_fd(struct perf_evsel *evsel, int cpu, int thread)
 	struct perf_evsel *leader = evsel->leader;
 	int fd;
 
-	if (!leader)
+	if (!perf_evsel__is_group_member(evsel))
 		return -1;
 
 	/*
