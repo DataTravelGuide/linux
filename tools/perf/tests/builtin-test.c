e7a8d0f99177629e12f128efb4ee9a2e13511eac
Jiri Olsa <jolsa@redhat.com>
2013-06-07 12:22:14 -0400

    perf/tests: Move pmu tests into separate object
    Patchwork-id: 60037
    O-Subject: [PATCH RHEL6.5 BZ970529 126/146] perf tests: Move pmu tests into separate object
    Bugzilla: 970529
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: 970529
    https://bugzilla.redhat.com/show_bug.cgi?id=970529
    
    upstream status
    ===============
    commit cff7f956ec4a1ede9b752cfae3c12f588292ad80
    Author: Jiri Olsa <jolsa@redhat.com>
    Date:   Sat Nov 10 01:46:50 2012 +0100
    
    description
    ===========
    Separating pmu's object tests into pmu object under tests directory.

--- tools/perf/tests/builtin-test.c
+++ tools/perf/tests/builtin-test.c
@@ -31,12 +31,6 @@
 #include <sched.h>
 
 
-
-static int test__perf_pmu(void)
-{
-	return perf_pmu__test();
-}
-
 static struct test {
 	const char *desc;
 	int (*func)(void);
@@ -67,7 +61,7 @@ static struct test {
 	},
 	{
 		.desc = "Test perf pmu format parsing",
-		.func = test__perf_pmu,
+		.func = test__pmu,
 	},
 	{
 		.desc = "Test dso data interface",
