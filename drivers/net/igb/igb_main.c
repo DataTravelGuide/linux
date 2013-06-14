5985d202f0aae337dc91631a3b6b0132d2baa02d
Stefan Assmann <sassmann@redhat.com>
2013-06-14 14:13:57 -0400

    igb: Remove artificial restriction on RQDPC stat reading
    Patchwork-id: 60296
    O-Subject: [RHEL6.5 PATCH 02/97] igb: Remove artificial restriction on RQDPC stat reading
    Bugzilla: 914835 923899 947526
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    From: Alexander Duyck <alexander.h.duyck@intel.com>
    
    For some reason the reading of the RQDPC register was being artificially
    limited to 4K.  Instead of limiting the value we should read the value and
    add the full amount.  Otherwise this can lead to a misleading number of
    dropped packets when the actual value is in fact much higher.
    
    Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
    Tested-by: Jeff Pieper   <jeffrey.e.pieper@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Stefan Assmann <sassmann@redhat.com>
    (cherry picked from commit ae1c07a6b7ced6c0c94c99e3b53f4e7856fa8bff)

--- drivers/net/igb/igb_main.c
+++ drivers/net/igb/igb_main.c
@@ -4618,10 +4618,14 @@ void igb_update_stats(struct igb_adapter *adapter)
 	bytes = 0;
 	packets = 0;
 	for (i = 0; i < adapter->num_rx_queues; i++) {
-		u32 rqdpc_tmp = rd32(E1000_RQDPC(i)) & 0x0FFF;
+		u32 rqdpc = rd32(E1000_RQDPC(i));
 		struct igb_ring *ring = adapter->rx_ring[i];
-		ring->rx_stats.drops += rqdpc_tmp;
-		net_stats->rx_fifo_errors += rqdpc_tmp;
+
+		if (rqdpc) {
+			ring->rx_stats.drops += rqdpc;
+			net_stats->rx_fifo_errors += rqdpc;
+		}
+
 		bytes += ring->rx_stats.bytes;
 		packets += ring->rx_stats.packets;
 	}
