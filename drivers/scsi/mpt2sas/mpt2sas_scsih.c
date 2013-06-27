effdd7937a535f610ff17cdff6576d16a0cc0aee
Tomas Henzl <thenzl@redhat.com>
2013-06-27 14:30:53 -0300

    mpt2sas: fix firmware failure with wrong task attribute
    Patchwork-id: 60792
    O-Subject: [RHEL6.5 PATCH 18/16] [SCSI] mpt2sas: fix firmware failure with wrong task attribute
    Bugzilla: 947078
    RH-Acked-by: Rob Evers <revers@redhat.com>
    
    Upstream: 48ba2efc382f94fae16ca8ca011e5961a81ad1ea
    
    When SCSI command is received with task attribute not set, set it to SIMPLE.
    Previously it is set to untagged. This causes the firmware to fail the commands.
    
    Signed-off-by: Sreekanth Reddy <Sreekanth.Reddy@lsi.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/mpt2sas/mpt2sas_scsih.c
+++ drivers/scsi/mpt2sas/mpt2sas_scsih.c
@@ -3983,11 +3983,7 @@ _scsih_qcmd(struct scsi_cmnd *scmd, void (*done)(struct scsi_cmnd *))
 			else
 				mpi_control |= MPI2_SCSIIO_CONTROL_SIMPLEQ;
 		} else
-/* MPI Revision I (UNIT = 0xA) - removed MPI2_SCSIIO_CONTROL_UNTAGGED */
-/*			mpi_control |= MPI2_SCSIIO_CONTROL_UNTAGGED;
- */
-			mpi_control |= (0x500);
-
+			mpi_control |= MPI2_SCSIIO_CONTROL_SIMPLEQ;
 	} else
 		mpi_control |= MPI2_SCSIIO_CONTROL_SIMPLEQ;
 	/* Make sure Device is not raid volume.
