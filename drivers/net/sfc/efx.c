e6ae220c9af488e09eeabf25e393925a74700b18
Nikolay Aleksandrov <nikolay@redhat.com>
2013-07-12 10:54:29 -0300

    sfc: Hold the RTNL lock for more of the suspend/resume cycle
    Patchwork-id: 61700
    O-Subject: [RHEL6.5 PATCH BZ 953929 22/95] sfc: Hold the RTNL lock for more of the suspend/resume cycle
    Bugzilla: 953929
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    From: Ben Hutchings <bhutchings@solarflare.com>
    
    I don't think these PM functions can race with userland net device
    operations, but it's much easier to reason about locking if state is
    consistently guarded by the same lock.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    (cherry picked from commit 61da026d86517def727eb67d7cb8ee5657a12dd9)
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/sfc/efx.c
+++ drivers/net/sfc/efx.c
@@ -2737,6 +2737,8 @@ static int efx_pm_freeze(struct device *dev)
 {
 	struct efx_nic *efx = pci_get_drvdata(to_pci_dev(dev));
 
+	rtnl_lock();
+
 	efx->state = STATE_UNINIT;
 
 	netif_device_detach(efx->net_dev);
@@ -2744,12 +2746,16 @@ static int efx_pm_freeze(struct device *dev)
 	efx_stop_all(efx);
 	efx_stop_interrupts(efx, false);
 
+	rtnl_unlock();
+
 	return 0;
 }
 
 static int efx_pm_thaw(struct device *dev)
 {
 	struct efx_nic *efx = pci_get_drvdata(to_pci_dev(dev));
+
+	rtnl_lock();
 
 	efx_start_interrupts(efx, false);
 
@@ -2764,6 +2770,8 @@ static int efx_pm_thaw(struct device *dev)
 	efx->state = STATE_READY;
 
 	efx->type->resume_wol(efx);
+
+	rtnl_unlock();
 
 	/* Reschedule any quenched resets scheduled during efx_pm_freeze() */
 	queue_work(reset_workqueue, &efx->reset_work);
