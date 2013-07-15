88e8a7f5b0dc01d843fce553ab8f192098179071
Jes Sorensen <Jes.Sorensen@redhat.com>
2013-07-15 12:19:20 -0400

    change the parameter of md thread
    Patchwork-id: 61857
    O-Subject: [RHEL6.5 PATCH 028/105] MD: change the parameter of md thread
    Bugzilla: 837097 869622
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Jonathan E Brassow <jbrassow@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Shaohua Li <shli@kernel.org>
    
    Change the thread parameter, so the thread can carry extra info. Next patch
    will use it.
    
    Signed-off-by: Shaohua Li <shli@fusionio.com>
    Signed-off-by: NeilBrown <neilb@suse.de>
    (cherry picked from commit 4ed8731d8e6bd2a88a30697fbf4f7e6e979a6c46)
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/md/raid5.c
+++ drivers/md/raid5.c
@@ -4639,8 +4639,9 @@ static int handle_active_stripes(struct r5conf *conf)
  * During the scan, completed stripes are saved for us by the interrupt
  * handler, so that they will not have to wait for our next wakeup.
  */
-static void raid5d(struct mddev *mddev)
+static void raid5d(struct md_thread *thread)
 {
+	struct mddev *mddev = thread->mddev;
 	struct r5conf *conf = mddev->private;
 	int handled;
 
