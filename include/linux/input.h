a8e3657485e61929e050f29fffba746e2f647c8c
Jaroslav Kysela <jkysela@redhat.com>
2013-07-15 14:40:27 -0400

    jack: Add "Line In" input jack constants
    Patchwork-id: 62508
    O-Subject: [RHEL 6.5 PATCH 545/556] ALSA/HDA/core: jack - Add "Line In" input jack constants
    Bugzilla: 916264
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Similar to Line Out, these constants form the base for future
    patches enabling input jack reporting for Line in jacks.
    
    Signed-off-by: David Henningsson <david.henningsson@canonical.com>
    Acked-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    
    (picked from kernel: 7c2f8e4009d4b66c8458e3a5c20510b4262853bb)
    rhbz#916264

--- include/linux/input.h
+++ include/linux/input.h
@@ -693,6 +693,7 @@ struct input_absinfo {
 #define SW_LINEOUT_INSERT	0x06  /* set = inserted */
 #define SW_JACK_PHYSICAL_INSERT 0x07  /* set = mechanical switch set */
 #define SW_VIDEOOUT_INSERT	0x08  /* set = inserted */
+#define SW_LINEIN_INSERT	0x0d  /* set = inserted */
 #define SW_MAX			0x0f
 #define SW_CNT			(SW_MAX+1)
 
