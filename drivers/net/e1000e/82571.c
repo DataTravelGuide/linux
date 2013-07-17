86a1e7f33bb863921443b864f16033e061c98101
Dean Nelson <dnelson@redhat.com>
2013-07-17 13:22:39 -0300

    e1000e: add support for hardware timestamping on some devices
    Patchwork-id: 62883
    O-Subject: [RHEL6.5 PATCH 22/90] e1000e: add support for hardware timestamping on some devices
    Bugzilla: 923897
    RH-Acked-by: John Green <jogreene@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Nikolay Aleksandrov <nikolay@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    Resolves BZ:
    https://bugzilla.redhat.com/show_bug.cgi?id=923897
    https://bugzilla.redhat.com/show_bug.cgi?id=914832
    
    Upstream status:
    This is a backport of the following from the linux tree...
    
    commit b67e191307a3f330525265af3e2877a74d557cbf
    Author: Bruce Allan <bruce.w.allan@intel.com>
    Date:   Thu Dec 27 08:32:33 2012 +0000
    
        e1000e: add support for hardware timestamping on some devices
    
        On 82574, 82583, 82579, I217 and I218 add support for hardware time
        stamping of all or no Rx packets and Tx packets which have the
        SKBTX_HW_TSTAMP flag set.  Update the .get_ts_info ethtool operation to
        report the supported time stamping modes, and enable and disable hardware
        time stamping with the SIOCSHWTSTAMP ioctl.
    
        Signed-off-by: Bruce Allan <bruce.w.allan@intel.com>
        Tested-by: Jeff Pieper <jeffrey.e.pieper@intel.com>
        Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    
    The differences between the following patch and the upstream commit are due
    to RHEL6 kABI, and the need to define struct ethtool_ops's get_ts_info in
    struct ethtool_ops_ext. And due to upstream's 2244d07bfa2097cb0060, which
    defined SKBTX_HW_TSTAMP and SKBTX_IN_PROGRESS, but wasn't backported.

--- drivers/net/e1000e/82571.c
+++ drivers/net/e1000e/82571.c
@@ -2047,6 +2047,7 @@ const struct e1000_info e1000_82574_info = {
 				  | FLAG_HAS_MSIX
 				  | FLAG_HAS_JUMBO_FRAMES
 				  | FLAG_HAS_WOL
+				  | FLAG_HAS_HW_TIMESTAMP
 				  | FLAG_APME_IN_CTRL3
 				  | FLAG_RX_CSUM_ENABLED
 				  | FLAG_HAS_SMART_POWER_DOWN
@@ -2069,6 +2070,7 @@ const struct e1000_info e1000_82583_info = {
 	.mac			= e1000_82583,
 	.flags			= FLAG_HAS_HW_VLAN_FILTER
 				  | FLAG_HAS_WOL
+				  | FLAG_HAS_HW_TIMESTAMP
 				  | FLAG_APME_IN_CTRL3
 				  | FLAG_RX_CSUM_ENABLED
 				  | FLAG_HAS_SMART_POWER_DOWN
