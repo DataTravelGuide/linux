061ab8dabe949ef0cda08bd76abfb9cfd68c3517
Jesper Brouer <brouer@redhat.com>
2013-07-18 16:41:20 -0400

    ipvs: fix ip_vs_try_bind_dest to rebind app and transmitter
    Patchwork-id: 63122
    O-Subject: [RHEL6.5 net PATCH 37/42] ipvs: fix ip_vs_try_bind_dest to rebind app and transmitter
    Bugzilla: 854454
    RH-Acked-by: Flavio Leitner <fbl@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Daniel Borkmann <dborkman@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Thomas Graf <tgraf@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: 854454
    
    Backport conflicted with two commits:
      commit fc723250 (IPVS: netns to services part 1)
      commit 6e67e586 (IPVS: netns, connection hash got net as param.)
      Which were simply ip_vs_find_dest() that does not take netns as argument
    
    Backport of upstream commit: 882a844bd5b
    
     commit 882a844bd5b3ffa35e059f21ee920cc113985a89
     Author: Julian Anastasov <ja@ssi.bg>
     Date:   Tue Apr 24 23:46:37 2012 +0300
    
        ipvs: fix ip_vs_try_bind_dest to rebind app and transmitter
    
        	Initially, when the synced connection is created we
        use the forwarding method provided by master but once we
        bind to destination it can be changed. As result, we must
        update the application and the transmitter.
    
        	As ip_vs_try_bind_dest is called always for connections
        that require dest binding, there is no need to validate the
        cp and dest pointers.
    
        Signed-off-by: Julian Anastasov <ja@ssi.bg>
        Signed-off-by: Simon Horman <horms@verge.net.au>
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/netfilter/ipvs/ip_vs_conn.c
+++ net/netfilter/ipvs/ip_vs_conn.c
@@ -609,14 +609,40 @@ struct ip_vs_dest *ip_vs_try_bind_dest(struct ip_vs_conn *cp)
 {
 	struct ip_vs_dest *dest;
 
-	if ((cp) && (!cp->dest)) {
-		dest = ip_vs_find_dest(cp->af, &cp->daddr, cp->dport,
-				       &cp->vaddr, cp->vport,
-				       cp->protocol, cp->fwmark, cp->flags);
+	dest = ip_vs_find_dest(cp->af, &cp->daddr,
+			       cp->dport, &cp->vaddr, cp->vport,
+			       cp->protocol, cp->fwmark, cp->flags);
+	if (dest) {
+		struct ip_vs_protocol *pp;
+
+		/* Applications work depending on the forwarding method
+		 * but better to reassign them always when binding dest */
+		if (cp->app)
+			ip_vs_unbind_app(cp);
+
 		ip_vs_bind_dest(cp, dest);
-		return dest;
-	} else
-		return NULL;
+
+		/* Update its packet transmitter */
+		cp->packet_xmit = NULL;
+#ifdef CONFIG_IP_VS_IPV6
+		if (cp->af == AF_INET6)
+			ip_vs_bind_xmit_v6(cp);
+		else
+#endif
+			ip_vs_bind_xmit(cp);
+
+		/* netns version
+		pd = ip_vs_proto_data_get(ip_vs_conn_net(cp), cp->protocol);
+		if (pd && atomic_read(&pd->appcnt))
+			ip_vs_bind_app(cp, pd->pp);
+		*/
+		/* RH note: differs from upstream commit 882a844bd5b */
+		pp = ip_vs_proto_get(cp->protocol);
+		if (pp && atomic_read(&pp->appcnt))
+			ip_vs_bind_app(cp, pp);
+
+	}
+	return dest;
 }
 
 
