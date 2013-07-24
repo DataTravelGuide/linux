9d39a192df4db2f4483b3455bc4e4e031839a398
Amir Vadai <avadai@redhat.com>
2013-07-24 18:28:47 -0400

    mlx4_en: Assigning TX irq per ring
    Patchwork-id: 64076
    O-Subject: [RHEL 6.5 PATCH 038/129] mlx4_en: Assigning TX irq per ring
    Bugzilla: 950401 950910 950913 950915
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Doug Ledford <dledford@redhat.com>
    
    From: Alexander Guller <alexg@mellanox.com>
    
    Until now only RX rings used irq per ring
    and TX used only one per port.
    >From now on, both of them will use the
    irq per ring while RX & TX ring[i] will
    use the same irq.
    
    Signed-off-by: Alexander Guller <alexg@mellanox.co.il>
    Signed-off-by: Sharon Cohen <sharonc@mellanox.co.il>
    Signed-off-by: Yevgeny Petrilin <yevgenyp@mellanox.co.il>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 76532d0c7e7424914ab6f24683c63e50f0a08f1c)
    
    Signed-off-by: Amir Vadai <avadai@redhat.com>
    
    Conflicts:
    	drivers/net/ethernet/mellanox/mlx4/en_netdev.c
    	drivers/net/ethernet/mellanox/mlx4/mlx4_en.h

--- drivers/net/mlx4/mlx4_en.h
+++ drivers/net/mlx4/mlx4_en.h
@@ -520,7 +520,6 @@ struct mlx4_en_priv {
 	u16 log_rx_info;
 
 	struct mlx4_en_tx_ring *tx_ring;
-	int tx_vector;
 	struct mlx4_en_rx_ring rx_ring[MAX_RX_RINGS];
 	struct mlx4_en_cq *tx_cq;
 	struct mlx4_en_cq rx_cq[MAX_RX_RINGS];
@@ -568,7 +567,8 @@ int mlx4_en_alloc_resources(struct mlx4_en_priv *priv);
 int mlx4_en_create_cq(struct mlx4_en_priv *priv, struct mlx4_en_cq *cq,
 		      int entries, int ring, enum cq_type mode);
 void mlx4_en_destroy_cq(struct mlx4_en_priv *priv, struct mlx4_en_cq *cq);
-int mlx4_en_activate_cq(struct mlx4_en_priv *priv, struct mlx4_en_cq *cq);
+int mlx4_en_activate_cq(struct mlx4_en_priv *priv, struct mlx4_en_cq *cq,
+			int cq_idx);
 void mlx4_en_deactivate_cq(struct mlx4_en_priv *priv, struct mlx4_en_cq *cq);
 int mlx4_en_set_cq_moder(struct mlx4_en_priv *priv, struct mlx4_en_cq *cq);
 int mlx4_en_arm_cq(struct mlx4_en_priv *priv, struct mlx4_en_cq *cq);
