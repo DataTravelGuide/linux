1c2453b9b5840c4602df0d0b1d4344aead190441
Amir Vadai <avadai@redhat.com>
2013-07-24 18:29:24 -0400

    mlx4_en: Add a service task
    Patchwork-id: 64120
    O-Subject: [RHEL 6.5 PATCH 075/129] net/mlx4_en: Add a service task
    Bugzilla: 950401 950910 950913 950915
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Doug Ledford <dledford@redhat.com>
    
    From: Amir Vadai <amirv@mellanox.com>
    
    Add a service task to run tasks that needed to be executed periodically.
    Currently the only task is a watchdog to catch NIC clock overflow, to make
    timestamping accurate.
    Will move the statistics task into this framework in a later patch.
    
    Signed-off-by: Amir Vadai <amirv@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit b6c39bfcf1d7d6368b8c00081cc8e941041ff478)
    
    Signed-off-by: Amir Vadai <avadai@redhat.com>

--- drivers/net/mlx4/mlx4_en.h
+++ drivers/net/mlx4/mlx4_en.h
@@ -110,6 +110,7 @@
 #define STAMP_SHIFT		31
 #define STAMP_VAL		0x7fffffff
 #define STATS_DELAY		(HZ / 4)
+#define SERVICE_TASK_DELAY	(HZ / 4)
 #define MAX_NUM_OF_FS_RULES	256
 
 /* Typical TSO descriptor with 16 gather entries is 352 bytes... */
@@ -383,6 +384,7 @@ struct mlx4_en_dev {
 	struct cyclecounter	cycles;
 	struct timecounter	clock;
 	unsigned long		last_overflow_check;
+	unsigned long		overflow_period;
 };
 
 
@@ -547,6 +549,7 @@ struct mlx4_en_priv {
 	struct work_struct watchdog_task;
 	struct work_struct linkstate_task;
 	struct delayed_work stats_task;
+	struct delayed_work service_task;
 	struct mlx4_en_perf_stats pstats;
 	struct mlx4_en_pkt_stats pkstats;
 	struct mlx4_en_port_stats port_stats;
@@ -659,6 +662,7 @@ int mlx4_en_setup_tc(struct net_device *dev, u8 up);
 #define MLX4_EN_NUM_SELF_TEST	5
 void mlx4_en_ex_selftest(struct net_device *dev, u32 *flags, u64 *buf);
 u64 mlx4_en_mac_to_u64(u8 *addr);
+void mlx4_en_ptp_overflow_check(struct mlx4_en_dev *mdev);
 
 /*
  * Functions for time stamping
