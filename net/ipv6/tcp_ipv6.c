33393689d282dbbe66bdf24c2ddfb1e7c541a43b
Jiri Benc <jbenc@redhat.com>
2013-09-05 16:26:09 -0400

    ipv6: Use ipv6_get_dsfield() instead of ipv6_tclass()
    Patchwork-id: 66293
    O-Subject: [RHEL6.5 net PATCH 1/2] ipv6: Use ipv6_get_dsfield() instead of ipv6_tclass()
    Bugzilla: 1004164
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Thomas Graf <tgraf@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    RH-Acked-by: Amerigo Wang <amwang@redhat.com>
    RH-Acked-by: Daniel Borkmann <dborkman@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1004164
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=6248221
    Testing: verified IPV6_RECVTCLASS still works
    
    commit e7219858ac1f98213a4714d0e24e7a003e1bf6a2
    Author: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
    Date:   Sun Jan 13 05:02:01 2013 +0000
    
        ipv6: Use ipv6_get_dsfield() instead of ipv6_tclass().
    
        Commit 7a3198a8 ("ipv6: helper function to get tclass") introduced
        ipv6_tclass(), but similar function is already available as
        ipv6_get_dsfield().
    
        We might be able to call ipv6_tclass() from ipv6_get_dsfield(),
        but it is confusing to have two versions.
    
        Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -1255,7 +1255,7 @@ static struct sock * tcp_v6_syn_recv_sock(struct sock *sk, struct sk_buff *skb,
 		newnp->opt	   = NULL;
 		newnp->mcast_oif   = inet6_iif(skb);
 		newnp->mcast_hops  = ipv6_hdr(skb)->hop_limit;
-		sk_extended(newsk)->rcv_tos = ipv6_tclass(ipv6_hdr(skb));
+		sk_extended(newsk)->rcv_tos = ipv6_get_dsfield(ipv6_hdr(skb));
 
 		/*
 		 * No need to charge this sock to the relevant IPv6 refcnt debug socks count
@@ -1356,7 +1356,7 @@ static struct sock * tcp_v6_syn_recv_sock(struct sock *sk, struct sk_buff *skb,
 	newnp->opt	  = NULL;
 	newnp->mcast_oif  = inet6_iif(skb);
 	newnp->mcast_hops = ipv6_hdr(skb)->hop_limit;
-	sk_extended(newsk)->rcv_tos = ipv6_tclass(ipv6_hdr(skb));
+	sk_extended(newsk)->rcv_tos = ipv6_get_dsfield(ipv6_hdr(skb));
 
 	/* Clone native IPv6 options from listening socket (if any)
 
@@ -1565,7 +1565,7 @@ ipv6_pktoptions:
 		if (np->rxopt.bits.rxhlim || np->rxopt.bits.rxohlim)
 			np->mcast_hops = ipv6_hdr(opt_skb)->hop_limit;
 		if (np->rxopt.bits.rxtclass)
-			sk_extended(sk)->rcv_tos = ipv6_tclass(ipv6_hdr(skb));
+			sk_extended(sk)->rcv_tos = ipv6_get_dsfield(ipv6_hdr(skb));
 		if (ipv6_opt_accepted(sk, opt_skb)) {
 			skb_set_owner_r(opt_skb, sk);
 			opt_skb = xchg(&np->pktoptions, opt_skb);
