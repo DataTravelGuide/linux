618269298243bae42785adae833f17159ce7fcf1
Thomas Graf <tgraf@redhat.com>
2013-09-09 07:29:18 -0400

    proc/ns: Fix ABI of proc_inode
    Patchwork-id: 66436
    O-Subject: [PATCH RHEL6.5 net] netns/proc: Fix ABI of proc_inode
    Bugzilla: 1005224
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    RH-Acked-by: Amerigo Wang <amwang@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1005224
    Upstream Status: RHEL regression, not upstream
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=6251988
    Tested: Verified by loading and running the drbd kmod from elrepo.org
    
    Although not on the whitelist directly, modules commonly use the
    PDE() inline function to access the procfs directory of an inode.
    
    PDE() was broken by the following commit due to usage of
    container_of() and changed offsets inside struct proc_inode.
    
      commit 99262d7adef3628ef34548e83fde60b32c0c9c80
      Author: Amerigo Wang <amwang@redhat.com>
      Date:   Sat Dec 29 06:43:55 2012 -0500
    
        [fs] proc/ns: proc files for namespace naming policy
    
        Message-id: <1356763479-13667-2-git-send-email-amwang@redhat.com>
        Patchwork-id: 56036
        O-Subject: [RHEL6.5 PATCH 01/45] ns: proc files for namespace naming policy.
        Bugzilla: 880142
        RH-Acked-by: Daniel Borkmann <dborkman@redhat.com>
        RH-Acked-by: Thomas Graf <tgraf@redhat.com>
        RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
        RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
        [...]
    
    Signed-off-by: Thomas Graf <tgraf@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/proc_fs.h
+++ include/linux/proc_fs.h
@@ -310,9 +310,9 @@ struct proc_inode {
 	struct proc_dir_entry *pde;
 	struct ctl_table_header *sysctl;
 	struct ctl_table *sysctl_entry;
+	struct inode vfs_inode;
 	void *ns;
 	const struct proc_ns_operations *ns_ops;
-	struct inode vfs_inode;
 };
 
 static inline struct proc_inode *PROC_I(const struct inode *inode)
