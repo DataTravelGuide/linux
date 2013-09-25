09f8f96b695732226068497760623f12d52b025e
Michal Schmidt <mschmidt@redhat.com>
2013-09-25 17:48:19 -0400

    disable the new NAPI weight error message for RHEL 6.5
    Patchwork-id: 67826
    O-Subject: [RHEL6.5 PATCH] net: disable the new NAPI weight error message for RHEL 6.5
    Bugzilla: 1012090
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Flavio Leitner <fbl@redhat.com>
    RH-Acked-by: Vlad Yasevich <vyasevic@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1012090
    
    Several network drivers in RHEL 6.5 trigger the newly added error message in
    netif_napi_add():
      "netif_napi_add() called with weight %d on device %s\n"
    
    The error message was added by:
    
      commit 1357e24301a7ef915cd616436b1c94ceb49ecf23
      Author: Michal Schmidt <mschmidt@redhat.com>
      Date:   Tue Jul 23 13:48:01 2013 -0400
    
          [net] introduce NAPI_POLL_WEIGHT
    
    The error is printed when the driver requests a NAPI weight greater than 64.
    Affected drivers should be fixed eventually. And in upstream most (but not all)
    of them have been fixed.
    
    By code review I found these drivers would trigger the error in RHEL 6.5:
    In drivers/net: jme, netxen, ps3_gelic, qlcnic, qeth, virtio_net
    In drivers/infiniband: iw_nes, ib_ipoib
    
    I pushed fixes to upstream for most of them. A couple of others already had
    been fixed by upstream. I've had no response yet to my patches for the
    infiniband ones.
    
    So given that:
     - not all the driver fixes are upstream yet, and
     - it's hard to find available hardware for testing some of the affected
       drivers,
    it's not realistic to fix all the drivers we ship in RHEL 6.5. We should just
    drop the error message for RHEL 6.5 in order to avoid increased support load
    from customers worried about the new error message. After all, the error is
    mostly harmless and has always been there.
    
    Brew:
    https://brewweb.devel.redhat.com/taskinfo?taskID=6322303
    
    Testing:
    I checked with virtio_net to confirm the error message is now gone.
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -3947,9 +3947,11 @@ void netif_napi_add(struct net_device *dev, struct napi_struct *napi,
 	napi->gro_list = NULL;
 	napi->skb = NULL;
 	napi->poll = poll;
+#if 0 /* Do not complain until all drivers shipped in RHEL are fixed. */
 	if (weight > NAPI_POLL_WEIGHT)
 		pr_err_once("netif_napi_add() called with weight %d on device %s\n",
 			    weight, dev->name);
+#endif
 	napi->weight = weight;
 	list_add(&napi->dev_list, &dev->napi_list);
 	napi->dev = dev;
