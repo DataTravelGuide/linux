06d96f1813ab4e2d9bbaccfb6a27d94e15a73627
Ivan Vecera <ivecera@redhat.com>
2013-09-30 16:03:56 -0400

    cleanups in RX queue allocation
    Patchwork-id: 67895
    O-Subject: [RHEL6.5 PATCH 3/3] net: cleanups in RX queue allocation
    Bugzilla: 1012388
    RH-Acked-by: Thomas Graf <tgraf@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Vlad Yasevich <vyasevic@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    BZ: #1012388
    
    Backport note:
    Only the 1st part of the upstream commit as the rest was already applied
    by Neil.
    
    Upstream commit:
    commit bd25fa7ba59cd26094319dfba0011b48465f7355
    Author: Tom Herbert <therbert@google.com>
    Date:   Mon Oct 18 18:00:16 2010 +0000
    
        net: cleanups in RX queue allocation
    
        Clean up in RX queue allocation.  In netif_set_real_num_rx_queues
        return error on attempt to set zero queues, or requested number is
        greater than number of allocated queues.  In netif_alloc_rx_queues,
        do BUG_ON if queue_count is zero.
    
        Signed-off-by: Tom Herbert <therbert@google.com>
        Acked-by: Eric Dumazet <eric.dumazet@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -1785,11 +1785,11 @@ int netif_set_real_num_rx_queues(struct net_device *dev, unsigned int rxq)
 {
 	int rc;
 
+	if (rxq < 1 || rxq > netdev_extended(dev)->rps_data.num_rx_queues)
+		return -EINVAL;
+
 	if (dev->reg_state == NETREG_REGISTERED) {
 		ASSERT_RTNL();
-
-		if (rxq > netdev_extended(dev)->rps_data.num_rx_queues)
-			return -EINVAL;
 
 		rc = net_rx_queue_update_kobjects(dev,
 				netdev_extended(dev)->real_num_rx_queues,
