fe4f086f48f508235473a11c23e3581f2b094ca9
Aristeu Rozanski <aris@redhat.com>
2013-10-29 14:45:39 -0400

    tty: ldisc, wait for ldisc infinitely in hangup
    Patchwork-id: 69920
    O-Subject: [RHEL6 PATCH 5/5] tty: ldisc, wait for ldisc infinitely in hangup
    Bugzilla: 983146
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    https://bugzilla.redhat.com/show_bug.cgi?id=983146
    brew: https://brewweb.devel.redhat.com/taskinfo?taskID=6333343
    tested: by me with reproducer for an hour and by customer for 15 days
    upstream: 0c73c08ec73dbe080b9ec56696ee21d32754d918
    
    60af22d2ed490554cc92c8d0fed0b5b9cf687568 omitted on purpose, we don't have
    tty_lock() in RHEL6
    
    	commit 0c73c08ec73dbe080b9ec56696ee21d32754d918
    	Author: Jiri Slaby <jslaby@suse.cz>
    	Date:   Wed Nov 16 16:27:09 2011 +0100
    
    	    TTY: ldisc, wait for ldisc infinitely in hangup
    
    	    For /dev/console case, we do not kill all ldisc users. It's due to
    	    redirected_tty_write test in __tty_hangup. In that case there still
    	    might be a process waiting e.g. in n_tty_read for input.
    
    	    We wait for such processes to disappear. The problem is that we use a
    	    timeout. After this timeout, we continue closing the ldisc and start
    	    freeing tty resources. It obviously leads to crashes when the other
    	    process is woken.
    
    	    So to fix this, we wait infinitely before reiniting the ldisc. (The
    	    tiocsetd remains untouched -- times out after 5s.)
    
    	    This is nicely reproducible with this run from shell:
    	      exec 0<>/dev/console 1<>/dev/console 2<>/dev/console
    	    and stopping a getty like:
    	      systemctl stop serial-getty@ttyS0.service
    
    	    The crash proper may be produced only under load or with constified
    	    timing the same as for 92f6fa09b.
    
    	    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    	    Cc: Dave Young <hidave.darkstar@gmail.com>
    	    Cc: Dave Jones <davej@redhat.com>
    	    Cc: Ben Hutchings <ben@decadent.org.uk>
    	    Cc: Dmitriy Matrosov <sgf.dma@gmail.com>
    	    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    	    Cc: stable <stable@vger.kernel.org>
    	    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/char/tty_ldisc.c
+++ drivers/char/tty_ldisc.c
@@ -36,6 +36,7 @@
 
 #include <linux/kmod.h>
 #include <linux/nsproxy.h>
+#include <linux/ratelimit.h>
 
 /*
  *	This guards the refcounted line discipline lists. The lock
@@ -795,12 +796,29 @@ void tty_ldisc_hangup(struct tty_struct *tty)
 	 */
 	mutex_lock(&tty->ldisc_mutex);
 	tty_ldisc_halt(tty);
+	mutex_unlock(&tty->ldisc_mutex);
+
+retry:
+	mutex_lock(&tty->ldisc_mutex);
 	/* At this point we have a closed ldisc and we want to
 	   reopen it. We could defer this to the next open but
 	   it means auditing a lot of other paths so this is
 	   a FIXME */
 	if (tty->ldisc) {	/* Not yet closed */
-		WARN_ON_ONCE(tty_ldisc_wait_idle(tty, 5 * HZ));
+		if (atomic_read(&tty->ldisc->users) != 1) {
+			char cur_n[TASK_COMM_LEN], tty_n[64];
+			long timeout = 3 * HZ;
+
+			while (tty_ldisc_wait_idle(tty, timeout) == -EBUSY) {
+				timeout = MAX_SCHEDULE_TIMEOUT;
+				printk_ratelimited(KERN_WARNING
+					"%s: waiting (%s) for %s took too long, but we keep waiting...\n",
+					__func__, get_task_comm(cur_n, current),
+					tty_name(tty, tty_n));
+			}
+			mutex_unlock(&tty->ldisc_mutex);
+			goto retry;
+		}
 
 		if (reset == 0) {
 
