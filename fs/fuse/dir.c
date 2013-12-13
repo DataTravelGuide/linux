61baddcc87668b7278ce4c7db1fda2c29dc9407e
Brian Foster <bfoster@redhat.com>
2013-12-13 13:11:09 -0500

    fuse: invalidate inode attributes on xattr modification
    Patchwork-id: 72004
    O-Subject: [RHEL6.6 PATCH 21/26] fuse: invalidate inode attributes on xattr modification
    Bugzilla: 1035422
    RH-Acked-by: Zach Brown <zab@redhat.com>
    
    commit d331a415aef98717393dda0be69b7947da08eba3
    Author: Anand Avati <avati@redhat.com>
    Date:   Tue Aug 20 02:21:07 2013 -0400
    
        fuse: invalidate inode attributes on xattr modification
    
        Calls like setxattr and removexattr result in updation of ctime.
        Therefore invalidate inode attributes to force a refresh.
    
        Signed-off-by: Anand Avati <avati@redhat.com>
        Reviewed-by: Brian Foster <bfoster@redhat.com>
        Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
        Cc: stable@vger.kernel.org
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -1627,6 +1627,8 @@ static int fuse_setxattr(struct dentry *entry, const char *name,
 		fc->no_setxattr = 1;
 		err = -EOPNOTSUPP;
 	}
+	if (!err)
+		fuse_invalidate_attr(inode);
 	return err;
 }
 
@@ -1756,6 +1758,8 @@ static int fuse_removexattr(struct dentry *entry, const char *name)
 		fc->no_removexattr = 1;
 		err = -EOPNOTSUPP;
 	}
+	if (!err)
+		fuse_invalidate_attr(inode);
 	return err;
 }
 
