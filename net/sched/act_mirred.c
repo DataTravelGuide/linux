2457889fcfe9b8bfd0b79c536e6f8b2686e2714a
WANG Cong <xiyou.wangcong@gmail.com>
2014-01-17 11:37:02 -0800

    net_sched: act: fetch hinfo from a->ops->hinfo
    Every action ops has a pointer to hash info, so we don't need to
    hard-code it in each module.
    
    Cc: Jamal Hadi Salim <jhs@mojatatu.com>
    Cc: David S. Miller <davem@davemloft.net>
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
    Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Conflict patch:
    0001-net_sched-act-fetch-hinfo-from-a-ops-hinfo.patch
    
    Merge log:
    patching file include/net/act_api.h
    patching file net/sched/act_api.c
    Hunk #3 merged at 152,155,158.
    Hunk #4 merged at 210,214.
    patching file net/sched/act_csum.c
    patching file net/sched/act_gact.c
    patching file net/sched/act_ipt.c
    patching file net/sched/act_mirred.c
    Hunk #2 NOT MERGED at 142-147.
    patching file net/sched/act_nat.c
    patching file net/sched/act_pedit.c
    patching file net/sched/act_police.c
    Hunk #5 merged at 259,262-264.
    patching file net/sched/act_simple.c
    patching file net/sched/act_skbedit.c
    (EScore specific version string)

--- net/sched/act_mirred.c
+++ net/sched/act_mirred.c
@@ -105,12 +105,11 @@ static int tcf_mirred_init(struct net *net, struct nlattr *nla,
 		dev = NULL;
 	}
 
-	pc = tcf_hash_check(parm->index, a, bind, &mirred_hash_info);
+	pc = tcf_hash_check(parm->index, a, bind);
 	if (!pc) {
 		if (dev == NULL)
 			return -EINVAL;
-		pc = tcf_hash_create(parm->index, est, a, sizeof(*m), bind,
-				     &mirred_hash_info);
+		pc = tcf_hash_create(parm->index, est, a, sizeof(*m), bind);
 		if (IS_ERR(pc))
 			return PTR_ERR(pc);
 		ret = ACT_P_CREATED;
@@ -137,8 +136,7 @@ static int tcf_mirred_init(struct net *net, struct nlattr *nla,
 	if (ret == ACT_P_CREATED) {
 		spin_lock_bh(&mirred_list_lock);
 		list_add(&m->tcfm_list, &mirred_list);
-		spin_unlock_bh(&mirred_list_lock);
-		tcf_hash_insert(pc, &mirred_hash_info);
+		tcf_hash_insert(pc, a->ops->hinfo);
 	}
 
 	return ret;
