75a87213c9c2e1ebf54c07a088529ee9b71fccc0
Francesco Fusco <ffusco@redhat.com>
2014-01-23 10:34:25 -0500

    bridge: don't try to update timers in case of broken MLD queries
    Patchwork-id: 74797
    O-Subject: [RHEL6 PATCH net 02/10] bridge: don't try to update timers in case of broken MLD queries
    Bugzilla: 1020579
    RH-Acked-by: Nikolay Aleksandrov <nikolay@redhat.com>
    RH-Acked-by: Daniel Borkmann <dborkman@redhat.com>
    RH-Acked-by: Thomas Graf <tgraf@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1020579
    Upstream Status: 248ba8ec05a2c3b118c2224e57eb10c128176ab1
    Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=6906121
    Tested: verified on x86_64
    
    Upstream Commit:
    commit 248ba8ec05a2c3b118c2224e57eb10c128176ab1
    Author: Linus Lussing <linus.luessing@web.de>
    Date:   Tue Aug 6 00:32:05 2013 +0200
    
        bridge: don't try to update timers in case of broken MLD queries
    
        Currently we are reading an uninitialized value for the max_delay
        variable when snooping an MLD query message of invalid length and would
        update our timers with that.
    
        Fixing this by simply ignoring such broken MLD queries (just like we do
        for IGMP already).
    
        This is a regression introduced by:
        "bridge: disable snooping if there is no querier" (b00589af3b04)
    
        Reported-by: Paul Bolle <pebolle@tiscali.nl>
        Signed-off-by: Linus Lussing <linus.luessing@web.de>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Francesco Fusco <ffusco@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/bridge/br_multicast.c
+++ net/bridge/br_multicast.c
@@ -1225,7 +1225,7 @@ static int br_ip6_multicast_query(struct net_bridge *br,
 		max_delay = msecs_to_jiffies(htons(mld->mld_maxdelay));
 		if (max_delay)
 			group = &mld->mld_mca;
-	} else if (skb->len >= sizeof(*mld2q)) {
+	} else {
 		if (!pskb_may_pull(skb, sizeof(*mld2q))) {
 			err = -EINVAL;
 			goto out;
