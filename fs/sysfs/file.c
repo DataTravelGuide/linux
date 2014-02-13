8656226be9da18d17a0f8a4210886aa64a979c34
Daniel Borkmann <dborkman@redhat.com>
2014-02-13 09:11:59 -0500

    sysfs: don't panic when in different netns
    Patchwork-id: 76772
    O-Subject: [RHEL6 PATCH net] redhat: net: sysfs: don't panic when in different netns
    Bugzilla: 1034696 1020145
    RH-Acked-by: Thomas Graf <tgraf@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1034696
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1020145
    Upstream Status: none, RHEL-specific
    Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=7041550
    Tested: reproducer from BZ
    
    Due to the basic netns pull-in in RHEL6, netns is only half
    complete. Because of unfixable kABI constraints in sysfs and kobj
    code, it was at that time not choosen to pull those parts into
    the RHEL6 kernel. However, with e.g. an upstream iproute2 version,
    netns can be created. Setting up netns in OpenStack/Neutron
    context is a legitimate use case though.
    
    However, creating network devices that also create sysfs files
    for their netns at setup time such as tunnel, bridge or other
    devices will let the kernel panic in RHEL6. We went back and
    forth in order to evaluate how unfixable those constraints are
    and came to the conclusion that a huge pile of backports are
    needed in kobj, sysfs and cgroups area, plus it appears that
    kABI workarounds on very core data structures are impossible.
    We therefore suggest to just gracefully fail instead on these
    pain points and have no sysfs support instead.
    
    Although this hack sucks, we think that this RHEL6 specific
    workaround is ok, because affected functions are allowed to fail
    internally and will return an error code that is being handled
    on caller site, thus, failing gracefully, undoing a setup, or
    continuing without sysfs support in their netns.
    
    Handling of tunnel devices (which appear to work now) that would
    otherwise panic:
    
    [root@dell-pet410-03 iproute2]# ./ip/ip netns exec ns1 tunctl -p -t t1
    tun: Universal TUN/TAP device driver, 1.6
    tun: (C) 1999-2004 Max Krasnyansky <maxk@qualcomm.com>
    Failed to create tun sysfs files <-- code allows to continue w/o sysfs support
    Set 't1' persistent and owned by uid 0
    [root@dell-pet410-03 iproute2]# ./ip/ip netns exec ns1 ip a
    8: lo: <LOOPBACK> mtu 16436 qdisc noop state DOWN
        link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
    9: t1: <BROADCAST,MULTICAST> mtu 1500 qdisc noop state DOWN qlen 500
        link/ether b6:01:e8:bc:d5:59 brd ff:ff:ff:ff:ff:ff
    
    Handling of bridge devices with graceful return:
    
    [root@dell-pet410-03 iproute2]# ./ip/ip netns exec ns1 brctl addbr br0
    Bridge firewalling registered
    br_sysfs_addbr: can't create group br0/bridge
    br_sysfs_addbr: can't create group br0/bridge
    add bridge failed: Unknown error 524
    [root@dell-pet410-03 iproute2]# ./ip/ip netns exec ns1 ip a
    8: lo: <LOOPBACK> mtu 16436 qdisc noop state DOWN
        link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
    9: t1: <BROADCAST,MULTICAST> mtu 1500 qdisc noop state DOWN qlen 500
        link/ether b6:01:e8:bc:d5:59 brd ff:ff:ff:ff:ff:ff
    
    Signed-off-by: Daniel Borkmann <dborkman@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/sysfs/file.c
+++ fs/sysfs/file.c
@@ -536,7 +536,17 @@ int sysfs_add_file(struct sysfs_dirent *dir_sd, const struct attribute *attr,
 
 int sysfs_create_file(struct kobject * kobj, const struct attribute * attr)
 {
-	BUG_ON(!kobj || !kobj->sd || !attr);
+	BUG_ON(!kobj || !attr);
+
+	/* RHEL specific
+	 *
+	 * RHEL6 has half-complete netns support that got pulled in, in
+	 * order to just add basic support for netns. kobj changes cannot
+	 * be backported due to kABI and failing gracefully here is the
+	 * only option.
+	 */
+	if (!kobj->sd)
+		return -ENOTSUPP;
 
 	return sysfs_add_file(kobj->sd, attr, SYSFS_KOBJ_ATTR);
 
