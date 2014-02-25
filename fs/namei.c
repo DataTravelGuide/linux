1d83abda82274951d9d02739489e3cde808349d4
Ian Kent <ikent@redhat.com>
2014-02-25 12:17:14 -0500

    vfs: rename user_path_umountat() to user_path_mountpoint_at()
    Patchwork-id: 77446
    O-Subject: [RHEL-6.6 PATCH 3/5] vfs - rename user_path_umountat() to user_path_mountpoint_at()
    Bugzilla: 999708
    RH-Acked-by: Jeff Layton <jlayton@redhat.com>
    
    From: Ian Kent <raven@themaw.net>
    
    Bugzilla bug: https://bugzilla.redhat.com/show_bug.cgi?id=999708
    Based on upstream commit: 197df04c7
    
    ... and move the extern from linux/namei.h to fs/internal.h.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/namei.c
+++ fs/namei.c
@@ -1624,7 +1624,7 @@ user_path_parent(int dfd, const char __user *path, struct nameidata *nd,
 }
 
 /**
- * umount_lookup_last - look up last component for umount
+ * mountpoint_last - look up last component for umount
  * @nd:   pathwalk nameidata - currently pointing at parent directory of "last"
  * @path: pointer to container for result
  *
@@ -1651,7 +1651,7 @@ user_path_parent(int dfd, const char __user *path, struct nameidata *nd,
  *         to the link, and nd->path will *not* be put.
  */
 static int
-umount_lookup_last(struct nameidata *nd, struct path *path)
+mountpoint_last(struct nameidata *nd, struct path *path)
 {
 	int error = 0;
 	struct dentry *dentry;
@@ -1721,7 +1721,7 @@ out:
 }
 
 /**
- * path_umountat - look up a path to a mount point
+ * path_mountpoint - look up a path to a mount point
  * @dfd:	directory file descriptor to start walk from
  * @name:	full pathname to walk
  * @flags:	lookup flags
@@ -1731,7 +1731,7 @@ out:
  * Returns 0 and "path" will be valid on success; Retuns error otherwise.
  */
 static int
-path_umountat(int dfd, struct filename *s, struct path *path, unsigned int flags)
+path_mountpoint(int dfd, struct filename *s, struct path *path, unsigned int flags)
 {
 	struct nameidata nd;
 	int err;
@@ -1745,7 +1745,7 @@ path_umountat(int dfd, struct filename *s, struct path *path, unsigned int flags
 	if (err)
 		goto out;
 
-	err = umount_lookup_last(&nd, path);
+	err = mountpoint_last(&nd, path);
 	while (err > 0) {
 		struct path link = *path;
 		err = -ELOOP;
@@ -1756,7 +1756,7 @@ path_umountat(int dfd, struct filename *s, struct path *path, unsigned int flags
 		err = __do_follow_link(&link, &nd);
 		if (err)
 			break;
-		err = umount_lookup_last(&nd, path);
+		err = mountpoint_last(&nd, path);
 	}
 out:
 	if (nd.root.mnt)
@@ -1766,7 +1766,7 @@ out:
 }
 
 /**
- * user_path_umountat - lookup a path from userland in order to umount it
+ * user_path_mountpoint_at - lookup a path from userland in order to umount it
  * @dfd:	directory file descriptor
  * @name:	pathname from userland
  * @flags:	lookup flags
@@ -1780,7 +1780,7 @@ out:
  * Returns 0 and populates "path" on success.
  */
 int
-user_path_umountat(int dfd, const char __user *name, unsigned int flags,
+user_path_mountpoint_at(int dfd, const char __user *name, unsigned int flags,
 			struct path *path)
 {
 	struct filename *s = getname(name);
@@ -1789,9 +1789,9 @@ user_path_umountat(int dfd, const char __user *name, unsigned int flags,
 	if (IS_ERR(s))
 		return PTR_ERR(s);
 
-	error = path_umountat(dfd, s, path, flags);
+	error = path_mountpoint(dfd, s, path, flags);
 	if (unlikely(error == -ESTALE))
-		error = path_umountat(dfd, s, path, flags | LOOKUP_REVAL);
+		error = path_mountpoint(dfd, s, path, flags | LOOKUP_REVAL);
 
 	if (likely(!error))
 		audit_inode(s, path->dentry, 0);
