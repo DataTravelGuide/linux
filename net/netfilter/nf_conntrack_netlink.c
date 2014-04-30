6d6582eefaf7ac89724af14a6be8bae2ecb929bc
Florian Westphal <fwestpha@redhat.com>
2014-04-30 11:55:59 -0400

    netfilter: ctnetlink: fix soft lockup when netlink adds new entries
    Patchwork-id: 79258
    O-Subject: [PATCH 2/6 RHEL6.6 net] netfilter: ctnetlink: fix soft lockup when netlink adds new entries (v2)
    Bugzilla: 1089562
    RH-Acked-by: Daniel Borkmann <dborkman@redhat.com>
    RH-Acked-by: Nikolay Aleksandrov <nikolay@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    
    BZ: 1089562
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=7399359
    Tested: "conntrack --create" with random addresses in loop
    
    drop conntrack zones, not in rhel6.
    drop network namespace support.
    squash upstream fix too, else we unlock spinlock we did not acquire.
    
    commit 7d367e06688dc7a2cc98c2ace04e1296e1d987e2
    Author: Jozsef Kadlecsik <kadlec@blackhole.kfki.hu>
    Date:   Fri Feb 24 11:45:49 2012 +0100
    
        netfilter: ctnetlink: fix soft lockup when netlink adds new entries (v2)
    
        Marcell Zambo and Janos Farago noticed and reported that when
        new conntrack entries are added via netlink and the conntrack table
        gets full, soft lockup happens. This is because the nf_conntrack_lock
        is held while nf_conntrack_alloc is called, which is in turn wants
        to lock nf_conntrack_lock while evicting entries from the full table.
    
        The patch fixes the soft lockup with limiting the holding of the
        nf_conntrack_lock to the minimum, where it's absolutely required.
        It required to extend (and thus change) nf_conntrack_hash_insert
        so that it makes sure conntrack and ctnetlink do not add the same entry
        twice to the conntrack table.
    
        Signed-off-by: Jozsef Kadlecsik <kadlec@blackhole.kfki.hu>
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    commit 8be619d1e430fd87a02587a2a6830b692cb91b84
    Author: Pablo Neira Ayuso <pablo@netfilter.org>
    Date:   Tue Mar 6 01:22:51 2012 +0000
    
        netfilter: ctnetlink: remove incorrect spin_[un]lock_bh on NAT module autoload
    
        Since 7d367e0, ctnetlink_new_conntrack is called without holding
        the nf_conntrack_lock spinlock. Thus, ctnetlink_parse_nat_setup
        does not require to release that spinlock anymore in the NAT module
        autoload case.
    
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/netfilter/nf_conntrack_netlink.c
+++ net/netfilter/nf_conntrack_netlink.c
@@ -906,16 +906,13 @@ ctnetlink_parse_nat_setup(struct nf_conn *ct,
 	if (!parse_nat_setup) {
 #ifdef CONFIG_MODULES
 		rcu_read_unlock();
-		spin_unlock_bh(&nf_conntrack_lock);
 		nfnl_unlock();
 		if (request_module("nf-nat-ipv4") < 0) {
 			nfnl_lock();
-			spin_lock_bh(&nf_conntrack_lock);
 			rcu_read_lock();
 			return -EOPNOTSUPP;
 		}
 		nfnl_lock();
-		spin_lock_bh(&nf_conntrack_lock);
 		rcu_read_lock();
 		if (nfnetlink_parse_nat_setup_hook)
 			return -EAGAIN;
@@ -1306,8 +1303,10 @@ ctnetlink_create_conntrack(const struct nlattr * const cda[],
 		ct->master = master_ct;
 	}
 
-	add_timer(&ct->timeout);
-	nf_conntrack_hash_insert(ct);
+	err = nf_conntrack_hash_check_insert(ct);
+	if (err < 0)
+		goto err2;
+
 	rcu_read_unlock();
 
 	return ct;
@@ -1327,6 +1326,8 @@ ctnetlink_new_conntrack(struct sock *ctnl, struct sk_buff *skb,
 	struct nf_conntrack_tuple otuple, rtuple;
 	struct nf_conntrack_tuple_hash *h = NULL;
 	struct nfgenmsg *nfmsg = nlmsg_data(nlh);
+	struct net *net = &init_net;
+	struct nf_conn *ct;
 	u_int8_t u3 = nfmsg->nfgen_family;
 	int err = 0;
 
@@ -1342,32 +1343,25 @@ ctnetlink_new_conntrack(struct sock *ctnl, struct sk_buff *skb,
 			return err;
 	}
 
-	spin_lock_bh(&nf_conntrack_lock);
 	if (cda[CTA_TUPLE_ORIG])
-		h = __nf_conntrack_find(&init_net, &otuple);
+		h = nf_conntrack_find_get(net, &otuple);
 	else if (cda[CTA_TUPLE_REPLY])
-		h = __nf_conntrack_find(&init_net, &rtuple);
+		h = nf_conntrack_find_get(net, &rtuple);
 
 	if (h == NULL) {
 		err = -ENOENT;
 		if (nlh->nlmsg_flags & NLM_F_CREATE) {
-			struct nf_conn *ct;
 			enum ip_conntrack_events events;
 
-			if (!cda[CTA_TUPLE_ORIG] || !cda[CTA_TUPLE_REPLY]) {
-				err = -EINVAL;
-				goto out_unlock;
-			}
+			if (!cda[CTA_TUPLE_ORIG] || !cda[CTA_TUPLE_REPLY])
+				return -EINVAL;
 
 			ct = ctnetlink_create_conntrack(cda, &otuple,
 							&rtuple, u3);
-			if (IS_ERR(ct)) {
-				err = PTR_ERR(ct);
-				goto out_unlock;
-			}
+			if (IS_ERR(ct))
+				return PTR_ERR(ct);
+
 			err = 0;
-			nf_conntrack_get(&ct->ct_general);
-			spin_unlock_bh(&nf_conntrack_lock);
 			if (test_bit(IPS_EXPECTED_BIT, &ct->status))
 				events = IPCT_RELATED;
 			else
@@ -1381,23 +1375,19 @@ ctnetlink_new_conntrack(struct sock *ctnl, struct sk_buff *skb,
 						      ct, NETLINK_CB(skb).pid,
 						      nlmsg_report(nlh));
 			nf_ct_put(ct);
-		} else
-			spin_unlock_bh(&nf_conntrack_lock);
+		}
 
 		return err;
 	}
 	/* implicit 'else' */
 
-	/* We manipulate the conntrack inside the global conntrack table lock,
-	 * so there's no need to increase the refcount */
 	err = -EEXIST;
+	ct = nf_ct_tuplehash_to_ctrack(h);
 	if (!(nlh->nlmsg_flags & NLM_F_EXCL)) {
-		struct nf_conn *ct = nf_ct_tuplehash_to_ctrack(h);
-
+		spin_lock_bh(&nf_conntrack_lock);
 		err = ctnetlink_change_conntrack(ct, cda);
+		spin_unlock_bh(&nf_conntrack_lock);
 		if (err == 0) {
-			nf_conntrack_get(&ct->ct_general);
-			spin_unlock_bh(&nf_conntrack_lock);
 			nf_conntrack_eventmask_report((1 << IPCT_STATUS) |
 						      (1 << IPCT_HELPER) |
 						      (1 << IPCT_PROTOINFO) |
@@ -1405,15 +1395,10 @@ ctnetlink_new_conntrack(struct sock *ctnl, struct sk_buff *skb,
 						      (1 << IPCT_MARK),
 						      ct, NETLINK_CB(skb).pid,
 						      nlmsg_report(nlh));
-			nf_ct_put(ct);
-		} else
-			spin_unlock_bh(&nf_conntrack_lock);
-
-		return err;
+		}
 	}
 
-out_unlock:
-	spin_unlock_bh(&nf_conntrack_lock);
+	nf_ct_put(ct);
 	return err;
 }
 
