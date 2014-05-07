a697fd72fb729fe4dd62c8280e083ad9d56ee551
Steve Best <sbest@redhat.com>
2014-05-07 17:11:26 -0400

    mce: Remove bios_cmci_threshold sysfs attribute
    Patchwork-id: 79487
    O-Subject: [PATCH RHEL6.6 BZ929317 3/3 v2] x86, MCE: Remove bios_cmci_threshold sysfs attribute
    Bugzilla: 929317
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    
    RHBZ#:
    ------
    https://bugzilla.redhat.com/show_bug.cgi?id=929317
    
    Description:
    ------------
    450cc201038f3 ("x86/mce: Provide boot argument to honour bios-set CMCI
    threshold") added the bios_cmci_threshold sysfs attribute which was
    supposed to communicate to userspace tools that BIOS CMCI threshold has
    been honoured.
    
    However, this info is not of any importance to userspace - it should
    rather get the actual error count it has been thresholded already from
    MCi_STATUS[38:52].
    
    So drop this before it becomes a used interface (good thing we caught
    this early in 3.7-rc1, right after the merge window closed).
    
    Cc: Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
    Acked-by: Tony Luck <tony.luck@intel.com>
    Link: http://lkml.kernel.org/r/20121017105940.GA14590@x1.osrc.amd.com
    Signed-off-by: Borislav Petkov <borislav.petkov@amd.com>
    
    V2 - Fix the issue Dean pointed where RHEL6 uses _SYSDEV_ATTR.
    
    RHEL Version Found:
    -------------------
    6.6
    
    Brew:
    -----
    Built on all platforms. All patches brewed together.
    http://brewweb.devel.redhat.com/brew/taskinfo?taskID=7426708
    
    Upstream Status:
    ----------------
    http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=5bc66170dc486556a1e36fd384463536573f4b82
    
    ----------------
    Steve Best
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kernel/cpu/mcheck/mce.c
+++ arch/x86/kernel/cpu/mcheck/mce.c
@@ -2168,11 +2168,6 @@ static struct sysdev_ext_attribute attr_cmci_disabled = {
 	&mce_cmci_disabled
 };
 
-static struct sysdev_ext_attribute attr_bios_cmci_threshold = {
-	_SYSDEV_ATTR(bios_cmci_threshold, 0644, sysdev_show_int, NULL),
-	&mce_bios_cmci_threshold
-};
-
 static struct sysdev_attribute *mce_attrs[] = {
 	&attr_tolerant.attr,
 	&attr_check_interval.attr,
@@ -2181,7 +2176,6 @@ static struct sysdev_attribute *mce_attrs[] = {
 	&attr_dont_log_ce.attr,
 	&attr_ignore_ce.attr,
 	&attr_cmci_disabled.attr,
-	&attr_bios_cmci_threshold.attr,
 	NULL
 };
 
