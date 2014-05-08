7a531db2af74e44bf00e7ed1301fbd194f2d4fad
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2014-05-07 22:34:06 -0400

    estimate number of events per packet
    Patchwork-id: 79496
    O-Subject: [RHEL6.6 PATCH 06/58] Input: estimate number of events per packet
    Bugzilla: 1076662
    RH-Acked-by: Peter Hutterer <peter.hutterer@redhat.com>
    RH-Acked-by: Aristeu S. Rozanski F. <arozansk@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1076662
    Upstream: since v2.6.39
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=7429436
    Test: Verified myself using the hid multitouch database and the hid test suite:
          https://github.com/bentiss/hid-devices
          https://github.com/bentiss/hid-test
    
    commit 80b4895aa4578e9372d76cd4063f82d0c3994d77
    Author: Jeff Brown <jeffbrown@google.com>
    Date:   Mon Apr 18 10:08:02 2011 -0700
    
        Input: estimate number of events per packet
    
        Calculate a default based on the number of ABS axes, REL axes,
        and MT slots for the device during input device registration.
    
        Signed-off-by: Jeff Brown <jeffbrown@android.com>
        Reviewed-by: Henrik Rydberg <rydberg@euromail.se>
        Signed-off-by: Dmitry Torokhov <dtor@mail.ru>
    
    Conflicts:
    
    	drivers/input/input.c -> RHEL does not uses absinfo
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -1528,6 +1528,42 @@ void input_set_capability(struct input_dev *dev, unsigned int type, unsigned int
 }
 EXPORT_SYMBOL(input_set_capability);
 
+static unsigned int input_estimate_events_per_packet(struct input_dev *dev)
+{
+	int mt_slots;
+	int i;
+	unsigned int events;
+
+	if (dev->mtsize) {
+		mt_slots = dev->mtsize;
+	} else if (test_bit(ABS_MT_TRACKING_ID, dev->absbit)) {
+		mt_slots = dev->absmax[ABS_MT_TRACKING_ID] -
+			   dev->absmin[ABS_MT_TRACKING_ID] + 1,
+		clamp(mt_slots, 2, 32);
+	} else if (test_bit(ABS_MT_POSITION_X, dev->absbit)) {
+		mt_slots = 2;
+	} else {
+		mt_slots = 0;
+	}
+
+	events = mt_slots + 1; /* count SYN_MT_REPORT and SYN_REPORT */
+
+	for (i = 0; i < ABS_CNT; i++) {
+		if (test_bit(i, dev->absbit)) {
+			if (input_is_mt_axis(i))
+				events += mt_slots;
+			else
+				events++;
+		}
+	}
+
+	for (i = 0; i < REL_CNT; i++)
+		if (test_bit(i, dev->relbit))
+			events++;
+
+	return events;
+}
+
 /**
  * input_register_device - register device with input core
  * @dev: device to be registered
@@ -1548,6 +1584,10 @@ int input_register_device(struct input_dev *dev)
 	int error;
 
 	__set_bit(EV_SYN, dev->evbit);
+
+	if (!dev->hint_events_per_packet)
+		dev->hint_events_per_packet =
+				input_estimate_events_per_packet(dev);
 
 	/*
 	 * If delay and period are pre-set by the driver, then autorepeating
