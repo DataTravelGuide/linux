fe673ad4a88052fb34b3ac27db12e33dfde3ed63
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2014-05-07 22:34:08 -0400

    properly assign return value of clamp() macro
    Patchwork-id: 79497
    O-Subject: [RHEL6.6 PATCH 08/58] Input: properly assign return value of clamp() macro.
    Bugzilla: 1076662
    RH-Acked-by: Peter Hutterer <peter.hutterer@redhat.com>
    RH-Acked-by: Aristeu S. Rozanski F. <arozansk@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1076662
    Upstream: since v3.0
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=7429436
    Test: Verified myself using the hid multitouch database and the hid test suite:
          https://github.com/bentiss/hid-devices
          https://github.com/bentiss/hid-test
    
    commit 8c127f0717b438e6abc3d92d4ae248c4224b9dcb
    Author: Hans Petter Selasky <hselasky@c2i.net>
    Date:   Wed May 25 09:24:32 2011 -0700
    
        Input: properly assign return value of clamp() macro.
    
        [dtor@mail.ru: added mousedev changes]
        Signed-off-by: Hans Petter Selasky <hselasky@c2i.net>
        Cc: stable@kernel.org
        Signed-off-by: Dmitry Torokhov <dtor@mail.ru>
    
    We only need the input.c changes, not the mousedev
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -1539,7 +1539,7 @@ static unsigned int input_estimate_events_per_packet(struct input_dev *dev)
 	} else if (test_bit(ABS_MT_TRACKING_ID, dev->absbit)) {
 		mt_slots = dev->absmax[ABS_MT_TRACKING_ID] -
 			   dev->absmin[ABS_MT_TRACKING_ID] + 1,
-		clamp(mt_slots, 2, 32);
+		mt_slots = clamp(mt_slots, 2, 32);
 	} else if (test_bit(ABS_MT_POSITION_X, dev->absbit)) {
 		mt_slots = 2;
 	} else {
