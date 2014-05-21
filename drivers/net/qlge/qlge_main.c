153aaf892d866df8296445c9e5b6aca31a8c7c41
Chad Dupuis <cdupuis@redhat.com>
2014-05-21 13:08:16 -0400

    qlge: Enhance nested VLAN Q-in-Q handling
    Patchwork-id: 80018
    O-Subject: [RHEL 6.6 PATCH 04/07] qlge: Enhance nested VLAN (Q-in-Q) handling.
    Bugzilla: 1054333
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla
    --------
    
    Bug 1054333 (https://bugzilla.redhat.com/show_bug.cgi?id=1054333)
    
    Upstream Status
    ---------------
    
    net commit id a45adbe8d35247a5911c8ee05e4154089cda6872
    
    Description
    -----------
    
    From: Jitendra Kalsaria <jitendra.kalsaria@qlogic.com>
    
    Subject: [PATCH 4/7] qlge: Enhance nested VLAN (Q-in-Q) handling.
    
    o Adapter doesn't handle packets with nested VLAN tags in
      Rx path. User can turn off VLAN tag stripping in the hardware
      and let the stack handle stripping of VLAN tags in the Rx path.
    
    o Users can enable or disable hardware VLAN acceleration using ethtool
    
    Signed-off-by: Jitendra Kalsaria <jitendra.kalsaria@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Himanshu Madhani <himanshu.madhani@qlogic.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/qlge/qlge_main.c
+++ drivers/net/qlge/qlge_main.c
@@ -404,8 +404,6 @@ static int ql_set_mac_addr_reg(struct ql_adapter *qdev, u8 *addr, u32 type,
 				      (qdev->
 				       func << CAM_OUT_FUNC_SHIFT) |
 					(0 << CAM_OUT_CQ_ID_SHIFT));
-			if (qdev->vlgrp)
-				cam_output |= CAM_OUT_RV;
 			/* route to NIC core */
 			ql_write32(qdev, MAC_ADDR_DATA, cam_output);
 			break;
@@ -1463,18 +1461,53 @@ static void ql_categorize_rx_err(struct ql_adapter *qdev, u8 rx_err,
 	}
 }
 
+/* Check for vlan tag in inbound path and strip it from skb if present */
+static inline void ql_check_rx_vlan(struct ql_adapter *qdev,
+				    struct sk_buff *skb, u16 *vid)
+{
+	struct ethhdr *eth_hdr;
+
+	if (!qdev->vlgrp)
+		return;
+	/* Check outer vlan tag */
+	if (!__vlan_get_tag(skb, vid)) {
+		eth_hdr = (struct ethhdr *)(skb->data);
+		memmove(skb->data + VLAN_HLEN, eth_hdr, ETH_ALEN * 2);
+		skb_pull(skb, VLAN_HLEN);
+	}
+}
+
+/*
+ * This routine will update the mac header length based on
+ * single or nested vlan tags if present
+ */
+static void ql_update_mac_hdr_len(struct ql_adapter *qdev,
+				  struct ib_mac_iocb_rsp *ib_mac_rsp,
+				  void *page, size_t *len)
+{
+	u16 *tags;
+
+	if (ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_V) {
+		tags = (u16 *)page;
+		/* Look for stacked vlan tags in ethertype field */
+		if (tags[6] == ETH_P_8021Q && tags[8] == ETH_P_8021Q)
+			*len += 2 * VLAN_HLEN;
+		else
+			*len += VLAN_HLEN;
+	}
+}
+
 /* Process an inbound completion from an rx ring. */
 static void ql_process_mac_rx_gro_page(struct ql_adapter *qdev,
 					struct rx_ring *rx_ring,
 					struct ib_mac_iocb_rsp *ib_mac_rsp,
-					u32 length,
-					u16 vlan_id)
+					u32 length)
 {
-	struct sk_buff *skb;
 	struct bq_desc *lbq_desc = ql_get_curr_lchunk(qdev, rx_ring);
-	struct skb_frag_struct *rx_frag;
-	int nr_frags;
 	struct napi_struct *napi = &rx_ring->napi;
+	struct skb_frag_struct *rx_frag;
+	struct sk_buff *skb;
+	int nr_frags;
 
 	/* Frame error, so drop the packet. */
 	if (ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_ERR_MASK) {
@@ -1509,24 +1542,22 @@ static void ql_process_mac_rx_gro_page(struct ql_adapter *qdev,
 	rx_ring->rx_bytes += length;
 	skb->ip_summed = CHECKSUM_UNNECESSARY;
 	skb_record_rx_queue(skb, rx_ring->cq_id);
-	if (vlan_id != 0xffff)
-		vlan_gro_frags(&rx_ring->napi, qdev->vlgrp, vlan_id);
-	else
-		napi_gro_frags(napi);
+	napi_gro_frags(napi);
 }
 
 /* Process an inbound completion from an rx ring. */
 static void ql_process_mac_rx_page(struct ql_adapter *qdev,
 					struct rx_ring *rx_ring,
 					struct ib_mac_iocb_rsp *ib_mac_rsp,
-					u32 length,
-					u16 vlan_id)
+					u32 length)
 {
-	struct net_device *ndev = qdev->ndev;
-	struct sk_buff *skb = NULL;
-	void *addr;
 	struct bq_desc *lbq_desc = ql_get_curr_lchunk(qdev, rx_ring);
 	struct napi_struct *napi = &rx_ring->napi;
+	struct net_device *ndev = qdev->ndev;
+	struct sk_buff *skb = NULL;
+	size_t hlen = ETH_HLEN;
+	u16 vlan_id = 0xffff;
+	void *addr;
 
 	skb = netdev_alloc_skb(ndev, length);
 	if (!skb) {
@@ -1546,28 +1577,32 @@ static void ql_process_mac_rx_page(struct ql_adapter *qdev,
 		goto err_out;
 	}
 
+	/* Update the MAC header length */
+	ql_update_mac_hdr_len(qdev, ib_mac_rsp, addr, &hlen);
+
 	/* The max framesize filter on this chip is set higher than
 	 * MTU since FCoE uses 2k frames.
 	 */
-	if (skb->len > ndev->mtu + ETH_HLEN) {
+	if (skb->len > ndev->mtu + hlen) {
 		netif_err(qdev, drv, qdev->ndev,
 			  "Segment too small, dropping.\n");
 		rx_ring->rx_dropped++;
 		goto err_out;
 	}
-	memcpy(skb_put(skb, ETH_HLEN), addr, ETH_HLEN);
+	memcpy(skb_put(skb, hlen), addr, hlen);
 	netif_printk(qdev, rx_status, KERN_DEBUG, qdev->ndev,
 		     "%d bytes of headers and data in large. Chain page to new skb and pull tail.\n",
 		     length);
 	skb_fill_page_desc(skb, 0, lbq_desc->p.pg_chunk.page,
-				lbq_desc->p.pg_chunk.offset+ETH_HLEN,
-				length-ETH_HLEN);
-	skb->len += length-ETH_HLEN;
-	skb->data_len += length-ETH_HLEN;
-	skb->truesize += length-ETH_HLEN;
+				lbq_desc->p.pg_chunk.offset + hlen,
+				length - hlen);
+	skb->len += length - hlen;
+	skb->data_len += length - hlen;
+	skb->truesize += length - hlen;
 
 	rx_ring->rx_packets++;
 	rx_ring->rx_bytes += skb->len;
+	ql_check_rx_vlan(qdev, skb, &vlan_id);
 	skb->protocol = eth_type_trans(skb, ndev);
 	skb->ip_summed = CHECKSUM_NONE;
 
@@ -1582,7 +1617,7 @@ static void ql_process_mac_rx_page(struct ql_adapter *qdev,
 				(ib_mac_rsp->flags3 & IB_MAC_IOCB_RSP_V4)) {
 			/* Unfragmented ipv4 UDP frame. */
 			struct iphdr *iph =
-				(struct iphdr *) ((u8 *)addr + ETH_HLEN);
+				(struct iphdr *) ((u8 *)addr + hlen);
 			if (!(iph->frag_off & htons(IP_MF|IP_OFFSET))) {
 				skb->ip_summed = CHECKSUM_UNNECESSARY;
 				netif_printk(qdev, rx_status, KERN_DEBUG,
@@ -1594,12 +1629,14 @@ static void ql_process_mac_rx_page(struct ql_adapter *qdev,
 
 	skb_record_rx_queue(skb, rx_ring->cq_id);
 	if (skb->ip_summed == CHECKSUM_UNNECESSARY) {
-		if (vlan_id != 0xffff)
+		if ((vlan_id != 0xffff) &&
+		    (ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_V))
 			vlan_gro_receive(napi, qdev->vlgrp, vlan_id, skb);
 		else
 			napi_gro_receive(napi, skb);
 	} else {
-		if (vlan_id != 0xffff)
+		if ((vlan_id != 0xffff) &&
+		    (ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_V))
 			vlan_hwaccel_receive_skb(skb, qdev->vlgrp, vlan_id);
 		else
 			netif_receive_skb(skb);
@@ -1614,13 +1651,14 @@ err_out:
 static void ql_process_mac_rx_skb(struct ql_adapter *qdev,
 					struct rx_ring *rx_ring,
 					struct ib_mac_iocb_rsp *ib_mac_rsp,
-					u32 length,
-					u16 vlan_id)
+					u32 length)
 {
-	struct net_device *ndev = qdev->ndev;
-	struct sk_buff *skb = NULL;
-	struct sk_buff *new_skb = NULL;
 	struct bq_desc *sbq_desc = ql_get_curr_sbuf(rx_ring);
+	struct net_device *ndev = qdev->ndev;
+	struct sk_buff *new_skb = NULL;
+	struct sk_buff *skb = NULL;
+	size_t hlen = ETH_HLEN;
+	u16 vlan_id = 0xffff;
 
 	skb = sbq_desc->p.skb;
 	/* Allocate new_skb and copy */
@@ -1649,10 +1687,13 @@ static void ql_process_mac_rx_skb(struct ql_adapter *qdev,
 		return;
 	}
 
+	/* Update the MAC header length */
+	ql_update_mac_hdr_len(qdev, ib_mac_rsp, skb->data, &hlen);
+
 	/* The max framesize filter on this chip is set higher than
 	 * MTU since FCoE uses 2k frames.
 	 */
-	if (skb->len > ndev->mtu + ETH_HLEN) {
+	if (skb->len > ndev->mtu + hlen) {
 		dev_kfree_skb_any(skb);
 		rx_ring->rx_dropped++;
 		return;
@@ -1676,6 +1717,7 @@ static void ql_process_mac_rx_skb(struct ql_adapter *qdev,
 
 	rx_ring->rx_packets++;
 	rx_ring->rx_bytes += skb->len;
+	ql_check_rx_vlan(qdev, skb, &vlan_id);
 	skb->protocol = eth_type_trans(skb, ndev);
 	skb->ip_summed = CHECKSUM_NONE;
 
@@ -1704,13 +1746,15 @@ static void ql_process_mac_rx_skb(struct ql_adapter *qdev,
 
 	skb_record_rx_queue(skb, rx_ring->cq_id);
 	if (skb->ip_summed == CHECKSUM_UNNECESSARY) {
-		if (vlan_id != 0xffff)
+		if ((vlan_id != 0xffff) &&
+		    (ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_V))
 			vlan_gro_receive(&rx_ring->napi, qdev->vlgrp,
 						vlan_id, skb);
 		else
 			napi_gro_receive(&rx_ring->napi, skb);
 	} else {
-		if (vlan_id != 0xffff)
+		if ((vlan_id != 0xffff) &&
+		    (ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_V))
 			vlan_hwaccel_receive_skb(skb, qdev->vlgrp, vlan_id);
 		else
 			netif_receive_skb(skb);
@@ -1740,11 +1784,12 @@ static struct sk_buff *ql_build_rx_skb(struct ql_adapter *qdev,
 				       struct rx_ring *rx_ring,
 				       struct ib_mac_iocb_rsp *ib_mac_rsp)
 {
-	struct bq_desc *lbq_desc;
-	struct bq_desc *sbq_desc;
+	struct bq_desc *lbq_desc = NULL;
+	struct bq_desc *sbq_desc = NULL;
 	struct sk_buff *skb = NULL;
 	u32 length = le32_to_cpu(ib_mac_rsp->data_len);
-       u32 hdr_len = le32_to_cpu(ib_mac_rsp->hdr_len);
+	u32 hdr_len = le32_to_cpu(ib_mac_rsp->hdr_len);
+	size_t hlen = ETH_HLEN;
 
 	/*
 	 * Handle the header buffer if present.
@@ -1871,9 +1916,10 @@ static struct sk_buff *ql_build_rx_skb(struct ql_adapter *qdev,
 			skb->data_len += length;
 			skb->truesize += length;
 			length -= length;
-			__pskb_pull_tail(skb,
-				(ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_V) ?
-				VLAN_ETH_HLEN : ETH_HLEN);
+			ql_update_mac_hdr_len(qdev, ib_mac_rsp,
+					      lbq_desc->p.pg_chunk.va,
+					      &hlen);
+			__pskb_pull_tail(skb, hlen);
 		}
 	} else {
 		/*
@@ -1928,8 +1974,9 @@ static struct sk_buff *ql_build_rx_skb(struct ql_adapter *qdev,
 			length -= size;
 			i++;
 		}
-		__pskb_pull_tail(skb, (ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_V) ?
-				VLAN_ETH_HLEN : ETH_HLEN);
+		ql_update_mac_hdr_len(qdev, ib_mac_rsp,
+				      lbq_desc->p.pg_chunk.va, &hlen);
+		__pskb_pull_tail(skb, hlen);
 	}
 	return skb;
 }
@@ -1937,11 +1984,12 @@ static struct sk_buff *ql_build_rx_skb(struct ql_adapter *qdev,
 /* Process an inbound completion from an rx ring. */
 static void ql_process_mac_split_rx_intr(struct ql_adapter *qdev,
 				   struct rx_ring *rx_ring,
-				   struct ib_mac_iocb_rsp *ib_mac_rsp,
-				   u16 vlan_id)
+				   struct ib_mac_iocb_rsp *ib_mac_rsp)
 {
 	struct net_device *ndev = qdev->ndev;
 	struct sk_buff *skb = NULL;
+	size_t hlen = ETH_HLEN;
+	u16 vlan_id = 0xffff;
 
 	QL_DUMP_IB_MAC_RSP(ib_mac_rsp);
 
@@ -1960,10 +2008,13 @@ static void ql_process_mac_split_rx_intr(struct ql_adapter *qdev,
 		return;
 	}
 
+	/* Update the MAC header length */
+	ql_update_mac_hdr_len(qdev, ib_mac_rsp, skb->data, &hlen);
+
 	/* The max framesize filter on this chip is set higher than
 	 * MTU since FCoE uses 2k frames.
 	 */
-	if (skb->len > ndev->mtu + ETH_HLEN) {
+	if (skb->len > ndev->mtu + hlen) {
 		dev_kfree_skb_any(skb);
 		rx_ring->rx_dropped++;
 		return;
@@ -1993,6 +2044,7 @@ static void ql_process_mac_split_rx_intr(struct ql_adapter *qdev,
 			     "Promiscuous Packet.\n");
 	}
 
+	ql_check_rx_vlan(qdev, skb, &vlan_id);
 	skb->protocol = eth_type_trans(skb, ndev);
 	skb->ip_summed = CHECKSUM_NONE;
 
@@ -2022,17 +2074,15 @@ static void ql_process_mac_split_rx_intr(struct ql_adapter *qdev,
 	rx_ring->rx_bytes += skb->len;
 	skb_record_rx_queue(skb, rx_ring->cq_id);
 	if (skb->ip_summed == CHECKSUM_UNNECESSARY) {
-		if (qdev->vlgrp &&
-			(ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_V) &&
-			(vlan_id != 0))
+		if ((vlan_id != 0xffff) &&
+		    (ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_V))
 			vlan_gro_receive(&rx_ring->napi, qdev->vlgrp,
-				vlan_id, skb);
+					 vlan_id, skb);
 		else
 			napi_gro_receive(&rx_ring->napi, skb);
 	} else {
-		if (qdev->vlgrp &&
-			(ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_V) &&
-			(vlan_id != 0))
+		if ((vlan_id != 0xffff) &&
+		    (ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_V))
 			vlan_hwaccel_receive_skb(skb, qdev->vlgrp, vlan_id);
 		else
 			netif_receive_skb(skb);
@@ -2045,9 +2095,6 @@ static unsigned long ql_process_mac_rx_intr(struct ql_adapter *qdev,
 					struct ib_mac_iocb_rsp *ib_mac_rsp)
 {
 	u32 length = le32_to_cpu(ib_mac_rsp->data_len);
-	u16 vlan_id = (ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_V) ?
-			((le16_to_cpu(ib_mac_rsp->vlan_id) &
-			IB_MAC_IOCB_RSP_VLAN_MASK)) : 0xffff;
 
 	QL_DUMP_IB_MAC_RSP(ib_mac_rsp);
 
@@ -2055,35 +2102,31 @@ static unsigned long ql_process_mac_rx_intr(struct ql_adapter *qdev,
 		/* The data and headers are split into
 		 * separate buffers.
 		 */
-		ql_process_mac_split_rx_intr(qdev, rx_ring, ib_mac_rsp,
-						vlan_id);
+		ql_process_mac_split_rx_intr(qdev, rx_ring, ib_mac_rsp);
 	} else if (ib_mac_rsp->flags3 & IB_MAC_IOCB_RSP_DS) {
 		/* The data fit in a single small buffer.
 		 * Allocate a new skb, copy the data and
 		 * return the buffer to the free pool.
 		 */
-		ql_process_mac_rx_skb(qdev, rx_ring, ib_mac_rsp,
-						length, vlan_id);
+		ql_process_mac_rx_skb(qdev, rx_ring, ib_mac_rsp, length);
 	} else if ((ib_mac_rsp->flags3 & IB_MAC_IOCB_RSP_DL) &&
 		!(ib_mac_rsp->flags1 & IB_MAC_CSUM_ERR_MASK) &&
-		(ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_T)) {
-		/* TCP packet in a page chunk that's been checksummed.
-		 * Tack it on to our GRO skb and let it go.
+		(ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_T) &&
+		!(ib_mac_rsp->flags2 & IB_MAC_IOCB_RSP_V)) {
+		/* TCP and non-VLAN packet in a page chunk that's been
+		 * checksummed, Tack it on to our GRO skb and let it go.
 		 */
-		ql_process_mac_rx_gro_page(qdev, rx_ring, ib_mac_rsp,
-						length, vlan_id);
+		ql_process_mac_rx_gro_page(qdev, rx_ring, ib_mac_rsp, length);
 	} else if (ib_mac_rsp->flags3 & IB_MAC_IOCB_RSP_DL) {
 		/* Non-TCP packet in a page chunk. Allocate an
 		 * skb, tack it on frags, and send it up.
 		 */
-		ql_process_mac_rx_page(qdev, rx_ring, ib_mac_rsp,
-						length, vlan_id);
+		ql_process_mac_rx_page(qdev, rx_ring, ib_mac_rsp, length);
 	} else {
 		/* Non-TCP/UDP large frames that span multiple buffers
 		 * can be processed corrrectly by the split frame logic.
 		 */
-		ql_process_mac_split_rx_intr(qdev, rx_ring, ib_mac_rsp,
-						vlan_id);
+		ql_process_mac_split_rx_intr(qdev, rx_ring, ib_mac_rsp);
 	}
 
 	return (unsigned long)length;
@@ -2331,12 +2374,6 @@ static void qlge_vlan_rx_register(struct net_device *ndev, struct vlan_group *gr
 	struct ql_adapter *qdev = netdev_priv(ndev);
 
 	qdev->vlgrp = grp;
-	if (grp) {
-		ql_write32(qdev, NIC_RCV_CFG, NIC_RCV_CFG_VLAN_MASK |
-			   NIC_RCV_CFG_VLAN_MATCH_AND_NON);
-	} else {
-		ql_write32(qdev, NIC_RCV_CFG, NIC_RCV_CFG_VLAN_MASK);
-	}
 }
 
 static void qlge_vlan_rx_add_vid(struct net_device *ndev, u16 vid)
@@ -3682,9 +3719,9 @@ static int ql_adapter_initialize(struct ql_adapter *qdev)
 	mask = value << 16;
 	ql_write32(qdev, SYS, mask | value);
 
-	/* Set the default queue, and VLAN behavior. */
-	value = NIC_RCV_CFG_DFQ | NIC_RCV_CFG_RV;
-	mask = NIC_RCV_CFG_DFQ_MASK | (NIC_RCV_CFG_RV << 16);
+	/* Set the default queue behavior. */
+	value = NIC_RCV_CFG_DFQ;
+	mask = NIC_RCV_CFG_DFQ_MASK;
 	ql_write32(qdev, NIC_RCV_CFG, (mask | value));
 
 	/* Set the MPI interrupt to enabled. */
@@ -4629,7 +4666,7 @@ static const struct net_device_ops qlge_netdev_ops = {
 	.ndo_set_mac_address	= qlge_set_mac_address,
 	.ndo_validate_addr	= eth_validate_addr,
 	.ndo_tx_timeout		= qlge_tx_timeout,
-	.ndo_vlan_rx_register	= qlge_vlan_rx_register,
+	.ndo_vlan_rx_register   = qlge_vlan_rx_register,
 	.ndo_vlan_rx_add_vid	= qlge_vlan_rx_add_vid,
 	.ndo_vlan_rx_kill_vid	= qlge_vlan_rx_kill_vid,
 };
@@ -4670,9 +4707,12 @@ static int __devinit qlge_probe(struct pci_dev *pdev,
 
 	qdev = netdev_priv(ndev);
 	SET_NETDEV_DEV(ndev, &pdev->dev);
-	hw_features = NETIF_F_SG | NETIF_F_IP_CSUM |
-		NETIF_F_TSO | NETIF_F_TSO_ECN |
-		NETIF_F_HW_VLAN_TX | NETIF_F_GRO;
+	hw_features = NETIF_F_SG |
+		      NETIF_F_IP_CSUM |
+		      NETIF_F_TSO |
+		      NETIF_F_TSO_ECN |
+		      NETIF_F_HW_VLAN_TX |
+		      NETIF_F_GRO;
 	ndev->features = hw_features |
 		NETIF_F_HW_VLAN_RX | NETIF_F_HW_VLAN_FILTER;
 	ndev->vlan_features = hw_features;
