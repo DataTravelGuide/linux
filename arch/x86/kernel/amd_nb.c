c95b6b42d16357cd831a7151ec8f3c7842605416
Myron Stowe <myron.stowe@redhat.com>
2014-05-23 18:22:31 -0400

    Use PCI method for enabling AMD extended config space before MSR method
    Patchwork-id: 80415
    O-Subject: [RHEL6.6 PATCH 01/29] x86: Use PCI method for enabling AMD extended config space before MSR method
    Bugzilla: 1040440
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1040440
    Upstream Status: 24d9b70b8c679264756a6980e668b96b3f964826
    
    commit 24d9b70b8c679264756a6980e668b96b3f964826
    Author: Jan Beulich <JBeulich@novell.com>
    Date:   Mon Jan 10 16:20:23 2011 +0000
    
        x86: Use PCI method for enabling AMD extended config space before MSR method
    
        While both methods should work equivalently well for the native
        case, the Xen Dom0 case can't reliably work with the MSR one,
        since there's no guarantee that the virtual CPUs it has
        available fully cover all necessary physical ones.
    
        As per the suggestion of Robert Richter the patch only adds the
        PCI method, but leaves the MSR one as a fallback to cover new
        systems the PCI IDs of which may not have got added to the code
        base yet.
    
        The only change in v2 is the breaking out of the new CPI
        initialization method into a separate function, as requested by
        Ingo.
    
        Signed-off-by: Jan Beulich <jbeulich@novell.com>
        Acked-by: Robert Richter <robert.richter@amd.com>
        Cc: Andreas Herrmann3 <Andreas.Herrmann3@amd.com>
        Cc: Joerg Roedel <joerg.roedel@amd.com>
        Cc: Jeremy Fitzhardinge <jeremy@goop.org>
        LKML-Reference: <4D2B3FD7020000780002B67D@vpn.id2.novell.com>
        Signed-off-by: Ingo Molnar <mingo@elte.hu>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kernel/amd_nb.c
+++ arch/x86/kernel/amd_nb.c
@@ -27,6 +27,13 @@ static struct pci_device_id amd_nb_link_ids[] = {
 	{}
 };
 
+const struct amd_nb_bus_dev_range amd_nb_bus_dev_ranges[] __initconst = {
+	{ 0x00, 0x18, 0x20 },
+	{ 0xff, 0x00, 0x20 },
+	{ 0xfe, 0x00, 0x20 },
+	{ }
+};
+
 struct amd_northbridge_info amd_northbridges;
 EXPORT_SYMBOL(amd_northbridges);
 
