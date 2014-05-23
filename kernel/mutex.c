41f507e4ed75c02b7899c2cd5793a52da053cdf8
Larry Woodman <lwoodman@redhat.com>
2014-05-23 19:25:22 -0400

    mutex: Unlock the mutex without the wait_lock
    Patchwork-id: 80454
    O-Subject: [RHEL6.6 PATCH 12/12] locking/mutexes: Unlock the mutex without the wait_lock
    Bugzilla: 1100855
    RH-Acked-by: Dave Anderson <anderson@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    
    This patch is a combination of 3 upstream patches.
    
    upstream commits:
            1d8fe7dc8078b23e060ec62ccb4cdc1ac3c41bf8
            6f008e72cd111a119b5d8de8c5438d892aae99eb
            a227960fe0cafcc229a8d6bb8b454a3a0b33719d
    
    Commit 1d8fe7dc8078b23e060ec62ccb4cdc1ac3c41bf8
    Author: Jason Low <jason.low2@hp.com>
    Date:   Tue Jan 28 11:13:14 2014 -0800
    
        locking/mutexes: Unlock the mutex without the wait_lock
    
        When running workloads that have high contention in mutexes on an 8 socket
        machine, mutex spinners would often spin for a long time with no lock owner.
    
        The main reason why this is occuring is in __mutex_unlock_common_slowpath(),
        if __mutex_slowpath_needs_to_unlock(), then the owner needs to acquire the
        mutex->wait_lock before releasing the mutex (setting lock->count to 1). When
        the wait_lock is contended, this delays the mutex from being released.
        We should be able to release the mutex without holding the wait_lock.
    
        Signed-off-by: Jason Low <jason.low2@hp.com>
        Cc: chegu_vinod@hp.com
        Cc: paulmck@linux.vnet.ibm.com
        Cc: Waiman.Long@hp.com
        Cc: torvalds@linux-foundation.org
        Cc: tglx@linutronix.de
        Cc: riel@redhat.com
        Cc: akpm@linux-foundation.org
        Cc: davidlohr@hp.com
        Cc: hpa@zytor.com
        Cc: andi@firstfloor.org
        Cc: aswin@hp.com
        Cc: scott.norton@hp.com
        Signed-off-by: Peter Zijlstra <peterz@infradead.org>
        Link: http://lkml.kernel.org/r/1390936396-3962-4-git-send-email-jason.low2@hp.com
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    	AND
    
    commit 6f008e72cd111a119b5d8de8c5438d892aae99eb
    Author: Peter Zijlstra <peterz@infradead.org>
    Date:   Wed Mar 12 13:24:42 2014 +0100
    
        locking/mutex: Fix debug checks
    
        OK, so commit:
    
          1d8fe7dc8078 ("locking/mutexes: Unlock the mutex without the wait_lock")
    
        generates this boot warning when CONFIG_DEBUG_MUTEXES=y:
    
          WARNING: CPU: 0 PID: 139 at /usr/src/linux-2.6/kernel/locking/mutex-debug.c:82 debug_mutex_unlock+0x155/0x180() DEBUG
    
        And that makes sense, because as soon as we release the lock a
        new owner can come in...
    
        One would think that !__mutex_slowpath_needs_to_unlock()
        implementations suffer the same, but for DEBUG we fall back to
        mutex-null.h which has an unconditional 1 for that.
    
        The mutex debug code requires the mutex to be unlocked after
        doing the debug checks, otherwise it can find inconsistent
        state.
    
        Reported-by: Ingo Molnar <mingo@kernel.org>
        Signed-off-by: Peter Zijlstra <peterz@infradead.org>
        Cc: jason.low2@hp.com
        Link: http://lkml.kernel.org/r/20140312122442.GB27965@twins.programming.kicks-ass.net
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    	AND
    
    commit a227960fe0cafcc229a8d6bb8b454a3a0b33719d
    Author: Peter Zijlstra <peterz@infradead.org>
    Date:   Thu Apr 10 16:15:59 2014 +0200
    
        locking/mutex: Fix debug_mutexes
    
        debug_mutex_unlock() would bail when !debug_locks and forgets to
        actually unlock.
    
        Reported-by: "Michael L. Semon" <mlsemon35@gmail.com>
        Reported-by: "Kirill A. Shutemov" <kirill@shutemov.name>
        Reported-by: Valdis Kletnieks <Valdis.Kletnieks@vt.edu>
        Fixes: 6f008e72cd11 ("locking/mutex: Fix debug checks")
        Tested-by: Dave Jones <davej@redhat.com>
        Cc: Jason Low <jason.low2@hp.com>
        Signed-off-by: Peter Zijlstra <peterz@infradead.org>
        Link: http://lkml.kernel.org/r/20140410141559.GE13658@twins.programming.kicks-ass.net
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/mutex.c
+++ kernel/mutex.c
@@ -32,6 +32,13 @@
 #ifdef CONFIG_DEBUG_MUTEXES
 # include "mutex-debug.h"
 # include <asm-generic/mutex-null.h>
+/*
+ * Must be 0 for the debug case so we do not do the unlock outside of the
+ * wait_lock region. debug_mutex_unlock() will do the actual unlock in this
+ * case.
+ */
+# undef __mutex_slowpath_needs_to_unlock
+# define  __mutex_slowpath_needs_to_unlock()   0
 #else
 # include "mutex.h"
 # include <asm/mutex.h>
@@ -555,10 +562,6 @@ __mutex_unlock_common_slowpath(atomic_t *lock_count, int nested)
 	struct mutex *lock = container_of(lock_count, struct mutex, count);
 	unsigned long flags;
 
-	spin_lock_mutex(&lock->wait_lock, flags);
-	mutex_release(&lock->dep_map, nested, _RET_IP_);
-	debug_mutex_unlock(lock);
-
 	/*
 	 * some architectures leave the lock unlocked in the fastpath failure
 	 * case, others need to leave it locked. In the later case we have to
@@ -566,6 +569,10 @@ __mutex_unlock_common_slowpath(atomic_t *lock_count, int nested)
 	 */
 	if (__mutex_slowpath_needs_to_unlock())
 		atomic_set(&lock->count, 1);
+
+	spin_lock_mutex(&lock->wait_lock, flags);
+	mutex_release(&lock->dep_map, nested, _RET_IP_);
+	debug_mutex_unlock(lock);
 
 	if (!MUTEX_LIST_EMPTY(&lock->wait_list)) {
 		/* get the first entry from the wait-list: */
