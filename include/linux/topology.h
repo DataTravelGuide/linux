45087010fd8cf3ef0f5acf3c9586120e4b7a7fbe
Larry Woodman <lwoodman@redhat.com>
2014-05-29 14:00:36 -0400

    sched: Periodically decay max cost of idle balance
    Patchwork-id: 81107
    O-Subject: [RHEL6.6 PATCH 05/12 V2] sched/balancing: Periodically decay max cost of idle balance
    Bugzilla: 1100855
    RH-Acked-by: Dave Anderson <anderson@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    
    commit f48627e686a69f5215cb0761e731edb3d9859dd9
     Author: Jason Low <jason.low2@hp.com>
     Date:   Fri Sep 13 11:26:53 2013 -0700
    
        sched/balancing: Periodically decay max cost of idle balance
    
        This patch builds on patch 2 and periodically decays that max value to
        do idle balancing per sched domain by approximately 1% per second. Also
        decay the rq's max_idle_balance_cost value.
    
        Signed-off-by: Jason Low <jason.low2@hp.com>
        Signed-off-by: Peter Zijlstra <peterz@infradead.org>
        Link: http://lkml.kernel.org/r/1379096813-3032-4-git-send-email-jason.low2@hp.com
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/topology.h
+++ include/linux/topology.h
@@ -108,6 +108,7 @@ int arch_update_cpu_topology(void);
 	.balance_interval	= 1,					\
 	.smt_gain		= 1178,	/* 15% */			\
 	.max_newidle_lb_cost	= 0,			\
+	.next_decay_max_lb_cost	= jiffies,		\
 }
 #endif
 #endif /* CONFIG_SCHED_SMT */
@@ -141,6 +142,7 @@ int arch_update_cpu_topology(void);
 	.last_balance		= jiffies,				\
 	.balance_interval	= 1,					\
 	.max_newidle_lb_cost	= 0,			\
+	.next_decay_max_lb_cost	= jiffies,		\
 }
 #endif
 #endif /* CONFIG_SCHED_MC */
@@ -175,6 +177,7 @@ int arch_update_cpu_topology(void);
 	.last_balance		= jiffies,				\
 	.balance_interval	= 1,					\
 	.max_newidle_lb_cost	= 0,			\
+	.next_decay_max_lb_cost	= jiffies,		\
 }
 #endif
 
@@ -202,6 +205,7 @@ int arch_update_cpu_topology(void);
 	.last_balance		= jiffies,				\
 	.balance_interval	= 64,					\
 	.max_newidle_lb_cost	= 0,			\
+	.next_decay_max_lb_cost	= jiffies,		\
 }
 
 #ifdef CONFIG_SCHED_BOOK
