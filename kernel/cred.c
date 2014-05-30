16e6f6d40feb931cd0a7920c5e5073c43db0ce4b
Radomir Vrbovsky <rvrbovsk@redhat.com>
2014-05-30 15:51:33 -0400

    cred: Fix memory leak in error handling
    Patchwork-id: 81252
    O-Subject: [RHEL6 PATCH 1/2] CRED: Fix memory leak in error handling
    Bugzilla: 1087544
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Mark Langsdorf <mlangsdo@redhat.com>
    
    Bugzilla: Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1087544
    Upstream: 570b8fb505896e007fd3bb07573ba6640e51851d
    
    Fix a memory leak on an OOM condition in prepare_usermodehelper_creds().
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: James Morris <jmorris@namei.org>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/cred.c
+++ kernel/cred.c
@@ -385,7 +385,7 @@ struct cred *prepare_usermodehelper_creds(void)
 
 	new = kmem_cache_alloc(cred_jar, GFP_ATOMIC);
 	if (!new)
-		return NULL;
+		goto free_tgcred;
 
 	kdebug("prepare_usermodehelper_creds() alloc %p", new);
 
@@ -418,6 +418,10 @@ struct cred *prepare_usermodehelper_creds(void)
 
 error:
 	put_cred(new);
+free_tgcred:
+#ifdef CONFIG_KEYS
+	kfree(tgcred);
+#endif
 	return NULL;
 }
 
