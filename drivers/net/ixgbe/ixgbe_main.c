a46f582690d69c3411d214b0804da8c27ebccccd
Andy Gospodarek <gospo@redhat.com>
2014-06-02 15:41:24 -0400

    ixgbe: bump version number
    Patchwork-id: 81611
    O-Subject: [PATCH RHEL6.6 44/84] ixgbe: bump version number
    Bugzilla: 1038784
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    Backport of upstream commit:
    
    commit f341c4e0a842cd6bb10770d71a9e42e6aced7188
    Author: Don Skidmore <donald.c.skidmore@intel.com>
    Date:   Fri Jan 17 01:21:37 2014 -0800
    
        ixgbe: bump version number

--- drivers/net/ixgbe/ixgbe_main.c
+++ drivers/net/ixgbe/ixgbe_main.c
@@ -55,7 +55,7 @@ static const char ixgbe_driver_string[] =
 			      "Intel(R) 10 Gigabit PCI Express Network Driver";
 char ixgbe_default_device_descr[] =
 			      "Intel(R) 10 Gigabit Network Connection";
-#define DRV_VERSION "3.15.1-k"
+#define DRV_VERSION "3.19.1-k"
 const char ixgbe_driver_version[] = DRV_VERSION;
 static const char ixgbe_copyright[] =
 				"Copyright (c) 1999-2013 Intel Corporation.";
