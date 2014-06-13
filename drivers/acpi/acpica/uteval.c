ae92be458272b41e5913794640d50fa2ce09085a
Lenny Szubowicz <lszubowi@redhat.com>
2014-06-13 16:15:20 -0400

    acpica: Add "Windows 2013" string to _OSI support
    Patchwork-id: 83930
    O-Subject: [RHEL 6.6 PATCH BZ 1093502 3/3] ACPICA: Add "Windows 2013" string to _OSI support.
    Bugzilla: 1093502
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1093502
    
    Description:
    
        Backport upstream patches to facilitate ACPI enumeration
        with some Broadwell BIOS's.
    
    Upstream Status: faae404ebdc6bba744919d82e64c16448eb24a36
    
        Upstream changes to: ./drivers/acpi/acpica/utosi.c
        Are in RHEL6 file:   ./drivers/acpi/acpica/uteval.c
    
    Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=7559705
    
    Tested:
    
        Test booted on a Intel Broadwell-based proto.
        - AML code on this platform inquired about Win 2012 and 2013
          and invoked this changed code.
        Sanity booted on an Intel Haswell-based system.
    
    commit faae404ebdc6bba744919d82e64c16448eb24a36
    Author: Bob Moore <Robert.Moore@intel.com>
    Date:   Tue Feb 11 10:25:27 2014 +0800
    
        ACPICA: Add "Windows 2013" string to _OSI support.
    
        This urgent patch is cherry picked from ACPICA upstream.
        It is reported that some platforms fail to boot without this new _OSI
        string.
    
        This change adds this string for Windows 8.1 and Server 2012 R2.
    
        Reported-by: Zhang Rui <rui.zhang@intel.com>
        Signed-off-by: Bob Moore <Robert.Moore@intel.com>
        Signed-off-by: Lv Zheng <lv.zheng@intel.com>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/acpi/acpica/uteval.c
+++ drivers/acpi/acpica/uteval.c
@@ -74,6 +74,7 @@ static struct acpi_interface_info acpi_interfaces_supported[] = {
 	{"Windows 2006 SP2", ACPI_OSI_WIN_VISTA_SP2},	/* Windows Vista SP2 - Added 09/2010 */
 	{"Windows 2009", ACPI_OSI_WIN_7},	/* Windows 7 and Server 2008 R2 - Added 09/2009 */
 	{"Windows 2012", ACPI_OSI_WIN_8},	/* Windows 8 and Server 2012 - Added 08/2012 */
+	{"Windows 2013", ACPI_OSI_WIN_8},	/* Windows 8.1 and Server 2012 R2 - Added 01/2014 */
 
 	/* Feature Group Strings */
 
