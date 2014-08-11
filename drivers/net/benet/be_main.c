b60eaea70c5ba6b522a5d64cbb0b0a31cf3afe20
Ivan Vecera <ivecera@redhat.com>
2014-08-11 13:33:46 -0400

    be2net: use adapter->flags to track SRIOV state
    Patchwork-id: 87620
    O-Subject: [RHEL6.6 PATCH 6/7] be2net: use adapter->flags to track SRIOV state
    Bugzilla: 1110417
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    BZ: #1110417
    
    Upstream commit(s):
    commit f174c7ec10d99e62f7722d0608b2a881ef091d21
    Author: Vasundhara Volam <vasundhara.volam@emulex.com>
    Date:   Thu Jul 17 16:20:31 2014 +0530
    
        be2net: use adapter->flags to track SRIOV state
    
        The driver so far used adapter->num_vfs value to check if SR-IOV is enabled
        or not. But, the patch bec84e6("create optimal number of queues on SR-IOV
        config") changed this logic. The adapter->num_vfs value is validated and set
        much before SR-IOV is enabled. So, we now use an explicit flag to track
        SR-IOV enabled state.
    
        Signed-off-by: Vasundhara Volam <vasundhara.volam@emulex.com>
        Signed-off-by: Sathya Perla <sathya.perla@emulex.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/benet/be_main.c
+++ drivers/net/benet/be_main.c
@@ -2956,6 +2956,7 @@ static void be_vf_clear(struct be_adapter *adapter)
 done:
 	kfree(adapter->vf_cfg);
 	adapter->num_vfs = 0;
+	adapter->flags &= ~BE_FLAGS_SRIOV_ENABLED;
 }
 
 static void be_clear_queues(struct be_adapter *adapter)
@@ -3134,6 +3135,8 @@ static int be_vf_setup(struct be_adapter *adapter)
 			goto err;
 		}
 	}
+
+	adapter->flags |= BE_FLAGS_SRIOV_ENABLED;
 	return 0;
 err:
 	dev_err(dev, "VF setup failed\n");
