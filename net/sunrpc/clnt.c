1ed27e4c8e2ad5445f859a7014bd4e5c719cdc53
Steve Dickson <SteveD@redhat.com>
2014-08-27 19:30:32 -0400

    sunrpc: Handle EPIPE in xprt_connect_status
    Patchwork-id: 89280
    O-Subject: [RHEL6.6 PATCH 8/8] SUNRPC: Handle EPIPE in xprt_connect_status
    Bugzilla: 1130619
    RH-Acked-by: Benjamin Coddington <bcodding@redhat.com>
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    
    From: Trond Myklebust <trond.myklebust@primarydata.com>
    
    The callback handler xs_error_report() can end up propagating an EPIPE
    error by means of the call to xprt_wake_pending_tasks(). Ensure that
    xprt_connect_status() does not automatically convert this into an
    EIO error.
    
    Reported-by: Weston Andros Adamson <dros@primarydata.com>
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    Upstream Id: 2fc193cf924ea6eb74f6a0cf73b94b2e62938ae5
    Bugzilla: 1130619

--- net/sunrpc/clnt.c
+++ net/sunrpc/clnt.c
@@ -1294,6 +1294,7 @@ call_connect_status(struct rpc_task *task)
 	case -ENETUNREACH:
 	case -EHOSTUNREACH:
 	case -ENOBUFS:
+	case -EPIPE:
 		if (RPC_IS_SOFTCONN(task))
 			break;
 		/* retry with existing socket, after a delay */
