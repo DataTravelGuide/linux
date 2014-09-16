e41751b47014de53ade55aec095eb783fb71e980
Prarit Bhargava <prarit@redhat.com>
2014-09-15 20:06:39 -0400

    revert "cxgb4vf: Ingress Queue Entry Size needs to be 64 bytes"
    Patchwork-id: 94856
    O-Subject: [RHEL6.6 PATCH BZ 1112417 22/50] Revert "[netdrv] cxgb4vf: Ingress Queue Entry Size needs to be 64 bytes"
    Bugzilla: 1140743
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    RH-Acked-by: Kyle McMartin <kmcmarti@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1112417
    
    This reverts commit fc2cef28708219c350f3e197246364184a5649f2.
    
    Cc: Ivan Vecera <ivecera@redhat.com>
    Cc: Peter Martuccelli <peterm@redhat.com>
    Cc: Kyle McMartin <kmcmarti@redhat.com>
    Cc: Neil Horman <nhorman@redhat.com>
    Cc: Sai Vemuri <svemuri@redhat.com>

--- drivers/net/cxgb4vf/cxgb4vf_main.c
+++ drivers/net/cxgb4vf/cxgb4vf_main.c
@@ -2276,7 +2276,6 @@ static void __devinit cfg_queues(struct adapter *adapter)
 {
 	struct sge *s = &adapter->sge;
 	int q10g, n10g, qidx, pidx, qs;
-	size_t iqe_size;
 
 	/*
 	 * We should not be called till we know how many Queue Sets we can
@@ -2321,13 +2320,6 @@ static void __devinit cfg_queues(struct adapter *adapter)
 	s->ethqsets = qidx;
 
 	/*
-	 * The Ingress Queue Entry Size for our various Response Queues needs
-	 * to be big enough to accommodate the largest message we can receive
-	 * from the chip/firmware; which is 64 bytes ...
-	 */
-	iqe_size = 64;
-
-	/*
 	 * Set up default Queue Set parameters ...  Start off with the
 	 * shortest interrupt holdoff timer.
 	 */
@@ -2335,7 +2327,7 @@ static void __devinit cfg_queues(struct adapter *adapter)
 		struct sge_eth_rxq *rxq = &s->ethrxq[qs];
 		struct sge_eth_txq *txq = &s->ethtxq[qs];
 
-		init_rspq(&rxq->rspq, 0, 0, 1024, iqe_size);
+		init_rspq(&rxq->rspq, 0, 0, 1024, L1_CACHE_BYTES);
 		rxq->fl.size = 72;
 		txq->q.size = 1024;
 	}
@@ -2344,7 +2336,8 @@ static void __devinit cfg_queues(struct adapter *adapter)
 	 * The firmware event queue is used for link state changes and
 	 * notifications of TX DMA completions.
 	 */
-	init_rspq(&s->fw_evtq, SGE_TIMER_RSTRT_CNTR, 0, 512, iqe_size);
+	init_rspq(&s->fw_evtq, SGE_TIMER_RSTRT_CNTR, 0, 512,
+		  L1_CACHE_BYTES);
 
 	/*
 	 * The forwarded interrupt queue is used when we're in MSI interrupt
@@ -2360,7 +2353,7 @@ static void __devinit cfg_queues(struct adapter *adapter)
 	 * any time ...
 	 */
 	init_rspq(&s->intrq, SGE_TIMER_RSTRT_CNTR, 0, MSIX_ENTRIES + 1,
-		  iqe_size);
+		  L1_CACHE_BYTES);
 }
 
 /*
