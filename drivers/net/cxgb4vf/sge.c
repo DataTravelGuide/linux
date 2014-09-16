116c54a3fbfe25393229c6c15b45dfd46deff009
Prarit Bhargava <prarit@redhat.com>
2014-09-15 20:06:31 -0400

    revert "cxgb4vf: do vlan cleanup"
    Patchwork-id: 94823
    O-Subject: [RHEL6.6 PATCH BZ 1112417 14/50] Revert "[netdrv] cxgb4vf: do vlan cleanup"
    Bugzilla: 1140743
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    RH-Acked-by: Kyle McMartin <kmcmarti@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1112417
    
    This reverts commit 44405f5520e889469bc2aae7a8b369c6e43a9f2d.
    
    Cc: Ivan Vecera <ivecera@redhat.com>
    Cc: Sai Vemuri <svemuri@redhat.com>
    Cc: Peter Martuccelli <peterm@redhat.com>
    Cc: Kyle McMartin <kmcmarti@redhat.com>
    Cc: Neil Horman <nhorman@redhat.com>

--- drivers/net/cxgb4vf/sge.c
+++ drivers/net/cxgb4vf/sge.c
@@ -1490,10 +1490,20 @@ static void do_gro(struct sge_eth_rxq *rxq, const struct pkt_gl *gl,
 	skb->ip_summed = CHECKSUM_UNNECESSARY;
 	skb_record_rx_queue(skb, rxq->rspq.idx);
 
-	if (pkt->vlan_ex)
-		__vlan_hwaccel_put_tag(skb, be16_to_cpu(pkt->vlan));
+	if (unlikely(pkt->vlan_ex)) {
+		struct port_info *pi = netdev_priv(rxq->rspq.netdev);
+		struct vlan_group *grp = pi->vlan_grp;
+
+		rxq->stats.vlan_ex++;
+		if (likely(grp)) {
+			ret = vlan_gro_frags(&rxq->rspq.napi, grp,
+					     be16_to_cpu(pkt->vlan));
+			goto stats;
+		}
+	}
 	ret = napi_gro_frags(&rxq->rspq.napi);
 
+stats:
 	if (ret == GRO_HELD)
 		rxq->stats.lro_pkts++;
 	else if (ret == GRO_MERGED || ret == GRO_MERGED_FREE)
@@ -1514,6 +1524,7 @@ int t4vf_ethrx_handler(struct sge_rspq *rspq, const __be64 *rsp,
 		       const struct pkt_gl *gl)
 {
 	struct sk_buff *skb;
+	struct port_info *pi;
 	const struct cpl_rx_pkt *pkt = (void *)&rsp[1];
 	bool csum_ok = pkt->csum_calc && !pkt->err_vec;
 	struct sge_eth_rxq *rxq = container_of(rspq, struct sge_eth_rxq, rspq);
@@ -1541,9 +1552,11 @@ int t4vf_ethrx_handler(struct sge_rspq *rspq, const __be64 *rsp,
 	__skb_pull(skb, PKTSHIFT);
 	skb->protocol = eth_type_trans(skb, rspq->netdev);
 	skb_record_rx_queue(skb, rspq->idx);
+	skb->dev->last_rx = jiffies;                  /* XXX removed 2.6.29 */
+	pi = netdev_priv(skb->dev);
 	rxq->stats.pkts++;
 
-	if (csum_ok && !pkt->err_vec &&
+	if (csum_ok && (pi->rx_offload & RX_CSO) && !pkt->err_vec &&
 	    (be32_to_cpu(pkt->l2info) & (RXF_UDP|RXF_TCP))) {
 		if (!pkt->ip_frag)
 			skb->ip_summed = CHECKSUM_UNNECESSARY;
@@ -1556,12 +1569,20 @@ int t4vf_ethrx_handler(struct sge_rspq *rspq, const __be64 *rsp,
 	} else
 		skb->ip_summed = CHECKSUM_NONE;
 
-	if (pkt->vlan_ex) {
-		rxq->stats.vlan_ex++;
-		__vlan_hwaccel_put_tag(skb, be16_to_cpu(pkt->vlan));
-	}
+	/*
+	 * Deliver the packet to the stack.
+	 */
+	if (unlikely(pkt->vlan_ex)) {
+		struct vlan_group *grp = pi->vlan_grp;
 
-	netif_receive_skb(skb);
+		rxq->stats.vlan_ex++;
+		if (likely(grp))
+			vlan_hwaccel_receive_skb(skb, grp,
+						 be16_to_cpu(pkt->vlan));
+		else
+			dev_kfree_skb_any(skb);
+	} else
+		netif_receive_skb(skb);
 
 	return 0;
 }
