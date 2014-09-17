6ff9abd959acbc9b34568a22e9d45cca1e0f70c9
Paul Moore <pmoore@redhat.com>
2014-09-16 22:16:04 -0400

    netlabel: fix the horribly broken catmap functions
    Patchwork-id: 95858
    O-Subject: [RHEL6 PATCH 2/3] netlabel: fix the horribly broken catmap functions
    Bugzilla: 1130197
    RH-Acked-by: Hannes Frederic Sowa <hannes@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1130197
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=7957748
    Upstream: present upstream, commit ID below
    Testing: tested, see BZ
    
    The normal differences due to backporting from upstream, nothing
    substantial.
    
    	commit: 4b8feff251da3d7058b5779e21b33a85c686b974
    	From: Paul Moore <pmoore@redhat.com>
    	Date: 2014-08-01 11:17:17 -0400
    
    	netlabel: fix the horribly broken catmap functions
    
    	The NetLabel secattr catmap functions, and the SELinux import/export
    	glue routines, were broken in many horrible ways and the SELinux glue
    	code fiddled with the NetLabel catmap structures in ways that we
    	probably shouldn't allow.  At some point this "worked", but that was
    	likely due to a bit of dumb luck and sub-par testing (both inflicted
    	by yours truly).  This patch corrects these problems by basically
    	gutting the code in favor of something less obtuse and restoring the
    	NetLabel abstractions in the SELinux catmap glue code.
    
    	Everything is working now, and if it decides to break itself in the
    	future this code will be much easier to debug than the code it
    	replaces.
    
    	One noteworthy side effect of the changes is that it is no longer
    	necessary to allocate a NetLabel catmap before calling one of the
    	NetLabel APIs to set a bit in the catmap.  NetLabel will automatically
    	allocate the catmap nodes when needed, resulting in less allocations
    	when the lowest bit is greater than 255 and less code in the LSMs.
    
    	Cc: stable@vger.kernel.org
    	Reported-by: Christian Evans <frodox@zoho.com>
    	Signed-off-by: Paul Moore <pmoore@redhat.com>
    	Tested-by: Casey Schaufler <casey@schaufler-ca.com>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/ipv4/cipso_ipv4.c
+++ net/ipv4/cipso_ipv4.c
@@ -1337,11 +1337,6 @@ static int cipso_v4_parsetag_rbm(const struct cipso_v4_doi *doi_def,
 	secattr->flags |= NETLBL_SECATTR_MLS_LVL;
 
 	if (tag_len > 4) {
-		secattr->attr.mls.cat =
-		                       netlbl_secattr_catmap_alloc(GFP_ATOMIC);
-		if (secattr->attr.mls.cat == NULL)
-			return -ENOMEM;
-
 		ret_val = cipso_v4_map_cat_rbm_ntoh(doi_def,
 						    &tag[4],
 						    tag_len - 4,
@@ -1433,11 +1428,6 @@ static int cipso_v4_parsetag_enum(const struct cipso_v4_doi *doi_def,
 	secattr->flags |= NETLBL_SECATTR_MLS_LVL;
 
 	if (tag_len > 4) {
-		secattr->attr.mls.cat =
-			               netlbl_secattr_catmap_alloc(GFP_ATOMIC);
-		if (secattr->attr.mls.cat == NULL)
-			return -ENOMEM;
-
 		ret_val = cipso_v4_map_cat_enum_ntoh(doi_def,
 						     &tag[4],
 						     tag_len - 4,
@@ -1528,11 +1518,6 @@ static int cipso_v4_parsetag_rng(const struct cipso_v4_doi *doi_def,
 	secattr->flags |= NETLBL_SECATTR_MLS_LVL;
 
 	if (tag_len > 4) {
-		secattr->attr.mls.cat =
-			               netlbl_secattr_catmap_alloc(GFP_ATOMIC);
-		if (secattr->attr.mls.cat == NULL)
-			return -ENOMEM;
-
 		ret_val = cipso_v4_map_cat_rng_ntoh(doi_def,
 						    &tag[4],
 						    tag_len - 4,
