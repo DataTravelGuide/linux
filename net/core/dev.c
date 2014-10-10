a1644b8f919e832ce93ea56c822166839b029e51
Jiri Pirko <jpirko@redhat.com>
2014-10-10 15:23:54 -0400

    vlan: Fix the ingress VLAN_FLAG_REORDER_HDR check
    Patchwork-id: 97420
    O-Subject: [patch rhel6 net 59/80] vlan: Fix the ingress VLAN_FLAG_REORDER_HDR check
    Bugzilla: 1135347
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Hannes Frederic Sowa <hannes@redhat.com>
    RH-Acked-by: Daniel Borkmann <dborkman@redhat.com>
    RH-Acked-by: John Greene <jogreene@redhat.com>
    RH-Acked-by: Vlad Yasevich <vyasevic@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    BZ1135347
    
    upstream commit 0b5c9db1b11d3175bb42b80663a9f072f801edf5
    Author: Jiri Pirko <jpirko@redhat.com>
    Date:   Fri Jun 10 06:56:58 2011 +0000
    
        vlan: Fix the ingress VLAN_FLAG_REORDER_HDR check
    
        Testing of VLAN_FLAG_REORDER_HDR does not belong in vlan_untag
        but rather in vlan_do_receive.  Otherwise the vlan header
        will not be properly put on the packet in the case of
        vlan header accelleration.
    
        As we remove the check from vlan_check_reorder_header
        rename it vlan_reorder_header to keep the naming clean.
    
        Fix up the skb->pkt_type early so we don't look at the packet
        after adding the vlan tag, which guarantees we don't goof
        and look at the wrong field.
    
        Use a simple if statement instead of a complicated switch
        statement to decided that we need to increment rx_stats
        for a multicast packet.
    
        Hopefully at somepoint we will just declare the case where
        VLAN_FLAG_REORDER_HDR is cleared as unsupported and remove
        the code.  Until then this keeps it working correctly.
    
    Signed-off-by: Jiri Pirko <jpirko@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -3313,7 +3313,7 @@ int __netif_receive_skb(struct sk_buff *skb)
 
 	skb_reset_network_header(skb);
 	skb_reset_transport_header(skb);
-	skb->mac_len = skb->network_header - skb->mac_header;
+	skb_reset_mac_len(skb);
 
 	pt_prev = NULL;
 
