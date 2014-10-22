1749215c33f9b807a16d589807d4b39449a5d4ba
Brian Foster <bfoster@redhat.com>
2014-10-22 13:04:19 -0400

    pipe: kill ->map() and ->unmap()
    Patchwork-id: 98045
    O-Subject: [RHEL6.7 PATCH 2/3] [fuse] pipe: kill ->map() and ->unmap()
    Bugzilla: 1144128
    RH-Acked-by: Phillip Lougher <plougher@redhat.com>
    RH-Acked-by: Carlos Maiolino <cmaiolino@redhat.com>
    
    - Partial backport of fuse bits only.
    - Fix k[un]map_atomic() calls to include type param.
    
    commit fbb32750a62df75d1ffea547f3908b21c5496d9f
    Author: Al Viro <viro@zeniv.linux.org.uk>
    Date:   Sun Feb 2 21:09:54 2014 -0500
    
        pipe: kill ->map() and ->unmap()
    
        all pipe_buffer_operations have the same instances of those...
    
        Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -644,7 +644,7 @@ static void fuse_copy_finish(struct fuse_copy_state *cs)
 		struct pipe_buffer *buf = cs->currbuf;
 
 		if (!cs->write) {
-			buf->ops->unmap(cs->pipe, buf, cs->mapaddr);
+			kunmap_atomic(cs->mapaddr, KM_USER0);
 		} else {
 			kunmap_atomic(cs->mapaddr, KM_USER0);
 			buf->len = PAGE_SIZE - cs->len;
@@ -683,7 +683,7 @@ static int fuse_copy_fill(struct fuse_copy_state *cs)
 
 			BUG_ON(!cs->nr_segs);
 			cs->currbuf = buf;
-			cs->mapaddr = buf->ops->map(cs->pipe, buf, 1);
+			cs->mapaddr = kmap_atomic(buf->page, KM_USER0);
 			cs->len = buf->len;
 			cs->buf = cs->mapaddr + buf->offset;
 			cs->pipebufs++;
@@ -858,7 +858,7 @@ static int fuse_try_move_page(struct fuse_copy_state *cs, struct page **pagep)
 out_fallback_unlock:
 	unlock_page(newpage);
 out_fallback:
-	cs->mapaddr = buf->ops->map(cs->pipe, buf, 1);
+	cs->mapaddr = kmap_atomic(buf->page, KM_USER0);
 	cs->buf = cs->mapaddr + buf->offset;
 
 	err = lock_request(cs->fc, cs->req);
