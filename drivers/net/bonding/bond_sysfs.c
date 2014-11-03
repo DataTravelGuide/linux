8164d16a329f8e8cc9889132502fafc7cc95d84a
Nikolay Aleksandrov <nikolay@redhat.com>
2014-11-03 15:09:33 -0500

    bonding: Fix deadlock in bonding driver resulting from internal locking when using netpoll
    Patchwork-id: 98860
    O-Subject: [RHEL6.7 PATCH BZ 1159818 150/657] bonding: Fix deadlock in bonding driver resulting from internal locking when using netpoll
    Bugzilla: 1159818
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    From: Neil Horman <nhorman@tuxdriver.com>
    
    The monitoring paths in the bonding driver take write locks that are shared by
    the tx path.  If netconsole is in use, these paths can call printk which puts us
    in the netpoll tx path, which, if netconsole is attached to the bonding driver,
    result in deadlock (the xmit_lock guards are useless in netpoll_send_skb, as the
    monitor paths in the bonding driver don't claim the xmit_lock, nor should they).
    The solution is to use a per cpu flag internal to the driver to indicate when a
    cpu is holding the lock in a path that might recusrse into the tx path for the
    driver via netconsole.  By checking this flag on transmit, we can defer the
    sending of the netconsole frames until a later time using the retransmit feature
    of netpoll_send_skb that is triggered on the return code NETDEV_TX_BUSY.  I've
    tested this and am able to transmit via netconsole while causing failover
    conditions on the bond slave links.
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit e843fa50887582c867d8b7995f81fe9c1a076806)
    
    Conflicts:
    	drivers/net/bonding/bond_main.c
    	- Due to missing netns commits. Merged the old code with the
    	  new.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/bonding/bond_sysfs.c
+++ drivers/net/bonding/bond_sysfs.c
@@ -1063,6 +1063,7 @@ static ssize_t bonding_store_primary(struct device *d,
 
 	if (!rtnl_trylock())
 		return restart_syscall();
+	block_netpoll_tx();
 	read_lock(&bond->lock);
 	write_lock_bh(&bond->curr_slave_lock);
 
@@ -1098,6 +1099,7 @@ static ssize_t bonding_store_primary(struct device *d,
 out:
 	write_unlock_bh(&bond->curr_slave_lock);
 	read_unlock(&bond->lock);
+	unblock_netpoll_tx();
 	rtnl_unlock();
 
 	return count;
@@ -1143,11 +1145,13 @@ static ssize_t bonding_store_primary_reselect(struct device *d,
 		bond->dev->name, pri_reselect_tbl[new_value].modename,
 		new_value);
 
+	block_netpoll_tx();
 	read_lock(&bond->lock);
 	write_lock_bh(&bond->curr_slave_lock);
 	bond_select_active_slave(bond);
 	write_unlock_bh(&bond->curr_slave_lock);
 	read_unlock(&bond->lock);
+	unblock_netpoll_tx();
 out:
 	rtnl_unlock();
 	return ret;
@@ -1229,6 +1233,8 @@ static ssize_t bonding_store_active_slave(struct device *d,
 
 	if (!rtnl_trylock())
 		return restart_syscall();
+
+	block_netpoll_tx();
 	read_lock(&bond->lock);
 	write_lock_bh(&bond->curr_slave_lock);
 
@@ -1285,6 +1291,8 @@ static ssize_t bonding_store_active_slave(struct device *d,
  out:
 	write_unlock_bh(&bond->curr_slave_lock);
 	read_unlock(&bond->lock);
+	unblock_netpoll_tx();
+
 	rtnl_unlock();
 
 	return count;
