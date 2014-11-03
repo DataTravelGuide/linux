4dea1d72da51f75a5cfcd5a9673c384aabfbacd8
Nikolay Aleksandrov <nikolay@redhat.com>
2014-11-03 15:12:53 -0500

    bonding: bond_sysfs.c checkpatch cleanup
    Patchwork-id: 99059
    O-Subject: [RHEL6.7 PATCH BZ 1159818 350/657] bonding: bond_sysfs.c checkpatch cleanup
    Bugzilla: 1159818
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    From: dingtianhong <dingtianhong@huawei.com>
    
    net/bonding/bond_sysfs.c:1302: ERROR: else should follow close brace '}'
    net/bonding/bond_sysfs.c:1314: ERROR: else should follow close brace '}'
    
    Signed-off-by: Ding Tianhong <dingtianhong@huawei.com>
    Cc: Jay Vosburgh <fubar@us.ibm.com>
    Cc: Andy Gospodarek <andy@greyhouse.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 38c4916a7874aff9dc07a68bf8e7e4136b00f8dd)
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/bonding/bond_sysfs.c
+++ drivers/net/bonding/bond_sysfs.c
@@ -1278,8 +1278,7 @@ static ssize_t bonding_store_active_slave(struct device *d,
 					bond->dev->name,
 					slave->dev->name);
 				goto out;
-			}
-			else {
+			} else {
 				if ((new_active) &&
 				    (old_active) &&
 				    (new_active->link == BOND_LINK_UP) &&
@@ -1291,8 +1290,7 @@ static ssize_t bonding_store_active_slave(struct device *d,
 					bond_unicast_sync(bond, old_active, new_active);
 					bond_change_active_slave(bond,
 								 new_active);
-				}
-				else {
+				} else {
 					pr_info("%s: Could not set %s as"
 						" active slave; either %s is"
 						" down or the link is down.\n",
