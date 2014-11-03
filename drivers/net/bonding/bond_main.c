8cb385a17ddbd38fa80cbad1a1f1318959530ef8
Nikolay Aleksandrov <nikolay@redhat.com>
2014-11-03 15:07:27 -0500

    revert "bonding: move bond_compute_features out of locks"
    Patchwork-id: 98733
    O-Subject: [RHEL6.7 PATCH BZ 1159818 024/657] Revert "[netdrv] bonding: move bond_compute_features out of locks"
    Bugzilla: 1159818
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    This reverts commit a47448263c805eaf878a115a3deafdda98b2e75a.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -1793,9 +1793,9 @@ int bond_enslave(struct net_device *bond_dev, struct net_device *slave_dev)
 	new_slave->delay = 0;
 	new_slave->link_failure_count = 0;
 
-	write_unlock_bh(&bond->lock);
-
 	bond_compute_features(bond);
+
+	write_unlock_bh(&bond->lock);
 
 	read_lock(&bond->lock);
 
