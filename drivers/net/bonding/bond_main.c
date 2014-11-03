89903ed8afe880716bfd107a3ede97fec38f688e
Nikolay Aleksandrov <nikolay@redhat.com>
2014-11-03 15:08:16 -0500

    revert "bonding: Fix deadlock in bonding driver resulting from internal locking when using netpoll"
    Patchwork-id: 98786
    O-Subject: [RHEL6.7 PATCH BZ 1159818 073/657] Revert "[net] bonding: Fix deadlock in bonding driver resulting from internal locking when using netpoll"
    Bugzilla: 1159818
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    This reverts commit 9f70fa4ed03a1c11cd6f39c2544be63326f47fb5.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -74,7 +74,6 @@
 #include <linux/if_vlan.h>
 #include <linux/if_bonding.h>
 #include <linux/jiffies.h>
-#include <linux/preempt.h>
 #include <net/route.h>
 #include <net/net_namespace.h>
 #include "bonding.h"
@@ -166,10 +165,6 @@ module_param(resend_igmp, int, 0);
 MODULE_PARM_DESC(resend_igmp, "Number of IGMP membership reports to send on link failure");
 
 /*----------------------------- Global variables ----------------------------*/
-
-#ifdef CONFIG_NET_POLL_CONTROLLER
-cpumask_var_t netpoll_block_tx;
-#endif
 
 static const char * const version =
 	DRV_DESCRIPTION ": v" DRV_VERSION " (" DRV_RELDATE ")\n";
@@ -314,7 +309,6 @@ static int bond_del_vlan(struct bonding *bond, unsigned short vlan_id)
 
 	pr_debug("bond: %s, vlan id %d\n", bond->dev->name, vlan_id);
 
-	block_netpoll_tx();
 	write_lock_bh(&bond->lock);
 
 	list_for_each_entry(vlan, &bond->vlan_list, vlan_list) {
@@ -349,7 +343,6 @@ static int bond_del_vlan(struct bonding *bond, unsigned short vlan_id)
 
 out:
 	write_unlock_bh(&bond->lock);
-	unblock_netpoll_tx();
 	return res;
 }
 
@@ -1999,7 +1992,6 @@ int bond_release(struct net_device *bond_dev, struct net_device *slave_dev)
 		return -EINVAL;
 	}
 
-	block_netpoll_tx();
 	netdev_bonding_change(bond_dev, NETDEV_BONDING_DESLAVE);
 	write_lock_bh(&bond->lock);
 
@@ -2010,7 +2002,6 @@ int bond_release(struct net_device *bond_dev, struct net_device *slave_dev)
 		       ": %s: %s not enslaved\n",
 		       bond_dev->name, slave_dev->name);
 		write_unlock_bh(&bond->lock);
-		unblock_netpoll_tx();
 		return -EINVAL;
 	}
 
@@ -2116,7 +2107,6 @@ int bond_release(struct net_device *bond_dev, struct net_device *slave_dev)
 	}
 
 	write_unlock_bh(&bond->lock);
-	unblock_netpoll_tx();
 
 	/* must do this from outside any spinlocks */
 	bond_destroy_slave_symlinks(bond_dev, slave_dev);
@@ -2232,7 +2222,6 @@ static int bond_release_all(struct net_device *bond_dev)
 
 	netdev_bonding_change(bond_dev, NETDEV_BONDING_DESLAVE);
 
-	block_netpoll_tx();
 	write_lock_bh(&bond->lock);
 
 	netif_carrier_off(bond_dev);
@@ -2337,7 +2326,6 @@ static int bond_release_all(struct net_device *bond_dev)
 
 out:
 	write_unlock_bh(&bond->lock);
-	unblock_netpoll_tx();
 
 	return 0;
 }
@@ -2387,11 +2375,9 @@ static int bond_ioctl_change_active(struct net_device *bond_dev, struct net_devi
 	    (old_active) &&
 	    (new_active->link == BOND_LINK_UP) &&
 	    IS_UP(new_active->dev)) {
-		block_netpoll_tx();
 		write_lock_bh(&bond->curr_slave_lock);
 		bond_change_active_slave(bond, new_active);
 		write_unlock_bh(&bond->curr_slave_lock);
-		unblock_netpoll_tx();
 	} else
 		res = -EINVAL;
 
@@ -2633,11 +2619,9 @@ static void bond_miimon_commit(struct bonding *bond)
 
 do_failover:
 		ASSERT_RTNL();
-		block_netpoll_tx();
 		write_lock_bh(&bond->curr_slave_lock);
 		bond_select_active_slave(bond);
 		write_unlock_bh(&bond->curr_slave_lock);
-		unblock_netpoll_tx();
 	}
 
 	bond_set_carrier(bond);
@@ -3096,13 +3080,11 @@ void bond_loadbalance_arp_mon(struct work_struct *work)
 	}
 
 	if (do_failover) {
-		block_netpoll_tx();
 		write_lock_bh(&bond->curr_slave_lock);
 
 		bond_select_active_slave(bond);
 
 		write_unlock_bh(&bond->curr_slave_lock);
-		unblock_netpoll_tx();
 	}
 
 re_arm:
@@ -3266,11 +3248,9 @@ static void bond_ab_arp_commit(struct bonding *bond, int delta_in_ticks)
 
 do_failover:
 		ASSERT_RTNL();
-		block_netpoll_tx();
 		write_lock_bh(&bond->curr_slave_lock);
 		bond_select_active_slave(bond);
 		write_unlock_bh(&bond->curr_slave_lock);
-		unblock_netpoll_tx();
 	}
 
 	bond_set_carrier(bond);
@@ -4760,13 +4740,6 @@ static netdev_tx_t bond_start_xmit(struct sk_buff *skb, struct net_device *dev)
 {
 	struct bonding *bond = netdev_priv(dev);
 
-	/*
-	 * If we risk deadlock from transmitting this in the
-	 * netpoll path, tell netpoll to queue the frame for later tx
-	 */
-	if (is_netpoll_tx_blocked(dev))
-		return NETDEV_TX_BUSY;
-
 	if (TX_QUEUE_OVERRIDE(bond->params.mode)) {
 		if (!bond_slave_override(bond, skb))
 			return NETDEV_TX_OK;
@@ -5511,13 +5484,6 @@ static int __init bonding_init(void)
 	if (res)
 		goto out;
 
-#ifdef CONFIG_NET_POLL_CONTROLLER
-	if (!alloc_cpumask_var(&netpoll_block_tx, GFP_KERNEL)) {
-		res = -ENOMEM;
-		goto out;
-	}
-#endif
-
 	bond_create_proc_dir();
 
 	for (i = 0; i < max_bonds; i++) {
@@ -5539,9 +5505,6 @@ err:
 	rtnl_lock();
 	bond_free_all();
 	rtnl_unlock();
-#ifdef CONFIG_NET_POLL_CONTROLLER
-	free_cpumask_var(netpoll_block_tx);
-#endif
 out:
 	return res;
 
@@ -5558,10 +5521,6 @@ static void __exit bonding_exit(void)
 	rtnl_lock();
 	bond_free_all();
 	rtnl_unlock();
-
-#ifdef CONFIG_NET_POLL_CONTROLLER
-	free_cpumask_var(netpoll_block_tx);
-#endif
 }
 
 module_init(bonding_init);
