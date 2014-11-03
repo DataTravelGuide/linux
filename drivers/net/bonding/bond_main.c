b626f6e687dee80ab0c5692aef7fbb813fad8c77
Nikolay Aleksandrov <nikolay@redhat.com>
2014-11-03 15:17:30 -0500

    bonding: destroy proc directory only after all bonds are gone
    Patchwork-id: 99335
    O-Subject: [RHEL6.7 PATCH BZ 1159818 627/657] bonding: destroy proc directory only after all bonds are gone
    Bugzilla: 1159818
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    From: Veaceslav Falico <vfalico@gmail.com>
    
    Currently we might arrive to bond_net_exit() with some bonds left (that
    were created while the module is unloading). We take care of that by
    destroying sysfs (the last possibility to add new bonds) and then
    destroying all the remaining bonds.
    
    However, we destroy the /proc/net/bonding directory before destroying those
    last bonds, and get a warning that we're trying to destroy a non-empty
    proc directory (containing /proc/net/bonding/bondX).
    
    Fix this by moving bond_destroy_proc_dir() after all the bonds are
    destroyed, so that we're sure that no bonds exist.
    
    CC: Jay Vosburgh <j.vosburgh@gmail.com>
    CC: Andy Gospodarek <andy@greyhouse.net>
    Signed-off-by: Veaceslav Falico <vfalico@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 23fa5c2caae08f919d906b1064b9fdc352b3024e)
    
    Conflicts:
    	drivers/net/bonding/bond_main.c
    	- due to missing sysfs function and namespace patches, merged
    	  the code.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -4866,8 +4866,6 @@ static void __net_exit bond_net_exit(struct net *net)
 	struct bonding *bond, *tmp_bond;
 	LIST_HEAD(list);
 
-	bond_destroy_proc_dir(bn);
-
 	/* Kill off any bonds created after unregistering bond rtnl ops */
 	rtnl_lock();
 	list_for_each_entry_safe(bond, tmp_bond, &bn->dev_list, bond_list)
@@ -4875,6 +4873,7 @@ static void __net_exit bond_net_exit(struct net *net)
 	unregister_netdevice_many(&list);
 	rtnl_unlock();
 
+	bond_destroy_proc_dir(bn);
 	kfree(bn);
 }
 
