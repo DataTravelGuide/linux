e400729c06df18c034caf112be8377e93a56e648
Nikolay Aleksandrov <nikolay@redhat.com>
2014-11-03 15:11:33 -0500

    bonding: Manage /proc/net/bonding/ entries from the netdev events
    Patchwork-id: 98982
    O-Subject: [RHEL6.7 PATCH BZ 1159818 270/657] bonding: Manage /proc/net/bonding/ entries from the netdev events
    Bugzilla: 1159818
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    From: "Eric W. Biederman" <ebiederm@xmission.com>
    
    It was recently reported that moving a bonding device between network
    namespaces causes warnings from /proc.  It turns out after the move we
    were trying to add and to remove the /proc/net/bonding entries from the
    wrong network namespace.
    
    Move the bonding /proc registration code into the NETDEV_REGISTER and
    NETDEV_UNREGISTER events where the proc registration and unregistration
    will always happen at the right time.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit a64d49c3dd504b685f9742a2f3dcb11fb8e4345f)
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -3377,6 +3377,12 @@ static int bond_master_netdev_event(unsigned long event,
 	switch (event) {
 	case NETDEV_CHANGENAME:
 		return bond_event_changename(event_bond);
+	case NETDEV_UNREGISTER:
+		bond_remove_proc_entry(event_bond);
+		break;
+	case NETDEV_REGISTER:
+		bond_create_proc_entry(event_bond);
+		break;
 	default:
 		break;
 	}
@@ -4550,8 +4556,6 @@ static void bond_uninit(struct net_device *bond_dev)
 
 	bond_work_cancel_all(bond);
 
-	bond_remove_proc_entry(bond);
-
 	bond_debug_unregister(bond);
 
 	netif_addr_lock_bh(bond_dev);
@@ -4955,7 +4959,6 @@ static int bond_init(struct net_device *bond_dev)
 
 	bond_set_lockdep_class(bond_dev);
 
-	bond_create_proc_entry(bond);
 	list_add_tail(&bond->bond_list, &bn->dev_list);
 
 	bond_prepare_sysfs_group(bond);
