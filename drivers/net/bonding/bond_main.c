208b8aab35980a64e27abc39359388b0db442d5e
Nikolay Aleksandrov <nikolay@redhat.com>
2014-11-03 15:08:12 -0500

    revert "bonding: rename NETDEV_BONDING_DESLAVE to NETDEV_RELEASE"
    Patchwork-id: 98777
    O-Subject: [RHEL6.7 PATCH BZ 1159818 069/657] Revert [net] net: rename NETDEV_BONDING_DESLAVE to NETDEV_RELEASE
    Bugzilla: 1159818
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    Commit 933413afee3579c93faa4ab6120309cab648483b
    Bonding part only.
    
    Signed-off-by: Nikolay Aleksandrov <nikolay@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -2002,7 +2002,7 @@ int bond_release(struct net_device *bond_dev, struct net_device *slave_dev)
 	}
 
 	block_netpoll_tx();
-	netdev_bonding_change(bond_dev, NETDEV_RELEASE);
+	netdev_bonding_change(bond_dev, NETDEV_BONDING_DESLAVE);
 	write_lock_bh(&bond->lock);
 
 	slave = bond_get_slave_by_dev(bond, slave_dev);
@@ -2232,7 +2232,7 @@ static int bond_release_all(struct net_device *bond_dev)
 	struct net_device *slave_dev;
 	struct sockaddr addr;
 
-	netdev_bonding_change(bond_dev, NETDEV_RELEASE);
+	netdev_bonding_change(bond_dev, NETDEV_BONDING_DESLAVE);
 
 	write_lock_bh(&bond->lock);
 
