804e520b6d72ccf8825d50710b64c98c677c7c86
Nikolay Aleksandrov <nikolay@redhat.com>
2014-11-03 15:17:09 -0500

    bonding: get rid of bond_option_active_slave_get()
    Patchwork-id: 99297
    O-Subject: [RHEL6.7 PATCH BZ 1159818 606/657] bonding: get rid of bond_option_active_slave_get()
    Bugzilla: 1159818
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    From: Eric Dumazet <edumazet@google.com>
    
    Only keep bond_option_active_slave_get_rcu() helper.
    
    bond_fill_info() uses a new bond_option_active_slave_get_ifindex()
    helper.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Veaceslav Falico <vfalico@gmail.com>
    Reviewed-by: Nikolay Aleksandrov <nikolay@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit e965f8049460569bab12fe7bb5381bb2279712e2)
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/bonding/bonding.h
+++ drivers/net/bonding/bonding.h
@@ -526,7 +526,6 @@ int bond_get_tx_queues(struct net *net, struct nlattr *tb[],
 int bond_netlink_init(void);
 void bond_netlink_fini(void);
 struct net_device *bond_option_active_slave_get_rcu(struct bonding *bond);
-struct net_device *bond_option_active_slave_get(struct bonding *bond);
 const char *bond_slave_link_status(s8 link);
 
 #ifdef CONFIG_PROC_FS
