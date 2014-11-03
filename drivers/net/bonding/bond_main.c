7cbb086d73f6298ee7eb02b402c69479adf2d07a
Nikolay Aleksandrov <nikolay@redhat.com>
2014-11-03 15:11:41 -0500

    bonding: rhel-specific: remove read_lock around netpoll_cleanup
    Patchwork-id: 98991
    O-Subject: [RHEL6.7 PATCH BZ 1159818 278/657] bonding: RHEL-specific: remove read_lock around netpoll_cleanup
    Bugzilla: 1159818
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    Since we cannot bring back 38e6bc185d9544dfad1774b3f8902a0b061aea25
    netpoll: make __netpoll_cleanup non-block
    We need to fix the situation and since netpoll_cleanup is called with
    rtnl and slave manipulation is also protected by rtnl we shouldn't really
    need the read_lock(). So just remove it.
    
    Signed-off-by: Nikolay Aleksandrov <nikolay@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -1434,9 +1434,7 @@ static void bond_netpoll_cleanup(struct net_device *bond_dev)
 {
 	struct bonding *bond = netdev_priv(bond_dev);
 
-	read_lock(&bond->lock);
 	__bond_netpoll_cleanup(bond);
-	read_unlock(&bond->lock);
 }
 
 static int bond_netpoll_setup(struct net_device *dev, struct netpoll_info *ni, gfp_t gfp)
