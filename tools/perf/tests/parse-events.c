daf159365ff87a797da10f5d127f0f5f13dbf580
Jiri Olsa <jolsa@redhat.com>
2014-11-06 10:19:52 -0500

    tools: Factor sysfs code into generic fs object
    Patchwork-id: 99683
    O-Subject: [PATCH RHEL6.7 BZ1159929 264/320] perf tools: Factor sysfs code into generic fs object
    Bugzilla: 1159929
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    
    Bugzilla: 1159929
    https://bugzilla.redhat.com/show_bug.cgi?id=1159929
    
    upstream
    ========
    commit 4299a549979783668d787959d61ba22b6b200877
    Author: Jiri Olsa <jolsa@redhat.com>
    Date: Tue Nov 5 15:14:45 2013 +0100
    
    description
    ===========
    Moving sysfs code into generic fs object and preparing it to carry
    procfs support.
    
    This should be merged with tools/lib/lk/debugfs.c at some point in the
    future.
    
    [ Added fs__ namespace qualifier to some more functions ]
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- tools/perf/tests/parse-events.c
+++ tools/perf/tests/parse-events.c
@@ -2,10 +2,10 @@
 #include "parse-events.h"
 #include "evsel.h"
 #include "evlist.h"
-#include "sysfs.h"
 /* XXX There's no support for HW_BREAKPOINT in RHEL6 yet.
 #include "../../../include/linux/hw_breakpoint.h"
 */
+#include "fs.h"
 #include <lk/debugfs.h>
 #include "tests.h"
 
