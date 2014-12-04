3a6bceb25e0d8c4a5ad3d7b405e6abee1cf5c4ea
Mike Snitzer <snitzer@redhat.com>
2014-12-04 01:45:40 -0500

    dm: sparse: Annotate field with __rcu for checking
    Patchwork-id: 101131
    O-Subject: [RHEL6.7 PATCH 07/18] dm: sparse: Annotate field with __rcu for checking
    Bugzilla: 1169431
    RH-Acked-by: Heinz Mauelshagen <heinzm@redhat.com>
    RH-Acked-by: Joe Thornber <thornber@redhat.com>
    
    BZ: 1169431
    
    Upstream linux-dm.git commit 6fa9952097747f71c5077f3e14ce3f8adee6f778
    Author: Pranith Kumar <bobby.prani@gmail.com>
    Date:   Tue Oct 28 15:09:57 2014 -0700
    
        dm: sparse: Annotate field with __rcu for checking
    
        Annotate the map field with __rcu since this is a rcu pointer which is checked
        by sparse.
    
        Signed-off-by: Pranith Kumar <bobby.prani@gmail.com>
        Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
        Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -193,7 +193,7 @@ struct mapped_device {
 	 * Use dm_get_live_table{_fast} or take suspend_lock for
 	 * dereference.
 	 */
-	struct dm_table *map;
+	struct dm_table __rcu *map;
 
 	/*
 	 * io objects are allocated from here.
