6af0350b6a82445581f9b91534b3318de0f81fc4
Hendrik Brueckner <brueckner@redhat.com>
2014-12-12 11:51:29 -0500

    dasd: add support for control unit initiated reconfiguration
    Patchwork-id: 101705
    O-Subject: [RHEL6.7 PATCH 3/3] [s390] s390/dasd: add support for control unit initiated reconfiguration
    Bugzilla: 1148119
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    Description
    -----------
    commit 5db8440c36a3308649b99f65b68195394dd7fed4
    Author: Stefan Haberland <stefan.haberland@de.ibm.com>
    Commit: Martin Schwidefsky <schwidefsky@de.ibm.com>
    
        s390/dasd: add support for control unit initiated reconfiguration
    
        Add support for Control Unit Initiated Reconfiguration (CUIR) to
        Linux, a storage server interface to reconcile concurrent hardware
        changes between storage and host.
    
        Reviewed-by: Stefan Weinhuber <wein@de.ibm.com>
        Signed-off-by: Stefan Haberland <stefan.haberland@de.ibm.com>
        Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    
    Bugzilla
    --------
    BZ 1148119
    https://bugzilla.redhat.com/show_bug.cgi?id=1148119
    
    Upstream status of the patch
    ----------------------------
    The patch is upstream as of kernel version v3.18-rc1
    http://git.kernel.org/?p=linux/kernel/git/torvalds/linux-2.6.git;a=commitdiff;h=5db8440c36a3308649b99f65b68195394dd7fed4
    
    Brew
    ----
    https://brewweb.devel.redhat.com/taskinfo?taskID=8350198
    
    Test status
    -----------
    The patch has been tested and fixes the problem.
    The fix has been verified by the IBM test department.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/s390/block/dasd_int.h
+++ drivers/s390/block/dasd_int.h
@@ -357,6 +357,7 @@ struct dasd_discipline {
 	int (*reload) (struct dasd_device *);
 
 	void (*kick_validate) (struct dasd_device *);
+	int (*check_attention)(struct dasd_device *, __u8);
 };
 
 extern struct dasd_discipline *dasd_diag_discipline_pointer;
@@ -382,6 +383,10 @@ struct dasd_path {
 	__u8 tbvpm;
 	__u8 ppm;
 	__u8 npm;
+	/* paths that are not used because of a special condition */
+	__u8 cablepm; /* miss-cabled */
+	__u8 hpfpm;   /* the HPF requirements of the other paths are not met */
+	__u8 cuirpm;  /* CUIR varied offline */
 };
 
 struct dasd_profile_info {
@@ -498,7 +503,10 @@ struct dasd_block {
 	struct dasd_profile profile;
 };
 
-
+struct dasd_attention_data {
+	struct dasd_device *device;
+	__u8 lpum;
+};
 
 /* reasons why device (ccw_device_start) was stopped */
 #define DASD_STOPPED_NOT_ACC 1         /* not accessible */
