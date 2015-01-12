b93210bcbe38897d096b248066f592225762915d
Myron Stowe <myron.stowe@redhat.com>
2015-01-12 14:59:45 -0500

    export __pci_remove_bus_device
    Patchwork-id: 102806
    O-Subject: [RHEL6.7 PATCH 05/22] PCI: export __pci_remove_bus_device
    Bugzilla: 1170329
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1170329
    Upstream Status: 6b22cf3f35fd332e4cc2c1b27056920b3643667a
    
    commit 6b22cf3f35fd332e4cc2c1b27056920b3643667a
    Author: Yinghai Lu <yinghai@kernel.org>
    Date:   Sat Feb 25 13:54:21 2012 -0800
    
        PCI: export __pci_remove_bus_device
    
        Don't switch to pci_remove_bus_device yet, keep the __ prefix for now
        (the behavior is still the same: remove without stopping first).
    
        This allows other out of tree users or pending patches to get notified
        from compiler warning.
    
        Signed-off-by: Yinghai Lu <yinghai@kernel.org>
        Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -729,6 +729,7 @@ extern void pci_remove_bus(struct pci_bus *b);
  * pci_stop_and_remove_bus_device instead
  */
 extern void pci_remove_bus_device(struct pci_dev *dev);
+extern void __pci_remove_bus_device(struct pci_dev *dev);
 extern void pci_stop_and_remove_bus_device(struct pci_dev *dev);
 extern void pci_stop_bus_device(struct pci_dev *dev);
 void pci_setup_cardbus(struct pci_bus *bus);
