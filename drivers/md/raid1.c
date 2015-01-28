4c03104f6abf60cb691f448bd60717db8671a63d
Jes Sorensen <Jes.Sorensen@redhat.com>
2015-01-27 23:00:51 -0500

    raid1: process_checks doesn't use its return value
    Patchwork-id: 103714
    O-Subject: [RHEL6.7 PATCH 35/53] md/raid1: process_checks doesn't use its return value.
    Bugzilla: 1136260 1149935 1152914 1153341 1177913
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Doug Ledford <dledford@redhat.com>
    
    From: NeilBrown <neilb@suse.de>
    
    process_checks() always returns '0', so change it to 'void'.
    
    Signed-off-by: NeilBrown <neilb@suse.de>
    (cherry picked from commit c95e6385e8098ef549008ab29d671bf528a50043)
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/md/raid1.c
+++ drivers/md/raid1.c
@@ -1961,7 +1961,7 @@ static int fix_sync_read_error(struct r1bio *r1_bio)
 	return 1;
 }
 
-static int process_checks(struct r1bio *r1_bio)
+static void process_checks(struct r1bio *r1_bio)
 {
 	/* We have read all readable devices.  If we haven't
 	 * got the block, then there is no hope left.
@@ -2062,7 +2062,6 @@ static int process_checks(struct r1bio *r1_bio)
 			       PAGE_SIZE);
 		}
 	}
-	return 0;
 }
 
 static void sync_request_write(struct mddev *mddev, struct r1bio *r1_bio)
@@ -2080,8 +2079,8 @@ static void sync_request_write(struct mddev *mddev, struct r1bio *r1_bio)
 			return;
 
 	if (test_bit(MD_RECOVERY_REQUESTED, &mddev->recovery))
-		if (process_checks(r1_bio) < 0)
-			return;
+		process_checks(r1_bio);
+
 	/*
 	 * schedule writes
 	 */
