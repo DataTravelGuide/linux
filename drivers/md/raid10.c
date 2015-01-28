0f2b414c834a1955885ad62f53c3c45651c6c9b0
Jes Sorensen <Jes.Sorensen@redhat.com>
2015-01-27 23:00:36 -0500

    raid10: always initialise ->state on newly allocated r10_bio
    Patchwork-id: 103717
    O-Subject: [RHEL6.7 PATCH 20/53] md/raid10: always initialise ->state on newly allocated r10_bio
    Bugzilla: 1136260 1149935 1152914 1153341 1177913
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Doug Ledford <dledford@redhat.com>
    
    From: NeilBrown <neilb@suse.de>
    
    Most places which allocate an r10_bio zero the ->state, some don't.
    As the r10_bio comes from a mempool, and the allocation function uses
    kzalloc it is often zero anyway.  But sometimes it isn't and it is
    best to be safe.
    
    I only noticed this because of the bug fixed by an earlier patch
    where the r10_bios allocated for a reshape were left around to
    be used by a subsequent resync.  In that case the R10BIO_IsReshape
    flag caused problems.
    
    Signed-off-by: NeilBrown <neilb@suse.de>
    (cherry picked from commit cb8b12b5d81cf8522076f99a90bc3b795825c3b3)
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/md/raid10.c
+++ drivers/md/raid10.c
@@ -3118,6 +3118,7 @@ static sector_t sync_request(struct mddev *mddev, sector_t sector_nr,
 			}
 
 			r10_bio = mempool_alloc(conf->r10buf_pool, GFP_NOIO);
+			r10_bio->state = 0;
 			raise_barrier(conf, rb2 != NULL);
 			atomic_set(&r10_bio->remaining, 0);
 
@@ -3301,6 +3302,7 @@ static sector_t sync_request(struct mddev *mddev, sector_t sector_nr,
 		if (sync_blocks < max_sync)
 			max_sync = sync_blocks;
 		r10_bio = mempool_alloc(conf->r10buf_pool, GFP_NOIO);
+		r10_bio->state = 0;
 
 		r10_bio->mddev = mddev;
 		atomic_set(&r10_bio->remaining, 0);
@@ -4426,6 +4428,7 @@ static sector_t reshape_request(struct mddev *mddev, sector_t sector_nr,
 read_more:
 	/* Now schedule reads for blocks from sector_nr to last */
 	r10_bio = mempool_alloc(conf->r10buf_pool, GFP_NOIO);
+	r10_bio->state = 0;
 	raise_barrier(conf, sectors_done != 0);
 	atomic_set(&r10_bio->remaining, 0);
 	r10_bio->mddev = mddev;
