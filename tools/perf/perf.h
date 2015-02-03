2cd9fa7d9d1f87d25c3816b333749c0019d3f90d
Jiri Olsa <jolsa@redhat.com>
2015-02-03 15:45:33 -0500

    tools: Consolidate types.h
    Patchwork-id: 104159
    O-Subject: [PATCH RHEL6.7 BZ1188336 159/280] tools: Consolidate types.h
    Bugzilla: 1188336
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Petr Holasek <pholasek@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    From: Jiri Olsa <jolsa@kernel.org>
    
    Bugzilla: 1188336
    https://bugzilla.redhat.com/show_bug.cgi?id=1188336
    
    upstream
    ========
    commit d944c4eebcf4c0d5e5d9728fec110cbf0047ad7f
    Author: Borislav Petkov <bp@suse.de>
    Date: Fri Apr 25 21:31:02 2014 +0200
    
    RHEL6
    =====
    Omitting tsc.c hunks, because it hasn't been backported yet.
    Omitting lockdep and virtion hunks. Also RHEL6 code still defines
    hlist_head and hlist_node include/linux/list.h, hence there's
    the difference in types.h file.
    
    description
    ===========
    Combine all definitions into a common tools/include/linux/types.h and
    kill the wild growth elsewhere. Move DECLARE_BITMAP to its proper
    bitmap.h header.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- tools/perf/perf.h
+++ tools/perf/perf.h
@@ -183,9 +183,8 @@
 #include <sys/types.h>
 #include <sys/syscall.h>
 
+#include <linux/types.h>
 #include "../../include/linux/perf_event.h"
-#include "util/types.h"
-#include <stdbool.h>
 
 /*
  * prctl(PR_TASK_PERF_EVENTS_DISABLE) will (cheaply) disable all
@@ -212,7 +211,6 @@ static inline unsigned long long rdclock(void)
 /*
  * Pick up some kernel type conventions:
  */
-#define __user
 #define asmlinkage
 
 #define unlikely(x)	__builtin_expect(!!(x), 0)
