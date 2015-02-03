7138d7e78ac87a4280b2170a132525919bd35f28
Jiri Olsa <jolsa@redhat.com>
2015-02-03 15:44:52 -0500

    hists: Add support for showing relative percentage
    Patchwork-id: 104123
    O-Subject: [PATCH RHEL6.7 BZ1188336 118/280] perf hists: Add support for showing relative percentage
    Bugzilla: 1188336
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Petr Holasek <pholasek@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    From: Jiri Olsa <jolsa@kernel.org>
    
    Bugzilla: 1188336
    https://bugzilla.redhat.com/show_bug.cgi?id=1188336
    
    upstream
    ========
    commit 1ab1fa5dfb429c533fbc791e524788cf0cc43775
    Author: Namhyung Kim <namhyung@kernel.org>
    Date: Thu Dec 26 15:11:52 2013 +0900
    
    description
    ===========
    When filtering by thread, dso or symbol on TUI it also update total
    period so that the output shows different result than no filter - the
    percentage changed to relative to filtered entries only.  Sometimes
    this is not desired since users might expect same results with filter.
    
    So new filtered_* fields to hists->stats to count them separately.
    They'll be controlled/used by user later.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- tools/perf/builtin-report.c
+++ tools/perf/builtin-report.c
@@ -125,6 +125,8 @@ static int report__add_mem_hist_entry(struct report *rep, struct addr_location *
 
 	evsel->hists.stats.total_period += cost;
 	hists__inc_nr_events(&evsel->hists, PERF_RECORD_SAMPLE);
+	if (!he->filtered)
+		evsel->hists.stats.nr_non_filtered_samples++;
 	err = hist_entry__append_callchain(he, sample);
 out:
 	return err;
@@ -178,6 +180,8 @@ static int report__add_branch_hist_entry(struct report *rep, struct addr_locatio
 
 			evsel->hists.stats.total_period += 1;
 			hists__inc_nr_events(&evsel->hists, PERF_RECORD_SAMPLE);
+			if (!he->filtered)
+				evsel->hists.stats.nr_non_filtered_samples++;
 		} else
 			goto out;
 	}
@@ -211,6 +215,8 @@ static int report__add_hist_entry(struct report *rep, struct perf_evsel *evsel,
 		err = hist_entry__inc_addr_samples(he, evsel->idx, al->addr);
 
 	evsel->hists.stats.total_period += sample->period;
+	if (!he->filtered)
+		evsel->hists.stats.nr_non_filtered_samples++;
 	hists__inc_nr_events(&evsel->hists, PERF_RECORD_SAMPLE);
 out:
 	return err;
