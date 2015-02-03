0d062a1ad109bac4fa5a1a315444524342f7be57
Jiri Olsa <jolsa@redhat.com>
2015-02-03 15:46:46 -0500

    tests: Add a test case for cumulating callchains
    Patchwork-id: 104240
    O-Subject: [PATCH RHEL6.7 BZ1188336 232/280] perf tests: Add a test case for cumulating callchains
    Bugzilla: 1188336
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Petr Holasek <pholasek@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    From: Jiri Olsa <jolsa@kernel.org>
    
    Bugzilla: 1188336
    https://bugzilla.redhat.com/show_bug.cgi?id=1188336
    
    upstream
    ========
    commit 0506aecce999d4370b979892f88cf1118cfe8dcb
    Author: Namhyung Kim <namhyung@kernel.org>
    Date: Fri May 23 18:04:42 2014 +0900
    
    description
    ===========
    Now it adds a new testcase to verify --children option working
    correctly.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- tools/perf/tests/builtin-test.c
+++ tools/perf/tests/builtin-test.c
@@ -123,6 +123,10 @@ static struct test {
 		.func = test__hists_output,
 	},
 	{
+		.desc = "Test cumulation of child hist entries",
+		.func = test__hists_cumulate,
+	},
+	{
 		.func = NULL,
 	},
 };
