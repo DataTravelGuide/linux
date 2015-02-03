50ee1e79fa607abff0ad03edf18223fe3c4b3012
Maurizio Lombardi <mlombard@redhat.com>
2015-02-03 13:20:11 -0500

    cnic: Update the rcu_access_pointer() usages
    Patchwork-id: 103997
    O-Subject: [RHEL6.7 e-stor PATCH 6/6] cnic: Update the rcu_access_pointer() usages
    Bugzilla: 1147594
    RH-Acked-by: Tomas Henzl <thenzl@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    1. Remove the rcu_read_lock/unlock around rcu_access_pointer
    2. Replace the rcu_dereference with rcu_access_pointer
    
    Signed-off-by: Tej Parkash <tej.parkash@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 8f4eb70059ee834522ce90a6fce0aa3078c18620)
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/cnic.c
+++ drivers/net/cnic.c
@@ -375,10 +375,8 @@ static int cnic_iscsi_nl_msg_recv(struct cnic_dev *dev, u32 msg_type,
 		if (l5_cid >= MAX_CM_SK_TBL_SZ)
 			break;
 
-		rcu_read_lock();
 		if (!rcu_access_pointer(cp->ulp_ops[CNIC_ULP_L4])) {
 			rc = -ENODEV;
-			rcu_read_unlock();
 			break;
 		}
 		csk = &cp->csk_tbl[l5_cid];
@@ -407,7 +405,6 @@ static int cnic_iscsi_nl_msg_recv(struct cnic_dev *dev, u32 msg_type,
 			}
 		}
 		csk_put(csk);
-		rcu_read_unlock();
 		rc = 0;
 	}
 	}
@@ -608,7 +605,7 @@ static int cnic_unregister_device(struct cnic_dev *dev, int ulp_type)
 		cnic_send_nlmsg(cp, ISCSI_KEVENT_IF_DOWN, NULL);
 
 	mutex_lock(&cnic_lock);
-	if (rcu_dereference(cp->ulp_ops[ulp_type])) {
+	if (rcu_access_pointer(cp->ulp_ops[ulp_type])) {
 		rcu_assign_pointer(cp->ulp_ops[ulp_type], NULL);
 		cnic_put(dev);
 	} else {
