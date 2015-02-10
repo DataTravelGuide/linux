d511036133c86f9f879c58817fbb48fec55661b1
Radomir Vrbovsky <rvrbovsk@redhat.com>
2015-02-10 16:43:10 +0100

    Revert "[kernel] ipc/sem: synchronize semop and semctl with IPC_RMID"
    This reverts commit 8dafbec5203899dff3efdf65785736a42bbda9b4.

--- ipc/sem.c
+++ ipc/sem.c
@@ -1101,12 +1101,6 @@ static int semctl_setval(struct ipc_namespace *ns, int semid, int semnum,
 
 	sem_lock(sma, NULL, -1);
 
-	if (sma->sem_perm.deleted) {
-		sem_unlock(sma, -1);
-		rcu_read_unlock();
-		return -EIDRM;
-	}
-
 	curr = &sma->sem_base[semnum];
 
 	assert_spin_locked(&sma->sem_perm.lock);
@@ -1161,14 +1155,12 @@ static int semctl_main(struct ipc_namespace *ns, int semid, int semnum,
 		int i;
 
 		sem_lock(sma, NULL, -1);
-		if (sma->sem_perm.deleted) {
-			err = -EIDRM;
-			goto out_unlock;
-		}
 		if(nsems > SEMMSL_FAST) {
 			if (!ipc_rcu_getref(sma)) {
+				sem_unlock(sma, -1);
+				rcu_read_unlock();
 				err = -EIDRM;
-				goto out_unlock;
+				goto out_free;
 			}
 			sem_unlock(sma, -1);
 			rcu_read_unlock();
@@ -1181,8 +1173,10 @@ static int semctl_main(struct ipc_namespace *ns, int semid, int semnum,
 			rcu_read_lock();
 			sem_lock_and_putref(sma);
 			if (sma->sem_perm.deleted) {
+				sem_unlock(sma, -1);
+				rcu_read_unlock();
 				err = -EIDRM;
-				goto out_unlock;
+				goto out_free;
 			}
 		}
 		for (i = 0; i < sma->sem_nsems; i++)
@@ -1200,8 +1194,8 @@ static int semctl_main(struct ipc_namespace *ns, int semid, int semnum,
 		struct sem_undo *un;
 
 		if (!ipc_rcu_getref(sma)) {
-			err = -EIDRM;
-			goto out_rcu_wakeup;
+			rcu_read_unlock();
+			return -EIDRM;
 		}
 		rcu_read_unlock();
 
@@ -1229,8 +1223,10 @@ static int semctl_main(struct ipc_namespace *ns, int semid, int semnum,
 		rcu_read_lock();
 		sem_lock_and_putref(sma);
 		if (sma->sem_perm.deleted) {
+			sem_unlock(sma, -1);
+			rcu_read_unlock();
 			err = -EIDRM;
-			goto out_unlock;
+			goto out_free;
 		}
 
 		for (i = 0; i < nsems; i++)
@@ -1254,10 +1250,6 @@ static int semctl_main(struct ipc_namespace *ns, int semid, int semnum,
 		goto out_rcu_wakeup;
 
 	sem_lock(sma, NULL, -1);
-	if (sma->sem_perm.deleted) {
-		err = -EIDRM;
-		goto out_unlock;
-	}
 	curr = &sma->sem_base[semnum];
 
 	switch (cmd) {
@@ -1663,10 +1655,6 @@ SYSCALL_DEFINE4(semtimedop, int, semid, struct sembuf __user *, tsops,
 	if (error)
 		goto out_rcu_wakeup;
 
-	error = -EIDRM;
-	locknum = sem_lock(sma, sops, nsops);
-	if (sma->sem_perm.deleted)
-		goto out_unlock_free;
 	/*
 	 * semid identifiers are not unique - find_alloc_undo may have
 	 * allocated an undo structure, it was invalidated by an RMID
@@ -1674,6 +1662,8 @@ SYSCALL_DEFINE4(semtimedop, int, semid, struct sembuf __user *, tsops,
 	 * This case can be detected checking un->semid. The existance of
 	 * "un" itself is guaranteed by rcu.
 	 */
+	error = -EIDRM;
+	locknum = sem_lock(sma, sops, nsops);
 	if (un && un->semid == -1)
 		goto out_unlock_free;
 
@@ -1871,12 +1861,6 @@ void exit_sem(struct task_struct *tsk)
 		}
 
 		sem_lock(sma, NULL, -1);
-		/* exit_sem raced with IPC_RMID, nothing to do */
-		if (sma->sem_perm.deleted) {
-			sem_unlock(sma, -1);
-			rcu_read_unlock();
-			continue;
-		}
 		un = __lookup_undo(ulp, semid);
 		if (un == NULL) {
 			/* exit_sem raced with IPC_RMID+semget() that created
