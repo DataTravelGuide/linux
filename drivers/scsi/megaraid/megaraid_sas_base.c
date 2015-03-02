35ca55691098841da0b5f3c2a813ecbf227f5d1a
Tomas Henzl <thenzl@redhat.com>
2015-03-02 17:25:15 -0500

    megaraid_sas: Revert "driver version upgrade and remove some meta data of driver"
    Patchwork-id: 106605
    O-Subject: [RHEL6.7 e-stor PATCH 1/1] Revert "[scsi] megaraid_sas: driver version upgrade and remove some meta data of driver"
    Bugzilla: 1197811
    RH-Acked-by: Rob Evers <revers@redhat.com>
    RH-Acked-by: David Milburn <dmilburn@redhat.com>
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    
    This reverts commit 01bf2b34ed5466ee4c33a6d1db0e956821135e8a -RHEL6
    d98a6deb75bce12784c0c7f1fdc02e6a67adac4d - scsi-queue
    
    In addition to that I've touched version string is updated.
    
    RH - only - Avago has asked us to revert this patch from the
    series we have previously added. Avago has found issues with
    an application and is going to post a revert to the mainline too.
    
    Signed-off-by: Tomas Henzl <thenzl@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/megaraid/megaraid_sas_base.c
+++ drivers/scsi/megaraid/megaraid_sas_base.c
@@ -6521,6 +6521,16 @@ static ssize_t megasas_sysfs_show_version(struct device_driver *dd, char *buf)
 static DRIVER_ATTR(version, S_IRUGO, megasas_sysfs_show_version, NULL);
 
 static ssize_t
+megasas_sysfs_show_release_date(struct device_driver *dd, char *buf)
+{
+	return snprintf(buf, strlen(MEGASAS_RELDATE) + 2, "%s\n",
+			MEGASAS_RELDATE);
+}
+
+static DRIVER_ATTR(release_date, S_IRUGO, megasas_sysfs_show_release_date,
+		   NULL);
+
+static ssize_t
 megasas_sysfs_show_support_poll_for_event(struct device_driver *dd, char *buf)
 {
 	return sprintf(buf, "%u\n", support_poll_for_event);
@@ -6823,7 +6833,8 @@ static int __init megasas_init(void)
 	/*
 	 * Announce driver version and other information
 	 */
-	pr_info("megasas: %s\n", MEGASAS_VERSION);
+	printk(KERN_INFO "megasas: %s %s\n", MEGASAS_VERSION,
+	       MEGASAS_EXT_VERSION);
 
 	spin_lock_init(&poll_aen_lock);
 
@@ -6858,6 +6869,10 @@ static int __init megasas_init(void)
 				  &driver_attr_version);
 	if (rval)
 		goto err_dcf_attr_ver;
+	rval = driver_create_file(&megasas_pci_driver.driver,
+				  &driver_attr_release_date);
+	if (rval)
+		goto err_dcf_rel_date;
 
 	rval = driver_create_file(&megasas_pci_driver.driver,
 				&driver_attr_support_poll_for_event);
@@ -6881,7 +6896,12 @@ err_dcf_support_device_change:
 err_dcf_dbg_lvl:
 	driver_remove_file(&megasas_pci_driver.driver,
 			&driver_attr_support_poll_for_event);
+
 err_dcf_support_poll_for_event:
+	driver_remove_file(&megasas_pci_driver.driver,
+			   &driver_attr_release_date);
+
+err_dcf_rel_date:
 	driver_remove_file(&megasas_pci_driver.driver, &driver_attr_version);
 err_dcf_attr_ver:
 	pci_unregister_driver(&megasas_pci_driver);
@@ -6901,6 +6921,8 @@ static void __exit megasas_exit(void)
 			&driver_attr_support_poll_for_event);
 	driver_remove_file(&megasas_pci_driver.driver,
 			&driver_attr_support_device_change);
+	driver_remove_file(&megasas_pci_driver.driver,
+			   &driver_attr_release_date);
 	driver_remove_file(&megasas_pci_driver.driver, &driver_attr_version);
 
 	pci_unregister_driver(&megasas_pci_driver);
