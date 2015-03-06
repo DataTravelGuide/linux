4a8b37f3465dabe7df1cb5cb7e9e0ac1230c6fe8
Prarit Bhargava <prarit@redhat.com>
2015-03-06 13:22:54 -0500

    Remove DEFINE_PCI_DEVICE_TABLE macro use
    Patchwork-id: 107028
    O-Subject: Re: [RHEL6.7 PATCH BZ 1198148 1/2] PCI: Remove DEFINE_PCI_DEVICE_TABLE macro use
    Bugzilla: 1198148
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Myron Stowe <mstowe@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1198148
    
    Conflicts: Obviously there has been code drift between upstream and RHEL6,
    so this patch doesn't exactly match the upstream version.  I have removed
    DEFINE_PCI_DEVICE_TABLE from use in our code to match that of upstream.
    
    commit 9baa3c34ac4e27f7e062f266f50cc5dbea26a6c1
    Author: Benoit Taine <benoit.taine@lip6.fr>
    Date:   Fri Aug 8 15:56:03 2014 +0200
    
        PCI: Remove DEFINE_PCI_DEVICE_TABLE macro use
    
        We should prefer `struct pci_device_id` over `DEFINE_PCI_DEVICE_TABLE` to
        meet kernel coding style guidelines.  This issue was reported by checkpatch.
    
        A simplified version of the semantic patch that makes this change is as
        follows (http://coccinelle.lip6.fr/):
    
        // <smpl>
    
        @@
        identifier i;
        declarer name DEFINE_PCI_DEVICE_TABLE;
        initializer z;
        @@
    
        - DEFINE_PCI_DEVICE_TABLE(i)
        + const struct pci_device_id i[]
        = z;
    
        // </smpl>
    
        [bhelgaas: add semantic patch]
        Signed-off-by: Benoit Taine <benoit.taine@lip6.fr>
        Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    
    v2: apply on top of sgruszka's wireless update for BZ 1189024
    
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Don Zickus <dzickus@redhat.com>
    Cc: Tony Camuso <tcamuso@redhat.com>
    Cc: Petr Holasek <pholasek@redhat.com>
    Cc: Myron Stowe <mstowe@redhat.com>
    Cc: Jes Sorensen <Jes.Sorensen@redhat.com>
    Cc: Rob Clark <rclark@redhat.com>
    Cc: Dave Airlie <airlied@redhat.com>
    Cc: Dean Nelson <dnelson@redhat.com>
    Cc: Neil Horman <nhorman@redhat.com>
    Cc: John Green <jogreene@redhat.com>
    Cc: Doug Ledford <dledford@redhat.com>
    Cc: Ivan Vecera <ivecera@redhat.com>
    Cc: Stefan Assmann <sassmann@redhat.com>
    Cc: Sai Vemuri <svemuri@redhat.com>
    Cc: Kyle McMartin <kmcmarti@redhat.com>
    Cc: Peter Martuccelli <peterm@redhat.com>
    Cc: Steve Best <sbest@redhat.com>
    Cc: Jiri Pirko <jpirko@redhat.com>
    Cc: David S. Miller <davem@redhat.com>
    Cc: Nikolay Aleksandrov <nikolay@redhat.com>
    Cc: Chad Dupuis <cdupuis@redhat.com>
    Cc: Stanislaw Gruszka <sgruszka@redhat.com>
    Cc: John Linville <linville@redhat.com>
    Cc: David Milburn <dmilburn@redhat.com>
    Cc: Ewan Milne <emilne@redhat.com>
    Cc: Tomas Henzl <thenzl@redhat.com>
    Cc: Rob Evers <revers@redhat.com>
    Cc: Jason Wang <jasowang@redhat.com>
    Cc: Vitaly Kuznetsov <vkuznets@redhat.com>
    Cc: Jaroslav Kysela <jkysela@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kernel/cpu/perf_event_intel_uncore_snbep.c
+++ arch/x86/kernel/cpu/perf_event_intel_uncore_snbep.c
@@ -1014,7 +1014,7 @@ static struct intel_uncore_type *snbep_pci_uncores[] = {
 	NULL,
 };
 
-static DEFINE_PCI_DEVICE_TABLE(snbep_uncore_pci_ids) = {
+static const struct pci_device_id snbep_uncore_pci_ids[] = {
 	{ /* Home Agent */
 		PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_UNC_HA),
 		.driver_data = UNCORE_PCI_DEV_DATA(SNBEP_PCI_UNCORE_HA, 0),
@@ -1607,7 +1607,7 @@ static struct intel_uncore_type *ivbep_pci_uncores[] = {
 	NULL,
 };
 
-static DEFINE_PCI_DEVICE_TABLE(ivbep_uncore_pci_ids) = {
+static const struct pci_device_id ivbep_uncore_pci_ids[] = {
 	{ /* Home Agent 0 */
 		PCI_DEVICE(PCI_VENDOR_ID_INTEL, 0xe30),
 		.driver_data = UNCORE_PCI_DEV_DATA(IVBEP_PCI_UNCORE_HA, 0),
@@ -2159,7 +2159,7 @@ static struct intel_uncore_type *hswep_pci_uncores[] = {
 	NULL,
 };
 
-static DEFINE_PCI_DEVICE_TABLE(hswep_uncore_pci_ids) = {
+static const struct pci_device_id hswep_uncore_pci_ids[] = {
 	{ /* Home Agent 0 */
 		PCI_DEVICE(PCI_VENDOR_ID_INTEL, 0x2f30),
 		.driver_data = UNCORE_PCI_DEV_DATA(HSWEP_PCI_UNCORE_HA, 0),
