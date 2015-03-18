0b3c301cae24d0f3bb2b66b1973ef087063c6b42
Herton R. Krzesinski <herton@redhat.com>
2015-03-18 04:23:27 -0400

    iommu/amd: Fix missing iommu_shutdown initialization in passthrough mode
    Patchwork-id: 107573
    O-Subject: [PATCH RHEL6 27/27] iommu/amd: Fix missing iommu_shutdown initialization in passthrough mode
    Bugzilla: 1198380
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Kyle McMartin <kmcmarti@redhat.com>
    RH-Acked-by: Myron Stowe <mstowe@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Alex Williamson <alex.williamson@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    
    From: Shuah Khan <shuah.khan@hp.com>
    
    The iommu_shutdown callback is not initialized when the AMD
    IOMMU driver runs in passthrough mode. Fix that by moving
    the callback initialization before the check for
    passthrough mode.
    
    Signed-off-by: Shuah Khan <shuah.khan@hp.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    (cherry picked from commit f2f12b6fc032c7b1419fd6db84e2868b5f05a878)
    [ herton: rediff changes against rhel 6 ]
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kernel/amd_iommu_init.c
+++ arch/x86/kernel/amd_iommu_init.c
@@ -1425,6 +1425,8 @@ static int __init amd_iommu_init(void)
 
 	amd_iommu_init_notifier();
 
+	x86_platform.iommu_shutdown = disable_iommus;
+
 	if (iommu_pass_through)
 		goto out;
 
@@ -1439,7 +1441,6 @@ static int __init amd_iommu_init(void)
 	else
 		printk(KERN_INFO "AMD-Vi: Lazy IO/TLB flushing enabled\n");
 
-	x86_platform.iommu_shutdown = disable_iommus;
 out:
 	return ret;
 
