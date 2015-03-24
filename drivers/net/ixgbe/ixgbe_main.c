6708a2a0ff9b8d00dab33d74676c2e404545b4e6
John Greene <jogreene@redhat.com>
2015-03-24 15:04:40 -0400

    ixgbe: fix crash on rmmod after probe fail
    Patchwork-id: 108190
    O-Subject: [RHEL6.7 PATCH v2 69/73] ixgbe: fix crash on rmmod after probe fail
    Bugzilla: 1150343
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Alexander Duyck <alexander.h.duyck@redhat.com>
    
    Bugzilla: 1150343
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=8869309
    Tested: In beaker. See BZ for details
    [redhat] deviated in ixgbe_remove due to the commit 5d7daa3 supporting
    mac_tables and b5b2ffc work around not being supported currently.
    
    The driver has logic to free up used data in case any of the checks in
    ixgbe_probe() fail, however there is a similar set of cleanups that can
    occur on driver unload in ixgbe_remove() which can cause the rmmod command
    to crash.
    
    This patch aims to fix the logic by moving pci_set_drvdata() after all error
    checks and then adds a check in ixgbe_remove() to skip it altogether if
    adapter comes up empty.
    
    Signed-off-by: Emil Tantilov <emil.s.tantilov@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit 0fb6a55cc31ff216ef86332404ad3e425cb669eb)

--- drivers/net/ixgbe/ixgbe_main.c
+++ drivers/net/ixgbe/ixgbe_main.c
@@ -7556,7 +7556,6 @@ static int ixgbe_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	SET_NETDEV_DEV(netdev, &pdev->dev);
 
 	adapter = netdev_priv(netdev);
-	pci_set_drvdata(pdev, adapter);
 
 	adapter->netdev = netdev;
 	adapter->pdev = pdev;
@@ -7835,6 +7834,8 @@ skip_sriov:
 	if (err)
 		goto err_register;
 
+	pci_set_drvdata(pdev, adapter);
+
 	/* power down the optics for 82599 SFP+ fiber */
 	if (hw->mac.ops.disable_tx_laser)
 		hw->mac.ops.disable_tx_laser(hw);
@@ -7913,8 +7914,13 @@ err_dma:
 static void ixgbe_remove(struct pci_dev *pdev)
 {
 	struct ixgbe_adapter *adapter = pci_get_drvdata(pdev);
-	struct net_device *netdev = adapter->netdev;
+	struct net_device *netdev;
 
+	/* if !adapter then we already cleaned up in probe */
+	if (!adapter)
+		return;
+
+	netdev  = adapter->netdev;
 	ixgbe_dbg_adapter_exit(adapter);
 
 	set_bit(__IXGBE_REMOVING, &adapter->state);
