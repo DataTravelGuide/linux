2735fe7ba198cc2a8461fcb0b25e3477180db1d3
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2015-04-02 21:12:55 -0400

    video: Change the default for video.use_native_backlight to 1
    Patchwork-id: 109384
    O-Subject: [RHEL6.7 PATCH 15/16] ACPI / video: Change the default for video.use_native_backlight to 1
    Bugzilla: 1205233
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Hans de Goede <hdegoede@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1205233
    Upstream: since v3.16
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=8941621
    Tested by me with a X1 Carbon 3 and several previous generation Lenovo laptops
    
    commit 751109aad5834375ca9ed0dcfcd85a00cbf872b5
    Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Date:   Thu Jun 5 22:47:35 2014 +0200
    
        ACPI / video: Change the default for video.use_native_backlight to 1
    
        Now that we're hoping to have resolved all of the problems with
        video.use_native_backlight=1, make that the default at last.
    
        Link: http://marc.info/?l=linux-acpi&m=139716088401106&w=2
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/acpi/video.c
+++ drivers/acpi/video.c
@@ -80,7 +80,7 @@ module_param(brightness_switch_enabled, bool, 0644);
  * For Windows 8 systems: used to decide if video module
  * should skip registering backlight interface of its own.
  */
-static int use_native_backlight_param = -1;
+static int use_native_backlight_param = 1;
 module_param_named(use_native_backlight, use_native_backlight_param, int, 0444);
 static bool use_native_backlight_dmi = false;
 
