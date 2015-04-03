d4cd490f9da93371959c2160bcdb59e860862bef
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2015-04-02 21:12:44 -0400

    backlight: introduce backlight_device_registered
    Patchwork-id: 109372
    O-Subject: [RHEL6.7 PATCH 04/16] backlight: introduce backlight_device_registered
    Bugzilla: 1205233
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Hans de Goede <hdegoede@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1205233
    Upstream: since v3.13
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=8941621
    Tested by me with a X1 Carbon 3 and several previous generation Lenovo laptops
    
    commit 5915a3db0c3983f1cd5e046bf70086c7d0c686d2
    Author: Aaron Lu <aaron.lu@intel.com>
    Date:   Fri Oct 11 21:27:43 2013 +0800
    
        backlight: introduce backlight_device_registered
    
        Introduce a new API for modules to query if a specific type of backlight
        device has been registered. This is useful for some backlight device
        provider module (e.g. ACPI video) to know if a native control
        interface(e.g. the interface created by i915) is available and then do
        things accordingly (e.g. avoid registering its own on Win8 systems).
    
        Signed-off-by: Aaron Lu <aaron.lu@intel.com>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
        Conflicts:
        	drivers/video/backlight/backlight.c -> adapted for RHEL 6
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/video/backlight/backlight.c
+++ drivers/video/backlight/backlight.c
@@ -18,6 +18,9 @@
 #include <asm/backlight.h>
 #endif
 
+static struct list_head backlight_dev_list;
+static struct mutex backlight_dev_list_mutex;
+
 #if defined(CONFIG_FB) || (defined(CONFIG_FB_MODULE) && \
 			   defined(CONFIG_BACKLIGHT_CLASS_DEVICE_MODULE))
 /* This callback gets called when something important happens inside a
@@ -317,9 +320,31 @@ struct backlight_device *backlight_device_register(const char *name,
 	mutex_unlock(&pmac_backlight_mutex);
 #endif
 
+	mutex_lock(&backlight_dev_list_mutex);
+	list_add(&new_bd->entry, &backlight_dev_list);
+	mutex_unlock(&backlight_dev_list_mutex);
+
 	return new_bd;
 }
 EXPORT_SYMBOL(backlight_device_register);
+
+bool backlight_device_registered(enum backlight_type type)
+{
+	bool found = false;
+	struct backlight_device *bd;
+
+	mutex_lock(&backlight_dev_list_mutex);
+	list_for_each_entry(bd, &backlight_dev_list, entry) {
+		if (bd->props.type == type) {
+			found = true;
+			break;
+		}
+	}
+	mutex_unlock(&backlight_dev_list_mutex);
+
+	return found;
+}
+EXPORT_SYMBOL(backlight_device_registered);
 
 /**
  * backlight_device_unregister - unregisters a backlight device object.
@@ -331,6 +356,10 @@ void backlight_device_unregister(struct backlight_device *bd)
 {
 	if (!bd)
 		return;
+
+	mutex_lock(&backlight_dev_list_mutex);
+	list_del(&bd->entry);
+	mutex_unlock(&backlight_dev_list_mutex);
 
 #ifdef CONFIG_PMAC_BACKLIGHT
 	mutex_lock(&pmac_backlight_mutex);
@@ -364,6 +393,8 @@ static int __init backlight_class_init(void)
 	backlight_class->dev_attrs = bl_device_attributes;
 	backlight_class->suspend = backlight_suspend;
 	backlight_class->resume = backlight_resume;
+	INIT_LIST_HEAD(&backlight_dev_list);
+	mutex_init(&backlight_dev_list_mutex);
 	return 0;
 }
 
