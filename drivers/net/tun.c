bce3b5e6ceaf42e3f537d98262c3ed907e3ce694
Jason Wang <jasowang@redhat.com>
2015-04-10 02:31:08 -0400

    tuntap: hardware vlan tx support
    Patchwork-id: 109628
    O-Subject: [RHEL6.7 PATCH 2/4] tuntap: hardware vlan tx support
    Bugzilla: 1036482
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Michael S. Tsirkin <mst@redhat.com>
    
    Bugzilla: 1036482
    Brew build: https://brewweb.devel.redhat.com/taskinfo?taskID=8955268
    Test status: Tested by myself
    Notes: Conflict because:
    - Multiqueue was not supported in RHEL6
    - Tun was not converted to use hw_features in RHEL6
    - 8021AD hardware vlan tx was not supported in RHEL6
    
    Inspired by commit f09e2249c4f5c7c13261ec73f5a7807076af0c8e (macvtap: restore
    vlan header on user read). This patch adds hardware vlan tx support for
    tuntap. This is done by copying vlan header directly into userspace in
    tun_put_user() instead of doing it through __vlan_put_tag() in
    dev_hard_start_xmit(). This eliminates one unnecessary memmove() in
    vlan_insert_tag() for 802.1ad and 802.1q traffic.
    
    pktgen test shows about 20% improvement for 802.1q traffic:
    
    Before:
      662149pps 317Mb/sec (317831520bps) errors: 0
    After:
      801033pps 384Mb/sec (384495840bps) errors: 0
    
    Cc: Basil Gor <basil.gor@gmail.com>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 6680ec68eff47d36f67b4351bc9836fd6cba9532)
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    
    Conflicts:
    	drivers/net/tun.c
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -58,6 +58,7 @@
 #include <linux/if_arp.h>
 #include <linux/if_ether.h>
 #include <linux/if_tun.h>
+#include <linux/if_vlan.h>
 #include <linux/crc32.h>
 #include <linux/nsproxy.h>
 #include <linux/virtio_net.h>
@@ -741,6 +742,7 @@ static ssize_t tun_put_user(struct tun_struct *tun,
 {
 	struct tun_pi pi = { 0, skb->protocol };
 	ssize_t total = 0;
+	int vlan_offset = 0;
 
 	if (!(tun->flags & TUN_NO_PI)) {
 		if ((len -= sizeof(pi)) < 0)
@@ -804,11 +806,40 @@ static ssize_t tun_put_user(struct tun_struct *tun,
 		total += tun->vnet_hdr_sz;
 	}
 
-	len = min_t(int, skb->len, len);
+	if (!vlan_tx_tag_present(skb)) {
+		len = min_t(int, skb->len, len);
+	} else {
+		int copy, ret;
+		struct {
+			__be16 h_vlan_proto;
+			__be16 h_vlan_TCI;
+		} veth;
 
-	skb_copy_datagram_const_iovec(skb, 0, iv, total, len);
-	total += skb->len;
+		veth.h_vlan_proto = htons(ETH_P_8021Q);
+		veth.h_vlan_TCI = htons(vlan_tx_tag_get(skb));
 
+		vlan_offset = offsetof(struct vlan_ethhdr, h_vlan_proto);
+		len = min_t(int, skb->len + VLAN_HLEN, len);
+
+		copy = min_t(int, vlan_offset, len);
+		ret = skb_copy_datagram_const_iovec(skb, 0, iv, total, copy);
+		len -= copy;
+		total += copy;
+		if (ret || !len)
+			goto done;
+
+		copy = min_t(int, sizeof(veth), len);
+		ret = memcpy_toiovecend(iv, (void *)&veth, total, copy);
+		len -= copy;
+		total += copy;
+		if (ret || !len)
+			goto done;
+	}
+
+	skb_copy_datagram_const_iovec(skb, vlan_offset, iv, total, len);
+	total += len;
+
+done:
 	tun->dev->stats.tx_packets++;
 	tun->dev->stats.tx_bytes += len;
 
@@ -1204,10 +1235,11 @@ static int set_offload(struct net_device *dev, unsigned long arg)
 	old_features = dev->features;
 	/* Unset features, set them as we chew on the arg. */
 	features = (old_features & ~(NETIF_F_SG | NETIF_F_FRAGLIST |
-				     TUN_USER_FEATURES));
+				     NETIF_F_HW_VLAN_TX | TUN_USER_FEATURES));
 
 	if (arg & TUN_F_CSUM) {
-		features |= NETIF_F_HW_CSUM|NETIF_F_SG|NETIF_F_FRAGLIST;
+		features |= NETIF_F_HW_CSUM|NETIF_F_SG|NETIF_F_FRAGLIST|
+			    NETIF_F_HW_VLAN_TX;
 		arg &= ~TUN_F_CSUM;
 
 		if (arg & (TUN_F_TSO4|TUN_F_TSO6)) {
