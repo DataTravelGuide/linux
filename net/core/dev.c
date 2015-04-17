365aba530afa55d72f4470919c89032b53fd4dab
Jiri Pirko <jpirko@redhat.com>
2015-04-17 15:44:24 +0000

    allow vlan traffic to be received under bond
    Patchwork-id: 110834
    O-Subject: [patch rhel6.5.z net 67/72] allow vlan traffic to be received under bond
    Bugzilla: 1173560
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Nikolay Aleksandrov <nikolay@redhat.com>
    RH-Acked-by: Vlad Yasevich <vyasevic@redhat.com>
    
    BZ1173560
    
    upstream commit 2425717b27eb92b175335ca4ff0bb218cbe0cb64
    Author: John Fastabend <john.r.fastabend@intel.com>
    Date:   Mon Oct 10 09:16:41 2011 +0000
    
        net: allow vlan traffic to be received under bond
    
        The following configuration used to work as I expected. At least
        we could use the fcoe interfaces to do MPIO and the bond0 iface
        to do load balancing or failover.
    
               ---eth2.228-fcoe
               |
        eth2 -----|
                  |
                  |---- bond0
                  |
        eth3 -----|
               |
               ---eth3.228-fcoe
    
        This worked because of a change we added to allow inactive slaves
        to rx 'exact' matches. This functionality was kept intact with the
        rx_handler mechanism. However now the vlan interface attached to the
        active slave never receives traffic because the bonding rx_handler
        updates the skb->dev and goto's another_round. Previously, the
        vlan_do_receive() logic was called before the bonding rx_handler.
    
        Now by the time vlan_do_receive calls vlan_find_dev() the
        skb->dev is set to bond0 and it is clear no vlan is attached
        to this iface. The vlan lookup fails.
    
        This patch moves the VLAN check above the rx_handler. A VLAN
        tagged frame is now routed to the eth2.228-fcoe iface in the
        above schematic. Untagged frames continue to the bond0 as
        normal. This case also remains intact,
    
        eth2 --> bond0 --> vlan.228
    
        Here the skb is VLAN tagged but the vlan lookup fails on eth2
        causing the bonding rx_handler to be called. On the second
        pass the vlan lookup is on the bond0 iface and completes as
        expected.
    
        Putting a VLAN.228 on both the bond0 and eth2 device will
        result in eth2.228 receiving the skb. I don't think this is
        completely unexpected and was the result prior to the rx_handler
        result.
    
        Note, the same setup is also used for other storage traffic that
        MPIO is used with eg. iSCSI and similar setups can be contrived    without storage protocols.
        without storage protocols.
    
    Signed-off-by: Jiri Pirko <jpirko@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -3294,6 +3294,17 @@ another_round:
 ncls:
 #endif
 
+	if (vlan_tx_tag_present(skb)) {
+		if (pt_prev) {
+			ret = deliver_skb(skb, pt_prev, orig_dev);
+			pt_prev = NULL;
+		}
+		if (vlan_do_receive(&skb))
+			goto another_round;
+		else if (unlikely(!skb))
+			goto out;
+	}
+
 	rx_handler = rcu_dereference(netdev_extended(skb->dev)->rx_handler);
 	if (rx_handler) {
 		if (pt_prev) {
@@ -3312,17 +3323,6 @@ ncls:
 		default:
 			BUG();
 		}
-	}
-
-	if (vlan_tx_tag_present(skb)) {
-		if (pt_prev) {
-			ret = deliver_skb(skb, pt_prev, orig_dev);
-			pt_prev = NULL;
-		}
-		if (vlan_do_receive(&skb))
-			goto another_round;
-		else if (unlikely(!skb))
-			goto out;
 	}
 
 	/* deliver only exact match when indicated */
