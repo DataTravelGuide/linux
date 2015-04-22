1560112df41c719e1a8d878f6c8e18651351b28a
Hendrik Brueckner <brueckner@redhat.com>
2015-04-22 08:18:53 -0400

    dasd: fix inability to set a DASD device offline
    Patchwork-id: 111132
    O-Subject: [RHEL6.7 PATCH] [s390] s390/dasd: fix inability to set a DASD device offline
    Bugzilla: 1213799
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    Description
    -----------
    commit f2608cd4a37479d11d0748f524a6a363ee38631f
    Author: Stefan Haberland <stefan.haberland@de.ibm.com>
    Commit: Martin Schwidefsky <schwidefsky@de.ibm.com>
    
        s390/dasd: fix inability to set a DASD device offline
    
        Fix ref counting for DASD devices leading to an inability to set a
        DASD device offline.
        Before a worker is scheduled the DASD device driver takes a reference
        to the device. If the worker was already scheduled this reference was
        never freed.
        Fix by giving the reference to the DASD device free when
        schedule_work() returns false.
    
        Signed-off-by: Stefan Haberland <stefan.haberland@de.ibm.com>
        Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    
    Bugzilla
    --------
    BZ 1213799
    https://bugzilla.redhat.com/show_bug.cgi?id=1213799
    
    Upstream status of the patch
    ----------------------------
    The patch is upstream as of kernel version 4.1
    http://git.kernel.org/?p=linux/kernel/git/torvalds/linux-2.6.git;a=commitdiff;h=f2608cd4a37479d11d0748f524a6a363ee38631f
    
    Brew
    ----
    https://brewweb.devel.redhat.com/taskinfo?taskID=9017395
    
    Test status
    -----------
    The patch has been tested and fixes the problem.
    The fix has been verified by the IBM test department.
    
    Signed-off-by: Kurt Stutsman <kstutsma@redhat.com>

--- drivers/s390/block/dasd.c
+++ drivers/s390/block/dasd.c
@@ -584,7 +584,8 @@ void dasd_kick_device(struct dasd_device *device)
 {
 	dasd_get_device(device);
 	/* queue call to dasd_kick_device to the kernel event daemon. */
-	schedule_work(&device->kick_work);
+	if (!schedule_work(&device->kick_work))
+		dasd_put_device(device);
 }
 
 /*
@@ -603,7 +604,8 @@ void dasd_reload_device(struct dasd_device *device)
 {
 	dasd_get_device(device);
 	/* queue call to dasd_reload_device to the kernel event daemon. */
-	schedule_work(&device->reload_device);
+	if (!schedule_work(&device->reload_device))
+		dasd_put_device(device);
 }
 EXPORT_SYMBOL(dasd_reload_device);
 
@@ -623,7 +625,8 @@ void dasd_restore_device(struct dasd_device *device)
 {
 	dasd_get_device(device);
 	/* queue call to dasd_restore_device to the kernel event daemon. */
-	schedule_work(&device->restore_device);
+	if (!schedule_work(&device->restore_device))
+		dasd_put_device(device);
 }
 
 /*
