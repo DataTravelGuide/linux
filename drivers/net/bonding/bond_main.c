e0fe48840f529eb90b264f49abfa5ad3a9c13f3f
Jarod Wilson <jarod@redhat.com>
2015-05-28 21:36:56 +0000

    bonding: fix error handling if slave is busy v2
    Patchwork-id: 114587
    O-Subject: [RHEL6.5z PATCH 1/6] [netdrv] bonding: fix error handling if slave is busy v2
    Bugzilla: 1222482
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    From: Nikolay Aleksandrov <nikolay@redhat.com>
    
    RHEL6.5z BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1222482
    
    Message-id: <1415027880-25844-249-git-send-email-nikolay@redhat.com>
    Patchwork-id: 98958
    O-Subject: [RHEL6.7 PATCH BZ 1159818 248/657] bonding: fix error handling if slave is busy (v2)
    Bugzilla: 1159818
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    From: stephen hemminger <shemminger@vyatta.com>
    
    If slave device already has a receive handler registered, then the
    error unwind of bonding device enslave function is broken.
    
    The following will leave a pointer to freed memory in the slave
    device list, causing a later kernel panic.
     # modprobe dummy
     # ip li add dummy0-1 link dummy0 type macvlan
     # modprobe bonding
     # echo +dummy0 >/sys/class/net/bond0/bonding/slaves
    
    The fix is to detach the slave (which removes it from the list)
    in the unwind path.
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Reviewed-by: Nicolas de Pesloan <nicolas.2p.debian@free.fr>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit f7d9821a6a9c83450ac35e76d3709e32fd38b76f)
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    
    Conflicts:
    	drivers/net/bonding/bond_main.c
    	 - Due to missing commits and difference in the
    	  netpoll handling, as it's out of the scope of this patch
    	  to change that I've kept the original netpoll handling.

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -2002,7 +2002,7 @@ int bond_enslave(struct net_device *bond_dev, struct net_device *slave_dev)
 
 	res = bond_create_slave_symlinks(bond_dev, slave_dev);
 	if (res)
-		goto err_close;
+		goto err_detach;
 
 	res = netdev_rx_handler_register(slave_dev, bond_handle_frame,
 					 new_slave);
@@ -2023,6 +2023,11 @@ int bond_enslave(struct net_device *bond_dev, struct net_device *slave_dev)
 /* Undo stages on error */
 err_dest_symlinks:
 	bond_destroy_slave_symlinks(bond_dev, slave_dev);
+
+err_detach:
+	write_lock_bh(&bond->lock);
+	bond_detach_slave(bond, new_slave);
+	write_unlock_bh(&bond->lock);
 
 err_close:
 	dev_close(slave_dev);
