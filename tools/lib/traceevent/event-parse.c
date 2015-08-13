0594c0a23d9c2512d181e465fb2facbd1b68b3cc
Jiri Olsa <jolsa@redhat.com>
2015-08-13 09:59:42 -0400

    tools lib traceevent: Fix a risk for doing free on uninitialized pointer
    Patchwork-id: 123559
    O-Subject: [PATCH RHEL6 BZ1216217 046/341] tools lib traceevent: Fix a risk for doing free on uninitialized pointer
    Bugzilla: 1216217
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Petr Holasek <pholasek@redhat.com>
    
    From: Jiri Olsa <jolsa@kernel.org>
    
    Bugzilla: 1216217
    https://bugzilla.redhat.com/show_bug.cgi?id=1216217
    
    upstream
    ========
    commit 21da83fb6c3ff67e969b8770bbb33138c9ede88e
    Author: Rickard Strandqvist <rickard_strandqvist@spectrumdigital.se>
    Date: Tue Jun 24 13:09:10 2014 +0200
    
    description
    ===========
    Fix a risk of doing free on an uninitialized pointer.
    
    This was found using a static code analysis program called cppcheck.

--- tools/lib/traceevent/event-parse.c
+++ tools/lib/traceevent/event-parse.c
@@ -2393,7 +2393,7 @@ process_flags(struct event_format *event, struct print_arg *arg, char **tok)
 {
 	struct print_arg *field;
 	enum event_type type;
-	char *token;
+	char *token = NULL;
 
 	memset(arg, 0, sizeof(*arg));
 	arg->type = PRINT_FLAGS;
@@ -2446,7 +2446,7 @@ process_symbols(struct event_format *event, struct print_arg *arg, char **tok)
 {
 	struct print_arg *field;
 	enum event_type type;
-	char *token;
+	char *token = NULL;
 
 	memset(arg, 0, sizeof(*arg));
 	arg->type = PRINT_SYMBOL;
@@ -2485,7 +2485,7 @@ process_hex(struct event_format *event, struct print_arg *arg, char **tok)
 {
 	struct print_arg *field;
 	enum event_type type;
-	char *token;
+	char *token = NULL;
 
 	memset(arg, 0, sizeof(*arg));
 	arg->type = PRINT_HEX;
