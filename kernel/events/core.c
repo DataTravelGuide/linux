8d93658776d157a146ee5adf89f4284812934f50
Jiri Olsa <jolsa@redhat.com>
2015-09-22 19:37:27 -0400

    Migrate per cpu event accounting
    Patchwork-id: 125000
    O-Subject: [PATCH RHEL6.8 BZ1252428 05/28] perf: Migrate per cpu event accounting
    Bugzilla: 1252428
    RH-Acked-by: Petr Holasek <pholasek@redhat.com>
    
    From: Jiri Olsa <jolsa@kernel.org>
    
    Bugzilla: 1252428
    https://bugzilla.redhat.com/show_bug.cgi?id=1252428
    
    upstream
    ========
    commit 9a545de019b536771feefb76f85e5038b65c2190
    Author: Frederic Weisbecker <fweisbec@gmail.com>
    Date: Tue Jul 23 02:31:03 2013 +0200
    
    description
    ===========
    When an event is migrated, move the event per-cpu
    accounting accordingly so that branch stack and cgroup
    events work correctly on the new CPU.
    
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -6965,6 +6965,7 @@ void perf_pmu_migrate_context(struct pmu *pmu, int src_cpu, int dst_cpu)
 	list_for_each_entry_safe(event, tmp, &src_ctx->event_list,
 				 event_entry) {
 		perf_remove_from_context(event, false);
+		unaccount_event_cpu(event, src_cpu);
 		put_ctx(src_ctx);
 		list_add(&event->event_entry, &events);
 	}
@@ -6977,6 +6978,7 @@ void perf_pmu_migrate_context(struct pmu *pmu, int src_cpu, int dst_cpu)
 		list_del(&event->event_entry);
 		if (event->state >= PERF_EVENT_STATE_OFF)
 			event->state = PERF_EVENT_STATE_INACTIVE;
+		account_event_cpu(event, dst_cpu);
 		perf_install_in_context(dst_ctx, event, dst_cpu);
 		get_ctx(dst_ctx);
 	}
