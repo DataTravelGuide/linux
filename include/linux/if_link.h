9e90a4dfb366da7f35b6d7b0d3954f733341bc1c
Paolo Abeni <pabeni@redhat.com>
2015-10-08 12:26:00 -0400

    bridge: bridge port parameters over netlink
    Patchwork-id: 125509
    O-Subject: [RHEL6.8 net PATCH 6/7] bridge: bridge port parameters over netlink
    Bugzilla: 1115904
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Phil Sutter <psutter@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1115904
    
    Upstream commit:
    commit 25c71c75ac87508528db053b818944f3650dd7a6
    Author: stephen hemminger <shemminger@vyatta.com>
    Date:   Tue Nov 13 07:53:05 2012 +0000
    
        bridge: bridge port parameters over netlink
    
        Expose bridge port parameter over netlink. By switching to a nested
        message, this can be used for other bridge parameters.
    
        This changes IFLA_PROTINFO attribute from one byte to a full nested
        set of attributes. This is safe for application interface because the
        old message used IFLA_PROTINFO and new one uses
         IFLA_PROTINFO | NLA_F_NESTED.
    
        The code adapts to old format requests, and therefore stays
        compatible with user mode RSTP daemon. Since the type field
        for nested and unnested attributes are different, and the old
        code in libnetlink doesn't do the mask, it is also safe to use
        with old versions of bridge monitor command.
    
        Note: although mode is only a boolean, treating it as a
        full byte since in the future someone will probably want to add more
        values (like macvlan has).
    
        Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- include/linux/if_link.h
+++ include/linux/if_link.h
@@ -222,6 +222,21 @@ enum {
 
 #define IFLA_BR_MAX	(__IFLA_BR_MAX - 1)
 
+enum {
+	BRIDGE_MODE_UNSPEC,
+	BRIDGE_MODE_HAIRPIN,
+};
+
+enum {
+	IFLA_BRPORT_UNSPEC,
+	IFLA_BRPORT_STATE,	/* Spanning tree state     */
+	IFLA_BRPORT_PRIORITY,	/* "             priority  */
+	IFLA_BRPORT_COST,	/* "             cost      */
+	IFLA_BRPORT_MODE,	/* mode (hairpin)          */
+	__IFLA_BRPORT_MAX
+};
+#define IFLA_BRPORT_MAX (__IFLA_BRPORT_MAX - 1)
+
 struct ifla_cacheinfo
 {
 	__u32	max_reasm_len;
