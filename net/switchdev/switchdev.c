649413ba176d85cf83eb6db556fea00643f91d6b
Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
2015-10-12 14:31:01 +0200

    switchdev: check if the vlan id is in the proper vlan range
    VLANs 0 and 4095 are reserved and shouldn't be used, add checks to
    switchdev similar to the bridge. Also make sure ids above 4095 cannot
    be passed either.
    
    Fixes: 47f8328bb1a4 ("switchdev: add new switchdev bridge setlink")
    Signed-off-by: Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
    Acked-by: Scott Feldman <sfeldma@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Conflict patch:
    0001-switchdev-check-if-the-vlan-id-is-in-the-proper-vlan.patch
    
    Merge log:
    patching file net/switchdev/switchdev.c
    Hunk #1 NOT MERGED at 19-23.
    Hunk #2 NOT MERGED at 722-728.
    (EScore specific version string)

--- net/switchdev/switchdev.c
+++ net/switchdev/switchdev.c
@@ -16,7 +16,7 @@
 #include <linux/notifier.h>
 #include <linux/netdevice.h>
 #include <linux/if_bridge.h>
-#include <linux/list.h>
+#include <linux/if_vlan.h>
 #include <net/ip_fib.h>
 #include <net/switchdev.h>
 
@@ -715,7 +715,9 @@ static int switchdev_port_br_afspec(struct net_device *dev,
 		if (nla_len(attr) != sizeof(struct bridge_vlan_info))
 			return -EINVAL;
 		vinfo = nla_data(attr);
-		vlan.flags = vinfo->flags;
+		if (!vinfo->vid || vinfo->vid >= VLAN_VID_MASK)
+			return -EINVAL;
+		vlan->flags = vinfo->flags;
 		if (vinfo->flags & BRIDGE_VLAN_INFO_RANGE_BEGIN) {
 			if (vlan.vid_begin)
 				return -EINVAL;
