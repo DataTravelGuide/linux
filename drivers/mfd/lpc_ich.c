8c409218f83e70bebd12d1b92de794903603c6c5
Prarit Bhargava <prarit@redhat.com>
2015-10-14 14:47:00 -0400

    lpc_ich: Add support for Intel Avoton GPIOs
    Patchwork-id: 125676
    O-Subject: [RHEL6.8 PATCH BZ 1084131 02/10] mfd: lpc_ich: Add support for Intel Avoton GPIOs
    Bugzilla: 1084131
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1084131
    
    commit facd9939403cb5769190054a600474399e776e3a
    Author: Vincent Donnefort <vdonnefort@gmail.com>
    Date:   Fri Feb 14 15:01:54 2014 +0100
    
        mfd: lpc_ich: Add support for Intel Avoton GPIOs
    
        Signed-off-by: Vincent Donnefort <vdonnefort@gmail.com>
        Signed-off-by: Lee Jones <lee.jones@linaro.org>
    
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- drivers/mfd/lpc_ich.c
+++ drivers/mfd/lpc_ich.c
@@ -504,6 +504,7 @@ struct lpc_ich_info lpc_chipset_info[] __devinitdata = {
 	[LPC_AVN] = {
 		.name = "Avoton SoC",
 		.iTCO_version = 1,
+		.gpio_version = AVOTON_GPIO,
 	},
 	[LPC_WPT_LP] = {
 		.name = "Lynx Point_LP",
