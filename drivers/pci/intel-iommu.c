8698e025e2a441e6fb1dd5243312b9d3c5bfc49c
Linda Knippers <lknipper@redhat.com>
2015-11-10 22:48:25 -0500

    iommu/vt-d: Only remove domain when device is removed
    Patchwork-id: 126541
    O-Subject: [RHEL6.8 PATCH 2/3] iommu/vt-d: Only remove domain when device is removed
    Bugzilla: 1053879
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    
    bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1053879
    Upstream Status: 1196c2fb0407683c2df92d3d09f9144d42830894
    
    This is a backport to RHEL6 of Myron's backport to RHEL7.
    http://post-office.corp.redhat.com/archives/rhkernel-list/2015-January/msg00218.html
    
    commit 1196c2fb0407683c2df92d3d09f9144d42830894
    Author: Joerg Roedel <jroedel@suse.de>
    Date:   Tue Sep 30 13:02:03 2014 +0200
    
        iommu/vt-d: Only remove domain when device is removed
    
        This makes sure any RMRR mappings stay in place when the
        driver is unbound from the device.
    
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
        Tested-by: Jerry Hoemann <jerry.hoemann@hp.com>
    
    Signed-off-by: Linda Knippers <lknipper@redhat.com>
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- drivers/pci/intel-iommu.c
+++ drivers/pci/intel-iommu.c
@@ -3486,18 +3486,19 @@ static int device_notifier(struct notifier_block *nb,
 	if (iommu_no_mapping(dev))
 		return 0;
 
+	if (action != BUS_NOTIFY_REMOVED_DEVICE)
+		return 0;
+
 	domain = find_domain(pdev);
 	if (!domain)
 		return 0;
 
-	if (action == BUS_NOTIFY_UNBOUND_DRIVER && !iommu_pass_through) {
-		domain_remove_one_dev_info(domain, pdev);
+	domain_remove_one_dev_info(domain, pdev);
 
-		if (!(domain->flags & DOMAIN_FLAG_VIRTUAL_MACHINE) &&
-		    !(domain->flags & DOMAIN_FLAG_STATIC_IDENTITY) &&
-		    list_empty(&domain->devices))
-			domain_exit(domain);
-	}
+	if (!(domain->flags & DOMAIN_FLAG_VIRTUAL_MACHINE) &&
+	    !(domain->flags & DOMAIN_FLAG_STATIC_IDENTITY) &&
+	    list_empty(&domain->devices))
+		domain_exit(domain);
 
 	return 0;
 }
