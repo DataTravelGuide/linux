3cb8792691e72acd6eba5776438c3d7f38b4c32c
Bill O'Donnell <billodo@redhat.com>
2015-11-12 17:57:33 -0500

    xfs: create global stats and stats_clear in sysfs
    Patchwork-id: 126620
    O-Subject: [RHEL6 PATCH v4 08/17] xfs: create global stats and stats_clear in sysfs
    Bugzilla: 1205640
    RH-Acked-by: Eric Sandeen <sandeen@redhat.com>
    RH-Acked-by: Brian Foster <bfoster@redhat.com>
    
    ---------------
    upstream -> rhel6 porting notes:
        -counter_val function only: was pulled from upstream
         commit 48776fd2 (xfs: use common code for quota statistics)
        -this patch dropped quota stats from the upstream version
         as they're non-existent in rhel6.
        -this patch varies from the upstream patch, particularly in
         hunks with debug involvement (/sys/fs/xfs/debug/ non-existent
         in rhel6, and not required in the feature addition associated
         with this patch).
    ---------------
    
    commit bb230c124730f21eea13deab433f9f8fc96bd5f3
    Author: Bill O'Donnell <billodo@redhat.com>
    Date:   Mon Oct 12 05:15:45 2015 +1100
    
    Currently, xfs global stats are in procfs. This patch introduces
    (replicates) the global stats in sysfs. Additionally a stats_clear file
    is introduced in sysfs.
    
    Signed-off-by: Bill O'Donnell <billodo@redhat.com>
    Reviewed-by: Eric Sandeen <sandeen@redhat.com>
    Signed-off-by: Dave Chinner <david@fromorbit.com>
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- fs/xfs/linux-2.6/xfs_super.c
+++ fs/xfs/linux-2.6/xfs_super.c
@@ -68,8 +68,9 @@
 static const struct super_operations xfs_super_operations;
 static kmem_zone_t *xfs_ioend_zone;
 mempool_t *xfs_ioend_pool;
+struct kset *xfs_kset;			/* top-level xfs sysfs dir */
+static struct xfs_kobj xfs_stats_kobj;	/* global stats sysfs attrs */
 
-struct kset *xfs_kset;                 /* top-level xfs sysfs dir */
 #ifdef DEBUG
 static struct xfs_kobj xfs_dbg_kobj;   /* global debug sysfs attrs */
 #endif
@@ -1749,20 +1750,26 @@ init_xfs_fs(void)
 	xfs_kset = kset_create_and_add("xfs", NULL, fs_kobj);
 	if (!xfs_kset) {
 		error = -ENOMEM;
-		goto out_sysctl_unregister;;
+		goto out_sysctl_unregister;
 	}
+
+	xfs_stats_kobj.kobject.kset = xfs_kset;
+	error = xfs_sysfs_init(&xfs_stats_kobj, &xfs_stats_ktype, NULL,
+			       "stats");
+	if (error)
+		goto out_kset_unregister;
 
 #ifdef DEBUG
 	xfs_dbg_kobj.kobject.kset = xfs_kset;
 	error = xfs_sysfs_init(&xfs_dbg_kobj, &xfs_dbg_ktype, NULL, "debug");
 	if (error)
-		goto out_kset_unregister;
+		goto out_remove_stats_kobj;
 #endif
 
 	xfs_alloc_wq = create_workqueue("xfsalloc");
 	if (!xfs_alloc_wq) {
 		error = ENOMEM;
-		goto out_remove_kobj;
+		goto out_remove_dbg_kobj;
 	}
 
 	xfs_eofblocks_wq = create_workqueue("xfseofblocks");
@@ -1782,11 +1789,13 @@ init_xfs_fs(void)
 	destroy_workqueue(xfs_eofblocks_wq);
  out_destroy_alloc_wq:
 	destroy_workqueue(xfs_alloc_wq);
- out_remove_kobj:
+ out_remove_dbg_kobj:
 #ifdef DEBUG
 	xfs_sysfs_del(&xfs_dbg_kobj);
- out_kset_unregister:
+ out_remove_stats_kobj:
 #endif
+	xfs_sysfs_del(&xfs_stats_kobj);
+ out_kset_unregister:
 	kset_unregister(xfs_kset);
  out_sysctl_unregister:
 	xfs_sysctl_unregister();
@@ -1814,6 +1823,7 @@ exit_xfs_fs(void)
 #ifdef DEBUG
 	xfs_sysfs_del(&xfs_dbg_kobj);
 #endif
+	xfs_sysfs_del(&xfs_stats_kobj);
 	kset_unregister(xfs_kset);
 	xfs_sysctl_unregister();
 	xfs_cleanup_procfs();
