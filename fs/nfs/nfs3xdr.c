4655d521fe82aa54e1e768ec0a2740ce36821512
Benjamin Coddington <bcodding@redhat.com>
2015-11-16 19:32:06 -0500

    nfsv3: Use the readdir fileid as the mounted-on-fileid
    Patchwork-id: 126765
    O-Subject: [RHEL6.8 PATCH fs BZ1223978 2/3] NFSv3: Use the readdir fileid as the mounted-on-fileid
    Bugzilla: 1223978
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    
    From: Trond Myklebust <trond.myklebust@primarydata.com>
    
    When we call readdirplus, set the fileid normally returned by readdir
    as the mounted-on-fileid, since that is commonly the case if there is
    a mountpoint. To ensure that we get it right, we only set the flag if
    the readdir fileid differs from the one returned in the readdirplus
    attributes.
    
    This again means that we can avoid the issues described in commit
    2ef47eb1aee17 ("NFS: Fix use of nfs_attr_use_mounted_on_fileid()"),
    which only fixed NFSv4.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1223978
    Upstream Id: 1ae04b252351188cfa66af1b8b0628512a72dd7b
    Signed-off-by: Benjamin Coddington <bcodding@redhat.com>
    
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- fs/nfs/nfs3xdr.c
+++ fs/nfs/nfs3xdr.c
@@ -636,6 +636,12 @@ nfs3_decode_dirent(struct xdr_stream *xdr, struct nfs_entry *entry, struct nfs_s
 		if (IS_ERR(p))
 			goto out_overflow_exit;
 		entry->d_type = nfs_umode_to_dtype(entry->fattr->mode);
+
+		if (entry->fattr->fileid != entry->ino) {
+			entry->fattr->mounted_on_fileid = entry->ino;
+			entry->fattr->valid |= NFS_ATTR_FATTR_MOUNTED_ON_FILEID;
+		}
+
 		/* In fact, a post_op_fh3: */
 		p = xdr_inline_decode(xdr, 4);
 		if (unlikely(!p))
