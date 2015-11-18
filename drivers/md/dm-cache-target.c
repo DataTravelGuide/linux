341245b27d4aa4724ef92110a57eb75e28e789c5
Mike Snitzer <snitzer@redhat.com>
2015-11-18 18:26:24 -0500

    dm cache: avoid calls to prealloc_free_structs if possible
    Patchwork-id: 126852
    O-Subject: [RHEL6.8 PATCH 46/96] dm cache: avoid calls to prealloc_free_structs() if possible
    Bugzilla: 1282900
    RH-Acked-by: Mikulas Patocka <mpatocka@redhat.com>
    RH-Acked-by: Heinz Mauelshagen <heinzm@redhat.com>
    RH-Acked-by: Joe Thornber <thornber@redhat.com>
    
    BZ: 1282900
    
    Upstream commit 665022d72f9b5762f21b5ea02fa0503d04802849
    Author: Mike Snitzer <snitzer@redhat.com>
    Date:   Thu Jul 16 21:48:55 2015 -0400
    
        dm cache: avoid calls to prealloc_free_structs() if possible
    
        If no work was performed then prealloc_data_structs() wasn't ever called
        so there isn't any need to call prealloc_free_structs().
    
        Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- drivers/md/dm-cache-target.c
+++ drivers/md/dm-cache-target.c
@@ -1997,6 +1997,7 @@ static int commit_if_needed(struct cache *cache)
 
 static void process_deferred_bios(struct cache *cache)
 {
+	bool prealloc_used = false;
 	unsigned long flags;
 	struct bio_list bios;
 	struct bio *bio;
@@ -2031,13 +2032,16 @@ static void process_deferred_bios(struct cache *cache)
 			process_discard_bio(cache, &structs, bio);
 		else
 			process_bio(cache, &structs, bio);
+		prealloc_used = true;
 	}
 
-	prealloc_free_structs(cache, &structs);
+	if (prealloc_used)
+		prealloc_free_structs(cache, &structs);
 }
 
 static void process_deferred_cells(struct cache *cache)
 {
+	bool prealloc_used = false;
 	unsigned long flags;
 	struct dm_bio_prison_cell *cell, *tmp;
 	struct list_head cells;
@@ -2065,9 +2069,11 @@ static void process_deferred_cells(struct cache *cache)
 		}
 
 		process_cell(cache, &structs, cell);
+		prealloc_used = true;
 	}
 
-	prealloc_free_structs(cache, &structs);
+	if (prealloc_used)
+		prealloc_free_structs(cache, &structs);
 }
 
 static void process_deferred_flush_bios(struct cache *cache, bool submit_bios)
@@ -2112,6 +2118,7 @@ static void process_deferred_writethrough_bios(struct cache *cache)
 
 static void writeback_some_dirty_blocks(struct cache *cache)
 {
+	bool prealloc_used = false;
 	dm_oblock_t oblock;
 	dm_cblock_t cblock;
 	struct prealloc structs;
@@ -2131,9 +2138,11 @@ static void writeback_some_dirty_blocks(struct cache *cache)
 		}
 
 		writeback(cache, &structs, oblock, cblock, old_ocell);
+		prealloc_used = true;
 	}
 
-	prealloc_free_structs(cache, &structs);
+	if (prealloc_used)
+		prealloc_free_structs(cache, &structs);
 }
 
 /*----------------------------------------------------------------
