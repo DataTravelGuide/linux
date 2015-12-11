78ca28fc5a71e2e4538a07c49a5e326f722ef9d2
Chad Dupuis <cdupuis@redhat.com>
2015-12-10 20:44:14 -0500

    qla2xxx: Fix virtual port configuration, when switch port is disabled/enabled
    Patchwork-id: 128788
    O-Subject: [PATCH RHEL 6.8 e-stor 14/32] qla2xxx: Fix virtual port configuration, when switch port is disabled/enabled.
    Bugzilla: 1252111
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    
    From: Sawan Chandak <sawan.chandak@qlogic.com>
    
    scsi commit ded6411fd88267510dcee6d6c29cfd0bd82d4f0e
    
    On some vendor switches, when switch port is toggled (down /up),
    then in some condition driver tries to configure virtual port,
    before FW is actually in ready state to process any commands on wire.
    At this time, configuring virtual port can fail. Add fix in driver
    to make driver wait, for FW to be ready state before
    
    Signed-off-by: Sawan Chandak <sawan.chandak@qlogic.com>
    Signed-off-by: Himanshu Madhani <himanshu.madhani@qlogic.com>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: James Bottomley <JBottomley@Odin.com>
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- drivers/scsi/qla2xxx/qla_isr.c
+++ drivers/scsi/qla2xxx/qla_isr.c
@@ -723,6 +723,7 @@ skip_rio:
 				memcpy(vha->port_name, wwpn, WWN_SIZE);
 			}
 
+			clear_bit(VP_CONFIG_OK, &vha->vp_flags);
 			vha->device_flags |= DFLG_NO_CABLE;
 			qla2x00_mark_all_devices_lost(vha, 1);
 		}
@@ -902,6 +903,8 @@ skip_rio:
 
 		set_bit(LOOP_RESYNC_NEEDED, &vha->dpc_flags);
 		set_bit(LOCAL_LOOP_UPDATE, &vha->dpc_flags);
+		set_bit(VP_CONFIG_OK, &vha->vp_flags);
+
 		break;
 
 	case MBA_RSCN_UPDATE:		/* State Change Registration */
