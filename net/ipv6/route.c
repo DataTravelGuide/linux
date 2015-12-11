4cfeb55babc468af984f9a92906fc74d21d46e61
Sabrina Dubroca <sdubroca@redhat.com>
2015-12-11 18:04:06 -0500

    ipv6: Fixed support for blackhole and prohibit routes
    Patchwork-id: 128869
    O-Subject: [PATCH RHEL6.8 net 3/5] IPv6: Fixed support for blackhole and prohibit routes
    Bugzilla: 1245357
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    RH-Acked-by: Phil Sutter <psutter@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1245357
    Upstream Status: 7150aede5dd241539686e17d9592f5ebd28a2cda
    Brew: https://brewweb.devel.redhat.com/brew/taskinfo?taskID=10196269
    Tested: in local VM
    
    changes from upstream: a few conflicts (rt->u.dst vs rt->dst)
    
    Upstream Commit:
    commit 7150aede5dd241539686e17d9592f5ebd28a2cda
    Author: Kamala R <kamala@aristanetworks.com>
    Date:   Mon Dec 2 19:55:21 2013 +0530
    
        IPv6: Fixed support for blackhole and prohibit routes
    
        The behaviour of blackhole and prohibit routes has been corrected by setting
        the input and output pointers of the dst variable appropriately. For
        blackhole routes, they are set to dst_discard and to ip6_pkt_discard and
        ip6_pkt_discard_out respectively for prohibit routes.
    
        ipv6: ip6_pkt_prohibit(_out) should not depend on
        CONFIG_IPV6_MULTIPLE_TABLES
    
        We need ip6_pkt_prohibit(_out) available without
        CONFIG_IPV6_MULTIPLE_TABLES
    
        Signed-off-by: Kamala R <kamala@aristanetworks.com>
        Acked-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Sabrina Dubroca <sdubroca@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -93,6 +93,8 @@ static int		 ip6_dst_gc(struct dst_ops *ops);
 
 static int		ip6_pkt_discard(struct sk_buff *skb);
 static int		ip6_pkt_discard_out(struct sk_buff *skb);
+static int		ip6_pkt_prohibit(struct sk_buff *skb);
+static int		ip6_pkt_prohibit_out(struct sk_buff *skb);
 static void		ip6_link_failure(struct sk_buff *skb);
 static void		ip6_rt_update_pmtu(struct dst_entry *dst, u32 mtu);
 
@@ -153,9 +155,6 @@ static struct rt6_info ip6_null_entry_template = {
 };
 
 #ifdef CONFIG_IPV6_MULTIPLE_TABLES
-
-static int ip6_pkt_prohibit(struct sk_buff *skb);
-static int ip6_pkt_prohibit_out(struct sk_buff *skb);
 
 static struct rt6_info ip6_prohibit_entry_template = {
 	.u = {
@@ -1289,21 +1288,24 @@ int ip6_route_add(struct fib6_config *cfg)
 				goto out;
 			}
 		}
-		rt->u.dst.output = ip6_pkt_discard_out;
-		rt->u.dst.input = ip6_pkt_discard;
 		rt->rt6i_flags = RTF_REJECT|RTF_NONEXTHOP;
 		switch (cfg->fc_type) {
 		case RTN_BLACKHOLE:
 			rt->u.dst.error = -EINVAL;
+			rt->u.dst.output = dst_discard;
+			rt->u.dst.input = dst_discard;
 			break;
 		case RTN_PROHIBIT:
 			rt->u.dst.error = -EACCES;
+			rt->u.dst.output = ip6_pkt_prohibit_out;
+			rt->u.dst.input = ip6_pkt_prohibit;
 			break;
 		case RTN_THROW:
-			rt->u.dst.error = -EAGAIN;
-			break;
 		default:
-			rt->u.dst.error = -ENETUNREACH;
+			rt->u.dst.error = (cfg->fc_type == RTN_THROW) ? -EAGAIN
+					  : -ENETUNREACH;
+			rt->u.dst.output = ip6_pkt_discard_out;
+			rt->u.dst.input = ip6_pkt_discard;
 			break;
 		}
 		goto install_route;
@@ -2004,8 +2006,6 @@ static int ip6_pkt_discard_out(struct sk_buff *skb)
 	return ip6_pkt_drop(skb, ICMPV6_NOROUTE, IPSTATS_MIB_OUTNOROUTES);
 }
 
-#ifdef CONFIG_IPV6_MULTIPLE_TABLES
-
 static int ip6_pkt_prohibit(struct sk_buff *skb)
 {
 	return ip6_pkt_drop(skb, ICMPV6_ADM_PROHIBITED, IPSTATS_MIB_INNOROUTES);
@@ -2016,8 +2016,6 @@ static int ip6_pkt_prohibit_out(struct sk_buff *skb)
 	skb->dev = skb_dst(skb)->dev;
 	return ip6_pkt_drop(skb, ICMPV6_ADM_PROHIBITED, IPSTATS_MIB_OUTNOROUTES);
 }
-
-#endif
 
 /*
  *	Allocate a dst for local (unicast / anycast) address.
