720a2614355ede93e7179f0585f75bbd3974740a
Jaroslav Kysela <jkysela@redhat.com>
2015-12-10 20:47:59 -0500

    alsa: driver core: create write-only attribute macros for devices and drivers
    Patchwork-id: 128797
    O-Subject: [RHEL 6.8 PATCH 01/02] ALSA: driver core: create write-only attribute macros for devices and drivers
    Bugzilla: 1178178
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    This creates the macros DRIVER_ATTR_WO() and DEVICE_ATTR_WO() for
    write-only attributes for drivers and devices.
    
    Upstream commmit: 1130c55c757660ac83bd2de220f08580c56b6855
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- include/linux/device.h
+++ include/linux/device.h
@@ -171,6 +171,8 @@ struct driver_attribute {
 	struct driver_attribute driver_attr_##_name = __ATTR_RW(_name)
 #define DRIVER_ATTR_RO(_name) \
 	struct driver_attribute driver_attr_##_name = __ATTR_RO(_name)
+#define DRIVER_ATTR_WO(_name) \
+	struct driver_attribute driver_attr_##_name = __ATTR_WO(_name)
 
 extern int __must_check driver_create_file(struct device_driver *driver,
 					   struct driver_attribute *attr);
@@ -330,6 +332,8 @@ struct device_attribute {
 struct device_attribute dev_attr_##_name = __ATTR(_name, _mode, _show, _store)
 #define DEVICE_ATTR_RW(_name) \
 	struct device_attribute dev_attr_##_name = __ATTR_RW(_name)
+#define DEVICE_ATTR_WO(_name) \
+	struct device_attribute dev_attr_##_name = __ATTR_WO(_name)
 #define DEVICE_ATTR_RO(_name) \
 	struct device_attribute dev_attr_##_name = __ATTR_RO(_name)
 
