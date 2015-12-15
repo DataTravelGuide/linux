16c6440b7dc3649ab6ea48186574c34c34899c4f
Don Zickus <dzickus@redhat.com>
2015-12-14 19:16:24 -0500

    add sysctl knob hardlockup_panic
    Patchwork-id: 129051
    O-Subject: [RHEL6 PATCH 2/3] kernel/watchdog.c: add sysctl knob hardlockup_panic
    Bugzilla: 1305919
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Aaron Tomlin <atomlin@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1290573
    
    commit ac1f591249d95372f3a5ab3828d4af5dfbf5efd3
    Author: Don Zickus <dzickus@redhat.com>
    Date:   Thu Nov 5 18:44:44 2015 -0800
    
        kernel/watchdog.c: add sysctl knob hardlockup_panic
    
        The only way to enable a hardlockup to panic the machine is to set
        'nmi_watchdog=panic' on the kernel command line.
    
        This makes it awkward for end users and folks who want to run automate
        tests (like myself).
    
        Mimic the softlockup_panic knob and create a /proc/sys/kernel/hardlockup_panic
        knob.
    
        Signed-off-by: Don Zickus <dzickus@redhat.com>
        Cc: Ulrich Obergfell <uobergfe@redhat.com>
        Acked-by: Jiri Kosina <jkosina@suse.cz>
        Reviewed-by: Aaron Tomlin <atomlin@redhat.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -314,6 +314,7 @@ extern int proc_dowatchdog_thresh(struct ctl_table *table, int write,
 				  size_t *lenp, loff_t *ppos);
 extern unsigned int  softlockup_panic;
 extern int softlockup_thresh;
+extern unsigned int  hardlockup_panic;
 void lockup_detector_init(void);
 #else
 static inline void touch_softlockup_watchdog(void)
--- kernel/watchdog.c
+++ kernel/watchdog.c
@@ -58,7 +58,7 @@ static unsigned long soft_lockup_nmi_warn;
  * Should we panic when a soft-lockup or hard-lockup occurs:
  */
 #ifdef CONFIG_HARDLOCKUP_DETECTOR
-static int hardlockup_panic = 
+unsigned int __read_mostly hardlockup_panic =
 			CONFIG_BOOTPARAM_HARDLOCKUP_PANIC_VALUE;
 static int hardlockup_enable = 
 			CONFIG_BOOTPARAM_HARDLOCKUP_ENABLED_VALUE;
