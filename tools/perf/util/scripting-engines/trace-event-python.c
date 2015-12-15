1dba4e11118385cb87a70cccbe341d4b8225d30c
Jiri Olsa <jolsa@redhat.com>
2015-12-15 13:38:22 -0500

    tools lib traceevent: Add support for __print_array
    Patchwork-id: 129319
    O-Subject: [PATCH RHEL6 BZ1291071 243/315] tools lib traceevent: Add support for __print_array()
    Bugzilla: 1291071
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    From: Jiri Olsa <jolsa@kernel.org>
    
    Bugzilla: 1291071
    https://bugzilla.redhat.com/show_bug.cgi?id=1291071
    
    upstream
    ========
    commit b839e1e846edb15cff322e262e8ce412d298232e
    Author: Javi Merino <javi.merino@arm.com>
    Date: Tue Mar 24 11:07:19 2015 +0000
    
    description
    ===========
    Since 6ea22486ba46 ("tracing: Add array printing helper") trace can
    generate traces with variable element size arrays.  Add support to
    parse them.
    
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- tools/perf/util/scripting-engines/trace-event-python.c
+++ tools/perf/util/scripting-engines/trace-event-python.c
@@ -227,6 +227,11 @@ static void define_event_symbols(struct event_format *event,
 		define_values(PRINT_SYMBOL, args->symbol.symbols, ev_name,
 			      cur_field_name);
 		break;
+	case PRINT_INT_ARRAY:
+		define_event_symbols(event, ev_name, args->int_array.field);
+		define_event_symbols(event, ev_name, args->int_array.count);
+		define_event_symbols(event, ev_name, args->int_array.el_size);
+		break;
 	case PRINT_STRING:
 		break;
 	case PRINT_TYPE:
