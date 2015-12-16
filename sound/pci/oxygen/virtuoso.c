4508296fd7dba3accf8fb2080df1d869bc030fce
Jaroslav Kysela <jkysela@redhat.com>
2015-12-16 11:14:38 -0500

    alsa: virtuoso: add Xonar Essence STX II support
    Patchwork-id: 129509
    O-Subject: [RHEL 6.8 PATCH 23/35] ALSA: virtuoso: add Xonar Essence STX II support
    Bugzilla: 1024771
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    Just add the PCI ID for the STX II.  It appears to work the same as the
    STX, except for the addition of the not-yet-supported daughterboard.
    
    Tested-by: Mario <fugazzi99@gmail.com>
    Tested-by: corubba <corubba@gmx.de>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    
    (picked from kernel: f42bb22243d2ae264d721b055f836059fe35321f)
    rhbz#1024771
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>
    
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- sound/pci/oxygen/virtuoso.c
+++ sound/pci/oxygen/virtuoso.c
@@ -52,6 +52,7 @@ static struct pci_device_id xonar_ids[] __devinitdata = {
 	{ OXYGEN_PCI_SUBID(0x1043, 0x835e) },
 	{ OXYGEN_PCI_SUBID(0x1043, 0x838e) },
 	{ OXYGEN_PCI_SUBID(0x1043, 0x8522) },
+	{ OXYGEN_PCI_SUBID(0x1043, 0x85f4) },
 	{ OXYGEN_PCI_SUBID_BROKEN_EEPROM },
 	{ }
 };
