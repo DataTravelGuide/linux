4b8604b5840ee1cc0edd489e5f4c323319478019
Jeremy McNicoll <jmcnicol@redhat.com>
2015-12-15 22:49:22 -0500

    i801: Add device ID for Intel Wildcat Point PCH
    Patchwork-id: 129482
    O-Subject: [RHEL6.8 PATCH] i2c: i801: Add device ID for Intel Wildcat Point PCH
    Bugzilla: 1291132
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1291132
    Build: https://brewweb.devel.redhat.com/taskinfo?taskID=10231908
    
    Note: Modified the 'Intel I/O Controller Hubs (ICH):' table
    	comment entry to match the existing format.
    
    commit b299de839157852c563b9f133c8b7e630545a9c3
    Author: Jean Delvare <jdelvare@suse.de>
    Date:   Thu Jul 17 15:04:41 2014 +0200
    
        i2c: i801: Add device ID for Intel Wildcat Point PCH
    
        Signed-off-by: Jean Delvare <jdelvare@suse.de>
        Signed-off-by: Wolfram Sang <wsa@the-dreams.de>
    
    Signed-off-by: Jeremy McNicoll <jmcnicol@redhat.com>
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- drivers/i2c/busses/i2c-i801.c
+++ drivers/i2c/busses/i2c-i801.c
@@ -59,6 +59,7 @@
   Wellsburg (PCH) MS    0x8d7f     32     hard     yes     yes     yes
   Wildcat Point-LP (PCH)   0x9ca2     32     hard     yes     yes     yes
   Coleto Creek (PCH)    0x23b0     32     hard     yes     yes     yes
+  Wildcat Point (PCH)   0x8ca2     32     hard     yes     yes     yes
   Sunrise Point-H (PCH) 0xa123     32     hard     yes     yes     yes
   Sunrise Point-LP (PCH)0x9d23     32     hard     yes     yes     yes
 
@@ -175,6 +176,7 @@
 
 /* Patsburg also has three 'Integrated Device Function' SMBus controllers */
 #define PCI_DEVICE_ID_INTEL_PATSBURG_SMBUS_IDF0	0x1d70
+#define PCI_DEVICE_ID_INTEL_WILDCATPOINT_SMBUS		0x8ca2
 #define PCI_DEVICE_ID_INTEL_PATSBURG_SMBUS_IDF1	0x1d71
 #define PCI_DEVICE_ID_INTEL_PATSBURG_SMBUS_IDF2	0x1d72
 #define PCI_DEVICE_ID_INTEL_PANTHERPOINT_SMBUS	0x1e22
@@ -746,6 +748,7 @@ static const struct pci_device_id i801_ids[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_COLETOCREEK_SMBUS) },
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_SUNRISEPOINT_H_SMBUS) },
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_SUNRISEPOINT_LP_SMBUS) },
+	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_WILDCATPOINT_SMBUS) },
 	{ 0, }
 };
 
