a2514f3220c44257ded4fb0bf58463d0731dba2a
Don Dutile <ddutile@redhat.com>
2015-12-17 02:05:56 -0500

    rdma/ocrdma: Add support for IB stack compliant stats in sysfs
    Patchwork-id: 130133
    O-Subject: [RHEL-6.8 PATCH 339/483] RDMA/ocrdma: Add support for IB stack compliant stats in sysfs.
    Bugzilla: 1120022 1142056 1192551 1246059 1253021 1271492
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Mitesh Ahuja <mitesh.ahuja@emulex.com>
    
    Add the following per-port sysfs traffic counters for RoCE:
    
            port_xmit_packets
            port_rcv_packets
            port_rcv_data
            port_xmit_data
    
    Signed-off-by: Mitesh Ahuja <mitesh.ahuja@emulex.com>
    Signed-off-by: Devesh Sharma <devesh.sharma@emulex.com>
    Signed-off-by: Selvin Xavier <selvin.xavier@emulex.com>
    Signed-off-by: Roland Dreier <roland@purestorage.com>
    (cherry picked from commit cad1fbb0fd0e72ae2e7c18d486ff33e26c275f6c)
    
    Conflicts:
    	drivers/infiniband/hw/ocrdma/ocrdma_ah.c
    	drivers/infiniband/hw/ocrdma/ocrdma_stats.c
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- drivers/infiniband/hw/ocrdma/ocrdma_ah.c
+++ drivers/infiniband/hw/ocrdma/ocrdma_ah.c
@@ -39,8 +39,6 @@
 
 #define OCRDMA_VID_PCP_SHIFT	0xD
 
-#define OCRDMA_VID_PCP_SHIFT	0xD
-
 static inline int set_av_attr(struct ocrdma_dev *dev, struct ocrdma_ah *ah,
 			struct ib_ah_attr *attr, union ib_gid *sgid,
 			int pdid, bool *isvlan)
