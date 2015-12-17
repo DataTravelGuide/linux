71c43960f23cdef8bc3a8da741985e9a0a32bd22
Don Dutile <ddutile@redhat.com>
2015-12-17 02:01:34 -0500

    mlx4_en: mlx4_en_gset_priv_flags can be static
    Patchwork-id: 129849
    O-Subject: [RHEL-6.8 PATCH 077/483] net/mlx4_en: mlx4_en_[gs]et_priv_flags() can be static
    Bugzilla: 1120022 1142056 1192551 1246059 1253021 1271492
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Fixes sparse warning intrduced by commit 0fef9d0 ("net/mlx4_en: Disable
    blueflame using ethtool private flags")
    
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Amir Vadai <amirv@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 3f6148e76a58eceef1435b65afecaf448b509cfd)
    
    RHEL6 note: [gs]et_priv_flags patches caused cherry-pick
                to pull all upstream code in
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- drivers/net/mlx4/en_ethtool.c
+++ drivers/net/mlx4/en_ethtool.c
@@ -1248,6 +1248,49 @@ static int mlx4_en_get_ts_info(struct net_device *dev,
 	return ret;
 }
 
+static int mlx4_en_set_priv_flags(struct net_device *dev, u32 flags)
+{
+	struct mlx4_en_priv *priv = netdev_priv(dev);
+	bool bf_enabled_new = !!(flags & MLX4_EN_PRIV_FLAGS_BLUEFLAME);
+	bool bf_enabled_old = !!(priv->pflags & MLX4_EN_PRIV_FLAGS_BLUEFLAME);
+	int i;
+
+	if (bf_enabled_new == bf_enabled_old)
+		return 0; /* Nothing to do */
+
+	if (bf_enabled_new) {
+		bool bf_supported = true;
+
+		for (i = 0; i < priv->tx_ring_num; i++)
+			bf_supported &= priv->tx_ring[i]->bf_alloced;
+
+		if (!bf_supported) {
+			en_err(priv, "BlueFlame is not supported\n");
+			return -EINVAL;
+		}
+
+		priv->pflags |= MLX4_EN_PRIV_FLAGS_BLUEFLAME;
+	} else {
+		priv->pflags &= ~MLX4_EN_PRIV_FLAGS_BLUEFLAME;
+	}
+
+	for (i = 0; i < priv->tx_ring_num; i++)
+		priv->tx_ring[i]->bf_enabled = bf_enabled_new;
+
+	en_info(priv, "BlueFlame %s\n",
+		bf_enabled_new ?  "Enabled" : "Disabled");
+
+	return 0;
+}
+
+static u32 mlx4_en_get_priv_flags(struct net_device *dev)
+{
+	struct mlx4_en_priv *priv = netdev_priv(dev);
+
+	return priv->pflags;
+}
+
+
 const struct ethtool_ops mlx4_en_ethtool_ops = {
 	.get_drvinfo = mlx4_en_get_drvinfo,
 	.get_settings = mlx4_en_get_settings,
