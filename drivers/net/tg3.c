8a2c3cba1f8786444be40e3fe79714c92f175e20
Ivan Vecera <ivecera@redhat.com>
2015-12-18 21:46:55 -0500

    tg3: Support for byte queue limits
    Patchwork-id: 130532
    O-Subject: [RHEL6.8 PATCH 1/8] tg3: Support for byte queue limits
    Bugzilla: 1253054
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    
    BZ: #1253054
    
    Upstream commit(s):
    commit 298376d3e8f00147548c426959ce79efc47b669a
    Author: Tom Herbert <therbert@google.com>
    Date:   Mon Nov 28 16:33:30 2011 +0000
    
        tg3: Support for byte queue limits
    
        Changes to tg3 to use byte queue limits.
    
        Signed-off-by: Tom Herbert <therbert@google.com>
        Acked-by: Eric Dumazet <eric.dumazet@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- drivers/net/tg3.c
+++ drivers/net/tg3.c
@@ -6527,6 +6527,7 @@ static void tg3_tx(struct tg3_napi *tnapi)
 	u32 sw_idx = tnapi->tx_cons;
 	struct netdev_queue *txq;
 	int index = tnapi - tp->napi;
+	unsigned int pkts_compl = 0, bytes_compl = 0;
 
 	if (tg3_flag(tp, ENABLE_TSS))
 		index--;
@@ -6587,6 +6588,9 @@ static void tg3_tx(struct tg3_napi *tnapi)
 			sw_idx = NEXT_TX(sw_idx);
 		}
 
+		pkts_compl++;
+		bytes_compl += skb->len;
+
 		dev_kfree_skb_any(skb);
 
 		if (unlikely(tx_bug)) {
@@ -6594,6 +6598,8 @@ static void tg3_tx(struct tg3_napi *tnapi)
 			return;
 		}
 	}
+
+	netdev_completed_queue(tp->dev, pkts_compl, bytes_compl);
 
 	tnapi->tx_cons = sw_idx;
 
@@ -8077,6 +8083,7 @@ static netdev_tx_t tg3_start_xmit(struct sk_buff *skb, struct net_device *dev)
 	}
 
 	skb_tx_timestamp(skb);
+	netdev_sent_queue(tp->dev, skb->len);
 
 	/* Sync BD data before updating mailbox */
 	wmb();
@@ -8501,6 +8508,7 @@ static void tg3_free_rings(struct tg3 *tp)
 			dev_kfree_skb_any(skb);
 		}
 	}
+	netdev_reset_queue(tp->dev);
 }
 
 /* Initialize tx/rx rings for packet processing.
