f002391c32d2b492ec2f10f9c00e38f5a77cb7d0
Stefan Assmann <sassmann@redhat.com>
2015-12-21 12:34:53 -0500

    i40e/i40evf: Simplify tunnel selection logic
    Patchwork-id: 131053
    O-Subject: [RHEL6.8 PATCH 009/153] i40e/i40evf: Simplify tunnel selection logic
    Bugzilla: 1249247
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    
    From: Anjali Singhai Jain <anjali.singhai@intel.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1249247
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=10265270
    
    Use l4_tunnel type generically to keep code flow simple.
    
    Change-ID: Ic52287e3b1ca4204e6b6e13431890c1a6ae9c422
    Signed-off-by: Anjali Singhai Jain <anjali.singhai@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Stefan Assmann <sassmann@redhat.com>
    (cherry picked from commit 4599120466c784f8b2afef6fb2b7a452a73b400e)
    
    Note: removed unused variable l4_tunnel to avoid build breakage.
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- drivers/net/i40evf/i40e_txrx.c
+++ drivers/net/i40evf/i40e_txrx.c
@@ -1459,6 +1459,13 @@ static void i40e_tx_enable_csum(struct sk_buff *skb, u32 tx_flags,
 
 #if 0 /* RHEL6 */
 	if (skb->encapsulation) {
+		switch (ip_hdr(skb)->protocol) {
+		case IPPROTO_UDP:
+			l4_tunnel = I40E_TXD_CTX_UDP_TUNNELING;
+			break;
+		default:
+			return;
+		}
 		network_hdr_len = skb_inner_network_header_len(skb);
 		this_ip_hdr = inner_ip_hdr(skb);
 		this_ipv6_hdr = inner_ipv6_hdr(skb);
@@ -1481,8 +1488,8 @@ static void i40e_tx_enable_csum(struct sk_buff *skb, u32 tx_flags,
 
 		/* Now set the ctx descriptor fields */
 		*cd_tunneling |= (skb_network_header_len(skb) >> 2) <<
-					I40E_TXD_CTX_QW0_EXT_IPLEN_SHIFT |
-				   I40E_TXD_CTX_UDP_TUNNELING            |
+				   I40E_TXD_CTX_QW0_EXT_IPLEN_SHIFT      |
+				   l4_tunnel                             |
 				   ((skb_inner_network_offset(skb) -
 					skb_transport_offset(skb)) >> 1) <<
 				   I40E_TXD_CTX_QW0_NATLEN_SHIFT;
