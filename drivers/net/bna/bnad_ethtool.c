b001e6e3665716a045b21b9e8deb7aefef078676
Ivan Vecera <ivecera@redhat.com>
2015-12-21 15:21:48 -0500

    bna: use netdev_* and dev_* instead of printk and pr_*
    Patchwork-id: 131285
    O-Subject: [RHEL6.8 PATCH 41/46] bna: use netdev_* and dev_* instead of printk and pr_*
    Bugzilla: 1258535
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    BZ: #1258535
    
    Upstream commit(s):
    commit ecc467896d977f4f1a6dfee77652aab117ebfb73
    Author: Ivan Vecera <ivecera@redhat.com>
    Date:   Thu Jun 11 15:52:31 2015 +0200
    
        bna: use netdev_* and dev_* instead of printk and pr_*
    
        ...and remove some of them. It is not necessary to log when .probe() and
        .remove() are called or when TxQ is started or stopped. Also log level
        of some of them was changed to more appropriate one (link up/down,
        firmware loading failure.
    
        Signed-off-by: Ivan Vecera <ivecera@redhat.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- drivers/net/bna/bnad_ethtool.c
+++ drivers/net/bna/bnad_ethtool.c
@@ -1084,7 +1084,7 @@ bnad_flash_device(struct net_device *netdev, struct ethtool_flash *eflash)
 
 	ret = request_firmware(&fw, eflash->data, &bnad->pcidev->dev);
 	if (ret) {
-		pr_err("BNA: Can't locate firmware %s\n", eflash->data);
+		netdev_err(netdev, "can't load firmware %s\n", eflash->data);
 		goto out;
 	}
 
@@ -1097,7 +1097,7 @@ bnad_flash_device(struct net_device *netdev, struct ethtool_flash *eflash)
 				bnad->id, (u8 *)fw->data, fw->size, 0,
 				bnad_cb_completion, &fcomp);
 	if (ret != BFA_STATUS_OK) {
-		pr_warn("BNA: Flash update failed with err: %d\n", ret);
+		netdev_warn(netdev, "flash update failed with err=%d\n", ret);
 		ret = -EIO;
 		spin_unlock_irq(&bnad->bna_lock);
 		goto out;
@@ -1107,8 +1107,9 @@ bnad_flash_device(struct net_device *netdev, struct ethtool_flash *eflash)
 	wait_for_completion(&fcomp.comp);
 	if (fcomp.comp_status != BFA_STATUS_OK) {
 		ret = -EIO;
-		pr_warn("BNA: Firmware image update to flash failed with: %d\n",
-			fcomp.comp_status);
+		netdev_warn(netdev,
+			    "firmware image update failed with err=%d\n",
+			    fcomp.comp_status);
 	}
 out:
 	release_firmware(fw);
