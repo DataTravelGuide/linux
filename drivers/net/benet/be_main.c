919217659f13829439e469eae0c405825ae3ca18
Ivan Vecera <ivecera@redhat.com>
2015-12-21 17:49:37 -0500

    be2net: remove redundant D0 power state set
    Patchwork-id: 131351
    O-Subject: [RHEL6.8 PATCH 39/60] be2net: remove redundant D0 power state set
    Bugzilla: 1253018
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    BZ: #1253018
    
    Upstream commit(s):
    commit bc23d6b7ab051106b700869e7ee09e8aec6864f2
    Author: Kalesh Purayil <kalesh.purayil@avagotech.com>
    Date:   Fri Jul 10 05:32:45 2015 -0400
    
        be2net: remove redundant D0 power state set
    
        pci_enable_device() call sets device power state to D0; there is no need
        doing it again.
    
        Signed-off-by: Kalesh AP <kalesh.purayil@avagotech.com>
        Signed-off-by: Sathya Perla <sathya.perla@avagotech.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- drivers/net/benet/be_main.c
+++ drivers/net/benet/be_main.c
@@ -5629,7 +5629,6 @@ static int be_pci_resume(struct pci_dev *pdev)
 	if (status)
 		return status;
 
-	pci_set_power_state(pdev, 0);
 	pci_restore_state(pdev);
 
 	status = be_resume(adapter);
@@ -5709,7 +5708,6 @@ static pci_ers_result_t be_eeh_reset(struct pci_dev *pdev)
 		return PCI_ERS_RESULT_DISCONNECT;
 
 	pci_set_master(pdev);
-	pci_set_power_state(pdev, 0);
 	pci_restore_state(pdev);
 
 	/* Check if card is ok and fw is ready */
