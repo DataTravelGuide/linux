b50619076dcb52641e0b78f13628e7708531a690
Honggang Li <honli@redhat.com>
2015-12-25 07:16:53 -0500

    SUNRPC: Pass a pointer to struct rpc_xprt to the connect callback
    Patchwork-id: 131532
    O-Subject: [RHEL-6.8 NFSoRDMA v2 012/106] SUNRPC: Pass a pointer to struct rpc_xprt to the connect callback
    Bugzilla: 1271499
    RH-Acked-by: Sachin Prabhu <sprabhu@redhat.com>
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    RH-Acked-by: Benjamin Coddington <bcodding@redhat.com>
    
    From: Trond Myklebust <Trond.Myklebust@netapp.com>
    
    Avoid another RCU dereference by passing the pointer to struct rpc_xprt
    from the caller.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    (cherry picked from commit 1b092092bf0e2e8b7af1c2a03f615b4e60b05d47)
    Signed-off-by: Honggang Li <honli@redhat.com>

--- net/sunrpc/xprtsock.c
+++ net/sunrpc/xprtsock.c
@@ -2229,6 +2229,7 @@ out:
 
 /**
  * xs_connect - connect a socket to a remote endpoint
+ * @xprt: pointer to transport structure
  * @task: address of RPC task that manages state of connect request
  *
  * TCP: If the remote end dropped the connection, delay reconnecting.
@@ -2240,9 +2241,8 @@ out:
  * If a UDP socket connect fails, the delay behavior here prevents
  * retry floods (hard mounts).
  */
-static void xs_connect(struct rpc_task *task)
+static void xs_connect(struct rpc_xprt *xprt, struct rpc_task *task)
 {
-	struct rpc_xprt *xprt = task->tk_xprt;
 	struct sock_xprt *transport = container_of(xprt, struct sock_xprt, xprt);
 
 	if (transport->sock != NULL && !RPC_IS_SOFTCONN(task)) {
