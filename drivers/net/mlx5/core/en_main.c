6db3370703ce6758402a835e0a0c3f6da40e008f
Kamal Heib <kheib@redhat.com>
2016-01-13 18:56:36 -0500

    net/mlx5e: Disable client vlan TX acceleration
    Patchwork-id: 132019
    O-Subject: [RHEL6.8 PATCH V2 031/117] net/mlx5e: Disable client vlan TX acceleration
    Bugzilla: 1246031
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    From: Saeed Mahameed <saeedm@mellanox.com>
    
    We need to resolve a HW configuration issue for enabling HW CVLAN
    insertion. Meanwhile, no need to implement the VLAN insertion in
    the driver, rather use the generic kernel VLAN insertion method.
    
    Divergences from upstream:
    - upstream: netdev->hw_features v.s RHEL6.8: netdev->features
    - upstream: NETIF_F_HW_VLAN_CTAG_FILTER v.s RHEL6.8: NETIF_F_HW_VLAN_FILTER
    - skb->vlan_proto doesn't exist in rhel6 use cpu_to_be16(ETH_P_8021Q)
    instead
    - skb_pull_inline doesn't exist in rhel6 use skb_pull instead
    - upstream: skb_vlan_tag_present vs. rhel6 vlan_tx_tag_present
    
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit cd58c714acb98a186051dc7e620d1b7ef4843311)
    Signed-off-by: Kamal Heib <kheib@redhat.com>
    
    Conflicts:
    	drivers/net/ethernet/mellanox/mlx5/core/en_main.c
    	drivers/net/ethernet/mellanox/mlx5/core/en_tx.c
    
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- drivers/net/mlx5/core/en_main.c
+++ drivers/net/mlx5/core/en_main.c
@@ -1760,7 +1760,6 @@ static void mlx5e_build_netdev(struct net_device *netdev)
 		netdev->vlan_features    |= NETIF_F_LRO;
 
 	netdev->features       = netdev->vlan_features;
-	netdev->features      |= NETIF_F_HW_VLAN_TX;
 	netdev->features      |= NETIF_F_HW_VLAN_RX;
 	netdev->features      |= NETIF_F_HW_VLAN_FILTER;
 
