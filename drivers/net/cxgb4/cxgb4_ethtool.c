6220aae6aaa677b85c269c4bff85e04019cd72cb
Sai Vemuri <svemuri@redhat.com>
2016-02-17 18:13:38 -0500

    cxgb4: Remove WOL get/set ethtool support
    Patchwork-id: 134878
    O-Subject: [RHEL6.8 BZ 1252598 41/76] cxgb4: Remove WOL get/set ethtool support
    Bugzilla: 1252598
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1252598
    
    commit e2d14b42c25cb765df07d585a919b8c1c8b82e78
    Author: Hariprasad Shenai <hariprasad@chelsio.com>
    Date:   Wed Jun 3 21:04:42 2015 +0530
    
        cxgb4: Remove WOL get/set ethtool support
    
        Remove ethtool get/set support for wake on lan, adapter doesn't support
        it.
    
        Signed-off-by: Hariprasad Shenai <hariprasad@chelsio.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Aristeu Rozanski <aris@redhat.com>

--- drivers/net/cxgb4/cxgb4_ethtool.c
+++ drivers/net/cxgb4/cxgb4_ethtool.c
@@ -951,37 +951,6 @@ static int set_flash(struct net_device *netdev, struct ethtool_flash *ef)
 	return ret;
 }
 
-#define WOL_SUPPORTED (WAKE_BCAST | WAKE_MAGIC)
-#define BCAST_CRC 0xa0ccc1a6
-
-static void get_wol(struct net_device *dev, struct ethtool_wolinfo *wol)
-{
-	wol->supported = WAKE_BCAST | WAKE_MAGIC;
-	wol->wolopts = netdev2adap(dev)->wol;
-	memset(&wol->sopass, 0, sizeof(wol->sopass));
-}
-
-static int set_wol(struct net_device *dev, struct ethtool_wolinfo *wol)
-{
-	int err = 0;
-	struct port_info *pi = netdev_priv(dev);
-
-	if (wol->wolopts & ~WOL_SUPPORTED)
-		return -EINVAL;
-	t4_wol_magic_enable(pi->adapter, pi->tx_chan,
-			    (wol->wolopts & WAKE_MAGIC) ? dev->dev_addr : NULL);
-	if (wol->wolopts & WAKE_BCAST) {
-		err = t4_wol_pat_enable(pi->adapter, pi->tx_chan, 0xfe, ~0ULL,
-					~0ULL, 0, false);
-		if (!err)
-			err = t4_wol_pat_enable(pi->adapter, pi->tx_chan, 1,
-						~6ULL, ~0ULL, BCAST_CRC, true);
-	} else {
-		t4_wol_pat_enable(pi->adapter, pi->tx_chan, 0, 0, 0, 0, false);
-	}
-	return err;
-}
-
 static u32 get_rx_csum(struct net_device *dev)
 {
 	struct port_info *p = netdev_priv(dev);
@@ -1155,8 +1124,6 @@ static const struct ethtool_ops cxgb_ethtool_ops = {
 	        .get_ethtool_stats = get_stats,
 	        .get_regs_len      = get_regs_len,
 	        .get_regs          = get_regs,
-	        .get_wol           = get_wol,
-	        .set_wol           = set_wol,
 		.set_tso           = set_tso,
 	        .set_flags         = set_flags,
 	        .flash_device      = set_flash,
