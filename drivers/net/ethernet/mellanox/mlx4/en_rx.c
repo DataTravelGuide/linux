753e3983d922639da2755a50a59f4741b2afce82
Kamal Heib <kamalh@mellanox.com>
2016-09-20 14:39:40 +0300

    net/mlx4_en: Fix wrong indentation
    Use tabs instead of spaces before if statement, no functional change.
    
    Fixes: e7c1c2c46201 ("mlx4_en: Added self diagnostics test implementation")
    Signed-off-by: Kamal Heib <kamalh@mellanox.com>
    Signed-off-by: Tariq Toukan <tariqt@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx4/en_rx.c
+++ drivers/net/ethernet/mellanox/mlx4/en_rx.c
@@ -946,7 +946,7 @@ xdp_drop:
 			goto next;
 		}
 
-                if (unlikely(priv->validate_loopback)) {
+		if (unlikely(priv->validate_loopback)) {
 			validate_loopback(priv, skb);
 			goto next;
 		}
