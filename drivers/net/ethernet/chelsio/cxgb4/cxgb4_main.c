0bfe10a51cfcdaa8a02e20996f88feeaa5ce78a5
Ganesh Goudar <ganeshgr@chelsio.com>
2017-01-12 12:23:21 +0530

    cxgb4: Initialize mbox lock and list for mgmt dev
    Initialize mbox lock and list for mgmt dev to avoid NULL pointer
    dereference when cxgb_set_vf_mac is called.
    
    And also allocate memory for private data while allocating mgmt
    netdev.
    
    Signed-off-by: Ganesh Goudar <ganeshgr@chelsio.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Conflict patch:
    0001-cxgb4-Initialize-mbox-lock-and-list-for-mgmt-dev.patch
    
    Merge log:
    patching file drivers/net/ethernet/chelsio/cxgb4/cxgb4_main.c
    Hunk #1 NOT MERGED at 4522-4527.
    (EScore specific version string)

--- drivers/net/ethernet/chelsio/cxgb4/cxgb4_main.c
+++ drivers/net/ethernet/chelsio/cxgb4/cxgb4_main.c
@@ -4519,7 +4519,8 @@ static int config_mgmt_dev(struct pci_dev *pdev)
 	int err;
 
 	snprintf(name, IFNAMSIZ, "mgmtpf%d%d", adap->adap_idx, adap->pf);
-	netdev = alloc_netdev(0, name, dummy_setup);
+	netdev = alloc_netdev(sizeof(struct port_info), name, NET_NAME_UNKNOWN,
+			      dummy_setup);
 	if (!netdev)
 		return -ENOMEM;
 
@@ -4993,6 +4994,8 @@ sriov:
 		err = -ENOMEM;
 		goto free_adapter;
 	}
+	spin_lock_init(&adapter->mbox_lock);
+	INIT_LIST_HEAD(&adapter->mlist.list);
 	pci_set_drvdata(pdev, adapter);
 	return 0;
 
