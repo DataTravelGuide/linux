03fb82ac0e58b16794e9aeb55fd205a5b4c5b0e9
Ivan Vecera <ivecera@redhat.com>
2017-02-03 12:59:06 -0500

    phy: Don't increment MDIO bus refcount unless it's a different owner
    Message-id: <20170203125906.16757-17-ivecera@redhat.com>
    Patchwork-id: 164986
    O-Subject: [RHEL7.4 net PATCH 65/65] phy: Don't increment MDIO bus refcount unless it's a different owner
    Bugzilla: 1382040
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    BZ#1382040
    
    Upstream commit(s):
    commit ec988ad78ed6d184a7f4ca6b8e962b0e8f1de461
    Author: Florian Fainelli <f.fainelli@gmail.com>
    Date:   Tue Dec 6 20:54:43 2016 -0800
    
        phy: Don't increment MDIO bus refcount unless it's a different owner
    
        Commit 3e3aaf649416 ("phy: fix mdiobus module safety") fixed the way we
        dealt with MDIO bus module reference count, but sort of introduced a
        regression in that, if an Ethernet driver registers its own MDIO bus
        driver, as is common, we will end up with the Ethernet driver's
        module->refnct set to 1, thus preventing this driver from any removal.
    
        Fix this by comparing the network device's device driver owner against
        the MDIO bus driver owner, and only if they are different, increment the
        MDIO bus module refcount.
    
        Fixes: 3e3aaf649416 ("phy: fix mdiobus module safety")
        Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -595,11 +595,17 @@ EXPORT_SYMBOL(phy_init_hw);
 int phy_attach_direct(struct net_device *dev, struct phy_device *phydev,
 		      u32 flags, phy_interface_t interface)
 {
+	struct module *ndev_owner = dev->dev.parent->driver->owner;
 	struct mii_bus *bus = phydev->bus;
 	struct device *d = &phydev->dev;
 	int err;
 
-	if (!try_module_get(bus->owner)) {
+	/* For Ethernet device drivers that register their own MDIO bus, we
+	 * will have bus->owner match ndev_mod, so we do not want to increment
+	 * our own module->refcnt here, otherwise we would not be able to
+	 * unload later on.
+	 */
+	if (ndev_owner != bus->owner && !try_module_get(bus->owner)) {
 		dev_err(&dev->dev, "failed to get the bus module\n");
 		return -EIO;
 	}
@@ -650,7 +656,8 @@ int phy_attach_direct(struct net_device *dev, struct phy_device *phydev,
 
 error:
 	put_device(d);
-	module_put(bus->owner);
+	if (ndev_owner != bus->owner)
+		module_put(bus->owner);
 	return err;
 }
 EXPORT_SYMBOL(phy_attach_direct);
@@ -698,6 +705,8 @@ EXPORT_SYMBOL(phy_attach);
  */
 void phy_detach(struct phy_device *phydev)
 {
+	struct net_device *dev = phydev->attached_dev;
+	struct module *ndev_owner = dev->dev.parent->driver->owner;
 	struct mii_bus *bus;
 	int i;
 
@@ -724,7 +733,8 @@ void phy_detach(struct phy_device *phydev)
 	bus = phydev->bus;
 
 	put_device(&phydev->dev);
-	module_put(bus->owner);
+	if (ndev_owner != bus->owner)
+		module_put(bus->owner);
 }
 EXPORT_SYMBOL(phy_detach);
 
