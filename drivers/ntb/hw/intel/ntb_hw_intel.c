1c485e6e133ea4b94a5872504400ebe2212a6982
Suravee Suthikulpanit <ssuthiku@redhat.com>
2017-02-10 09:20:03 -0500

    Move files in preparation for NTB abstraction
    Message-id: <1486718506-16087-45-git-send-email-ssuthiku@redhat.com>
    Patchwork-id: 165653
    O-Subject: [RHEL7.4 PATCH BZ 1303727 044/147] NTB: Move files in preparation for NTB abstraction
    Bugzilla: 1303727
    RH-Acked-by: Torez Smith <torez@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Myron Stowe <mstowe@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1303727
    
    commit ec110bc7cc48d7806c9b65094e6afb19452d458f
    Author: Allen Hubbe <Allen.Hubbe@emc.com>
    Date:   Thu May 7 06:45:21 2015 -0400
    
        NTB: Move files in preparation for NTB abstraction
    
        This patch only moves files to their new locations, before applying the
        next two patches adding the NTB Abstraction layer.  Splitting this patch
        from the next is intended make distinct which code is changed only due
        to moving the files, versus which are substantial code changes in adding
        the NTB Abstraction layer.
    
        Signed-off-by: Allen Hubbe <Allen.Hubbe@emc.com>
        Signed-off-by: Jon Mason <jdmason@kudzu.us>
    
    Cc: Lance Richardson <lrichard@redhat.com>
    Cc: Sabrina Dubroca <sdubroca@redhat.com>
    Cc: Suravee Suthikulpanit <ssuthiku@redhat.com>
    Cc: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflict patch:
    11703-ntb-Move-files-in-preparation-for-NTB-abstraction.patch
    
    Merge log:
    patching file drivers/net/Kconfig
    patching file drivers/net/ntb_netdev.c
    patching file drivers/ntb/Makefile
    patching file drivers/ntb/hw/intel/ntb_hw_intel.c
    patching file drivers/ntb/hw/intel/ntb_hw_intel.h
    patching file drivers/ntb/ntb_hw.c
    Hunk #1 already applied at 1.
    File drivers/ntb/ntb_hw.c is not empty after patch; not deleting
    patching file drivers/ntb/ntb_hw.h
    patching file drivers/ntb/ntb_regs.h
    patching file drivers/ntb/ntb_transport.c
    patching file include/linux/ntb.h
    patching file include/linux/ntb_transport.h
    (EScore specific version string)

--- drivers/ntb/hw/intel/ntb_hw_intel.c
+++ drivers/ntb/hw/intel/ntb_hw_intel.c
@@ -53,8 +53,7 @@
 #include <linux/pci.h>
 #include <linux/random.h>
 #include <linux/slab.h>
-#include "ntb_hw.h"
-#include "ntb_regs.h"
+#include "ntb_hw_intel.h"
 
 #define NTB_NAME	"Intel(R) PCI-E Non-Transparent Bridge Driver"
 #define NTB_VER		"1.0"
@@ -1170,11 +1169,9 @@ static int ntb_setup_snb_msix(struct ntb_device *ndev, int msix_entries)
 	if (msix_entries < ndev->limits.msix_cnt)
 		return -ENOSPC;
 
-	rc = pci_enable_msix(pdev, ndev->msix_entries, msix_entries);
+	rc = pci_enable_msix_exact(pdev, ndev->msix_entries, msix_entries);
 	if (rc < 0)
 		return rc;
-	else if (rc > 0)
-		return -ENOSPC;
 
 	for (i = 0; i < msix_entries; i++) {
 		msix = &ndev->msix_entries[i];
@@ -1220,18 +1217,10 @@ static int ntb_setup_bwd_msix(struct ntb_device *ndev, int msix_entries)
 	struct msix_entry *msix;
 	int rc, i;
 
-retry:
-	rc = pci_enable_msix(pdev, ndev->msix_entries, msix_entries);
-	if (rc < 0)
-		return rc;
-	else if (rc > 0) {
-		dev_warn(&pdev->dev,
-			 "Only %d MSI-X vectors. "
-			 "Limiting the number of queues to that number.\n",
-			 rc);
-		msix_entries = rc;
-		goto retry;
-	}
+	msix_entries = pci_enable_msix_range(pdev, ndev->msix_entries,
+					     1, msix_entries);
+	if (msix_entries < 0)
+		return msix_entries;
 
 	for (i = 0; i < msix_entries; i++) {
 		msix = &ndev->msix_entries[i];
