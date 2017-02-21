847de7856afb180c473521ded70a526505d5f671
Bhupesh Sharma <bhsharma@redhat.com>
2017-02-21 12:14:09 -0500

    standardize mmap_rnd() usage
    Message-id: <1487679257-29320-3-git-send-email-bhsharma@redhat.com>
    Patchwork-id: 166648
    O-Subject: [RHEL7.4 PATCH v4 02/10] x86: standardize mmap_rnd() usage
    Bugzilla: 1415893
    RH-Acked-by: Baoquan He <bhe@redhat.com>
    RH-Acked-by: Josh Poimboeuf <jpoimboe@redhat.com>
    
    From: Kees Cook <keescook@chromium.org>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1415893
    Upstream Status: commit 82168140bc4cec7ec9bad39705518541149ff8b7
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=12585279
    
    commit 82168140bc4cec7ec9bad39705518541149ff8b7
    Author: Kees Cook <keescook@chromium.org>
    Date:   Tue Apr 14 15:47:45 2015 -0700
    
        x86: standardize mmap_rnd() usage
    
        In preparation for splitting out ET_DYN ASLR, this refactors the use of
        mmap_rnd() to be used similarly to arm, and extracts the checking of
        PF_RANDOMIZE.
    
        Signed-off-by: Kees Cook <keescook@chromium.org>
        Reviewed-by: Ingo Molnar <mingo@kernel.org>
        Cc: Oleg Nesterov <oleg@redhat.com>
        Cc: Andy Lutomirski <luto@amacapital.net>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
        (cherry picked from commit 82168140bc4cec7ec9bad39705518541149ff8b7)
    
    Signed-off-by: Bhupesh Sharma <bhsharma@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflict patch:
    05355-x86-standardize-mmap_rnd-usage.patch
    
    Merge log:
    patching file arch/x86/mm/mmap.c
    Hunk #1 already applied at 70,73-80,84.
    Hunk #2 already applied at 93,100,105.
    Hunk #3 NOT MERGED at 115-122, already applied at 134.
    (EScore specific version string)

--- arch/x86/mm/mmap.c
+++ arch/x86/mm/mmap.c
@@ -118,6 +118,11 @@ void arch_pick_mmap_layout(struct mm_struct *mm)
 
 	mm->mmap_legacy_base = mmap_legacy_base(random_factor);
 
+	if (current->flags & PF_RANDOMIZE)
+		random_factor = mmap_rnd();
+
+	mm->mmap_legacy_base = mmap_legacy_base(random_factor);
+
 	if (mmap_is_legacy()) {
 		mm->mmap_base = mm->mmap_legacy_base;
 		mm->get_unmapped_area = arch_get_unmapped_area;
