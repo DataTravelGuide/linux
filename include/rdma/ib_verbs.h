74f97150e1717c375ac2def26ca6f595579295e3
Don Dutile <ddutile@redhat.com>
2017-03-07 02:15:37 -0500

    ib/core: Introduce inner flow steering
    Message-id: <1488853076-16738-202-git-send-email-ddutile@redhat.com>
    Patchwork-id: 172011
    O-Subject: [PATCH RHEL-7.4 201/340] IB/core: Introduce inner flow steering
    Bugzilla: 1386703 1417286
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1417286
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1386703
    
    commit fbf46860b19ddb485f00bef1ad1a43aabc9f71ad
    Author: Moses Reuben <mosesr@mellanox.com>
    Date:   Mon Nov 14 19:04:51 2016 +0200
    
        IB/core: Introduce inner flow steering
    
        For a tunneled packet which contains external and internal headers,
        we refer to the external headers as "outer fields" and the internal
        headers as "inner fields".
    
        Example of a tunneled packet:
    
        { L2 | L3 | L4 | tunnel header | L2 | L3 | l4 | data }
          |     |    |         |         |    |    |
        {       outer fields           }{ inner fields }
    
        This patch introduces a new flag for flow steering rules
        - IB_FLOW_SPEC_INNER - which specifies that the rule applies
        to the inner fields, rather than to the outer fields of the packet.
    
        Signed-off-by: Moses Reuben <mosesr@mellanox.com>
        Reviewed-by: Maor Gottlieb <maorg@mellanox.com>
        Signed-off-by: Leon Romanovsky <leon@kernel.org>
        Signed-off-by: Doug Ledford <dledford@redhat.com>
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -1594,9 +1594,10 @@ enum ib_flow_spec_type {
 	IB_FLOW_SPEC_TCP		= 0x40,
 	IB_FLOW_SPEC_UDP		= 0x41,
 	IB_FLOW_SPEC_VXLAN_TUNNEL	= 0x50,
+	IB_FLOW_SPEC_INNER		= 0x100,
 };
 #define IB_FLOW_SPEC_LAYER_MASK	0xF0
-#define IB_FLOW_SPEC_SUPPORT_LAYERS 4
+#define IB_FLOW_SPEC_SUPPORT_LAYERS 8
 
 /* Flow steering rule priority is set according to it's domain.
  * Lower domain value means higher priority.
@@ -1624,7 +1625,7 @@ struct ib_flow_eth_filter {
 };
 
 struct ib_flow_spec_eth {
-	enum ib_flow_spec_type	  type;
+	u32			  type;
 	u16			  size;
 	struct ib_flow_eth_filter val;
 	struct ib_flow_eth_filter mask;
@@ -1638,7 +1639,7 @@ struct ib_flow_ib_filter {
 };
 
 struct ib_flow_spec_ib {
-	enum ib_flow_spec_type	 type;
+	u32			 type;
 	u16			 size;
 	struct ib_flow_ib_filter val;
 	struct ib_flow_ib_filter mask;
@@ -1663,7 +1664,7 @@ struct ib_flow_ipv4_filter {
 };
 
 struct ib_flow_spec_ipv4 {
-	enum ib_flow_spec_type	   type;
+	u32			   type;
 	u16			   size;
 	struct ib_flow_ipv4_filter val;
 	struct ib_flow_ipv4_filter mask;
@@ -1681,7 +1682,7 @@ struct ib_flow_ipv6_filter {
 };
 
 struct ib_flow_spec_ipv6 {
-	enum ib_flow_spec_type	   type;
+	u32			   type;
 	u16			   size;
 	struct ib_flow_ipv6_filter val;
 	struct ib_flow_ipv6_filter mask;
@@ -1695,7 +1696,7 @@ struct ib_flow_tcp_udp_filter {
 };
 
 struct ib_flow_spec_tcp_udp {
-	enum ib_flow_spec_type	      type;
+	u32			      type;
 	u16			      size;
 	struct ib_flow_tcp_udp_filter val;
 	struct ib_flow_tcp_udp_filter mask;
@@ -1710,7 +1711,7 @@ struct ib_flow_tunnel_filter {
  * the tunnel_id from val has the vni value
  */
 struct ib_flow_spec_tunnel {
-	enum ib_flow_spec_type	      type;
+	u32			      type;
 	u16			      size;
 	struct ib_flow_tunnel_filter  val;
 	struct ib_flow_tunnel_filter  mask;
@@ -1718,7 +1719,7 @@ struct ib_flow_spec_tunnel {
 
 union ib_flow_spec {
 	struct {
-		enum ib_flow_spec_type	type;
+		u32			type;
 		u16			size;
 	};
 	struct ib_flow_spec_eth		eth;
