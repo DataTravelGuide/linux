c328f100201928aac0ef05c0eb8ad4f61d5d3abb
Bill O'Donnell <billodo@redhat.com>
2017-03-06 23:57:28 -0500

    btrfs: revert: btrfs: synchronize incompat feature bits with sysfs files
    Message-id: <20170307000403.5752-91-billodo@redhat.com>
    Patchwork-id: 171277
    O-Subject: [RHEL7.4 PATCH BZ 1399590 090/485] btrfs: revert: btrfs: synchronize incompat feature bits with sysfs files
    Bugzilla: 1399590
    RH-Acked-by: Carlos Maiolino <cmaiolino@redhat.com>
    RH-Acked-by: Eric Sandeen <esandeen@redhat.com>
    
    Bugzilla: 1399590
    Upstream Status: Committed upstream
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=12695624
    Tested: Locally with fstests
    
    Backport of the following upstream commit:
    
        commit e410e34fad913dd568ec28d2a9949694324c14db
        Author: Chris Mason <clm@fb.com>
        Date:   Fri Jan 29 08:19:37 2016 -0800
    
        Revert "btrfs: synchronize incompat feature bits with sysfs files"
    
        This reverts commit 14e46e04958df740c6c6a94849f176159a333f13.
    
        This ends up doing sysfs operations from deep in balance (where we
        should be GFP_NOFS) and under heavy balance load, we're making races
        against sysfs internals.
    
        Revert it for now while we figure things out.
    
        Signed-off-by: Chris Mason <clm@fb.com>
    
    Signed-off-by: Bill O'Donnell <billodo@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -1457,8 +1457,6 @@ int btrfs_defrag_file(struct inode *inode, struct file *file,
 
 	if (range->compress_type == BTRFS_COMPRESS_LZO) {
 		btrfs_set_fs_incompat(root->fs_info, COMPRESS_LZO);
-		btrfs_sysfs_feature_update(root->fs_info,
-			BTRFS_FEATURE_INCOMPAT_COMPRESS_LZO, FEAT_INCOMPAT);
 	}
 
 	ret = defrag_count;
@@ -4147,8 +4145,6 @@ static long btrfs_ioctl_default_subvol(struct file *file, void __user *argp)
 	btrfs_free_path(path);
 
 	btrfs_set_fs_incompat(root->fs_info, DEFAULT_SUBVOL);
-	btrfs_sysfs_feature_update(root->fs_info,
-		BTRFS_FEATURE_INCOMPAT_DEFAULT_SUBVOL, FEAT_INCOMPAT);
 	btrfs_end_transaction(trans, root);
 out:
 	mnt_drop_write_file(file);
