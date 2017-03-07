ff8e709c7b4cb9007f78483853ffd74dbf0a1067
Don Dutile <ddutile@redhat.com>
2017-03-07 02:14:44 -0500

    chcr - checking for IS_ERR() instead of NULL
    Message-id: <1488853076-16738-149-git-send-email-ddutile@redhat.com>
    Patchwork-id: 171870
    O-Subject: [PATCH RHEL-7.4 148/340] crypto: chcr - checking for IS_ERR() instead of NULL
    Bugzilla: 1417286
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1417286
    
    commit 9a97ffd49ca9421c17ecde672a58b618d61f4f58
    Author: Dan Carpenter <dan.carpenter@oracle.com>
    Date:   Thu Dec 1 23:49:37 2016 +0300
    
        crypto: chcr - checking for IS_ERR() instead of NULL
    
        The create_hash_wr() function never returns error pointers.  It returns
        NULL on error.
    
        Fixes: 358961d1cd1e ("crypto: chcr - Added new structure chcr_wr")
        Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
        Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/crypto/chelsio/chcr_algo.c
+++ drivers/crypto/chelsio/chcr_algo.c
@@ -896,9 +896,8 @@ static int chcr_ahash_update(struct ahash_request *req)
 	req_ctx->result = 0;
 	req_ctx->data_len += params.sg_len + params.bfr_len;
 	skb = create_hash_wr(req, &params);
-
-	if (IS_ERR(skb))
-		return PTR_ERR(skb);
+	if (!skb)
+		return -ENOMEM;
 
 	if (remainder) {
 		u8 *temp;
@@ -961,8 +960,8 @@ static int chcr_ahash_final(struct ahash_request *req)
 		params.more = 0;
 	}
 	skb = create_hash_wr(req, &params);
-	if (IS_ERR(skb))
-		return PTR_ERR(skb);
+	if (!skb)
+		return -ENOMEM;
 
 	skb->dev = u_ctx->lldi.ports[0];
 	set_wr_txq(skb, CPL_PRIORITY_DATA, ctx->tx_channel_id);
@@ -1012,8 +1011,8 @@ static int chcr_ahash_finup(struct ahash_request *req)
 	}
 
 	skb = create_hash_wr(req, &params);
-	if (IS_ERR(skb))
-		return PTR_ERR(skb);
+	if (!skb)
+		return -ENOMEM;
 
 	skb->dev = u_ctx->lldi.ports[0];
 	set_wr_txq(skb, CPL_PRIORITY_DATA, ctx->tx_channel_id);
@@ -1063,8 +1062,8 @@ static int chcr_ahash_digest(struct ahash_request *req)
 	}
 
 	skb = create_hash_wr(req, &params);
-	if (IS_ERR(skb))
-		return PTR_ERR(skb);
+	if (!skb)
+		return -ENOMEM;
 
 	skb->dev = u_ctx->lldi.ports[0];
 	set_wr_txq(skb, CPL_PRIORITY_DATA, ctx->tx_channel_id);
