b2328c6584e8e3e2aab0b855d40b01548cf7f7c3
David Arcari <darcari@redhat.com>
2017-03-07 20:57:09 -0500

    bluetooth: Send control open and close messages for HCI user channels
    Message-id: <1488920286-50873-75-git-send-email-darcari@redhat.com>
    Patchwork-id: 172627
    O-Subject: [RHEL7.4 PATCH BZ 1415327 119/176] Bluetooth: Send control open and close messages for HCI user channels
    Bugzilla: 1415327
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1415327
    
    commit aa1638dde75d00e4f549902017d0df48b77e86ff
    Author: Marcel Holtmann <marcel@holtmann.org>
    Date:   Thu Sep 1 19:48:28 2016 +0200
    
        Bluetooth: Send control open and close messages for HCI user channels
    
        When opening and closing HCI user channel, send monitoring messages to
        be able to trace its behavior.
    
        Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
        Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Cc: David Arcari <darcari@redhat.com>
    Cc: Ivan Vecera <ivecera@redhat.com>
    Cc: Don Zickus <dzickus@redhat.com>
    Cc: Neil Horman <nhorman@redhat.com>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/bluetooth/hci_sock.c
+++ net/bluetooth/hci_sock.c
@@ -485,6 +485,11 @@ static struct sk_buff *create_monitor_ctrl_command(struct sock *sk, u16 index,
 		ver[0] = BT_SUBSYS_VERSION;
 		put_unaligned_le16(BT_SUBSYS_REVISION, ver + 1);
 		break;
+	case HCI_CHANNEL_USER:
+		format = 0x0001;
+		ver[0] = BT_SUBSYS_VERSION;
+		put_unaligned_le16(BT_SUBSYS_REVISION, ver + 1);
+		break;
 	case HCI_CHANNEL_CONTROL:
 		format = 0x0002;
 		mgmt_fill_version_info(ver);
@@ -606,6 +611,7 @@ static void hci_si_event(struct hci_dev *hdev, int type, int dlen, void *data)
 
 	switch (hci_pi(sk)->channel) {
 	case HCI_CHANNEL_RAW:
+	case HCI_CHANNEL_USER:
 	case HCI_CHANNEL_CONTROL:
 		break;
 	default:
@@ -745,6 +751,7 @@ static int hci_sock_release(struct socket *sock)
 		atomic_dec(&monitor_promisc);
 		break;
 	case HCI_CHANNEL_RAW:
+	case HCI_CHANNEL_USER:
 	case HCI_CHANNEL_CONTROL:
 		/* Send event to monitor */
 		skb = create_monitor_ctrl_close(sk);
@@ -1088,7 +1095,35 @@ static int hci_sock_bind(struct socket *sock, struct sockaddr *addr,
 		}
 
 		hci_pi(sk)->channel = haddr.hci_channel;
+
+		if (!hci_sock_gen_cookie(sk)) {
+			/* In the case when a cookie has already been assigned,
+			 * this socket will transition from a raw socket into
+			 * an user channel socket. For a clean transition, send
+			 * the close notification first.
+			 */
+			skb = create_monitor_ctrl_close(sk);
+			if (skb) {
+				hci_send_to_channel(HCI_CHANNEL_MONITOR, skb,
+						    HCI_SOCK_TRUSTED, NULL);
+				kfree_skb(skb);
+			}
+		}
+
+		/* The user channel is restricted to CAP_NET_ADMIN
+		 * capabilities and with that implicitly trusted.
+		 */
+		hci_sock_set_flag(sk, HCI_SOCK_TRUSTED);
+
 		hci_pi(sk)->hdev = hdev;
+
+		/* Send event to monitor */
+		skb = create_monitor_ctrl_open(sk);
+		if (skb) {
+			hci_send_to_channel(HCI_CHANNEL_MONITOR, skb,
+					    HCI_SOCK_TRUSTED, NULL);
+			kfree_skb(skb);
+		}
 
 		atomic_inc(&hdev->promisc);
 		break;
