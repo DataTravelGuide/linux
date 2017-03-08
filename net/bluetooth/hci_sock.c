575f8a3b0470e8dcc311e79a3aaa66dff8c26b37
David Arcari <darcari@redhat.com>
2017-03-07 20:56:53 -0500

    bluetooth: Use individual flags for certain management events
    Message-id: <1488920286-50873-59-git-send-email-darcari@redhat.com>
    Patchwork-id: 172689
    O-Subject: [RHEL7.4 PATCH BZ 1415327 103/176] Bluetooth: Use individual flags for certain management events
    Bugzilla: 1415327
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1415327
    
    commit 5504c3a31061704512707bb23bd7835e8a5281e4
    Author: Marcel Holtmann <marcel@holtmann.org>
    Date:   Mon Aug 29 06:19:46 2016 +0200
    
        Bluetooth: Use individual flags for certain management events
    
        Instead of hiding everything behind a general managment events flag,
        introduce indivdual flags that allow fine control over which events are
        send to a given management channel.
    
        Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
        Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Cc: David Arcari <darcari@redhat.com>
    Cc: Don Zickus <dzickus@redhat.com>
    Cc: Ivan Vecera <ivecera@redhat.com>
    Cc: Neil Horman <nhorman@redhat.com>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflict patch:
    11223-net-bluetooth-Use-individual-flags-for-certain-management-events.patch
    
    Merge log:
    patching file include/net/bluetooth/hci.h
    patching file net/bluetooth/hci_sock.c
    Hunk #1 merged at 1043-1046.
    patching file net/bluetooth/mgmt.c
    (EScore specific version string)

--- net/bluetooth/hci_sock.c
+++ net/bluetooth/hci_sock.c
@@ -1040,7 +1040,10 @@ static int hci_sock_bind(struct socket *sock, struct sockaddr *addr,
 		if (haddr.hci_channel == HCI_CHANNEL_CONTROL) {
 			hci_sock_set_flag(sk, HCI_MGMT_INDEX_EVENTS);
 			hci_sock_set_flag(sk, HCI_MGMT_UNCONF_INDEX_EVENTS);
-			hci_sock_set_flag(sk, HCI_MGMT_GENERIC_EVENTS);
+			hci_sock_set_flag(sk, HCI_MGMT_OPTION_EVENTS);
+			hci_sock_set_flag(sk, HCI_MGMT_SETTING_EVENTS);
+			hci_sock_set_flag(sk, HCI_MGMT_DEV_CLASS_EVENTS);
+			hci_sock_set_flag(sk, HCI_MGMT_LOCAL_NAME_EVENTS);
 		}
 		break;
 	}
