8cbbbe4e25244726a48f1643c9542d91e504cea5
Steve Dickson <SteveD@redhat.com>
2017-03-10 15:02:26 -0500

    pnfs: Fix atime updates on pNFS clients
    Message-id: <20170310150235.10315-159-steved@redhat.com>
    Patchwork-id: 173143
    O-Subject: [RHEL7.4 fs PATCH 158/167] pNFS: Fix atime updates on pNFS clients
    Bugzilla: 1349668
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Benjamin Coddington <bcodding@redhat.com>
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Scott Mayhew <smayhew@redhat.com>
    
    From: Trond Myklebust <trond.myklebust@primarydata.com>
    
    Fix the code so that we always mark the atime as invalid in nfs4_read_done().
    Currently, the expectation appears to be that the pNFS drivers should always
    do this, with the result that most of them don't.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>
    (cherry picked from commit bfc505ded01e3c57d12c6f939f352200655d8635)
    Bugzilla: 1349668
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflict patch:
    08036-fs-pnfs-Fix-atime-updates-on-pNFS-clients.patch
    
    Merge log:
    patching file fs/nfs/internal.h
    Hunk #1 merged at 529.
    patching file fs/nfs/nfs4proc.c
    patching file fs/nfs/pnfs.c
    (EScore specific version string)

--- fs/nfs/internal.h
+++ fs/nfs/internal.h
@@ -526,7 +526,6 @@ extern ssize_t nfs_dreq_bytes_left(struct nfs_direct_req *dreq);
 extern void nfs_direct_set_resched_writes(struct nfs_direct_req *dreq);
 
 /* nfs4proc.c */
-extern void __nfs4_read_done_cb(struct nfs_pgio_header *);
 extern struct nfs_client *nfs4_init_client(struct nfs_client *clp,
 			    const struct nfs_client_initdata *);
 extern int nfs40_walk_client_list(struct nfs_client *clp,
--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -4529,11 +4529,6 @@ static bool nfs4_error_stateid_expired(int err)
 	return false;
 }
 
-void __nfs4_read_done_cb(struct nfs_pgio_header *hdr)
-{
-	nfs_invalidate_atime(hdr->inode);
-}
-
 static int nfs4_read_done_cb(struct rpc_task *task, struct nfs_pgio_header *hdr)
 {
 	struct nfs_server *server = NFS_SERVER(hdr->inode);
@@ -4553,7 +4548,6 @@ static int nfs4_read_done_cb(struct rpc_task *task, struct nfs_pgio_header *hdr)
 		}
 	}
 
-	__nfs4_read_done_cb(hdr);
 	if (task->tk_status > 0)
 		renew_lease(server, hdr->timestamp);
 	return 0;
@@ -4582,6 +4576,8 @@ static int nfs4_read_done(struct rpc_task *task, struct nfs_pgio_header *hdr)
 		return -EAGAIN;
 	if (nfs4_read_stateid_changed(task, &hdr->args))
 		return -EAGAIN;
+	if (task->tk_status > 0)
+		nfs_invalidate_atime(hdr->inode);
 	return hdr->pgio_done_cb ? hdr->pgio_done_cb(task, hdr) :
 				    nfs4_read_done_cb(task, hdr);
 }
