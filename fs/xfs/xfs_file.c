632a9a8c7025a568b5000fa69b3649588731bd92
Brian Foster <bfoster@redhat.com>
2017-04-13 13:41:33 -0400

    xfs: sync eofblocks scans under iolock are livelock prone
    Message-id: <1492090894-25690-3-git-send-email-bfoster@redhat.com>
    Patchwork-id: 176720
    O-Subject: [PATCH RHEL7 2/3] xfs: sync eofblocks scans under iolock are livelock prone
    Bugzilla: 1441364
    RH-Acked-by: Eric Sandeen <esandeen@redhat.com>
    RH-Acked-by: Carlos Maiolino <cmaiolino@redhat.com>
    RH-Acked-by: Bill O'Donnell <billodo@redhat.com>
    
    - Dropped changes to reflink bits that don't exist in rhel7.
    - Use xfs_rw_iunlock() in xfs_file_buffered_aio_write().
    
    commit c3155097ad89a956579bc305856a1f2878494e52
    Author: Brian Foster <bfoster@redhat.com>
    Date:   Fri Jan 27 23:22:56 2017 -0800
    
        xfs: sync eofblocks scans under iolock are livelock prone
    
        The xfs_eofblocks.eof_scan_owner field is an internal field to
        facilitate invoking eofb scans from the kernel while under the iolock.
        This is necessary because the eofb scan acquires the iolock of each
        inode. Synchronous scans are invoked on certain buffered write failures
        while under iolock. In such cases, the scan owner indicates that the
        context for the scan already owns the particular iolock and prevents a
        double lock deadlock.
    
        eofblocks scans while under iolock are still livelock prone in the event
        of multiple parallel scans, however. If multiple buffered writes to
        different inodes fail and invoke eofblocks scans at the same time, each
        scan avoids a deadlock with its own inode by virtue of the
        eof_scan_owner field, but will never be able to acquire the iolock of
        the inode from the parallel scan. Because the low free space scans are
        invoked with SYNC_WAIT, the scan will not return until it has processed
        every tagged inode and thus both scans will spin indefinitely on the
        iolock being held across the opposite scan. This problem can be
        reproduced reliably by generic/224 on systems with higher cpu counts
        (x16).
    
        To avoid this problem, simplify the semantics of eofblocks scans to
        never invoke a scan while under iolock. This means that the buffered
        write context must drop the iolock before the scan. It must reacquire
        the lock before the write retry and also repeat the initial write
        checks, as the original state might no longer be valid once the iolock
        was dropped.
    
        Signed-off-by: Brian Foster <bfoster@redhat.com>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
        Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflict patch:
    12693-fs-xfs-sync-eofblocks-scans-under-iolock-are-livelock-prone.patch
    
    Merge log:
    patching file fs/xfs/xfs_file.c
    Hunk #2 NOT MERGED at 924-930.
    patching file fs/xfs/xfs_icache.c
    patching file fs/xfs/xfs_icache.h
    (EScore specific version string)

--- fs/xfs/xfs_file.c
+++ fs/xfs/xfs_file.c
@@ -906,9 +906,11 @@ xfs_file_buffered_aio_write(
 	struct xfs_inode	*ip = XFS_I(inode);
 	ssize_t			ret;
 	int			enospc = 0;
-	int			iolock = XFS_IOLOCK_EXCL;
+	int			iolock;
 	size_t			count = ocount;
 
+write_retry:
+	iolock = XFS_IOLOCK_EXCL;
 	xfs_rw_ilock(ip, iolock);
 
 	ret = xfs_file_aio_write_checks(file, &pos, &count, &iolock);
@@ -918,7 +920,9 @@ xfs_file_buffered_aio_write(
 	/* We can write back this queue in page reclaim */
 	current->backing_dev_info = mapping->backing_dev_info;
 
-write_retry:
+	trace_xfs_file_buffered_write(ip, count, iocb->ki_pos);
+	ret = generic_file_buffered_write(iocb, iovp, nr_segs,
+			pos, &iocb->ki_pos, count, 0);
 	trace_xfs_file_buffered_write(ip, iov_iter_count(from), iocb->ki_pos);
 	ret = generic_perform_write(file, from, iocb->ki_pos);
 	if (likely(ret >= 0))
@@ -937,15 +941,18 @@ write_retry:
 	 * running at the same time.
 	 */
 	if (ret == -EDQUOT && !enospc) {
+		xfs_rw_iunlock(ip, iolock);
 		enospc = xfs_inode_free_quota_eofblocks(ip);
 		if (enospc)
 			goto write_retry;
+		iolock = 0;
 	} else if (ret == -ENOSPC && !enospc) {
 		struct xfs_eofblocks eofb = {0};
 
 		enospc = 1;
 		xfs_flush_inodes(ip->i_mount);
-		eofb.eof_scan_owner = ip->i_ino; /* for locking */
+
+		xfs_rw_iunlock(ip, iolock);
 		eofb.eof_flags = XFS_EOF_FLAGS_SYNC;
 		xfs_icache_free_eofblocks(ip->i_mount, &eofb);
 		goto write_retry;
@@ -953,7 +960,8 @@ write_retry:
 
 	current->backing_dev_info = NULL;
 out:
-	xfs_rw_iunlock(ip, iolock);
+	if (iolock)
+		xfs_rw_iunlock(ip, iolock);
 	return ret;
 }
 
