ae44615edcc9165f3bf77ea647145871833f52b4
Bhupesh Sharma <bhsharma@redhat.com>
2017-04-24 19:29:22 -0400

    mm: Add support for runtime configuration of ASLR limits
    Message-id: <1493062162-11805-8-git-send-email-bhsharma@redhat.com>
    Patchwork-id: 177063
    O-Subject: [RHEL7.4 PATCH 7/7] powerpc/mm: Add support for runtime configuration of ASLR limits
    Bugzilla: 1412802
    RH-Acked-by: Pingfan Liu <piliu@redhat.com>
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1412802
    Upstream Status: 9fea59bd7ca541e5d0851f0b6dbca83c60ea90cd (powerpc next - https://git.kernel.org/powerpc/c/9fea59bd7ca541e5d0851f0b6dbca8)
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=13071347
                (also tested on a PPC64 Beaker machine).
    
    Back-Ported for RHEL with the following changes:
    -----------------------------------------------
    
    1. Upstream powerpc code now allows using 128TB virtual address space
       by default (i.e. TASK_SIZE become 47 bits) for Book3S 64 processors.
       However, this change is not present in RHEL and would require
       considerable verification for making it a default selection.
    
       Hence this patch this uses the conservative 64TB (i.e. TASK_SIZE
       remains 46-bits) for all powerpc 64 processors.
    
    2. After the upstream patch was applied to the maintainer's tree a minor
       nit was suggested (which can be seen here: http://patchwork.ozlabs.org/patch/752832/)
    
       The backported patch includes this fix.
    -----------------------------------------------
    
        powerpc/mm: Add support for runtime configuration of ASLR limits
    
        Add powerpc support for mmap_rnd_bits and mmap_rnd_compat_bits, which are two
        sysctls that allow a user to configure the number of bits of randomness used for
        ASLR.
    
        Because of the way the Kconfig for ARCH_MMAP_RND_BITS is defined, we have to
        construct at least the MIN value in Kconfig, vs in a header which would be more
        natural. Given that we just go ahead and do it all in Kconfig.
    
        At least according to the code (the documentation makes no mention of it), the
        value is defined as the number of bits of randomisation *of the page*, not the
        address. This makes some sense, with larger page sizes more of the low bits are
        forced to zero, which would reduce the randomisation if we didn't take the
        PAGE_SIZE into account. However it does mean the min/max values have to change
        depending on the PAGE_SIZE in order to actually limit the amount of address
        space consumed by the randomisation.
    
        The result of that is that we have to define the default values based on both
        32-bit vs 64-bit, but also the configured PAGE_SIZE. Furthermore now that we
        have 128TB address space support on Book3S, we also have to take that into
        account.
    
        Finally we can wire up the value in arch_mmap_rnd().
    
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
        Signed-off-by: Bhupesh Sharma <bhsharma@redhat.com>
        Tested-by: Bhupesh Sharma <bhsharma@redhat.com>
        Reviewed-by: Kees Cook <keescook@chromium.org>
        Reviewed-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    
    Signed-off-by: Bhupesh Sharma <bhsharma@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflict patch:
    13124-powerpc-mm-Add-support-for-runtime-configuration-of-ASLR-limits.patch
    
    Merge log:
    patching file arch/powerpc/Kconfig
    patching file arch/powerpc/mm/mmap_64.c
    Hunk #1 NOT MERGED at 58-69, merged at 71-72, merged at 75-77.
    (EScore specific version string)

--- arch/powerpc/mm/mmap_64.c
+++ arch/powerpc/mm/mmap_64.c
@@ -55,20 +55,14 @@ static inline int mmap_is_legacy(void)
 
 unsigned long arch_mmap_rnd(void)
 {
-	unsigned long rnd = 0;
+	unsigned long shift, rnd;
 
-	if (current->flags & PF_RANDOMIZE) {
-		/* 8MB for 32bit, 1GB for 64bit */
-		if (is_32bit_task())
-			rnd = (long)(get_random_int() % (1<<(23-PAGE_SHIFT)));
-		else
-			rnd = (long)(get_random_int() % (1<<(30-PAGE_SHIFT)));
-
-	/* 8MB for 32bit, 1GB for 64bit */
+	shift = mmap_rnd_bits;
+#ifdef CONFIG_COMPAT
 	if (is_32bit_task())
-		rnd = get_random_long() % (1<<(23-PAGE_SHIFT));
-	else
-		rnd = get_random_long() % (1UL<<(30-PAGE_SHIFT));
+		shift = mmap_rnd_compat_bits;
+#endif
+	rnd = get_random_long() % (1UL << shift);
 
 	return rnd << PAGE_SHIFT;
 }
