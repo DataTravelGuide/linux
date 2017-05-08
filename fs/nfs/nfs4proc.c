104bb42b5011f24803463174c368c8f575ba5f93
Benjamin Coddington <bcodding@redhat.com>
2017-05-08 13:25:13 -0400

    nfs: move nfs4_set_lock_state call into caller
    Message-id: <5aadc7f703048139f94cb1c79d02a84fe47d3f0e.1494249737.git.bcodding@redhat.com>
    Patchwork-id: 177891
    O-Subject: [RHEL7 fs PATCH v3 09/23] nfs: move nfs4_set_lock_state call into caller
    Bugzilla: 1377710
    RH-Acked-by: Scott Mayhew <smayhew@redhat.com>
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    
    From: Jeff Layton <jlayton@redhat.com>
    
    We need to have this info set up before adding the waiter to the
    waitqueue, so move this out of the _nfs4_proc_setlk and into the
    caller. That's more efficient anyway since we don't need to do
    this more than once if we end up waiting on the lock.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>
    (cherry picked from commit 1ea67dbd9828278188fb846972c3c368b0ef23a1)
    
    Conflict patch:
    13060-fs-nfs-move-nfs4_set_lock_state-call-into-caller.patch
    
    Merge log:
    patching file fs/nfs/nfs4proc.c
    Hunk #2 merged at 6340-6347.
    (EScore specific version string)

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -6240,15 +6240,8 @@ static int _nfs4_proc_setlk(struct nfs4_state *state, int cmd, struct file_lock 
 	struct nfs_inode *nfsi = NFS_I(state->inode);
 	struct nfs4_state_owner *sp = state->owner;
 	unsigned char fl_flags = request->fl_flags;
-	int status = -ENOLCK;
+	int status;
 
-	if ((fl_flags & FL_POSIX) &&
-			!test_bit(NFS_STATE_POSIX_LOCKS, &state->flags))
-		goto out;
-	/* Is this a delegated open? */
-	status = nfs4_set_lock_state(state, request);
-	if (status != 0)
-		goto out;
 	request->fl_flags |= FL_ACCESS;
 	status = locks_lock_inode_wait(state->inode, request);
 	if (status < 0)
@@ -6335,6 +6328,14 @@ nfs4_proc_lock(struct file *filp, int cmd, struct file_lock *request)
 		if (!(filp->f_mode & FMODE_WRITE))
 			return -EBADF;
 	}
+
+	if ((request->fl_flags & FL_POSIX) &&
+	    !test_bit(NFS_STATE_POSIX_LOCKS, &state->flags))
+		return -ENOLCK;
+
+	status = nfs4_set_lock_state(state, request);
+	if (status != 0)
+		return status;
 
 	do {
 		status = nfs4_proc_setlk(state, cmd, request);
