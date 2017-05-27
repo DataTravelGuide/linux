5435b48da23e5c35826a41734c4082da3691f73a
Ming Lei <ming.lei@redhat.com>
2017-05-27 07:03:51 -0400

    avoid to use blk_mq_abort_requeue_list()
    Message-id: <20170527070352.24744-7-ming.lei@redhat.com>
    Patchwork-id: 179130
    O-Subject: [PATCH RHEL7.4 6/7] nvme: avoid to use blk_mq_abort_requeue_list()
    Bugzilla: 1445595
    RH-Acked-by: David Milburn <dmilburn@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    BZ: 1445595
    
    commit 986f75c876dbafed98eba7cb516c5118f155db23
    Author: Ming Lei <ming.lei@redhat.com>
    Date:   Mon May 22 23:05:04 2017 +0800
    
        nvme: avoid to use blk_mq_abort_requeue_list()
    
        NVMe may add request into requeue list simply and not kick off the
        requeue if hw queues are stopped. Then blk_mq_abort_requeue_list()
        is called in both nvme_kill_queues() and nvme_ns_remove() for
        dealing with this issue.
    
        Unfortunately blk_mq_abort_requeue_list() is absolutely a
        race maker, for example, one request may be requeued during
        the aborting. So this patch just calls blk_mq_kick_requeue_list() in
        nvme_kill_queues() to handle this issue like what nvme_start_queues()
        does. Now all requests in requeue list when queues are stopped will be
        handled by blk_mq_kick_requeue_list() when queues are restarted, either
        in nvme_start_queues() or in nvme_kill_queues().
    
        Cc: stable@vger.kernel.org
        Reported-by: Zhang Yi <yizhan@redhat.com>
        Reviewed-by: Keith Busch <keith.busch@intel.com>
        Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
        Signed-off-by: Ming Lei <ming.lei@redhat.com>
        Signed-off-by: Christoph Hellwig <hch@lst.de>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -1650,7 +1650,6 @@ static void nvme_ns_remove(struct nvme_ns *ns)
 		sysfs_remove_group(&disk_to_dev(ns->disk)->kobj,
 					&nvme_ns_attr_group);
 		del_gendisk(ns->disk);
-		blk_mq_abort_requeue_list(ns->queue);
 		blk_cleanup_queue(ns->queue);
 	}
 
@@ -1983,7 +1982,6 @@ void nvme_kill_queues(struct nvme_ctrl *ctrl)
 			revalidate_disk(ns->disk);
 
 		blk_set_queue_dying(ns->queue);
-		blk_mq_abort_requeue_list(ns->queue);
 
 		/*
 		 * Forcibly start all queues to avoid having stuck requests.
@@ -1991,6 +1989,9 @@ void nvme_kill_queues(struct nvme_ctrl *ctrl)
 		 * when the final removal happens.
 		 */
 		blk_mq_start_hw_queues(ns->queue);
+
+		/* draining requests in requeue list */
+		blk_mq_kick_requeue_list(ns->queue);
 	}
 	mutex_unlock(&ctrl->namespaces_mutex);
 }
