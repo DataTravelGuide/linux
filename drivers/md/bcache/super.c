82d0813aceec30228dbc7bd1c997dffb8cf276c5
kcchuang <kc.chuang@easystack.cn>
2017-06-23 18:09:08 +0800

    bcache: release the allocated id, not its multiple of BCACHE_MINORS
    The id passed to ida_simple_remove has to be the one originally
    allocated with ida_simple_get, not the one after multipling by
    BCACHE_MINORS.- by Liang Chen.
    
    Signed-off-by: kcchuang <kc.chuang@easystack.cn>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -740,7 +740,8 @@ static void bcache_device_free(struct bcache_device *d)
 	if (d->disk && d->disk->queue)
 		blk_cleanup_queue(d->disk->queue);
 	if (d->disk) {
-		ida_simple_remove(&bcache_minor, d->disk->first_minor);
+		ida_simple_remove(&bcache_minor, 
+				d->disk->first_minor / BCACHE_MINORS);
 		put_disk(d->disk);
 	}
 
@@ -769,7 +770,7 @@ static int bcache_device_init(struct bcache_device *d, unsigned block_size)
 				sizeof(struct bio_vec) * BIO_MAX_PAGES)) ||
 	    bio_split_pool_init(&d->bio_split_hook) ||
 	    !(d->disk = alloc_disk(BCACHE_MINORS))) {
-		ida_simple_remove(&bcache_minor, minor);
+		ida_simple_remove(&bcache_minor, minor / BCACHE_MINORS);
 		return -ENOMEM;
 	}
 
