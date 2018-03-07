31914e474c243819fdbf7eaa74283c60670107ce
Nigel Croxon <ncroxon@redhat.com>
2018-03-07 17:20:12 +0100

    Call wait_barrier twice when underlaying device is blocked
    Message-id: <1520443212-30674-6-git-send-email-ncroxon@redhat.com>
    Patchwork-id: 206357
    O-Subject: [RHEL7.4.z V2 PATCH 5/5] [md] Call wait_barrier twice when underlaying device is blocked
    Bugzilla: 1499926
    Z-Bugzilla: 1527598
    
    From: Xiao Ni <xni@redhat.com>
    
    RHEL7-only patch, not upstream
    
    In raid1 write I/O path if the underlayer disk is blocked, it needs to wait the
    disk to be un-blocked. Before this it needs to call allow_barrier to decrease
    conf->nr_pending[idx]. But it shouldn't call wait_barrier after function
    md_wait_for_blocked_rdev. Because it will call wait_barrier after retry_write.
    
    Fixes: 1eb8f930c3([md] fix relationship between wait_barrier and allow_barrier)
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1527875
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=14908362
    Signed-off-by: Xiao Ni <xni@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <brdeoliv@redhat.com>
    (cherry picked from commit f050392b4e8139f63f3b0a6438a94c9053ad1b33)
    Signed-off-by: Denys Vlasenko <dvlasenk@redhat.com>

--- drivers/md/raid1.c
+++ drivers/md/raid1.c
@@ -1415,9 +1415,8 @@ static void raid1_write_request(struct mddev *mddev, struct bio *bio,
 			if (r1_bio->bios[j])
 				rdev_dec_pending(conf->mirrors[j].rdev, mddev);
 		r1_bio->state = 0;
-		allow_barrier(conf, bio->bi_sector);
+		allow_barrier(conf, r1_bio->sector);
 		md_wait_for_blocked_rdev(blocked_rdev, mddev);
-		wait_barrier(conf, bio->bi_sector);
 		goto retry_write;
 	}
 
