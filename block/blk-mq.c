7906c4314b56ea95ccd5e67b6bf0a50aed41a20c
Ming Lei <ming.lei@redhat.com>
2019-10-21 00:11:06 -0400

    blk-mq: simplify blk_mq_make_request()
    Message-id: <20191021001108.14265-3-ming.lei@redhat.com>
    Patchwork-id: 280425
    O-Subject: [RHEL8.2 V2 2/4] blk-mq: simplify blk_mq_make_request()
    Bugzilla: 1759380
    Z-Bugzilla: 1782181
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    
    From: Bart Van Assche <bvanassche@acm.org>
    
    BZ: 1759380
    
    commit 970d168de636ddac8221cbd4a11d7678943e7379
    Author: Bart Van Assche <bvanassche@acm.org>
    Date:   Mon Jul 1 08:47:30 2019 -0700
    
        blk-mq: simplify blk_mq_make_request()
    
        Move the blk_mq_bio_to_request() call in front of the if-statement.
    
        Cc: Hannes Reinecke <hare@suse.com>
        Cc: Omar Sandoval <osandov@fb.com>
        Reviewed-by: Minwoo Im <minwoo.im.dev@gmail.com>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Ming Lei <ming.lei@redhat.com>
        Signed-off-by: Bart Van Assche <bvanassche@acm.org>
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    There isn't 3rd argument in blk_mq_bio_to_request() because we can't
    backport 14ccb66b3f585b ("block: remove the bi_phys_segments field in struct bio")
    which depends on multi-page bvec.
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Phillip Lougher <plougher@redhat.com>
    [herton: refresh patch for 8.1.z]
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1968,10 +1968,10 @@ static blk_qc_t blk_mq_make_request(struct request_queue *q, struct bio *bio)
 
 	cookie = request_to_qc_t(data.hctx, rq);
 
+	blk_mq_bio_to_request(rq, bio);
+
 	plug = current->plug;
 	if (unlikely(is_flush_fua)) {
-		blk_mq_bio_to_request(rq, bio);
-
 		/* bypass scheduler for flush rq */
 		blk_insert_flush(rq);
 		blk_mq_run_hw_queue(data.hctx, true);
@@ -1982,8 +1982,6 @@ static blk_qc_t blk_mq_make_request(struct request_queue *q, struct bio *bio)
 		 */
 		unsigned int request_count = plug->rq_count;
 		struct request *last = NULL;
-
-		blk_mq_bio_to_request(rq, bio);
 
 		if (!request_count)
 			trace_block_plug(q);
@@ -1998,8 +1996,6 @@ static blk_qc_t blk_mq_make_request(struct request_queue *q, struct bio *bio)
 
 		blk_add_rq_to_plug(plug, rq);
 	} else if (plug && !blk_queue_nomerges(q)) {
-		blk_mq_bio_to_request(rq, bio);
-
 		/*
 		 * We do limited plugging. If the bio can be merged, do that.
 		 * Otherwise the existing request in the plug list will be
@@ -2024,10 +2020,8 @@ static blk_qc_t blk_mq_make_request(struct request_queue *q, struct bio *bio)
 		}
 	} else if ((q->nr_hw_queues > 1 && is_sync) || (!q->elevator &&
 			!data.hctx->dispatch_busy)) {
-		blk_mq_bio_to_request(rq, bio);
 		blk_mq_try_issue_directly(data.hctx, rq, &cookie);
 	} else {
-		blk_mq_bio_to_request(rq, bio);
 		blk_mq_sched_insert_request(rq, false, true, true);
 	}
 
