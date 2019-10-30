bf2b0e2472c0fd830d2c6740e54b2fb3234ed973
Dave Airlie <airlied@redhat.com>
2019-10-30 00:03:03 -0400

    drm/i915: Support ro ppgtt mapped cmdparser shadow buffers
    Message-id: <20191030000310.9444-6-airlied@redhat.com>
    Patchwork-id: 9633
    O-Subject: [kernel team] [RHEL 8.2.0 05/12] drm/i915: Support ro ppgtt mapped cmdparser shadow buffers
    Bugzilla: 1756873
    Z-Bugzilla: 1756871
    CVE: CVE-2019-0155
    RH-Acked-by: Karol Herbst <kherbst@redhat.com>
    RH-Acked-by: Lyude Paul <lyude@redhat.com>
    
    From: Jon Bloomfield <jon.bloomfield@intel.com>
    
    Bugzilla: 1756876
    Upstream Status: Embargoed
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=24292594
    CVE: CVE-2019-0155
    
    For Gen7, the original cmdparser motive was to permit limited
    use of register read/write instructions in unprivileged BB's.
    This worked by copying the user supplied bb to a kmd owned
    bb, and running it in secure mode, from the ggtt, only if
    the scanner finds no unsafe commands or registers.
    
    For Gen8+ we can't use this same technique because running bb's
    from the ggtt also disables access to ppgtt space. But we also
    do not actually require 'secure' execution since we are only
    trying to reduce the available command/register set. Instead we
    will copy the user buffer to a kmd owned read-only bb in ppgtt,
    and run in the usual non-secure mode.
    
    Note that ro pages are only supported by ppgtt (not ggtt), but
    luckily that's exactly what we need.
    
    Add the required paths to map the shadow buffer to ppgtt ro for Gen8+
    
    v2: IS_GEN7/IS_GEN (Mika)
    v3: rebase
    v4: rebase
    v5: rebase
    
    Signed-off-by: Jon Bloomfield <jon.bloomfield@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -2463,6 +2463,12 @@ static inline unsigned int i915_sg_segment_size(void)
 #define HAS_BLT(dev_priv)	HAS_ENGINE(dev_priv, BCS)
 #define HAS_VEBOX(dev_priv)	HAS_ENGINE(dev_priv, VECS)
 
+/*
+ * The Gen7 cmdparser copies the scanned buffer to the ggtt for execution
+ * All later gens can run the final buffer from the ppgtt
+ */
+#define CMDPARSER_USES_GGTT(dev_priv) IS_GEN(dev_priv, 7)
+
 #define HAS_LLC(dev_priv)	(INTEL_INFO(dev_priv)->has_llc)
 #define HAS_SNOOP(dev_priv)	(INTEL_INFO(dev_priv)->has_snoop)
 #define HAS_EDRAM(dev_priv)	(!!((dev_priv)->edram_cap & EDRAM_ENABLED))
@@ -2877,6 +2883,14 @@ i915_gem_object_ggtt_pin(struct drm_i915_gem_object *obj,
 
 int i915_gem_object_unbind(struct drm_i915_gem_object *obj);
 void i915_gem_release_mmap(struct drm_i915_gem_object *obj);
+
+struct i915_vma * __must_check
+i915_gem_object_pin(struct drm_i915_gem_object *obj,
+		    struct i915_address_space *vm,
+		    const struct i915_ggtt_view *view,
+		    u64 size,
+		    u64 alignment,
+		    u64 flags);
 
 void i915_gem_runtime_suspend(struct drm_i915_private *dev_priv);
 
