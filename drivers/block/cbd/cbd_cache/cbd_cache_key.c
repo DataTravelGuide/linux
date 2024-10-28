f380863f1155d93257addef68d27b5e68317960f
Dongsheng Yang <dongsheng.yang@linux.dev>
2024-10-28 03:19:23 +0000

    cache_key cleanup
    Signed-off-by: Dongsheng Yang <dongsheng.yang@linux.dev>

--- drivers/block/cbd/cbd_cache/cbd_cache_key.c
+++ drivers/block/cbd/cbd_cache/cbd_cache_key.c
@@ -1,8 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-#include "../cbd_internal.h"
-#include "../cbd_transport.h"
 #include "cbd_cache_internal.h"
-#include "../cbd_transport.h"
 
 void cache_key_init(struct cbd_cache *cache, struct cbd_cache_key *key)
 {
